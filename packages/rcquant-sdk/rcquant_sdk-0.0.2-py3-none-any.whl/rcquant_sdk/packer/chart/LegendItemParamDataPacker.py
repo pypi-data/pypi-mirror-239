4f586a8f04e1fcf772e66c49d514051bb7c6d654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:09 +0000

    Add package rcquant-sdk/0.0.2/rcquant_sdk-0.0.2-py3-none-any.whl

---
+++ packages/rcquant-sdk/rcquant_sdk-0.0.2-py3-none-any.whl/rcquant_sdk/packer/chart/LegendItemParamDataPacker.py
@@ -0,0 +1,32 @@
+from ...interface import IPacker
+
+
+class LegendItemParamDataPacker(IPacker):
+    def __init__(self, obj) -> None:
+        super().__init__(obj)
+
+    def obj_to_tuple(self):
+        return [int(self._obj.PlotIndex), int(self._obj.HAlgin), int(self._obj.VAlgin), int(self._obj.ItemMargin),
+                int(self._obj.ItemSpace), int(self._obj.HOffset), int(
+                    self._obj.VOffset), int(self._obj.MaxColumns), int(
+                    self._obj.BorderWidth), str(self._obj.BorderColor),
+                int(self._obj.BorderRadius), int(self._obj.BackGroundAlpha), str(self._obj.BackGroundColor)]
+
+    def tuple_to_obj(self, t):
+        if len(t) >= 13:
+            self._obj.PlotIndex = t[0]
+            self._obj.HAlgin = t[1]
+            self._obj.VAlgin = t[2]
+            self._obj.ItemMargin = t[3]
+            self._obj.ItemSpace = t[4]
+            self._obj.HOffset = t[5]
+            self._obj.VOffset = t[6]
+            self._obj.MaxColumns = t[7]
+            self._obj.BorderWidth = t[8]
+            self._obj.BorderColor = t[9]
+            self._obj.BorderRadius = t[10]
+            self._obj.BackGroundAlpha = t[11]
+            self._obj.BackGroundColor = t[12]
+
+            return True
+        return False

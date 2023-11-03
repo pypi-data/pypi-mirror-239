64a0ef159e55a1896cba4ebb9863e3a1d1a2d654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:09:49 +0000

    Add package holms/0.5.0/holms-0.5.0.tar.gz

---
+++ packages/holms/holms-0.5.0.tar.gz/holms-0.5.0/holms/__init__.py
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------------
+#  es7s/holms
+#  (c) 2023 A. Shavykin <0.delameter@gmail.com>
+# ------------------------------------------------------------------------------
+
+from .core import Char as Char
+
+from ._version import __version__, __updated__
+
+APP_NAME = "holms"
+APP_VERSION = __version__
+APP_UPDATED = __updated__

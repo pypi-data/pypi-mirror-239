bda190507d943c3900649f3205c6d1ee5b0a89e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:11 +0000

    Add package dynamicscrapper/3.3.0/dynamicscrapper-3.3.0.tar.gz

---
+++ packages/dynamicscrapper/dynamicscrapper-3.3.0.tar.gz/dynamicscrapper-3.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='dynamicscrapper',
+    version='3.3.0',
+    author='Saidi Souhaieb',
+    author_email='Saidisouhaieb@takiacademyteam.com',
+    description='A package to scrap the web dynamically',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

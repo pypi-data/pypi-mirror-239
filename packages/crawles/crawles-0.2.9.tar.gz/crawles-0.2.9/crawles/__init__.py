edaaf4b2a183fc987866e4c771909695b18323b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:17 +0000

    Add package crawles/0.2.9/crawles-0.2.9.tar.gz

---
+++ packages/crawles/crawles-0.2.9.tar.gz/crawles-0.2.9/crawles/__init__.py
@@ -0,0 +1,27 @@
+from ._api.api import get, post, session_get, session_post
+from ._data_save.image_save import image_save
+from ._speedy.MyThread import decorator_thread, MyThread
+from ._speedy.ThreadPool import Item, ThreadPool, Pipeline
+from ._speedy.curl_analysis import curl_anal, curl_anal_cls,curl_anal_thread
+from ._speedy.head_format import head_format
+from ._speedy.js_call import execjs
+
+__version__ = "0.2.9"
+
+__all__ = [
+    'get',
+    'post',
+    'session_get',
+    'session_post',
+    'image_save',
+    'decorator_thread',
+    'execjs',
+    'head_format',
+    'MyThread',
+    'curl_anal',
+    'curl_anal_thread',
+    'curl_anal_cls',
+    'Item',
+    'ThreadPool',
+    'Pipeline',
+]

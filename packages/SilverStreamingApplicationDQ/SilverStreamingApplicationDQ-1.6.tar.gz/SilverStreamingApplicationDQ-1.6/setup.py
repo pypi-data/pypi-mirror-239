ed1fe299d213c610240f7ef21b220dbfe243c1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 15:03:36 +0000

    Add package SilverStreamingApplicationDQ/1.6/SilverStreamingApplicationDQ-1.6.tar.gz

---
+++ packages/SilverStreamingApplicationDQ/SilverStreamingApplicationDQ-1.6.tar.gz/SilverStreamingApplicationDQ-1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SilverStreamingApplicationDQ',
+    version='1.6',
+    packages=['com.phida.main', 'com.phida.main.bronze', 'com.phida.tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='Performs bronze as well as silver operations'
+)

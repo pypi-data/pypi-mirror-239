e3eaa349b07d3689c76cd62941e19bea5f567d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:05 +0000

    Add package pali-torch/0.0.6/pali_torch-0.0.6.tar.gz

---
+++ packages/pali-torch/pali_torch-0.0.6.tar.gz/pali_torch-0.0.6/pali/model.py
@@ -0,0 +1,99 @@
+import torch
+from pali.transformer import ViTransformerWrapper, Encoder, XTransformer
+from transformers import AutoTokenizer
+
+
+class VitModel:
+    def __init__(
+        self, image_size=256, patch_size=32, dim=512, depth=6, heads=8, *args, **kwargs
+    ):
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.dim = dim
+
+        self.depth = depth
+        self.heads = heads
+        self.vit = ViTransformerWrapper(
+            image_size=image_size,
+            patch_size=patch_size,
+            attn_layers=Encoder(dim=dim, depth=depth, heads=heads),
+        )
+
+    def process(self, img):
+        if img is None:
+            raise ValueError("Input image cannot be None")
+        if img.shape[1:] != (3, self.image_size, self.image_size):
+            raise ValueError(
+                "Input image must have the shape [*, 3, {}, {}]".format(
+                    self.image_size, self.image_size
+                )
+            )
+
+        return self.vit(img, return_embeddings=True)
+
+
+class Pali:
+    def __init__(
+        self,
+        model_name=None,
+        image_size=256,
+        patch_size=32,
+        dim=512,
+        depth=6,
+        heads=8,
+        enc_num_tokens=256,
+        enc_max_seq_len=1024,
+        dec_num_tokens=256,
+        dec_max_seq_len=1024,
+        enc_depth=6,
+        enc_heads=8,
+        dec_depth=6,
+        dec_heads=8,
+    ):
+        self.model_name = model_name
+        self.tokenizer = None
+        self.vit_model = VitModel(
+            image_size=image_size,
+            patch_size=patch_size,
+            dim=dim,
+            depth=depth,
+            heads=heads,
+        )
+
+        self.pali_model = XTransformer(
+            dim=dim,
+            enc_num_tokens=enc_num_tokens,
+            enc_depth=enc_depth,
+            enc_heads=enc_heads,
+            enc_max_seq_len=enc_max_seq_len,
+            dec_num_tokens=dec_num_tokens,
+            dec_depth=dec_depth,
+            dec_heads=dec_heads,
+            dec_max_seq_len=dec_max_seq_len,
+        )
+
+    def forward(self, img, prompt, output, mask):
+        img_embeds = self.vit_model.process(img)
+        result = self.ul2(prompt, output, mask=mask, src_prepend_embeds=img_embeds)
+
+        return result
+
+    def generate(self, text, seq_len=1024, mask=None, attn_mask=None, model_name=None):
+        if model_name:
+            self.model_name = model_name
+
+        if not self.model_name:
+            raise ValueError(
+                "model_name must be specidfied either in the class constructor or in the generate method"
+            )
+
+        if not self.tokenizer:
+            self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+        inputs = self.tokenizer.encode(text, return_tensors="pt")
+        seq_out_start = torch.zeros(1, 1).long()
+        result = self.pali_model.generate(
+            inputs, seq_out_start, seq_len, mask, attn_mask
+        )
+        result_text = self.tokenizer.decode(result[0], skip_special_tokens=True)
+        return result_text

b9a09824a9eefd80daa7db9ac7d4e4d317df3e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:13 +0000

    Add package dynamicscrapper/0.2.0/dynamicscrapper-0.2.0.tar.gz

---
+++ packages/dynamicscrapper/dynamicscrapper-0.2.0.tar.gz/dynamicscrapper-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dynamicscrapper',
+    version='0.2.0',
+    author='Saidi Souhaieb',
+    author_email='Saidisouhaieb@takiacademyteam.com',
+    description='A package to scrap the web dynamically',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

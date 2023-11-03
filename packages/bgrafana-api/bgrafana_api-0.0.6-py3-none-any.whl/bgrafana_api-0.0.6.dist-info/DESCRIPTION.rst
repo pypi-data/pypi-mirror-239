d0e8699687ae68f392389bace4bebd6c24a01730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:00 +0000

    Add package bgrafana-api/0.0.6/bgrafana_api-0.0.6-py3-none-any.whl

---
+++ packages/bgrafana-api/bgrafana_api-0.0.6-py3-none-any.whl/bgrafana_api-0.0.6.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+The Grafana backend exposes an HTTP API, the same API is used by the frontend to do everything from saving dashboards, creating users and updating data sources.
+
+
---
+++ packages/bgrafana-api/bgrafana_api-0.0.6-py3-none-any.whl/bgrafana_api/api/__init__.py
@@ -0,0 +1,46 @@
+# flake8: noqa
+
+# import apis into api package
+from bgrafana_api.api.access_control_api import AccessControlApi
+from bgrafana_api.api.access_control_provisioning_api import AccessControlProvisioningApi
+from bgrafana_api.api.admin_api import AdminApi
+from bgrafana_api.api.admin_ldap_api import AdminLdapApi
+from bgrafana_api.api.admin_provisioning_api import AdminProvisioningApi
+from bgrafana_api.api.admin_users_api import AdminUsersApi
+from bgrafana_api.api.annotations_api import AnnotationsApi
+from bgrafana_api.api.api_keys_api import ApiKeysApi
+from bgrafana_api.api.correlations_api import CorrelationsApi
+from bgrafana_api.api.dashboard_permissions_api import DashboardPermissionsApi
+from bgrafana_api.api.dashboard_versions_api import DashboardVersionsApi
+from bgrafana_api.api.dashboards_api import DashboardsApi
+from bgrafana_api.api.datasource_permissions_api import DatasourcePermissionsApi
+from bgrafana_api.api.datasources_api import DatasourcesApi
+from bgrafana_api.api.ds_api import DsApi
+from bgrafana_api.api.enterprise_api import EnterpriseApi
+from bgrafana_api.api.folder_permissions_api import FolderPermissionsApi
+from bgrafana_api.api.folders_api import FoldersApi
+from bgrafana_api.api.get_current_org_api import GetCurrentOrgApi
+from bgrafana_api.api.ldap_debug_api import LdapDebugApi
+from bgrafana_api.api.legacy_alerts_api import LegacyAlertsApi
+from bgrafana_api.api.legacy_alerts_notification_channels_api import LegacyAlertsNotificationChannelsApi
+from bgrafana_api.api.library_elements_api import LibraryElementsApi
+from bgrafana_api.api.licensing_api import LicensingApi
+from bgrafana_api.api.org_api import OrgApi
+from bgrafana_api.api.org_invites_api import OrgInvitesApi
+from bgrafana_api.api.org_preferences_api import OrgPreferencesApi
+from bgrafana_api.api.orgs_api import OrgsApi
+from bgrafana_api.api.playlists_api import PlaylistsApi
+from bgrafana_api.api.provisioning_api import ProvisioningApi
+from bgrafana_api.api.query_history_api import QueryHistoryApi
+from bgrafana_api.api.recording_rules_api import RecordingRulesApi
+from bgrafana_api.api.reports_api import ReportsApi
+from bgrafana_api.api.saml_api import SamlApi
+from bgrafana_api.api.search_api import SearchApi
+from bgrafana_api.api.service_accounts_api import ServiceAccountsApi
+from bgrafana_api.api.signed_in_user_api import SignedInUserApi
+from bgrafana_api.api.snapshots_api import SnapshotsApi
+from bgrafana_api.api.sync_team_groups_api import SyncTeamGroupsApi
+from bgrafana_api.api.teams_api import TeamsApi
+from bgrafana_api.api.user_preferences_api import UserPreferencesApi
+from bgrafana_api.api.users_api import UsersApi
+

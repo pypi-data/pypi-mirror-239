d591362c1b49f1bcbd73186c9a3acf42e76e321f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 15:03:29 +0000

    Add package jigi/1.0.3/jigi-1.0.3.tar.gz

---
+++ packages/jigi/jigi-1.0.3.tar.gz/jigi-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='jigi',
+    version='1.0.3',
+    description='JIGI',
+    author='Baxromov Shahzodbek',
+    author_email='baxromov.shahzodbek@gmail.com',
+    url='https://github.com/baxromov/JIGI.git',
+    packages=find_packages(),
+    install_requires=required
+)

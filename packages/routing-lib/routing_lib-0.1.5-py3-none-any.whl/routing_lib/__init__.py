90fbd044ab35b1e228998d5ffc7047115c09945d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 15:03:38 +0000

    Add package routing-lib/0.1.5/routing_lib-0.1.5-py3-none-any.whl

---
+++ packages/routing-lib/routing_lib-0.1.5-py3-none-any.whl/routing_lib/__init__.py
@@ -0,0 +1,7 @@
+"""Small package with alternative routing algorithms and measures. Author: Ludovico Lemma"""
+
+__version__ = '0.1.5'
+
+from .routing_algorithms import *
+from .routing_measures import *
+from .routing_utils import *

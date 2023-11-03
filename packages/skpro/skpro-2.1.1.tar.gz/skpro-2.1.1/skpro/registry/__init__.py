8705f1efa4cc724ad797780b194891f0ed45d060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 15:03:53 +0000

    Add package skpro/2.1.1/skpro-2.1.1.tar.gz

---
+++ packages/skpro/skpro-2.1.1.tar.gz/skpro-2.1.1/skpro/registry/__init__.py
@@ -0,0 +1,16 @@
+"""Registry and lookup functionality."""
+
+from skpro.registry._lookup import all_objects, all_tags
+from skpro.registry._tags import (
+    OBJECT_TAG_LIST,
+    OBJECT_TAG_REGISTER,
+    check_tag_is_valid,
+)
+
+__all__ = [
+    "OBJECT_TAG_LIST",
+    "OBJECT_TAG_REGISTER",
+    "all_objects",
+    "all_tags",
+    "check_tag_is_valid",
+]

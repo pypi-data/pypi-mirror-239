4e06a137c448f88165905628f63901a5cae9eae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 15:03:47 +0000

    Add package CharActor/0.3.2/CharActor-0.3.2.tar.gz

---
+++ packages/CharActor/CharActor-0.3.2.tar.gz/CharActor-0.3.2/CharActor/__init__.py
@@ -0,0 +1,37 @@
+from typing import Union as _Union
+from .__log__ import logger, log
+from ._charactor import dicts as _dicts
+from ._charactor import create, BaseCharacters as _BaseCharacters, character_bank
+from ._objects._items import _Armory, _Goods
+from . import _entity
+
+log('Initializing CharActor.')
+log('Initializing character bank.')
+
+class _Catalogues:
+    log('Initializing catalogues.')
+    Armory = None
+    Goods = None
+    def __init__(self):
+        self.Armory = _Armory()
+        self.Goods = _Goods()
+        
+    def get(self, item_name: str = None, grid = None, cell = None):
+        if item_name is None:
+            return
+        if None not in [grid, cell] and isinstance(cell, str):
+            cell = grid[cell]
+        item_name = item_name
+        if item_name in self.Armory:
+            return self.Armory.get(item_name, grid, cell)
+        elif item_name in self.Goods:
+            return self.Goods.get(item_name, grid, cell)
+        else:
+            print(f'Item {item_name} not found in catalogues.')
+            return None
+
+log('Creating catalogue instance.')
+        
+Catalogues = _Catalogues()
+
+del log, logger
---
+++ packages/CharActor/CharActor-0.3.2.tar.gz/CharActor-0.3.2/CharActor/_charactor/actor/_actor/_base_character.py
@@ -0,0 +1,23 @@
+from ...._entity import _base_entity
+from ._base_class_actors import *
+from ._base_race_actors import *
+
+_base_characters = {}
+_char_list = []
+
+for race in RACE_ATTRIBUTES.keys():
+    _base_characters[race.replace('-','')] = {}
+    for role in ROLE_ATTRIBUTES.keys():
+        _base_characters[race.replace('-','')][role] = type(f'{race.replace("-","")}{role}', (BaseCharacter, ), {})
+
+
+globals().update(_base_characters)
+
+for k, v in _base_characters.items():
+    _char_list.extend(f'{k}{K}' for K in v.keys())
+
+for race in RACE_ATTRIBUTES:
+    race = race.replace('-', '')
+    for role in ROLE_ATTRIBUTES:
+        setattr(_base_entity, f'{race}{role}', _base_characters[race][role])
+

951c858bc53304d35862c4250d3898faa9867a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:15 +0000

    Add package sydney-py/0.17.3/sydney_py-0.17.3-py3-none-any.whl

---
+++ packages/sydney-py/sydney_py-0.17.3-py3-none-any.whl/sydney/exceptions.py
@@ -0,0 +1,23 @@
+class NoConnectionException(Exception):
+    pass
+
+class NoResponseException(Exception):
+    pass
+
+class ThrottledRequestException(Exception):
+    pass
+
+class CaptchaChallengeException(Exception):
+    pass
+
+class ConversationLimitException(Exception):
+    pass
+
+class CreateConversationException(Exception):
+    pass
+
+class GetConversationsException(Exception):
+    pass
+
+class ImageUploadException(Exception):
+    pass

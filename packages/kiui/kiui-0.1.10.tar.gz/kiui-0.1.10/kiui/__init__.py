4c0e97fabb88ca2b83cdf203bd935f2b869590bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:10:18 +0000

    Add package kiui/0.1.10/kiui-0.1.10.tar.gz

---
+++ packages/kiui/kiui-0.1.10.tar.gz/kiui-0.1.10/kiui/__init__.py
@@ -0,0 +1,15 @@
+import lazy_loader
+
+# lazy import that equals:
+# from . import op, vis, bg, mesh
+# from .utils import *
+# from .env import env
+
+__getattr__, __dir__, _ = lazy_loader.attach(
+    __name__,
+    submodules=["op", "vis", "cam", "mesh", "sr", "render", "cli", "lpips"],
+    submod_attrs={
+        "utils": ["lo", "read_json", "write_json", "read_image", "write_image"],
+        "env": ["env"],
+    },
+)
---
+++ packages/kiui/kiui-0.1.10.tar.gz/kiui-0.1.10/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="kiui",
+        version="0.1.10",
+        description="self-use toolkits",
+        long_description=open("README.md", encoding="utf-8").read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/ashawkey/kiuikit",
+        author="kiui",
+        author_email="ashawkey1999@gmail.com",
+        packages=find_packages(),
+        include_package_data=True,
+        classifiers=[
+            "Programming Language :: Python :: 3 ",
+        ],
+        keywords="utility",
+        install_requires=[
+            "lazy_loader",
+            "varname",
+            "objprint",
+        ],
+        extras_require={
+            "full": [
+                "tqdm",
+                "rich",
+                "numpy",
+                "scipy",
+                "scikit-image",
+                "scikit-learn",
+                "pandas",
+                "trimesh",
+                "pygltflib",
+                "numpytorch",
+                "matplotlib",
+                "opencv-python",
+                "imageio",
+                "imageio-ffmpeg",
+                "rembg[gpu,cli]",
+                "dearpygui",
+                'huggingface_hub',
+                'transformers',
+                'accelerate',
+                'diffusers',
+            ],
+        },
+    )

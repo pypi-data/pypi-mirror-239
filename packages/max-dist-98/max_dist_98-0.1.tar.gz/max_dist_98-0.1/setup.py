f5107e5b84406e100ee69af9ae98296e0e41bb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:08:19 +0000

    Add package max-dist-98/0.1/max_dist_98-0.1.tar.gz

---
+++ packages/max-dist-98/max_dist_98-0.1.tar.gz/max_dist_98-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='max_dist_98',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['max_dist_98'],
+      author = 'Max Rim',
+      zip_safe=False)

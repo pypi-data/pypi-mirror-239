aa61c17e9e8a454a61e142db3894617f885cea4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:05:55 +0000

    Add package lilac/0.1.16/lilac-0.1.16-py3-none-any.whl

---
+++ packages/lilac/lilac-0.1.16-py3-none-any.whl/lilac/test_utils.py
@@ -0,0 +1,30 @@
+"""Utilities for unit tests."""
+
+import os
+import pathlib
+import uuid
+from typing import Union
+
+import pyarrow.parquet as pq
+
+from .schema import ROWID, DataType, Field, Item, Schema, schema_to_arrow_schema
+
+
+def read_items(
+  data_dir: Union[str, pathlib.Path], filepaths: list[str], schema: Schema
+) -> list[Item]:
+  """Read the source items from a dataset output directory."""
+  items: list[Item] = []
+  schema.fields[ROWID] = Field(dtype=DataType.STRING)
+  for filepath in filepaths:
+    items.extend(
+      pq.read_table(
+        os.path.join(data_dir, filepath), schema=schema_to_arrow_schema(schema)
+      ).to_pylist()
+    )
+  return items
+
+
+def fake_uuid(id: bytes) -> uuid.UUID:
+  """Create a test UUID."""
+  return uuid.UUID((id * 16).hex())
---
+++ packages/lilac/lilac-0.1.16-py3-none-any.whl/lilac/web/_app/immutable/chunks/navigation.cd48615f.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.c4b72190.js";const e=o("goto");export{e as g};

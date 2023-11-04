03ab5e2a5d34fa244739f75727dedd455e2ea8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 16:05:41 +0000

    Add package Nebula-Beta/0.1.2/Nebula-Beta-0.1.2.tar.gz

---
+++ packages/Nebula-Beta/Nebula-Beta-0.1.2.tar.gz/Nebula-Beta-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='Nebula-Beta',
+    version='0.1.2',
+    description='An Engine/Framework For Crafting 2d Games With Python+Pygame-Ce.',
+    url='https://github.com/setoyuma/NebulaEngine',
+    author='Setoichi',
+    author_email='setoichi.dev@gmail.com',
+    license='MIT',
+    packages=['Nebula'],
+    install_requires=[
+        'pygame-ce',
+        'pygame-gui',
+        'tkinter'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta'
+    ]
+)

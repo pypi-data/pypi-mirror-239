acbfac929167063527085a8a6c9dd7ff44605ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:05:50 +0000

    Add package diagraph/0.1.3/diagraph-0.1.3-py3-none-any.whl

---
+++ packages/diagraph/diagraph-0.1.3-py3-none-any.whl/diagraph/utils/build_graph.py
@@ -0,0 +1,24 @@
+from typing import TypeVar
+
+from ..utils.annotations import get_annotations, get_dependency
+
+
+T = TypeVar("T")
+
+
+def build_graph(*_nodes: T):
+    graph = {}
+    seen = set()
+    nodes: list[T] = list(_nodes)
+    for node in nodes:
+        graph[node] = graph.get(node, set())
+        if node not in seen:
+            seen.add(node)
+            for _, val in get_annotations(node):
+                dep = get_dependency(val)
+                if dep not in seen:
+                    nodes.append(dep)
+
+                graph[node].add(dep)
+
+    return graph

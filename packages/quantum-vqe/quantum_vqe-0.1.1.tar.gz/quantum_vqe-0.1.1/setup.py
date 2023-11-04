e9bf7cc325766fc940ca8be524574a039bb2119e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 07:03:40 +0000

    Add package quantum-vqe/0.1.1/quantum_vqe-0.1.1.tar.gz

---
+++ packages/quantum-vqe/quantum_vqe-0.1.1.tar.gz/quantum_vqe-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quantum_vqe",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["qiskit", "numpy", "scipy"],
+    author="Viktor Veselov",
+    description="A quantum VQE package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SweatyCrayfish/quantum_vqe",
+)

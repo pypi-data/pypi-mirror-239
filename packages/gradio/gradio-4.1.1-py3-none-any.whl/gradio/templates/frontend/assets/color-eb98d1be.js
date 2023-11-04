9ab4765bbd796b88d3e34ed46e29232dfcca2875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:06:13 +0000

    Add package gradio/4.1.1/gradio-4.1.1-py3-none-any.whl

---
+++ packages/gradio/gradio-4.1.1-py3-none-any.whl/gradio/templates/frontend/assets/color-eb98d1be.js
@@ -0,0 +1,8 @@
+import { o as ordered_colors } from './Button-cca1a2fa.js';
+
+const get_next_color = (index) => {
+  return ordered_colors[index % ordered_colors.length];
+};
+
+export { get_next_color as g };
+//# sourceMappingURL=color-eb98d1be.js.map
---
+++ packages/gradio/gradio-4.1.1-py3-none-any.whl/gradio/templates/frontend/assets/index-145ac21b.js
@@ -0,0 +1,6 @@
+export { L as Loader, S as StatusTracker, S as default } from './Index-cef3d034.js';
+export { T as Toast } from './Blocks-104bb89f.js';
+import './index-0ae82880.js';
+import './svelte/svelte.js';
+import './Button-cca1a2fa.js';
+//# sourceMappingURL=index-145ac21b.js.map

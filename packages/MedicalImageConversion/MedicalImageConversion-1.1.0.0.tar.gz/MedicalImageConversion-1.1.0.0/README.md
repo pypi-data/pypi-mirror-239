4edba95bccc587523ebb9e2066136e156fcdfe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:25 +0000

    Add package MedicalImageConversion/1.1.0.0/MedicalImageConversion-1.1.0.0.tar.gz

---
+++ packages/MedicalImageConversion/MedicalImageConversion-1.1.0.0.tar.gz/MedicalImageConversion-1.1.0.0/README.md
@@ -0,0 +1,2 @@
+# MedicalImageConversion
+Convert 1) Dicom to Nifti 2)Nifti to Dicom
---
+++ packages/MedicalImageConversion/MedicalImageConversion-1.1.0.0.tar.gz/MedicalImageConversion-1.1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='MedicalImageConversion',
+      version='1.0',
+      description='Medical image conversion',
+      packages=['MedicalImageConversion'],
+      author_email='tekhinvasiliy@gmail.com',
+      zip_safe=False)

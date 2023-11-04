178e99850255ce3db89e8cae2a7b48828d98a067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:04:19 +0000

    Add package fastapi_config/0.1.3/fastapi-config-0.1.3.tar.gz

---
+++ packages/fastapi_config/fastapi-config-0.1.3.tar.gz/fastapi-config-0.1.3/tests/conftest.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+import pytest
+from sqlalchemy_database import AsyncDatabase, Database
+from sqlmodel import SQLModel
+
+from fastapi_config import DbConfigStore
+
+# sqlite
+sync_db = Database.create("sqlite:///amisadmin.db?check_same_thread=False")
+async_db = AsyncDatabase.create("sqlite+aiosqlite:///amisadmin.db?check_same_thread=False")
+
+
+@pytest.fixture(params=[async_db, sync_db])
+async def db(request) -> Union[Database, AsyncDatabase]:
+    database = request.param
+    await database.async_run_sync(SQLModel.metadata.create_all, is_session=False)
+    yield database
+    await database.async_run_sync(SQLModel.metadata.drop_all, is_session=False)
+    await database.async_close()
+
+
+@pytest.fixture
+def config_store(db) -> DbConfigStore:
+    return DbConfigStore(db=db)

1cd7fb2a0da06310b17ead91dcdd75ef3e97e06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:05:53 +0000

    Add package keycloak-fast-sso/2023.11.5/Keycloak-fast-sso-2023.11.5.tar.gz

---
+++ packages/keycloak-fast-sso/Keycloak-fast-sso-2023.11.5.tar.gz/Keycloak-fast-sso-2023.11.5/setup.py
@@ -0,0 +1,53 @@
+"""
+MIT License
+
+Copyright (c) 2023 Alexandre Meline <alexandre.meline.dev@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.rst')) as f:
+    description = f.read()
+
+setup(
+    name="Keycloak-fast-sso",
+    version='2023.11.5',
+    url="https://github.com/alexandre-meline/keycloakfastsso",
+    author="Alexandre Meline",
+    author_email="alexandre.meline.dev@gmail.com",
+    long_description=description,
+    packages=find_packages(),  
+    install_requires=[
+        "python-keycloak",
+        "fastapi",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    keywords="keycloak fastapi sso",
+    python_requires=">=3.6",
+
+)

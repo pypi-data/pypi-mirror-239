209598385985d323a5d75de2feba353e824cfb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:04:29 +0000

    Add package dynamicscrapper/9.4.2/dynamicscrapper-9.4.2.tar.gz

---
+++ packages/dynamicscrapper/dynamicscrapper-9.4.2.tar.gz/dynamicscrapper-9.4.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='dynamicscrapper',
+    version='9.4.2',
+    author='Saidi Souhaieb',
+    author_email='Saidisouhaieb@takiacademyteam.com',
+    description='A package to scrap the web dynamically',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

3b15278c21136f6072e0e46e013622303e63d7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:06:00 +0000

    Add package MqttLibPy/1.1.4/mqttlibpy-1.1.4-py3-none-any.whl

---
+++ packages/MqttLibPy/mqttlibpy-1.1.4-py3-none-any.whl/MqttLibPy/client.py
@@ -0,0 +1,163 @@
+import json
+import hashlib
+
+from paho.mqtt.publish import single
+from paho.mqtt.client import MQTTv5, Client
+
+from logging import getLogger
+from typing import Union
+
+from .serializer import Serializer
+
+
+class MqttClient:
+
+    def __init__(self, hostname: str, port: int, prefix: str = "", suffix: str = "", uuid=""):
+        self.prefix = prefix
+        self.suffix = suffix
+        self.uuid = uuid
+
+        self.hostname = hostname
+        self.port = port
+
+        self.routes = []
+        self.files = {}
+
+        self.client = Client("", userdata=None, protocol=MQTTv5)
+
+        def _on_connect(client: Client, _, __, ___, ____):
+            for route in self.routes:
+                client.subscribe(route)
+
+        self.client.on_connect = _on_connect
+
+        self.logger = getLogger("Mqtt Client")
+
+    def send_message(self, topic: str, payload: dict):
+        print(f'Sending message to {topic}')
+        json_payload = json.dumps(payload)
+
+        self._send_string(topic, json_payload)
+
+    def send_message_serialized(self, message: Union[list[dict], str], route,
+                                encodeb64: bool = False, valid_json=False, error=False):
+        """
+        :param message: List of dicts or string to send.
+        :param route: topic to send message to
+        :param encodeb64: Not implemented
+        :param valid_json: Indicates "message" is a valid parsable json (list[dict])
+        :param error: Indicates this is an error message
+        """
+        json_messages = Serializer(self.uuid).serialize(message, encodeb64, valid_json, is_error=error)
+
+        for serialized_message in json_messages:
+            self.send_message(route, serialized_message)
+
+    def _send_string(self, topic: str, payload: Union[str, bytes]):
+        print(f"Sending string to {topic}")
+        single(topic, payload, hostname=self.hostname, port=self.port, protocol=MQTTv5)
+
+    def send_bytes(self, message: bytes, route: str, filename: str = '', metadata: dict = None):
+        if metadata is None:
+            metadata = {}
+
+        # Mandar la metadata por route y el archivo por route/
+        serialized_message = Serializer(self.uuid).serialize(message, filename=filename, metadata=metadata)
+
+        for msg in serialized_message:
+            self.send_message(route, msg)
+            self._send_string(f"{route}/file", message)
+
+    def send_file(self, route: str, filepath: str, metadata: dict = None):
+        if metadata is None:
+            metadata = {}
+        with open(filepath, "rb") as f:
+            file_name = f.name.split("/")[-1]
+            file_bytes = f.read()
+        print(f"Sending {file_name} of length {len(file_bytes)}")
+        self.send_bytes(file_bytes, route, file_name, metadata)
+
+    def register_route(self, route, callback):
+        topic = f'{self.prefix}{route}{self.suffix}'
+        self.routes.append(topic)
+        print(f"Listening to topic: {topic}")
+        self.client.message_callback_add(topic, callback)
+
+    def listen(self):
+        print(f"Connecting to {self.hostname}:{self.port}")
+        self.client.connect(self.hostname, self.port)
+        self.client.loop_forever()
+
+    @staticmethod
+    def wrapper(client: Client, _, message):
+        parsed_message = json.loads(Serializer.decode_bytes(message.payload))
+        return client, _, parsed_message
+
+    def endpoint(self, route: str, force_json=False, is_file=False):
+        """
+        :param route: part of the route to listen to, the final route will be of the form {prefix}{route}{suffix}
+        :param force_json: The message payload is in json format, and will be passed to the callback as a dict
+        :param is_file: Indicates if the type of payload is a bytes object
+        :return:
+        """
+        def decorator(func):
+
+            def wrapper_json(client: Client, _, message):
+                parsed_message = json.loads(Serializer.decode_bytes(message.payload))
+                return func(client, _, parsed_message['data'])
+
+            def wrapper_files(client: Client, user_data, message):
+                file_bytes = message.payload
+                md5_hash = hashlib.md5(file_bytes).hexdigest()
+                if md5_hash in self.files:
+                    self.files[md5_hash]['bytes'] = file_bytes
+                else:
+                    # File arrived before the metadata, this shouldn't happen
+                    self.logger.warning(f"File arrived before metadata. Hash: {md5_hash}")
+                    self.files[md5_hash] = {}
+                    self.files[md5_hash]['bytes'] = file_bytes
+                    return
+
+                func(client, user_data, self.files[md5_hash])
+
+                # Cleanup
+                del self.files[md5_hash]['bytes']
+
+            def wrapper_files_metadata(client: Client, user_data, message):
+                parsed_message = json.loads(Serializer.decode_bytes(message.payload))
+
+                if parsed_message['type'] != 'file':
+                    self.logger.warning(f"A message of type {parsed_message['type']} was received on a file endpoint")
+                    return
+
+                if parsed_message['md5_hash'] in self.files:
+                    # Metadata arrived late
+                    self.files[parsed_message['md5_hash']]['md5_hash'] = parsed_message['md5_hash']
+                    self.files[parsed_message['md5_hash']]['filename'] = parsed_message['data'][0]['filename']
+                    self.files[parsed_message['md5_hash']]['from'] = parsed_message['from']
+                    self.files[parsed_message['md5_hash']]['data'] = parsed_message['data']
+                    func(client, user_data, parsed_message['md5_hash'])
+                    del self.files[parsed_message['md5_hash']]['bytes']
+                else:
+                    self.files[parsed_message['md5_hash']] = {
+                                                                'md5_hash': parsed_message['md5_hash'],
+                                                                'filename': parsed_message['data'][0]['filename'],
+                                                                'from': parsed_message['from'],
+                                                                'bytes': b'',
+                                                                'data': parsed_message['data']
+                                                              }
+
+            if force_json:
+                self.register_route(route, wrapper_json)
+            elif is_file:
+                self.register_route(route, wrapper_files_metadata)
+                self.register_route(f"{route}/file", wrapper_files)
+            else:
+                self.register_route(route, func)
+
+            def inner(*args, **kwargs):
+                pass
+
+            return inner
+
+        return decorator
---
+++ packages/MqttLibPy/mqttlibpy-1.1.4-py3-none-any.whl/MqttLibPy/serializer.py
@@ -0,0 +1,150 @@
+import json
+import json as jsn
+import base64
+import re
+import hashlib
+
+from textwrap import wrap
+from typing import Union
+
+
+class Serializer:
+    _MAX_MESSAGE_LENGTH_BYTES = 10 * 1000 * 1000  # 10MB
+    _MAX_MESSAGE_LENGTH = 268435448
+    _SEQ = 0
+    # Almost max 32bit signed int value
+    _MAX_SEQ = 2147483640
+
+    def __init__(self, uuid: str):
+        # Pasar esto a la db
+        self.id = uuid
+
+    def serialize(self, message: Union[str, list[dict], bytes], encodeb64: bool = False,
+                  valid_json=False, is_error=False, token="", filename: str = "", metadata: dict = None) -> list[dict]:
+        if encodeb64 and not valid_json and not isinstance(message, bytes):
+            if isinstance(message, list):
+                message = json.dumps(message)
+            message = base64.b64encode(message.encode('utf-8'))
+            if len(message) > self._MAX_MESSAGE_LENGTH:
+                fragments = wrap(message.decode('utf-8'), self._MAX_MESSAGE_LENGTH)
+            else:
+                fragments = [message.decode('utf-8')]
+            message_type = "text"
+        elif isinstance(message, bytes):
+            if len(message) > self.MAX_MESSAGE_LENGTH:
+                raise Exception(f"Max payload size exceeded ({self.MAX_MESSAGE_LENGTH}B)")
+            if metadata is None:
+                metadata = {}
+            metadata.update({"filename": filename or hashlib.md5(message).hexdigest()})
+            fragments = [message]
+            message_type = "file"
+        elif isinstance(message, list) and valid_json:
+            fragments = self._naive_knapsack(message)
+            message_type = "json"
+        elif not encodeb64 and isinstance(message, str):
+            fragments = [message]
+            message_type = "text"
+        else:
+            if not valid_json:
+                raise RuntimeError("Type must be str for non json messages")
+            else:
+                raise RuntimeError(f"Incorrect data type for message: {type(message)}")
+
+        current_seq = self.seq
+
+        return [{
+            "data": fragment if message_type != "file" else metadata,
+            "seq": current_seq,
+            "current_fragment": n,
+            "total_fragments": len(fragments),
+            "last_fragment": n == len(fragments) - 1,
+            "encoded": encodeb64,
+            "is_valid_json": valid_json,
+            "encoding": "b64 utf-8" if encodeb64 else False,
+            "error": is_error,
+            "token": token,
+            "type": message_type,
+            "md5_hash": "" if message_type != "file" else hashlib.md5(fragment).hexdigest(),
+            "from": self.id
+        } for n, fragment in enumerate(fragments)]
+
+    def deserialize(self, message: str):
+        """
+        Doesnt support multi part messages yet
+        @param message: raw str payload
+        @return: Parsed, usable packet body
+        """
+        try:
+            packet = json.loads(message)
+            if packet["total_fragments"] > 1:
+                raise NotImplementedError("Multi part messages are not implemented yet")
+            if packet["encoded"]:
+                str_message = packet["data"].decode('utf-8')
+            else:
+                str_message = packet["data"]
+
+            if packet["is_valid_json"]:
+                parsed_message = json.loads(str_message)
+            else:
+                parsed_message = str_message
+
+            return parsed_message
+        except Exception as e:
+            print(f"An error has occurred during the parsing of a message {str(e)}")
+
+    @property
+    def seq(self):
+        current = Serializer._SEQ
+        Serializer._SEQ += 1
+        return current
+
+    def _as_str(self, obj):
+        return jsn.dumps(obj, ensure_ascii=False)
+
+    def _len(self, obj: Union[dict, list[dict]]):
+        return len(self._as_str(obj))
+
+    @property
+    def MAX_MESSAGE_LENGTH(self):
+        return self._MAX_MESSAGE_LENGTH_BYTES
+
+    @MAX_MESSAGE_LENGTH.setter
+    def MAX_MESSAGE_LENGTH(self, val):
+        self._MAX_MESSAGE_LENGTH_BYTES = val
+
+    @staticmethod
+    def decode_bytes(message):
+        return message.decode("utf-8")
+
+    def _naive_knapsack(self, objects: list[dict]) -> list[list[dict]]:
+        """
+        Takes a list of objects and returns a list of lists which sum of stringified
+        objects are lower than self.MAX_MESSAGE_LENGTH.
+        It's not optimized to return the best possible combinations, but it works.
+
+        @postcondition all([sum(self._len(elem) for elem) < self.MAX_MESSAGE_LENGTH in message for message in output])
+        """
+
+        messages = []
+        current_message = []
+        for i, obj in enumerate(objects):
+            if self._len([obj, *current_message]) < self.MAX_MESSAGE_LENGTH and obj:
+                current_message.append(obj)
+            elif self._len([obj, *current_message]) >= self.MAX_MESSAGE_LENGTH:
+                messages.append(current_message)
+                if self._len(obj) > self.MAX_MESSAGE_LENGTH:
+                    raise Exception("Length of object is bigger than the maximum "
+                                    "allowed by this protocol. To solve this, enable fragmentation "
+                                    "with [{\"encode\": true}]")
+                current_message = [obj]
+            if i == len(objects) - 1 and len(current_message) != 0:
+                messages.append(current_message)
+        return messages
+
+    def _naive_knapsack_bytes(self, objects: list[bytes]) -> list[list[bytes]]:
+        return objects
+
+    def filter_html_tags(self, text: str):
+        pattern = r'<.*?>'
+        filtered_string = re.sub(pattern, '', text)
+        return filtered_string

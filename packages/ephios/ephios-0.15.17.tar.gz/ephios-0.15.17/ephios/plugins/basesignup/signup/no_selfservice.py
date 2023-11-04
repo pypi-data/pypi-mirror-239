f38479f4e361c2c7ba4a4b365f85bf7fc4c147ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:06:17 +0000

    Add package ephios/0.15.17/ephios-0.15.17.tar.gz

---
+++ packages/ephios/ephios-0.15.17.tar.gz/ephios-0.15.17/ephios/plugins/basesignup/signup/no_selfservice.py
@@ -0,0 +1,55 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from ephios.core.models import AbstractParticipation
+from ephios.core.signup.methods import ActionDisallowedError, BaseSignupMethod
+from ephios.plugins.basesignup.signup.common import (
+    NoSignupSignupView,
+    RenderParticipationPillsShiftStateMixin,
+)
+
+
+class NoSelfserviceConfigurationForm(forms.Form):
+    template_name = "core/signup_configuration_form.html"
+    no_selfservice_explanation = forms.CharField(
+        label=_("Explanation"),
+        required=False,
+        initial="",
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.event = kwargs.pop("event")
+        super().__init__(*args, **kwargs)
+
+
+class NoSelfserviceSignupMethod(RenderParticipationPillsShiftStateMixin, BaseSignupMethod):
+    slug = "no_selfservice"
+    verbose_name = _("No Signup (only disposition)")
+    description = _("""This method allows no signup by users.""")
+    uses_requested_state = False
+    configuration_form_class = NoSelfserviceConfigurationForm
+    signup_view_class = NoSignupSignupView
+
+    def _configure_participation(
+        self, participation: AbstractParticipation, **kwargs
+    ) -> AbstractParticipation:
+        raise TypeError(f"{self.__class__} does not support signup")
+
+    @staticmethod
+    def signup_is_disabled(method, participant):
+        return ActionDisallowedError(
+            method.configuration.no_selfservice_explanation
+            or _("Signup for this shift is disabled.")
+        )
+
+    @property
+    def _signup_checkers(self):
+        return [
+            self.signup_is_disabled,
+        ]
+
+    @property
+    def _decline_checkers(self):
+        return [
+            self.signup_is_disabled,
+        ]

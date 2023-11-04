9c4c7faf5f07ea70a43f83432343a1084b24e6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 16:05:42 +0000

    Add package district5/0.1.11/district5-0.1.11.tar.gz

---
+++ packages/district5/district5-0.1.11.tar.gz/district5-0.1.11/radio_duck/__init__.py
@@ -0,0 +1,14 @@
+# __all__ = ['globals', 'db', 'sqlalchemy', 'exceptions', 'db_types', 'helper', 'queries']  # noqa: E501,B950
+# __all__ = ['globals', 'db', 'sqlalchemy', 'exceptions', 'db_types']  # noqa: E501,B950
+# __all__ = ['globals']  # noqa: E501,B950
+
+# connect_close_resource_msg = "connect_resource_closure"
+
+# from radio_duck.db import *
+
+# works...
+from radio_duck.exceptions import *  # noqa: F403,F401
+from radio_duck.globals import *  # noqa: F403,F401
+
+# import radio_duck.exceptions
+# import radio_duck.globals  # noqa: F401

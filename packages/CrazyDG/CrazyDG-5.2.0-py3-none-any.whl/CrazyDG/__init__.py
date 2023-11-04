d656b79a055f7ae468a0eba3ebb550077a4331b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 15:05:13 +0000

    Add package CrazyDG/5.2.0/CrazyDG-5.2.0-py3-none-any.whl

---
+++ packages/CrazyDG/CrazyDG-5.2.0-py3-none-any.whl/CrazyDG/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "5.2.0"
+
+from .control import Controller
+
+from .crazy import CrazyDragon
+
+from .guidance import Guidance
+
+from .guidance import utils
+
+from .navigation import Navigation
+
+from .recorder import Recorder

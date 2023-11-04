a5202db9530bba9c58e1c93adf2345429d6a92d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:22 +0000

    Add package rethink-note/0.1.13/rethink-note-0.1.13.tar.gz

---
+++ packages/rethink-note/rethink-note-0.1.13.tar.gz/rethink-note-0.1.13/tests/test_api.py
@@ -0,0 +1,449 @@
+import datetime
+import os
+import unittest
+
+from fastapi.testclient import TestClient
+from httpx import Response
+
+from rethink import const
+from rethink.application import app
+from rethink.models import database
+from rethink.models.utils import jwt_decode
+from . import utils
+
+
+class PublicApiTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        utils.set_env(".env.test.local")
+        database.init()
+
+    def setUp(self) -> None:
+        self.client = TestClient(app)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        database.drop_all()
+        utils.drop_env(".env.test.local")
+
+    def test_home(self):
+        resp = self.client.get("/")
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual("text/html; charset=utf-8", resp.headers["content-type"])
+        self.assertEqual(Response, type(resp))
+
+    def test_register(self):
+        resp = self.client.put("/api/user", json={
+            "email": "a@c.com",
+            "password": "a",
+            "language": const.Language.EN.value,
+            "requestId": "xxx"
+        })
+        rj = resp.json()
+        self.assertEqual(const.Code.ONE_USER_MODE.value, rj["code"])
+        self.assertEqual(len(rj["token"]), 0)
+        self.assertEqual("xxx", rj["requestId"])
+
+
+class TokenApiTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        utils.set_env(".env.test.local")
+
+    def setUp(self) -> None:
+        database.init()
+        self.client = TestClient(app)
+        resp = self.client.post("/api/login", json={
+            "email": const.DEFAULT_USER["email"],
+            "password": "",
+        })
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.token = rj["token"]
+
+    def tearDown(self) -> None:
+        database.drop_all()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        utils.drop_env(".env.test.local")
+
+    def test_get_user(self):
+        resp = self.client.get(
+            "/api/user",
+            params={"rid": "xxx"},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("rethink", rj["user"]["nickname"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertGreater(
+            datetime.datetime.strptime(rj["user"]["createdAt"], "%Y-%m-%dT%H:%M:%SZ").timestamp(),
+            0., msg=rj["user"]["createdAt"])
+
+    def test_update_user(self):
+        resp = self.client.post("/api/user", json={
+            "requestId": "xxx",
+            "nickname": "new nickname",
+            "avatar": "http://new.avatar/aa.png",
+            "nodeDisplayMethod": const.NodeDisplayMethod.LIST.value,
+        }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+
+        resp = self.client.get("/api/user", params={"rid": "xxx"}, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("new nickname", rj["user"]["nickname"])
+        self.assertEqual("http://new.avatar/aa.png", rj["user"]["avatar"])
+        self.assertEqual(const.NodeDisplayMethod.LIST.value, rj["user"]["nodeDisplayMethod"])
+        self.assertEqual("xxx", rj["requestId"])
+
+    def test_recent_search(self):
+        resp = self.client.put(
+            "/api/search/recent",
+            json={
+                "requestId": "xxx",
+                "query": "aaa",
+            }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.get(
+            "/api/search/recent",
+            params={"rid": "xxx"},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertEqual(["aaa"], rj["queries"])
+
+    def test_node(self):
+        resp = self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertGreater(len(rj["data"]["nodes"]), 0)
+
+        self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+
+        resp = self.client.put("/api/node", json={
+            "requestId": "xxx",
+            "md": "node1\ntext",
+            "type": const.NodeType.MARKDOWN.value,
+        }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        node = rj["node"]
+
+        resp = self.client.get(
+            "/api/node",
+            params={"rid": "xxx", "nid": node["id"]},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        n = rj["node"]
+        self.assertEqual("node1", n["title"], msg=rj)
+        self.assertEqual("node1\ntext", n["md"])
+        self.assertEqual(const.NodeType.MARKDOWN.value, n["type"])
+
+        resp = self.client.post(
+            "/api/node",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+                "md": "node2\ntext"
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+
+        resp = self.client.get(
+            "/api/node",
+            params={"rid": "xxx", "nid": node["id"]},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        n = rj["node"]
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertEqual("node2", n["title"])
+        self.assertEqual("node2\ntext", n["md"])
+        self.assertEqual(const.NodeType.MARKDOWN.value, n["type"])
+
+        resp = self.client.put(
+            "/api/trash",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.get(
+            "/api/trash",
+            params={"rid": "xxx", "p": 0, "ps": 10},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(1, len(rj["data"]["nodes"]))
+
+        resp = self.client.post(
+            "/api/trashRestore",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.post(
+            "/api/search/cursor",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+                "textBeforeCursor": "How",
+            },
+            headers={"token": self.token},
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertEqual(1, len(rj["nodes"]))
+        self.assertEqual("How do I record", rj["nodes"][0]["title"])
+
+        self.client.put(
+            "/api/trash",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+            },
+            headers={"token": self.token}
+        )
+        resp = self.client.delete(
+            f"/api/trash/{node['id']}",
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.delete(
+            "/api/trash/ssa",
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(const.Code.NODE_NOT_EXIST.value, rj["code"])
+
+        resp = self.client.get(
+            "/api/node",
+            params={"rid": "xxx", "nid": node["id"]},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(const.Code.NODE_NOT_EXIST.value, rj["code"])
+
+    def test_captcha(self):
+        resp = self.client.get(
+            "/api/captcha/img",
+        )
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual("image/png", resp.headers["content-type"])
+        self.assertGreater(len(resp.headers["x-captcha-token"]), 0)
+
+        code_str = jwt_decode(resp.headers["x-captcha-token"])["code"]
+        resp = self.client.post(
+            "/api/captcha/img",
+            json={
+                "requestId": "xxx",
+                "token": resp.headers["x-captcha-token"],
+                "codeStr": code_str,
+            }
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+
+    def test_batch(self):
+        base_count = 2
+        for i in range(10):
+            resp = self.client.put("/api/node", json={
+                "requestId": "xxx",
+                "md": f"node{i}\ntext",
+                "type": const.NodeType.MARKDOWN.value,
+            }, headers={"token": self.token})
+            rj = resp.json()
+            self.assertEqual(0, rj["code"])
+        resp = self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(5, len(rj["data"]["nodes"]))
+        self.assertEqual(10 + base_count, rj["data"]["total"])
+
+        resp = self.client.put(
+            "/api/trash/batch",
+            json={
+                "requestId": "xxx",
+                "nids": [n["id"] for n in rj["data"]["nodes"][:3]],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        resp = self.client.get(
+            "/api/trash",
+            params={"rid": "xxx", "p": 0, "ps": 10},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(3, len(rj["data"]["nodes"]))
+        self.assertEqual(3, rj["data"]["total"])
+
+        resp = self.client.post(
+            "/api/trashRestore/batch",
+            json={
+                "requestId": "xxx",
+                "nids": [n["id"] for n in rj["data"]["nodes"][:2]],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        resp = self.client.get(
+            "/api/trash",
+            params={"rid": "xxx", "p": 0, "ps": 10},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(1, len(rj["data"]["nodes"]))
+        self.assertEqual(1, rj["data"]["total"])
+
+        resp = self.client.post(
+            "/api/trashDelete/batch",
+            json={
+                "requestId": "xxx",
+                "nids": [n["id"] for n in rj["data"]["nodes"]],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        resp = self.client.get(
+            "/api/trash",
+            params={"rid": "xxx", "p": 0, "ps": 10},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(0, len(rj["data"]["nodes"]))
+        self.assertEqual(0, rj["data"]["total"])
+
+    def test_update_obsidian(self):
+        os.makedirs("temp", exist_ok=True)
+        f1 = open("temp/test1.md", "wb+")
+        f1.write("test\n[[qa]]".encode("utf-8"))
+        f2 = open("temp/test2.md", "wb+")
+        f2.write("test2\n\nasdq[[test]]".encode("utf-8"))
+
+        resp = self.client.post(
+            "/api/files/obsidian",
+            files=[
+                ("files", f1),
+                ("files", f2),
+            ],
+            headers={
+                "token": self.token
+            },
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("", rj["failedFilename"])
+
+        resp = self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(4, len(rj["data"]["nodes"]))
+
+        f1.close()
+        f2.close()
+        os.remove("temp/test1.md")
+        os.remove("temp/test2.md")
+        os.rmdir("temp")
+
+    def test_upload_text(self):
+        os.makedirs("temp", exist_ok=True)
+        f1 = open("temp/test1.txt", "wb+")
+        f1.write("dasd".encode("utf-8"))
+        f2 = open("temp/test2.txt", "wb+")
+        f2.write("asdq".encode("utf-8"))
+
+        resp = self.client.post(
+            "/api/files/text",
+            files=[
+                ("files", f1),
+                ("files", f2),
+            ],
+            headers={
+                "token": self.token
+            },
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("", rj["failedFilename"])
+
+        resp = self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(4, len(rj["data"]["nodes"]))
+
+        f1.close()
+        f2.close()
+        os.remove("temp/test1.txt")
+        os.remove("temp/test2.txt")
+        os.rmdir("temp")
---
+++ packages/rethink-note/rethink-note-0.1.13.tar.gz/rethink-note-0.1.13/tests/test_models_utils.py
@@ -0,0 +1,102 @@
+import unittest
+from textwrap import dedent
+
+from bson import ObjectId
+
+from rethink.models import utils
+
+
+class UtilsTest(unittest.TestCase):
+    def test_short_uuid(self):
+        for _ in range(10):
+            uid = utils.short_uuid()
+            self.assertEqual(22, len(uid))
+
+    def test_md2txt(self):
+        text = utils.md2txt(md="# 123\n## 456\n### 789\n")
+        self.assertEqual("123\n456\n789", text)
+
+    def test_pinyin(self):
+        res = utils.txt2search_keys('中心English')
+        self.assertEqual({'zhongxinenglish', '中心english', 'ㄓㄨㄥㄒㄧㄣenglish'}, set(res.split(" ")))
+
+    def test_replace_inner_link(self):
+        md = dedent("""\
+            # 123
+            ddd qwd [[123]] 345
+            [[我哦]]
+            """)
+        o1 = str(ObjectId())
+        o2 = str(ObjectId())
+        filename2nid = {"123": o1, "我哦": o2}
+        res = utils.replace_inner_link(md, filename2nid=filename2nid)
+        self.assertEqual(dedent(f"""\
+            # 123
+            ddd qwd [@123](/n/{o1}) 345
+            [@我哦](/n/{o2})
+            """), res)
+
+        self.assertEqual({"123": o1, "我哦": o2}, filename2nid)
+
+    def test_replace_inner_link_new(self):
+        md = dedent("""\
+            # 123
+            ddd qwd [[123]] 345
+            [[我哦]]
+            """)
+
+        o1 = str(ObjectId())
+        filename2nid = {"123": o1}
+        res = utils.replace_inner_link(md, filename2nid=filename2nid)
+        self.assertEqual(dedent(f"""\
+            # 123
+            ddd qwd [@123](/n/{o1}) 345
+            [@我哦](/n/{filename2nid['我哦']})
+            """), res)
+
+        self.assertIn("我哦", filename2nid)
+
+    def test_change_link_title(self):
+        md = dedent("""\
+            # 123
+            ddd qwd [@我是谁](/n/sqaaq) 345
+            [@ssw](/n/weee)
+            """)
+
+        new_md = utils.change_link_title(md, nid="sqaaq", new_title="哇塞")
+        self.assertEqual(dedent(f"""\
+            # 123
+            ddd qwd [@哇塞](/n/sqaaq) 345
+            [@ssw](/n/weee)
+            """), new_md)
+
+        new_md = utils.change_link_title(md, nid="weee", new_title="哇塞")
+        self.assertEqual(dedent(f"""\
+            # 123
+            ddd qwd [@我是谁](/n/sqaaq) 345
+            [@哇塞](/n/weee)
+            """), new_md)
+
+        md = dedent("""\
+            # 123
+            ddd qwd [@我是谁](/n/sqaaq) 345
+            [@我是谁](/n/sqaaq)
+            """)
+        new_md = utils.change_link_title(md, nid="sqaaq", new_title="哇塞")
+        self.assertEqual(dedent(f"""\
+            # 123
+            ddd qwd [@哇塞](/n/sqaaq) 345
+            [@哇塞](/n/sqaaq)
+            """), new_md)
+
+        md = dedent("""\
+            # 123
+            ddd qwd 345
+            [@我](/n/1) [@我](/n/2)
+            """)
+        new_md = utils.change_link_title(md, nid="2", new_title="哇塞")
+        self.assertEqual(dedent(f"""\
+            # 123
+            ddd qwd 345
+            [@我](/n/1) [@哇塞](/n/2)
+            """), new_md)

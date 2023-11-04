5dfadb410358cb8f1fbf18704a32390a28bd705e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 15:05:20 +0000

    Add package magpai/1.1.0/magpai-1.1.0-py3-none-any.whl

---
+++ packages/magpai/magpai-1.1.0-py3-none-any.whl/tests/unit/test_workflows.py
@@ -0,0 +1,17 @@
+from magpai import *
+from tests import *
+from tests.helpers import *
+import os
+
+class TestVersion(unittest.TestCase):
+
+    def test_workflow_run(self):
+        job = Magpai(os.environ["MAGPAI_API"]).run("yJXfa30diyekbQZdvmRQ", {"Name": "Elmo"})
+        print(job)
+        for output in job['outputs']:
+            if output['name'] == 'out_1':
+                self.assertEqual(output['value'], "My name is Elmo")
+            if output['name'] == 'out_2':
+                self.assertEqual(output['value'], "42")
+            if output['name'] == 'out_3':
+                self.assertEqual(output['value'], "#FE3E3EFF")

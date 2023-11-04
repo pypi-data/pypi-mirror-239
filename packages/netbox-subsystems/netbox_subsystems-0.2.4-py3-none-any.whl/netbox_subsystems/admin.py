ea2233f43eed829de24741d43af7e16e13a5b42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:04:58 +0000

    Add package netbox-subsystems/0.2.4/netbox_subsystems-0.2.4-py3-none-any.whl

---
+++ packages/netbox-subsystems/netbox_subsystems-0.2.4-py3-none-any.whl/netbox_subsystems/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Subsystem, System, SystemGroup
+
+
+@admin.register(Subsystem)
+class SubsystemAdmin(admin.ModelAdmin):
+    list_display = ("name", 'system', 'parent', 'security_id')
+
+
+@admin.register(System)
+class SystemAdmin(admin.ModelAdmin):
+    list_display = ("name", 'tenant',  'parent', 'security_id')
+
+
+@admin.register(SystemGroup)
+class SystemGroupAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+# @admin.register(SystemConfLevel)
+# class SystemConfLevelAdmin(admin.ModelAdmin):
+#     list_display = ("name",)

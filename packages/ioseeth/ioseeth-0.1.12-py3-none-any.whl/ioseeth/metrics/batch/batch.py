6392465ceaf2b282a339f4f9a0de07f4212201de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:06:10 +0000

    Add package ioseeth/0.1.12/ioseeth-0.1.12-py3-none-any.whl

---
+++ packages/ioseeth/ioseeth-0.1.12-py3-none-any.whl/ioseeth/metrics/batch/batch.py
@@ -0,0 +1,58 @@
+"""Evaluate the probability that multiple transfers were bundled in a transaction."""
+
+from web3 import Web3
+
+import ioseeth.indicators.batch
+import ioseeth.metrics.probabilities
+
+# CONFIDENCE ##################################################################
+
+def confidence_score(
+    log: TransactionEvent,
+    w3: Web3,
+    min_transfer_count: int=8,
+    min_transfer_total_erc20: int=0,
+    min_transfer_total_native: int=10**18,
+    max_batching_fee: int=2*10**17,
+    **kwargs
+) -> float:
+    """Evaluate the probability that multiple transfers were bundled in a transaction."""
+    _scores = []
+    # parse the log
+    _block = int(log.block.number)
+    _from = str(getattr(log.transaction, 'from_', '')).lower()
+    _data = str(getattr(log.transaction, 'data', '')).lower()
+    _value = int(getattr(log.transaction, 'value', ''))
+    _logs = tuple(log.logs)
+    # method selector
+    _scores.append(ioseeth.metrics.probabilities.indicator_to_probability(
+        indicator=ioseeth.indicators.batch.input_data_has_batching_selector(_data),
+        true_score=0.9, # almost certainty
+        false_score=0.5)) # not all selectors are in the wordlist: neutral
+    # list of recipients and amounts with same length
+    _scores.append(ioseeth.metrics.probabilities.indicator_to_probability(
+        indicator=ioseeth.indicators.batch.input_data_has_matching_arrays_of_values_and_addresses(data=_data, min_length=min_transfer_count),
+        true_score=0.8, # having both lists is necessary, and rarely seen in other transactions
+        false_score=0.1)) # without lists of recipients and amounts, there is little chance the contract performs batching
+    return ioseeth.metrics.probabilities.conflation(_scores)
+
+# MALICIOUS ###################################################################
+
+#TODO: "to" address keeps tokens (instead of redistributing them)
+
+def malicious_score(
+    log: TransactionEvent,
+    w3: Web3,
+    min_transfer_count: int=8,
+    max_batching_fee: int=2*10**17,
+    **kwargs
+) -> float:
+    """Evaluate the provabability that a batch transaction is malicious."""
+    _scores = []
+    _logs = tuple(log.logs)
+    # transfer of amount 0
+    _scores.append(ioseeth.metrics.probabilities.indicator_to_probability(
+        indicator=ioseeth.indicators.batch.log_has_erc20_transfer_of_null_amount(logs=_logs),
+        true_score=0.9, # certainty
+        false_score=0.5)) # neutral
+    return ioseeth.metrics.probabilities.conflation(_scores)
---
+++ packages/ioseeth/ioseeth-0.1.12-py3-none-any.whl/ioseeth/metrics/batch/ft.py
@@ -0,0 +1,46 @@
+"""Evaluate the probability that a transaction resulted in transfers of ERC20 tokens."""
+
+from web3 import Web3
+
+import ioseeth.indicators.batch
+import ioseeth.metrics.probabilities
+
+# CONFIDENCE ##################################################################
+
+# TODO: add ERC115
+
+def confidence_score(
+    log: TransactionEvent,
+    w3: Web3,
+    min_transfer_count: int=8,
+    min_transfer_total: int=0,
+    **kwargs
+) -> float:
+    """Evaluate the probability that a transaction handled ERC20 tokens."""
+    _scores = []
+    _logs = tuple(log.logs)
+    # events
+    _scores.append(ioseeth.metrics.probabilities.indicator_to_probability(
+        indicator=ioseeth.indicators.batch.log_has_multiple_erc20_transfer_events(logs=_logs, min_count=min_transfer_count, min_total=min_transfer_total),
+        true_score=0.9, # certainty
+        false_score=0.2)) # the token could follow another std
+    return ioseeth.metrics.probabilities.conflation(_scores)
+
+# MALICIOUS ###################################################################
+
+# TODO: the ERC20 balance of the contract increased
+
+def malicious_score(
+    log: TransactionEvent,
+    w3: Web3,
+    **kwargs
+) -> float:
+    """Evaluate the provabability that an ERC20 transaction is malicious."""
+    _scores = []
+    _logs = tuple(log.logs)
+    # transfer of amount 0
+    _scores.append(ioseeth.metrics.probabilities.indicator_to_probability(
+        indicator=ioseeth.indicators.batch.log_has_erc20_transfer_of_null_amount(logs=_logs),
+        true_score=0.9, # certainty
+        false_score=0.5)) # neutral
+    return ioseeth.metrics.probabilities.conflation(_scores)

db6cf2fab726f6bf95aed4a34eacd9e22f0cc366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:03:49 +0000

    Add package CrawlersTools/1.4.77/CrawlersTools-1.4.77.tar.gz

---
+++ packages/CrawlersTools/CrawlersTools-1.4.77.tar.gz/CrawlersTools-1.4.77/CrawlersTools/logs/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Project : CrawlersTools
+# @Time    : 2022/8/11 21:05
+# @Author  : MuggleK
+# @File    : logs.py
+
+from .logger import init_logger
+from .handlers import default_handler, logstash_handler

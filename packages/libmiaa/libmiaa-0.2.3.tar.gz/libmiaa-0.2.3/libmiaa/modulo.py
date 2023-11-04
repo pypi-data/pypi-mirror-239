82dbdb953949135496d9579abab9087079b9f921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:04:22 +0000

    Add package libmiaa/0.2.3/libmiaa-0.2.3.tar.gz

---
+++ packages/libmiaa/libmiaa-0.2.3.tar.gz/libmiaa-0.2.3/libmiaa/modulo.py
@@ -0,0 +1,28 @@
+
+
+class operacion:
+    """ 
+    Clase operacion 
+    """
+
+    def __init__(self, a, b):
+        """
+        Constructor de la clase operacion
+        """
+        self.a = a
+        self.b = b
+    
+    def suma(self):
+        """
+        Metodo que suma dos numeros
+        """
+        return self.a + self.b
+
+class prueba:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def suma(self):
+        return self.a + self.b

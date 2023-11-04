0d4f793129a4b24caa9cc736c20741fa7b897ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:06:21 +0000

    Add package KeycloakFastSso/2023.14.0/KeycloakFastSso-2023.14.0.tar.gz

---
+++ packages/KeycloakFastSso/KeycloakFastSso-2023.14.0.tar.gz/KeycloakFastSso-2023.14.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+description = """
+`keycloakfastsso` est un package Python qui facilite l'intégration de l'authentification Keycloak dans des applications construites avec le framework web FastAPI.
+"""
+# Lis la version depuis le fichier __init__.py
+# __version__ = None
+setup(
+    name="KeycloakFastSso",
+    version='2023.14.0',
+    url="https://github.com/alexandre-meline/keycloakfasty",
+    author="Alexandre Meline",
+    author_email="alexandre.meline.dev@gmail.com",
+    long_description=description,
+    packages=find_packages(),  
+    install_requires=[], 
+)

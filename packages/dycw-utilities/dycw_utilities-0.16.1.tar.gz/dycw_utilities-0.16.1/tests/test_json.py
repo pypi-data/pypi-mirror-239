da3ca5eb779e9e558e850db771817f28c665f029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:04:33 +0000

    Add package dycw-utilities/0.16.1/dycw_utilities-0.16.1.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.16.1.tar.gz/dycw_utilities-0.16.1/tests/test_json.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import datetime as dt
+from pathlib import Path
+from typing import Any
+
+from pytest import mark, param, raises
+
+from utilities.datetime import UTC
+from utilities.json import serialize
+from utilities.platform import IS_NOT_WINDOWS, IS_WINDOWS
+
+
+class TestSerialize:
+    @mark.parametrize(
+        ("x", "expected"),
+        [
+            param(dt.date(2000, 1, 1), '"2000-01-01"'),
+            param(
+                dt.datetime(2000, 1, 1, 12, tzinfo=UTC),
+                '"2000-01-01T12:00:00+00:00"',
+            ),
+            param(
+                Path("a", "b", "c"),
+                '"a/b/c"',
+                marks=mark.skipif(condition=IS_WINDOWS, reason="non-Windows only"),
+            ),
+            param(
+                Path("a", "b", "c"),
+                '"a\\\\b\\\\c"',
+                marks=mark.skipif(condition=IS_NOT_WINDOWS, reason="Windows only"),
+            ),
+            param({1, 2, 3}, '"set([1, 2, 3])"'),
+            param({"a", "b", "c"}, '"set([\\"a\\", \\"b\\", \\"c\\"])"'),
+            param(frozenset([1, 2, 3]), '"frozenset([1, 2, 3])"'),
+            param(
+                frozenset(["a", "b", "c"]),
+                '"frozenset([\\"a\\", \\"b\\", \\"c\\"])"',
+            ),
+        ],
+    )
+    def test_main(self, *, x: Any, expected: str) -> None:
+        assert serialize(x) == expected
+
+    def test_error(self) -> None:
+        class Example:
+            pass
+
+        with raises(TypeError, match="Invalid type"):
+            _ = serialize(Example())
---
+++ packages/dycw-utilities/dycw_utilities-0.16.1.tar.gz/dycw_utilities-0.16.1/tests/test_polars.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+from polars import DataFrame, Float64
+from pytest import raises
+
+from utilities.polars import (
+    DataFrameColumnsError,
+    DataFrameDTypesError,
+    DataFrameHeightError,
+    DataFrameSchemaError,
+    DataFrameShapeError,
+    DataFrameSortedError,
+    DataFrameUniqueError,
+    DataFrameWidthError,
+    check_dataframe,
+)
+
+
+class TestCheckDataFrame:
+    def test_main(self) -> None:
+        df = DataFrame()
+        check_dataframe(df)
+
+    def test_columns_pass(self) -> None:
+        df = DataFrame()
+        check_dataframe(df, columns=[])
+
+    def test_columns_error(self) -> None:
+        df = DataFrame()
+        with raises(DataFrameColumnsError):
+            check_dataframe(df, columns=["value"])
+
+    def test_dtypes_pass(self) -> None:
+        df = DataFrame()
+        check_dataframe(df, dtypes=[])
+
+    def test_dtypes_error(self) -> None:
+        df = DataFrame()
+        with raises(DataFrameDTypesError):
+            check_dataframe(df, dtypes=[Float64])
+
+    def test_height_pass(self) -> None:
+        df = DataFrame()
+        check_dataframe(df, height=0)
+
+    def test_height_error(self) -> None:
+        df = DataFrame()
+        with raises(DataFrameHeightError):
+            check_dataframe(df, height=1)
+
+    def test_schema_pass(self) -> None:
+        df = DataFrame()
+        check_dataframe(df, schema={})
+
+    def test_schema_error(self) -> None:
+        df = DataFrame()
+        with raises(DataFrameSchemaError):
+            check_dataframe(df, schema={"value": Float64})
+
+    def test_shape_pass(self) -> None:
+        df = DataFrame()
+        check_dataframe(df, shape=(0, 0))
+
+    def test_shape_error(self) -> None:
+        df = DataFrame()
+        with raises(DataFrameShapeError):
+            check_dataframe(df, shape=(1, 1))
+
+    def test_sorted_pass(self) -> None:
+        df = DataFrame({"value": [0.0, 1.0]})
+        check_dataframe(df, sorted="value")
+
+    def test_sorted_error(self) -> None:
+        df = DataFrame({"value": [1.0, 0.0]})
+        with raises(DataFrameSortedError):
+            check_dataframe(df, sorted="value")
+
+    def test_unique_pass(self) -> None:
+        df = DataFrame({"value": [0.0, 1.0]})
+        check_dataframe(df, unique="value")
+
+    def test_unique_error(self) -> None:
+        df = DataFrame({"value": [0.0, 0.0]})
+        with raises(DataFrameUniqueError):
+            check_dataframe(df, unique="value")
+
+    def test_width_pass(self) -> None:
+        df = DataFrame()
+        check_dataframe(df, width=0)
+
+    def test_width_error(self) -> None:
+        df = DataFrame()
+        with raises(DataFrameWidthError):
+            check_dataframe(df, width=1)

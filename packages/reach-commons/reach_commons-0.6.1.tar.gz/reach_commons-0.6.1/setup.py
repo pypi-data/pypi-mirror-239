89194d81eddb2fa10e2ad1eeb991867929357f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 07:03:38 +0000

    Add package reach-commons/0.6.1/reach_commons-0.6.1.tar.gz

---
+++ packages/reach-commons/reach_commons-0.6.1.tar.gz/reach_commons-0.6.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="reach_commons",
+    version="0.6.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Wilson Moraes",
+    author_email="wmoraes@getreach.ai",
+    description="Uma descrição curta da sua biblioteca",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

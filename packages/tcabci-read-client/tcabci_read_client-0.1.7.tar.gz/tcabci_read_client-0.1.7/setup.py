2c08a9f55c1813f47b8a5f9dade777b91cf5af9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:12 +0000

    Add package tcabci-read-client/0.1.7/tcabci_read_client-0.1.7.tar.gz

---
+++ packages/tcabci-read-client/tcabci_read_client-0.1.7.tar.gz/tcabci_read_client-0.1.7/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open(os.path.join(os.getcwd(), 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="tcabci_read_client",
+    version="v0.1.7",
+    author="transferchain",
+    description="transferchain abci read client",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/transferchain/tcabci-read-python-client",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)

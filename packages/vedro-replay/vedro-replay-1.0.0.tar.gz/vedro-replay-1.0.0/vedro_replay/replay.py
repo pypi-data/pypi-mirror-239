f4ea50305cde7dd598d091c862c322cc9798d71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:06:06 +0000

    Add package vedro-replay/1.0.0/vedro-replay-1.0.0.tar.gz

---
+++ packages/vedro-replay/vedro-replay-1.0.0.tar.gz/vedro-replay-1.0.0/vedro_replay/replay.py
@@ -0,0 +1,17 @@
+import os
+from typing import Any, Callable
+
+from vedro import params
+
+from .parse_requests import parse_requests
+
+
+def replay(requests_file: str) -> Callable[..., Any]:
+    assert os.path.exists(requests_file)
+
+    def wrapped(fn: Callable[..., Any]) -> Callable[..., Any]:
+        for request in parse_requests(requests_file):
+            params(request.comment, request)(fn)
+        return fn
+
+    return wrapped

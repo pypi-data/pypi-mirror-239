c1b1ef23e0c0b50af87be340e9ec8692cda3737f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:06:14 +0000

    Add package aind-data-schema/0.15.25/aind_data_schema-0.15.25-py3-none-any.whl

---
+++ packages/aind-data-schema/aind_data_schema-0.15.25-py3-none-any.whl/aind_data_schema/rig.py
@@ -0,0 +1,92 @@
+""" Schemas for Neurophysiology and Behavior Rigs"""
+
+from __future__ import annotations
+
+from datetime import date
+from typing import List, Optional, Union
+
+from pydantic import Field
+from pydantic.typing import Annotated
+
+from aind_data_schema.base import AindCoreModel
+from aind_data_schema.data_description import Modality
+from aind_data_schema.device import (
+    Calibration,
+    CameraAssembly,
+    DAQDevice,
+    Detector,
+    Device,
+    DigitalMicromirrorDevice,
+    Disc,
+    EphysAssembly,
+    Filter,
+    HarpDevice,
+    Laser,
+    LaserAssembly,
+    Lens,
+    LightEmittingDiode,
+    Monitor,
+    NeuropixelsBasestation,
+    Objective,
+    Olfactometer,
+    OpenEphysAcquisitionBoard,
+    Patch,
+    PockelsCell,
+    PolygonalScanner,
+    RewardDelivery,
+    Speaker,
+    StickMicroscopeAssembly,
+    Treadmill,
+    Tube,
+    Wheel,
+)
+
+
+class Rig(AindCoreModel):
+    """Description of a rig"""
+
+    schema_version: str = Field("0.1.4", description="schema version", title="Version", const=True)
+    rig_id: str = Field(..., description="room_stim apparatus_version", title="Rig ID")
+    modification_date: date = Field(..., title="Date of modification")
+    modalities: List[Modality] = Field(..., title="Modalities", unique_items=True)
+    mouse_platform: Annotated[
+        Union[Disc, Treadmill, Tube, Wheel], Field(..., title="Mouse platform", discriminator="device_type")
+    ]
+    stimulus_devices: Optional[
+        Annotated[
+            List[Union[Monitor, Olfactometer, RewardDelivery, Speaker]],
+            Field(None, title="Stimulus devices", unique_items=True, discriminator="device_type"),
+        ]
+    ]
+    cameras: Optional[List[CameraAssembly]] = Field(None, title="Camera assemblies", unique_items=True)
+    daqs: Optional[
+        Annotated[
+            List[Union[HarpDevice, NeuropixelsBasestation, OpenEphysAcquisitionBoard, DAQDevice]],
+            Field(None, title="Data acquisition devices", discriminator="device_type"),
+        ]
+    ]
+    ephys_assemblies: Optional[List[EphysAssembly]] = Field(None, title="Ephys probes", unique_items=True)
+    stick_microscopes: Optional[List[StickMicroscopeAssembly]] = Field(None, title="Stick microscopes")
+    laser_assemblies: Optional[List[LaserAssembly]] = Field(None, title="Laser modules", unique_items=True)
+    patch_cords: Optional[List[Patch]] = Field(None, title="Patch cords", unique_items=True)
+    light_sources: Optional[
+        Annotated[
+            List[Union[Laser, LightEmittingDiode]],
+            Field(None, title="Light sources", unique_items=True, discriminator="device_type"),
+        ]
+    ]
+    detectors: Optional[List[Detector]] = Field(None, title="Detectors", unique_items=True)
+    objectives: Optional[List[Objective]] = Field(None, title="Objectives", unique_items=True)
+    filters: Optional[List[Filter]] = Field(None, title="Filters", unique_items=True)
+    lenses: Optional[List[Lens]] = Field(None, title="Lenses", unique_items=True)
+    digital_micromirror_devices: Optional[List[DigitalMicromirrorDevice]] = Field(None, title="DMDs", unique_items=True)
+    polygonal_scanners: Optional[List[PolygonalScanner]] = Field(None, title="Polygonal scanners", unique_items=True)
+    pockels_cells: Optional[List[PockelsCell]] = Field(None, title="Pockels cells", unique_items=True)
+    additional_devices: Optional[List[Device]] = Field(None, title="Additional devices", unique_items=True)
+    calibrations: List[Calibration] = Field(..., title="Full calibration of devices", unique_items=True)
+    ccf_coordinate_transform: Optional[str] = Field(
+        None,
+        title="CCF coordinate transform",
+        description="Path to file that details the CCF-to-lab coordinate transform",
+    )
+    notes: Optional[str] = Field(None, title="Notes")

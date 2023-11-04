67432a03ca6f99e595bdf5b39fa50477d925675b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:06:15 +0000

    Add package knewkarma/1.6.0.0/knewkarma-1.6.0.0.tar.gz

---
+++ packages/knewkarma/knewkarma-1.6.0.0.tar.gz/knewkarma-1.6.0.0/knewkarma/api.py
@@ -0,0 +1,296 @@
+from typing import Optional, Union
+
+import requests
+
+from . import __version__
+from .coreutils import log
+from .messages import message
+
+
+class Api:
+    def __init__(
+        self,
+        base_reddit_endpoint: str,
+        base_github_api_endpoint: str,
+    ):
+        """
+        Initialise the API class with various Reddit API endpoints.
+        """
+        self.updates_endpoint = (
+            f"{base_github_api_endpoint}/repos/bellingcat/knewkarma/releases/latest"
+        )
+        # :param username: The Reddit username
+        self.user_profile_endpoint = f"{base_reddit_endpoint}/user/%s/about.json"
+
+        # :param username: The Reddit username
+        # :param sort: Sorting criterion ('new', 'hot', etc.)
+        # :param limit: Number of posts to fetch
+        self.user_posts_endpoint = (
+            f"{base_reddit_endpoint}/user/%s/submitted.json?sort=%s&limit=%s"
+        )
+
+        # :param username: The Reddit username
+        # :param sort: Sorting criterion ('new', 'hot', etc.)
+        # :param limit: Number of comments to fetch
+        self.user_comments_endpoint = (
+            f"{base_reddit_endpoint}/user/%s/comments.json?sort=%s&limit=%s"
+        )
+
+        # :param subreddit: The subreddit name
+        self.subreddit_profile_endpoint = f"{base_reddit_endpoint}/r/%s/about.json"
+
+        # :param subreddit: The subreddit name
+        # :param sort: Sorting criterion ('new', 'hot', etc.)
+        # :param limit: Number of posts to fetch
+        self.subreddit_posts_endpoint = (
+            f"{base_reddit_endpoint}/r/%s.json?sort=%s&limit=%s"
+        )
+
+        # :param subreddit: The subreddit name
+        # :param post_id: The Reddit post ID
+        # :param sort: Sorting criterion ('new', 'hot', etc.)
+        # :param limit: Number of comments to fetch
+        self.post_comments_endpoint = (
+            f"{base_reddit_endpoint}/r/%s/comments/%s.json?sort=%s&limit=%s"
+        )
+
+        # :param query: Search query
+        # :param sort: Sorting criterion ('new', 'hot', etc.)
+        # :param limit: Number of search results to fetch
+        self.search_endpoint = (
+            f"{base_reddit_endpoint}/search.json?q=%s&sort=%s&limit=%s"
+        )
+
+        # :param limit: Number of popular posts to fetch
+        self.post_listings_endpoint = (
+            f"{base_reddit_endpoint}/r/%s.json?sort=%s&limit=%s"
+        )
+
+        # :param sort: Sorting criterion ('new', 'hot', etc.)
+        # :param limit: Number of front-page posts to fetch
+        self.front_page_endpoint = f"{base_reddit_endpoint}/.json?sort=%s&limit=%s"
+
+    @staticmethod
+    def get_data(endpoint: str) -> Optional[Union[dict, list, None]]:
+        """
+        Fetches JSON data from a given API endpoint.
+
+        :param endpoint: The API endpoint to fetch data from.
+        :return: Returns JSON data as a dictionary or list. Returns None if fetching fails.
+        """
+        from sys import version as python_version
+
+        try:
+            with requests.Session() as session:
+                with session.get(
+                    url=endpoint,
+                    headers={
+                        "User-Agent": f"Knew-Karma/{__version__} "
+                        f"(Python {python_version}; +https://about.me/rly0nheart)"
+                    },
+                ) as response:
+                    if response.status_code == 200:
+                        return response.json()
+                    else:
+                        error_message = response.json()
+                        log.error(
+                            message(
+                                message_type="error",
+                                message_key="api_error",
+                                error_message=error_message,
+                            )
+                        )
+        except requests.exceptions.RequestException as error:
+            log.error(
+                message(
+                    message_type="error",
+                    message_key="http_error",
+                    error_message=error,
+                )
+            )
+        except Exception as error:
+            log.critical(
+                message(
+                    message_type="error",
+                    message_key="unexpected_error",
+                    error_message=error,
+                )
+            )
+
+    def check_updates(self):
+        """
+        Checks if there's a new release of a project on GitHub.
+        If there is, it shows a notification to the user about the release.
+        """
+        import os
+
+        from plyer import notification
+
+        from .coreutils import CURRENT_FILE_DIRECTORY
+
+        # Make a GET request to the GitHub API to get the latest release of the project.
+        response = self.get_data(endpoint=self.updates_endpoint)
+
+        if response:
+            remote_version = response.get("tag_name")
+
+            # Check if the remote version tag matches the current version tag.
+            if remote_version != __version__:
+                # Set icon file to show in the desktop notification
+                icon_file = "icon.ico" if os.name == "nt" else "icon.png"
+
+                try:
+                    # Notify user about the new release.
+                    notification.notify(
+                        title="Knew Karma",
+                        message=message(
+                            message_type="info",
+                            message_key="update_found",
+                            program_name="Knew Karma",
+                            program_call_name="knewkarma",
+                            release_version=remote_version,
+                            current_version=__version__,
+                        ),
+                        app_icon=f"{os.path.join(CURRENT_FILE_DIRECTORY, 'icons', icon_file)}",
+                        timeout=60,
+                    )
+                except NotImplementedError:  # Gets raised on Termux
+                    log.info(
+                        message(
+                            message_type="info",
+                            message_key="update_found",
+                            program_name="Knew Karma",
+                            program_call_name="knewkarma",
+                            release_version=remote_version,
+                        )
+                    )
+
+    def get_user_profile(self, username: str) -> dict:
+        """
+        Gets a specified Reddit user's profile data.
+
+        :param username: Username to query.
+        :returns: A JSON object containing a user's profile if data is valid,
+           otherwise return an empty dictionary.
+        """
+        data = self.get_data(endpoint=self.user_profile_endpoint % username)
+        return data.get("data") if data else {}
+
+    def get_user_posts(self, username: str, sort: str, limit: int) -> list:
+        """
+        Gets a specified Reddit user's posts.
+
+        :param username: Username to query.
+        :param sort: Sorting criterion ('new', 'hot', etc.).
+        :param limit: Number of posts to fetch.
+        :returns: A list of JSON objects containing post if data is valid,
+           otherwise return an empty list.
+        """
+        posts = self.get_data(
+            endpoint=self.user_posts_endpoint % (username, sort, limit)
+        )
+        return posts.get("data").get("children") if posts else []
+
+    def get_user_comments(self, username: str, sort: str, limit: int) -> list:
+        """
+        Gets a specified Reddit user's comments.
+
+        :param username: Username to query.
+        :param sort: Sorting criterion ('new', 'hot', etc.).
+        :param limit: Number of comments to fetch.
+        :returns: A list of JSON objects containing comments' data if valid,
+           otherwise return an empty list.
+        """
+        comments = self.get_data(
+            endpoint=self.user_comments_endpoint % (username, sort, limit)
+        )
+        return comments.get("data").get("children") if comments else []
+
+    def get_subreddit_profile(self, subreddit: str) -> dict:
+        """
+        Gets a specified Reddit subreddit's profile data.
+
+        :param subreddit: Subreddit to query.
+        :returns: A JSON object containing a subreddit's profile if data is valid,
+           otherwise return an empty dictionary.
+        """
+        data = self.get_data(endpoint=self.subreddit_profile_endpoint % subreddit)
+        return data.get("data") if data else {}
+
+    def get_subreddit_posts(self, subreddit: str, sort: str, limit: int) -> list:
+        """
+        Gets a specified Subreddit's posts.
+
+        :param subreddit: Subreddit to query.
+        :param sort: Sorting criterion ('new', 'hot', etc.).
+        :param limit: Number of posts to fetch.
+        :returns: A list of JSON objects containing posts' data if valid,
+           otherwise return an empty list.
+        """
+        posts = self.get_data(
+            endpoint=self.subreddit_posts_endpoint % (subreddit, sort, limit)
+        )
+        return posts.get("data").get("children") if posts else []
+
+    def get_search_results(self, query: str, sort: str, limit: int):
+        """
+        Gets posts that match a user-provided query.
+
+        :param query: Search query
+        :param sort: Posts' sorting criterion.
+        :param limit: Maximum number of posts to return.
+        """
+        results = self.get_data(self.search_endpoint % (query, sort, limit))
+        return results.get("data").get("children")
+
+    def get_post_data(
+        self, subreddit: str, post_id: str, sort: str, limit: int
+    ) -> tuple:
+        """
+        Gets a post's data.
+
+        :param subreddit: The subreddit in which the post was posted.
+        :param post_id: ID of the post.
+        :param sort: Sorting criterion ('new', 'hot', etc.).
+        :param limit: Maximum of comments to fetch.
+        :returns: A tuple of a post's data (post_information, list_of_comments) if valid,
+           otherwise return a tuple containing an empty dict and list.
+        """
+        data = self.get_data(
+            endpoint=self.post_comments_endpoint % (subreddit, post_id, sort, limit)
+        )
+        return (
+            (
+                data[0].get("data").get("children")[0].get("data"),
+                data[1].get("data").get("children") if data else exit(),
+            )
+            if data[0] or data[1]
+            else ({}, [])
+        )
+
+    def get_post_listings(self, listing: str, sort: str, limit: int) -> list:
+        """
+        Gets posts from a specified listing.
+
+        :param listing: Listing to get posts from
+        :param sort: Sorting criterion ('new', 'hot', etc.).
+        :param limit: Maximum of comments to fetch.
+        :returns: A list of JSON objects containing posts' data if valid,
+           otherwise return an empty list.
+        """
+        posts = self.get_data(
+            endpoint=self.post_listings_endpoint % (listing, sort, limit)
+        )
+        return posts.get("data").get("children") if posts else []
+
+    def get_front_page_posts(self, sort: str, limit: int) -> list:
+        """
+        Gets posts from the Reddit front-page.
+
+        :param sort: Sorting criterion ('new', 'hot', etc.).
+        :param limit: Maximum of comments to fetch.
+        :returns: A list of JSON objects containing posts' data if valid,
+           otherwise return an empty list.
+        """
+        posts = self.get_data(endpoint=self.front_page_endpoint % (sort, limit))
+        return posts.get("data").get("children") if posts else []
---
+++ packages/knewkarma/knewkarma-1.6.0.0.tar.gz/knewkarma-1.6.0.0/knewkarma/coreutils.py
@@ -0,0 +1,260 @@
+import argparse
+import csv
+import json
+import logging
+import os
+from datetime import datetime
+
+from rich.logging import RichHandler
+from rich.markdown import Markdown
+from rich_argparse import RichHelpFormatter
+
+
+def path_finder():
+    """
+    Creates file directories if they don't already exist.
+    """
+    file_directories = [CSV_DIRECTORY, JSON_DIRECTORY]
+    for directory in file_directories:
+        os.makedirs(directory, exist_ok=True)
+
+
+def save_data(
+    data: dict,
+    filename: str,
+    save_to_json: bool = False,
+    save_to_csv: bool = False,
+):
+    """
+    Save the given data to JSON and/or CSV files based on the arguments.
+
+    :param data: The data to be saved, which is a list of dictionaries.
+    :param filename: The base filename to use when saving.
+    :param save_to_json: A boolean value to indicate whether to save data as a JSON file.
+    :param save_to_csv: A boolean value to indicate whether to save data as a CSV file.
+    """
+    # Save to JSON if save_json is True
+    if save_to_json:
+        with open(os.path.join(JSON_DIRECTORY, f"{filename}.json"), "w") as json_file:
+            json.dump(data, json_file)
+        log.info(f"JSON data saved to {json_file.name}")
+
+    # Save to CSV if save_csv is True
+    if save_to_csv:
+        with open(
+            os.path.join(CSV_DIRECTORY, f"{filename}.csv"), "w", newline=""
+        ) as csv_file:
+            writer = csv.writer(csv_file)
+            # Write the header based on keys from the first dictionary
+            header = data.keys()
+            writer.writerow(header)
+
+            # Write each row
+            writer.writerow(data.values())
+        log.info(f"CSV data saved to {csv_file.name}")
+
+
+def convert_timestamp_to_datetime(timestamp: float) -> str:
+    """
+    Converts a Unix timestamp to a formatted datetime string.
+
+    :param timestamp: The Unix timestamp to be converted.
+    :return: A formatted datetime string in the format "dd MMMM yyyy, hh:mm:ssAM/PM".
+    """
+    utc_from_timestamp = datetime.fromtimestamp(timestamp)
+    datetime_object = utc_from_timestamp.strftime("%d %B %Y, %I:%M:%S%p")
+    return datetime_object
+
+
+def setup_logging(debug_mode: bool) -> logging.getLogger:
+    """
+    Configure and return a logging object with the specified log level.
+
+    :param debug_mode: A boolean value indicating whether log level should be set to DEBUG.
+    :return: A logging object configured with the specified log level.
+    """
+    logging.basicConfig(
+        level="NOTSET" if debug_mode else "INFO",
+        format="%(message)s",
+        handlers=[
+            RichHandler(
+                markup=True, log_time_format="%I:%M:%S %p", show_level=debug_mode
+            )
+        ],
+    )
+    return logging.getLogger("Knew Karma")
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """
+    Creates and configures an argument parser for the command line arguments.
+
+    :return: A configured argparse.ArgumentParser object ready to parse the command line arguments.
+    """
+    from . import (
+        __description__,
+        __epilog__,
+        __post_examples__,
+        __posts_examples__,
+        __search_examples__,
+        __user_examples__,
+        __subreddit_examples__,
+        __operations_description__,
+        __version__,
+    )
+
+    parser = argparse.ArgumentParser(
+        description=Markdown(__description__, style="argparse.text"),
+        epilog=Markdown(__epilog__, style="argparse.text"),
+        formatter_class=RichHelpFormatter,
+    )
+
+    subparsers = parser.add_subparsers(dest="mode", help="Operation mode")
+
+    # User mode
+    user_parser = subparsers.add_parser(
+        "user",
+        help="User operations",
+        description=Markdown(
+            __operations_description__.format("User"), style="argparse.text"
+        ),
+        epilog=Markdown(__user_examples__),
+        formatter_class=RichHelpFormatter,
+    )
+    user_parser.add_argument("username", help="Username to query")
+    user_parser.add_argument(
+        "-c",
+        "--comments",
+        action="store_true",
+        help="Get a user's comments",
+    )
+    user_parser.add_argument(
+        "-p", "--posts", action="store_true", help="Get a user's posts"
+    )
+
+    # Subreddit mode
+    subreddit_parser = subparsers.add_parser(
+        "subreddit",
+        help="Subreddit operations",
+        description=Markdown(
+            __operations_description__.format("Subreddit"), style="argparse.text"
+        ),
+        epilog=Markdown(__subreddit_examples__),
+        formatter_class=RichHelpFormatter,
+    )
+    subreddit_parser.add_argument(
+        "subreddit",
+        help="Subreddit to query",
+    )
+    subreddit_parser.add_argument(
+        "-p",
+        "--posts",
+        action="store_true",
+        help="Get a subreddit's posts",
+    )
+
+    # Post mode
+    post_parser = subparsers.add_parser(
+        "post",
+        help="Post operations",
+        description=Markdown(
+            __operations_description__.format("Post"), style="argparse.text"
+        ),
+        epilog=Markdown(__post_examples__),
+        formatter_class=RichHelpFormatter,
+    )
+    post_parser.add_argument("post_id", help="Post ID")
+    post_parser.add_argument("post_subreddit", help="Source subreddit")
+    post_parser.add_argument(
+        "-c", "--comments", dest="comments", action="store_true", help="Show comments"
+    )
+    post_parser.add_argument(
+        "-a", "--awards", dest="awards", action="store_true", help=argparse.SUPPRESS
+    )
+
+    # Posts mode
+    posts_parser = subparsers.add_parser(
+        "posts",
+        help="Posts operations",
+        description=Markdown(
+            __operations_description__.format("Posts"), style="argparse.text"
+        ),
+        epilog=Markdown(__posts_examples__),
+        formatter_class=RichHelpFormatter,
+    )
+    posts_parser.add_argument(
+        "-l",
+        "--listing",
+        help="Post listing name",
+        choices=["best", "controversial", "popular", "rising"],
+        default="all",
+    )
+
+    # Search mode
+    search_parser = subparsers.add_parser(
+        "search",
+        help="Search posts",
+        description=Markdown(
+            __operations_description__.format("Search"), style="argparse.text"
+        ),
+        epilog=Markdown(__search_examples__),
+        formatter_class=RichHelpFormatter,
+    )
+    search_parser.add_argument("query", help="Search query")
+
+    # Global options
+    parser.add_argument(
+        "-s",
+        "--sort",
+        default="all",
+        choices=DATA_SORT_LISTINGS,
+        help="Bulk data sort criterion (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        default=10,
+        type=int,
+        help="Maximum number of bulk data to get (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-j",
+        "--json",
+        help="Write data to a JSON file.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c",
+        "--csv",
+        help="Write data to a CSV file.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        help="Run Knew Karma in debug mode.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        version=__version__,
+        action="version",
+    )
+
+    return parser
+
+
+DATA_SORT_LISTINGS = ["controversial", "new", "top", "best", "hot", "rising"]
+arguments = create_parser().parse_args()
+log = setup_logging(debug_mode=arguments.debug)
+
+# Construct path to the program's directory
+PROGRAM_DIRECTORY = os.path.expanduser(os.path.join("~", "knewkarma"))
+
+# Construct path to the current file's directory
+CURRENT_FILE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+# Construct paths to directories of CSV and JSON files.
+CSV_DIRECTORY = os.path.join(PROGRAM_DIRECTORY, "csv")
+JSON_DIRECTORY = os.path.join(PROGRAM_DIRECTORY, "json")

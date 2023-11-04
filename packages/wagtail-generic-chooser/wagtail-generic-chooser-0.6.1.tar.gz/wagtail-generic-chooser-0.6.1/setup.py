3c4c451f32628172097da9e4339694dda4729f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:08:56 +0000

    Add package wagtail-generic-chooser/0.6.1/wagtail-generic-chooser-0.6.1.tar.gz

---
+++ packages/wagtail-generic-chooser/wagtail-generic-chooser-0.6.1.tar.gz/wagtail-generic-chooser-0.6.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wagtail-generic-chooser',
+    version='0.6.1',
+    description="A toolkit for custom chooser popups in Wagtail",
+    author='Matthew Westcott',
+    author_email='matthew.westcott@torchbox.com',
+    url='https://github.com/wagtail/wagtail-generic-chooser',
+    packages=find_packages(exclude=("tests", "tests.*")),
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.11.1,<3.0',
+    ],
+    license='BSD',
+    long_description="""
+        Base classes for building chooser popups and form widgets for the Wagtail admin,
+        matching Wagtail's built-in choosers and backed by either models or a REST API
+    """,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Framework :: Django',
+        'Framework :: Django :: 3',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 4',
+        'Framework :: Wagtail :: 5',
+    ],
+)

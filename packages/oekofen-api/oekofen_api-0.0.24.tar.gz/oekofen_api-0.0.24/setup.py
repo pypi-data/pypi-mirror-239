6243104fa0b01d11f812d6501dafa8505c2dd782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:05:39 +0000

    Add package oekofen-api/0.0.24/oekofen_api-0.0.24.tar.gz

---
+++ packages/oekofen-api/oekofen_api-0.0.24.tar.gz/oekofen_api-0.0.24/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+VERSION = "0.0.24"
+
+setup(
+    name="oekofen_api",
+    version=VERSION,
+    author="Christian Karrié",
+    author_email="ckarrie@gmail.com",
+    description="A python library to retrieve statistics from your Oekofen Pelletronic",
+    download_url=f'https://github.com/ckarrie/oekofen-api/archive/refs/tags/v{VERSION}.tar.gz',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ckarrie/oekofen-api",
+    packages=find_packages(),
+    install_requires=[
+        'voluptuous>=0.13.1',
+        'yarl>=1.8.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

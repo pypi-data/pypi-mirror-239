f925c1649ef6b02a8615edb8ad3e267c7d0e5a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:05:00 +0000

    Add package pyolive/1.0.8/pyolive-1.0.8-py3-none-any.whl

---
+++ packages/pyolive/pyolive-1.0.8-py3-none-any.whl/pyolive/__init__.py
@@ -0,0 +1,1 @@
+from .app import Olive as Olive
---
+++ packages/pyolive/pyolive-1.0.8-py3-none-any.whl/pyolive/__main__.py
@@ -0,0 +1,14 @@
+"""
+Olive worker
+"""
+
+import sys
+
+
+def main():  # needed for console script
+    if __package__ == '':
+        pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())

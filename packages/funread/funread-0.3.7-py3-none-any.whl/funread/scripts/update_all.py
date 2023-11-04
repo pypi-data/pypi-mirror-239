98f48bb2ae66177a809986537f93059fd1554dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:06:13 +0000

    Add package funread/0.3.7/funread-0.3.7-py3-none-any.whl

---
+++ packages/funread/funread-0.3.7-py3-none-any.whl/funread/scripts/update_all.py
@@ -0,0 +1,16 @@
+import argparse
+
+from funread.legado.task import GenerateSourceTask
+from funread.legado.task import ReadODSProgressDataTask
+from funread.legado.task import ReadODSSourceDataTask
+from funread.legado.task import ReadODSUrlDataTask
+from funread.legado.task import UpdateRssTask
+
+
+# ReadODSUrlDataTask().run()
+# ReadODSSourceDataTask().run()
+#ReadODSProgressDataTask().run()
+GenerateSourceTask().run()
+GenerateSourceTask().run()
+UpdateRssTask().run()
+UpdateRssTask().update_main()

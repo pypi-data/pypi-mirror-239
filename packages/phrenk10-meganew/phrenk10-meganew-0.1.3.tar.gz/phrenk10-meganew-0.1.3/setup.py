538465e065d683b55f63a79a3828faa48829c944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 15:04:55 +0000

    Add package phrenk10-meganew/0.1.3/phrenk10-meganew-0.1.3.tar.gz

---
+++ packages/phrenk10-meganew/phrenk10-meganew-0.1.3.tar.gz/phrenk10-meganew-0.1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phrenk10-meganew",
+    version="0.1.3",
+    description="testest",
+    author="test",
+    packages=find_packages(),
+)

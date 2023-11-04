02bebceba97dac926835527ebed8cd4249bc46ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 07:03:41 +0000

    Add package ivette-client/0.0.13/ivette-client-0.0.13.tar.gz

---
+++ packages/ivette-client/ivette-client-0.0.13.tar.gz/ivette-client-0.0.13/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ivette-client',
+    version='0.0.13',
+    description='Python client for Ivette Computational chemistry and Bioinformatics project',
+    author='Eduardo Bogado',
+    py_modules=['runJob', 'package.IO_module', 'package.prisma_module', 'package.supabase_module'],  # Include 'runJob.py' as a module
+    entry_points={
+        'console_scripts': [
+            'ivette=runJob:main',
+        ],
+    },
+)

79d7b5ddcdd4ae0a0714a1559bad722c911e2383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:03:51 +0000

    Add package iwashi/2.0.4/iwashi-2.0.4.tar.gz

---
+++ packages/iwashi/iwashi-2.0.4.tar.gz/iwashi-2.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.rst", mode="r", encoding="utf-8") as f:
+    long_description = f.read()
+
+author = "am230"
+name = "iwashi"
+py_modules = [name]
+
+setup(
+    name=name,
+    version="2.0.4",
+    keywords=["generator", "sns"],
+    description="Link Tree Generator",
+    long_description=long_description,
+    install_requires=["httpx", "bs4", "yt-dlp", "loguru", "typingdict", "click"],
+    license="MIT Licence",
+    long_description_content_type="text/x-rst",
+    packages=find_packages(),
+    url=f"https://github.com/{author}/{name}",
+    author=author,
+    author_email="am.230@outlook.jp",
+    py_modules=py_modules,
+    platforms="any",
+)

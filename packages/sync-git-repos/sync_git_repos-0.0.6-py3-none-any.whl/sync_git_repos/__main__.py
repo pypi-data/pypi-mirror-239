50960fd2785026c48c540a7eaf13db945bb59dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:03:27 +0000

    Add package sync-git-repos/0.0.6/sync_git_repos-0.0.6-py3-none-any.whl

---
+++ packages/sync-git-repos/sync_git_repos-0.0.6-py3-none-any.whl/sync_git_repos/__main__.py
@@ -0,0 +1,122 @@
+import argparse
+import logging
+import sys
+import time
+from typing import Optional, Any, Dict, Callable, Union, List
+
+from sync_git_repos import utils, hooks
+
+parser = argparse.ArgumentParser()
+
+group = parser.add_argument_group('General settings')
+group.add_argument('--settings', required=False, type=str, default='settings.json', help='Path to settings file')
+group.add_argument('--repo', required=False, type=str, default=None, help='Repo key for sync')
+group.add_argument('--sleep_timeout', required=False, type=int, default=-1,
+                   help='If the parameter is set, the synchronization is '
+                        'performed with the specified frequency (in sec.) indefinitely')
+
+
+def _get_settings(settings: dict, name: str) -> Optional[Any]:
+    value = settings.get(name)
+    if not value:
+        raise Exception(f'Setting "{name}" is none or empty. Check "settings.json".')
+
+    return value
+
+
+def _load_repos(settings: dict) -> Dict[str, utils.SyncSetting]:
+    repos = _get_settings(settings, 'repos')
+    result = {}
+    for key, repo in repos.items():
+        branches = repo.get('branches')
+        if branches is not None:
+            branches = tuple(branches)
+
+        hooks_cfg = repo.get('hooks', {})
+
+        result[key] = utils.SyncSetting(key=key,
+                                        branches=branches,
+                                        force_push=repo.get('force_push', False),
+                                        check_base_name=repo.get('check_base_name', True),
+                                        delete_after_sync=repo.get('delete_after_sync', False),
+                                        from_repo_url=utils.prepare_repo_url(_get_settings(repo, 'from_repo_url')),
+                                        to_repo_url=utils.prepare_repo_url(_get_settings(repo, 'to_repo_url')),
+                                        hooks={
+                                            'before_push': _deserialize_list_of_hooks(
+                                                hooks_cfg.get('before_push')) if hooks_cfg else [],
+                                            'after': _deserialize_list_of_hooks(
+                                                hooks_cfg.get('after')) if hooks_cfg else [],
+                                            'before': _deserialize_list_of_hooks(
+                                                hooks_cfg.get('before')) if hooks_cfg else [],
+                                        })
+
+    return result
+
+
+def _deserialize_list_of_hooks(hooks_cfg: Optional[List[Union[str, dict]]] = None) -> list:
+    if not hooks_cfg:
+        return []
+
+    result = []
+    for hook in hooks_cfg:
+        is_str = isinstance(hook, str)
+        name = hook if is_str else hook.get('name')
+
+        if name:
+            args = [] if is_str else hook.get('args', [])
+            kwargs = {} if is_str else hook.get('kwargs', {})
+            result.append(hooks.create_hook(name, *args, **kwargs))
+    return result
+
+
+def _run_forever(sleep_timeout: int, func: Callable, *args, **kwargs):
+    while True:
+        func(*args, **kwargs)
+        time.sleep(sleep_timeout)
+
+
+def _sync_all_repos(logger, sync_folder, repos):
+    for repo in repos.values():
+        utils.sync_git_repo(logger, sync_folder, repo)
+
+
+def main(logger, args):
+    settings = utils.load_file_as_dict(args.settings)
+
+    sync_folder = _get_settings(settings, 'sync_folder')
+    status, message = utils.create_folder_if_not_exist(sync_folder)
+    if status > 0:
+        logger.info(message)
+    else:
+        logger.error(message)
+
+    repos = _load_repos(settings)
+
+    if args.repo:
+        repo = repos.get(args.repo)
+        if not repo:
+            raise Exception(f'Repo "{args.repo}" not found. Check "settings.json".')
+        if args.sleep_timeout > 0:
+            _run_forever(args.sleep_timeout, utils.sync_git_repo, logger, sync_folder, repo)
+        else:
+            utils.sync_git_repo(logger, sync_folder, repo)
+    else:
+        if args.sleep_timeout > 0:
+            _run_forever(args.sleep_timeout, _sync_all_repos, logger, sync_folder, repos)
+        else:
+            _sync_all_repos(logger, sync_folder, repos)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format="[%(asctime)s - %(levelname)s - %(name)s] %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    main_logger = logging.getLogger('SYNC PROCESS')
+    main_logger.setLevel(logging.INFO)
+
+    try:
+        main(main_logger, parser.parse_args())
+    except Exception as ex:
+        logging.error(ex)
---
+++ packages/sync-git-repos/sync_git_repos-0.0.6-py3-none-any.whl/sync_git_repos/utils.py
@@ -0,0 +1,193 @@
+import json
+import os
+import shlex
+import shutil
+import subprocess
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, Tuple, Optional, Union
+
+
+@dataclass
+class Hooks:
+    name: str
+    args: Optional[list] = None
+    kwargs: Optional[dict] = None
+
+
+@dataclass
+class SyncSetting:
+    """ Settings """
+    # from repo url
+    from_repo_url: str
+    # to repo url
+    to_repo_url: str
+    # Key and local repo  dir
+    key: str
+    # Delete local repo dir if True
+    delete_after_sync: Optional[bool] = False
+    # Force push if True. Execute push with '--force' flag
+    force_push: Optional[bool] = False
+    # List of branches for syncing(push with branch), if is not set execute push with '--mirror' flag
+    branches: Optional[Tuple] = None
+    check_base_name: Optional[bool] = True
+    # For example
+    # {
+    #     "after": ["sleep"],
+    #     "before": [{
+    #         "name": "wait_input",
+    #         "args": [],
+    #         "kwargs": {"message": "Press any key to continuous"}
+    #     }]
+    # }
+    hooks: Optional[dict] = None
+
+    def execute_hooks(self, key: str):
+        if self.hooks:
+            hooks = self.hooks.get(key)
+            if hooks:
+                for hook in hooks:
+                    hook()
+
+
+def prepare_repo_url(repo_url: str):
+    envs = ['GITHUB_TOKEN']
+    for env in envs:
+        env_val = os.environ.get(env)
+        if env_val:
+            repo_url = repo_url.replace(f'${env}', env_val)
+
+    return repo_url
+
+
+def create_folder_if_not_exist(folder_path: Union[str, os.PathLike]) -> Tuple[int, str]:
+    if os.path.exists(folder_path):
+        return 2, f'Folder "{folder_path}" already exists.'
+
+    os.makedirs(folder_path)
+
+    return 1, 'Folder "{folder_path}" created successfully.'
+
+
+def load_file_as_dict(file: str) -> Dict:
+    """
+    Load file content
+    :param file: pile path
+    :return: content as dict
+    """
+    with open(file, encoding="utf-8") as json_file:
+        return json.load(json_file)
+
+
+def sh(*args, **kwargs) -> str:
+    """
+    Get subprocess output
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    return subprocess.check_output(*args, **kwargs).decode().strip()
+
+
+def sync_git_repo(logger, base_dir: str, sync_setting: SyncSetting) -> None:
+    """
+    Sync one git repo
+    :param logger: logger for printing
+    :param base_dir: base dir
+    :param sync_setting: sync setting
+    :return: status and message
+    """
+    logger.info(f'Syncing from "{sync_setting.from_repo_url}" to "{sync_setting.to_repo_url}"...')
+    from_base_name = os.path.basename(os.path.normpath(sync_setting.from_repo_url))
+    to_base_name = os.path.basename(os.path.normpath(sync_setting.to_repo_url))
+    if sync_setting.check_base_name and from_base_name != to_base_name:
+        raise Exception(f'{from_base_name} != {to_base_name}')
+
+    target_dest = Path(base_dir) / sync_setting.key
+    try:
+        sync_setting.execute_hooks('before')
+
+        create_folder_status, create_folder_message = create_folder_if_not_exist(target_dest)
+        if create_folder_status > 0:
+            logger.info(create_folder_message)
+        else:
+            logger.error(create_folder_message)
+
+        # Folder created
+        if create_folder_status == 1:
+            output = clone_repo(sync_setting.from_repo_url, target_dest)
+            if output:
+                logger.info(output)
+        else:
+            output = fetch(str(target_dest))
+            if output:
+                logger.info(output)
+
+            output = pull()
+            if output:
+                logger.info(output)
+
+        sync_setting.execute_hooks('before_push')
+
+        if sync_setting.branches:
+            for branch in sync_setting.branches:
+                output = push(sync_setting.to_repo_url, str(target_dest), branch, force=sync_setting.force_push)
+                if output:
+                    logger.info(output)
+        else:
+            output = push(sync_setting.to_repo_url, str(target_dest), mirror=True)
+            if output:
+                logger.info(output)
+
+        logger.info(f'Success syncing to {sync_setting.to_repo_url}!')
+    except Exception as repo_ex:
+        logger.error(repo_ex)
+    finally:
+        if sync_setting.delete_after_sync:
+            shutil.rmtree(target_dest, ignore_errors=True)
+
+        sync_setting.execute_hooks('after')
+
+
+def fetch(dest: str) -> str:
+    return sh(['git', 'fetch', 'origin'], cwd=dest)
+
+
+def pull() -> str:
+    return sh(['git', 'pull', '--ff-only'])
+
+
+def push(repo_url: str, dest: str, branch: Optional[str] = None, force: Optional[bool] = False,
+         mirror: Optional[bool] = False) -> str:
+    main_args = ['git', 'push', repo_url]
+    if branch:
+        main_args.append(branch)
+
+    if force:
+        main_args.append('--force')
+
+    if mirror:
+        main_args.append('--mirror')
+
+    return sh(main_args, cwd=dest)
+
+
+def clone_repo(repo_url: str, dest: Union[os.PathLike, str]) -> str:
+    if not repo_exists(dest):
+        return sh(['git', 'clone', '--no-checkout', '--bare', repo_url, str(dest)])
+
+    return f'Repo "{repo_url}" already clone.'
+
+
+def get_repo_info(dest: Union[os.PathLike, str]) -> dict:
+    if not repo_exists(dest):
+        raise ValueError(f'No repo found at {dest}')
+
+    current_remote = sh(shlex.split('git config --get remote.origin.url'), cwd=dest)
+    current_branch = sh(shlex.split('git rev-parse --abbrev-ref HEAD'), cwd=dest)
+
+    return {'current_remote': current_remote, 'current_branch': current_branch}
+
+
+def repo_exists(dest: Union[os.PathLike, str]):
+    return os.path.exists(os.path.join(dest, 'HEAD'))

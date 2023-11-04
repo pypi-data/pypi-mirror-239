07f497a0e6b781d9815940df630bd5b803d17cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:09:11 +0000

    Add package aineko/0.2.1/aineko-0.2.1.tar.gz

---
+++ packages/aineko/aineko-0.2.1.tar.gz/aineko-0.2.1/aineko/cli/run.py
@@ -0,0 +1,40 @@
+# Copyright 2023 Aineko Authors
+# SPDX-License-Identifier: Apache-2.0
+"""Module to run a pipeline from the command line."""
+import logging
+import time
+import traceback
+from typing import Optional
+
+from aineko.core.runner import Runner
+
+logger = logging.getLogger(__name__)
+
+
+def main(
+    pipeline_config_file: str,
+    pipeline_name: Optional[str] = None,
+    retry: bool = False,
+) -> None:
+    """Main function to run a pipeline from the command line.
+
+    Args:
+        pipeline_config_file: Path to the file containing the pipeline config
+        pipeline_name: Name of the pipeline to run, overrides pipeline config
+        retry: If true, retry running the pipeline on failure every 10 seconds
+    """
+    while True:
+        try:
+            runner = Runner(
+                pipeline_config_file=pipeline_config_file,
+                pipeline_name=pipeline_name,
+            )
+            runner.run()
+        except Exception as e:  # pylint: disable=broad-except
+            if not retry:
+                raise e
+            else:
+                logger.error("Error running pipeline: %s", e)
+                logger.debug(traceback.format_exc())
+                time.sleep(10)
+                continue

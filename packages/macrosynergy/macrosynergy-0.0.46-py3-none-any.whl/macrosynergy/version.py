555483f8e2c31197de4f3439e7acc86dac222153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:04 +0000

    Add package macrosynergy/0.0.46/macrosynergy-0.0.46-py3-none-any.whl

---
+++ packages/macrosynergy/macrosynergy-0.0.46-py3-none-any.whl/macrosynergy/version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM macrosynergy SETUP.PY
+short_version: str = '0.0.46'
+version: str = '0.0.46'
+full_version: str = '0.0.46'
+git_revision: str = '275ac18d42a5bac2980c329cdbec0f5ab6129cd4'
+release: bool = True
+if not release:
+    version = full_version

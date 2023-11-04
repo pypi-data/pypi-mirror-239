bc1e37645aa15a2aa7f33dcc738f3aa6095742f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 15:05:17 +0000

    Add package frequency-regularization/0.1.0/frequency_regularization-0.1.0.tar.gz

---
+++ packages/frequency-regularization/frequency_regularization-0.1.0.tar.gz/frequency_regularization-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='frequency_regularization',
+    version='0.1.0',
+    description='Frequency Regularization Python Package',
+    url='https://github.com/guanfangdong/pytorch-frequency-regularization',
+    author='Zhao, Chenqiu and Dong, Guanfang and Zhang, Shupei and Tan, Zijie and Basu, Anup',
+    packages=find_packages(where="src"),
+    license="Apache License 2.0",
+    install_requires=['numpy',
+                      'imageio',
+                      'torch',
+                      'matplotlib',
+                      ],
+    python_requires=">=3.7, <4",
+    # pytorch_requires=">=1.10.0, <2.1.0"
+
+    classifiers=[
+    #     'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)

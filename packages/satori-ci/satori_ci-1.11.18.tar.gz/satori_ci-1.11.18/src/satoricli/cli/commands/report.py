febb9293c7e91a6916877e9e8744c015a4a1c573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:20 +0000

    Add package satori-ci/1.11.18/satori_ci-1.11.18.tar.gz

---
+++ packages/satori-ci/satori_ci-1.11.18.tar.gz/satori_ci-1.11.18/src/satoricli/cli/commands/report.py
@@ -0,0 +1,80 @@
+from argparse import ArgumentParser
+from typing import Literal, Optional
+
+import httpx
+from rich.progress import Progress
+
+from satoricli.api import client
+from satoricli.cli.utils import autoformat, console, format_outputs
+
+from .base import BaseCommand
+
+
+class ReportCommand(BaseCommand):
+    name = "report"
+
+    def register_args(self, parser: ArgumentParser):
+        parser.add_argument("id", metavar="ID", nargs="?")
+        parser.add_argument(
+            "action",
+            metavar="ACTION",
+            choices=("show", "output", "stop", "files", "delete", "public"),
+            nargs="?",
+            default="show",
+            help="action to perform",
+        )
+        parser.add_argument("-p", "--page", type=int, default=1)
+        parser.add_argument("-l", "--limit", type=int, default=20)
+        parser.add_argument("-f", "--filter")
+
+    def __call__(
+        self,
+        id: Optional[str],
+        action: Literal["show", "output", "stop", "files", "delete", "public"],
+        page: int,
+        limit: int,
+        filter: Optional[str],
+        **kwargs,
+    ):
+        if action == "show":
+            res = client.get(
+                f"/reports/{id or ''}",
+                params={"page": page, "limit": limit, "filter": filter},
+            ).json()
+
+            if not id and not kwargs["json"]:
+                if res["count"] == 0:
+                    console.print("No reports found")
+                    return
+
+                autoformat(res["list"], list_separator="-" * 48)
+                console.print(
+                    f"[b]Page:[/] {res['current_page']} of {res['total_pages']}"
+                )
+            else:
+                autoformat(res, jsonfmt=kwargs["json"])
+        elif action == "output":
+            r = client.get(f"/outputs/{id}")
+            with httpx.stream("GET", r.json()["url"], timeout=300) as s:
+                format_outputs(s.iter_lines())
+        elif action == "files":
+            r = client.get(f"/outputs/{id}/files")
+            with httpx.stream("GET", r.json()["url"], timeout=300) as s:
+                total = int(s.headers["Content-Length"])
+
+                with Progress() as progress:
+                    task = progress.add_task("Downloading...", total=total)
+
+                    with open(f"satorici-files-{id}.tar.gz", "wb") as f:
+                        for chunk in s.iter_raw():
+                            progress.update(task, advance=len(chunk))
+                            f.write(chunk)
+        elif action == "stop":
+            res = client.get(f"/reports/{id}/stop").json()
+            autoformat(res, jsonfmt=kwargs["json"])
+        elif action == "delete":
+            client.delete(f"/reports/{id}")
+            print("Report deleted")
+        elif action == "public":
+            res = client.patch(f"/reports/{id}", json={"public": "invert"}).json()
+            autoformat(res)
---
+++ packages/satori-ci/satori_ci-1.11.18.tar.gz/satori_ci-1.11.18/src/satoricli/cli/commands/team.py
@@ -0,0 +1,117 @@
+from argparse import ArgumentParser
+from typing import Literal, Optional
+
+from satoricli.api import client
+from satoricli.cli.utils import autoformat, console
+
+from .base import BaseCommand
+
+
+class TeamCommand(BaseCommand):
+    name = "team"
+
+    def register_args(self, parser: ArgumentParser):
+        parser.add_argument("--role", default="READ", help="User role")
+
+        # Use with the add command
+        parser.add_argument("--repo", help="Repo name")
+        parser.add_argument("--email", help="User email")
+        parser.add_argument("--github", help="Github user name")
+        parser.add_argument("--monitor", help="Monitor id")
+
+        parser.add_argument("id", nargs="?")
+        parser.add_argument(
+            "action",
+            nargs="?",
+            choices=(
+                "show",
+                "create",
+                "members",
+                "repos",
+                "get_config",
+                "set_config",
+                "get_token",
+                "refresh_token",
+                "delete",
+                "add",
+            ),
+            default="show",
+        )
+        parser.add_argument("config_name", nargs="?")
+        parser.add_argument("config_value", nargs="?")
+
+    def __call__(
+        self,
+        id: Optional[str],
+        action: Literal[
+            "show",
+            "create",
+            "members",
+            "repos",
+            "get_config",
+            "set_config",
+            "get_token",
+            "refresh_token",
+            "delete",
+            "add",
+        ],
+        role: Optional[str],
+        repo: Optional[str],
+        email: Optional[str],
+        github: Optional[str],
+        monitor: Optional[str],
+        config_name: Optional[str],
+        config_value: Optional[str],
+        **kwargs,
+    ):
+        if action == "show":
+            info = client.get("/teams").json()
+        elif action == "create":
+            info = client.post(f"/teams/{id}").json()
+        elif action == "members":
+            info = client.get(f"/teams/{id}/members").json()
+        elif action == "add":
+            if email:
+                info = client.post(
+                    f"/teams/{id}/members", json={"email": email, "role": role}
+                ).json()
+            elif github:
+                info = client.post(
+                    f"/teams/{id}/members", json={"github": github, "role": role}
+                ).json()
+            elif repo:
+                info = client.post(f"/teams/{id}/repos", json={"repo": repo}).json()
+            elif monitor:
+                info = client.post(
+                    f"/teams/{id}/monitors", json={"monitor": monitor}
+                ).json()
+            else:
+                raise Exception("Use --repo, --member or --monitor")
+        elif action == "repos":
+            info = client.get(f"/teams/{id}/repos").json()["rows"]
+        elif action == "get_config":
+            info = client.get(f"/teams/{id}/config/{config_name}").json()
+        elif action == "set_config":
+            info = client.put(
+                f"/teams/{id}/config",
+                json={"name": config_name, "value": config_value},
+            ).json()
+        elif action == "get_token":
+            info = client.get(f"/teams/{id}/token").json()
+        elif action == "refresh_token":
+            info = client.put(f"/teams/{id}/token").json()
+        elif action == "delete":
+            if email:
+                client.request("DELETE", f"/teams/{id}/members", json={"email": email})
+                console.print("Team member deleted")
+            elif github:
+                client.request(
+                    "DELETE", f"/teams/{id}/members", json={"github": github}
+                )
+                console.print("Team member deleted")
+            else:
+                client.delete(f"/teams/{id}")
+                console.print("Team deleted")
+            return
+
+        autoformat(info, jsonfmt=kwargs["json"], list_separator="*" * 48, table=True)

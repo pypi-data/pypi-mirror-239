e271c2dfa94cb9eacf9fccae14f4d98a093b6447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 15:05:02 +0000

    Add package phrenktest/0.1.0/phrenktest-0.1.0.tar.gz

---
+++ packages/phrenktest/phrenktest-0.1.0.tar.gz/phrenktest-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phrenktest",
+    version="0.1.0",
+    description="testest",
+    author="test",
+    packages=find_packages(),
+    include_package_data=True,  # Include package data such as non-.py files
+)

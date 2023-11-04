b85f506beb3717a84c3dd42400e3cfb59cf202a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:06:25 +0000

    Add package panther/3.1.1/panther-3.1.1.tar.gz

---
+++ packages/panther/panther-3.1.1.tar.gz/panther-3.1.1/panther/db/models.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+import bson
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import Field, field_validator
+
+from panther.configs import config
+from panther.db.queries import Query
+
+if config['db_engine'] == 'pantherdb':
+    IDType = int
+else:
+    IDType = str
+
+
+class Model(PydanticBaseModel, Query):
+    id: IDType | None = Field(None, validation_alias='_id')
+
+    @field_validator('id', mode='before')
+    def validate_id(cls, value):
+        if IDType is str:
+            if isinstance(value, str):
+                try:
+                    bson.ObjectId(value)
+                except bson.objectid.InvalidId:
+                    raise ValueError('Invalid ObjectId')
+            elif not isinstance(value, bson.ObjectId):
+                raise ValueError('ObjectId required')
+            value = str(value)
+        return value
+
+    @property
+    def _id(self):
+        if IDType is int:
+            return self.id
+        else:
+            return bson.ObjectId(self.id) if self.id else None
+
+    def dict(self, *args, **kwargs):
+        return self.model_dump(*args, **kwargs)
+
+
+class BaseUser(Model):
+    first_name: str = Field('', max_length=64)
+    last_name: str = Field('', max_length=64)
+    last_login: datetime = None
+
+    def update_last_login(self):
+        self.update(last_login=datetime.now())

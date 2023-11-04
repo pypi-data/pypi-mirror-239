e37b42f73ee154512c4f78f5ad3dcb3f0f27f150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:04:18 +0000

    Add package empresa4/1.0.2/empresa4-1.0.2-py3-none-any.whl

---
+++ packages/empresa4/empresa4-1.0.2-py3-none-any.whl/empresa4/datasets.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+nombres_datasets = ["01_producto_estrella", "01_productos_todos", "01_por_cliente"]
+
+
+def get_nombres_datasets():
+    return nombres_datasets
+
+
+def get_dataset(dataset_name):
+    df = None
+
+    if dataset_name == "01_producto_estrella":
+        df = pd.read_csv("./tb_sellout_01_producto_estrella.csv")
+
+    elif dataset_name == "01_productos_todos":
+        df = pd.read_csv("./tb_sellout_01_productos_todos.csv")
+
+    elif dataset_name == "01_por_cliente":
+        df = pd.read_csv("./tb_sellout_01_por_cliente.csv")
+
+    else:
+        raise Exception(
+            f"Dataset not found. Usar uno de los siguientes: {nombres_datasets}"
+        )
+
+    return df

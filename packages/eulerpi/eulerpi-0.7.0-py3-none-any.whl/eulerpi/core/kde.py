8089bb26c0f085aa6b8ffa6d424b745a81fe221d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:03 +0000

    Add package eulerpi/0.7.0/eulerpi-0.7.0-py3-none-any.whl

---
+++ packages/eulerpi/eulerpi-0.7.0-py3-none-any.whl/eulerpi/core/kde.py
@@ -0,0 +1,97 @@
+"""This module provides functions to handle the Kernel Densitiy Estimation (KDE_) in EPI.
+
+
+.. _KDE: https://en.wikipedia.org/wiki/Kernel_density_estimation
+"""
+
+import typing
+
+import jax.numpy as jnp
+from jax import jit
+from jax.scipy.stats import cauchy, norm
+
+
+@jit
+def eval_kde_cauchy(
+    data: jnp.ndarray, sim_res: jnp.ndarray, scales: jnp.ndarray
+) -> typing.Union[jnp.double, jnp.ndarray]:
+    r"""Evaluates a Cauchy Kernel Density estimator in one or several simulation results.
+        Assumes that each data point is a potentially high-dimensional sample
+        from a joint data distribution.
+        This is for example given for time-series data, where each evaluation
+        time is one dimension of the data point.
+        In the following formula x are the evaluation points (sim_res) and y is the data.
+
+        .. math::
+            density_{i} = \frac{1}{samples} \sum_{s=1}^{samples} \prod_{d=1}^{dims} \frac{1}{(\frac{x_{i,d} - y_{s,d}}{scales_d})^2 \; \pi \; scales_d}
+
+    Args:
+      data(jnp.ndarray): data for the model: 2D array with shape (#Samples, #MeasurementDimensions)
+      sim_res(jnp.ndarray): evaluation coordinates array of shape (#nEvals, #MeasurementDimensions) or (#MeasurementDimensions,)
+      scales(jnp.ndarray): one scale for each dimension
+
+    Returns:
+        typing.Union[jnp.double, jnp.ndarray]: estimated kernel density evaluated at the simulation result(s), shape: (#nEvals,) or ()
+
+    """
+
+    return (
+        jnp.sum(
+            jnp.prod(
+                cauchy.pdf(sim_res[..., jnp.newaxis, :], data, scales),
+                axis=-1,  # prod over #measurementDimensions
+            ),
+            axis=-1,  # sum over sampleDim
+        )
+        / data.shape[0]
+    )
+
+
+@jit
+def eval_kde_gauss(
+    data: jnp.ndarray, sim_res: jnp.ndarray, scales: jnp.ndarray
+) -> typing.Union[jnp.double, jnp.ndarray]:
+    """Evaluates a Gaussian Kernel Density estimator in one or severalsimulation result.
+    Assumes that each data point is a potentially high-dimensional sample from a joint data distribution.
+    This is for example given for time-series data, where each evaluation time is one dimension of the data point.
+    While it is possible to define different standard deviations for different measurement dimensions, it is so far not possible to define covariances.
+
+    Args:
+        data(jnp.ndarray): data for the model: 2D array with shape (#Samples, #MeasurementDimensions)
+        sim_res(jnp.ndarray): evaluation coordinates array of shape (#nEvals, #MeasurementDimensions) or (#MeasurementDimensions,)
+        scales(jnp.ndarray): one scale for each dimension
+
+    Returns:
+        typing.Union[jnp.double, jnp.ndarray]: estimated kernel density evaluated at the simulation result(s), shape: (#nEvals,) or ()
+    """
+
+    return (
+        jnp.sum(
+            jnp.prod(
+                norm.pdf(sim_res[..., jnp.newaxis, :], data, scales),
+                axis=-1,  # prod over #measurementDimensions
+            ),
+            axis=-1,  # sum over sampleDim
+        )
+        / data.shape[0]
+    )
+
+
+@jit
+def calc_kernel_width(data: jnp.ndarray) -> jnp.ndarray:
+    """Sets the width of the kernels used for density estimation of the data according to the Silverman rule
+
+    Args:
+        data(jnp.ndarray): data for the model: 2D array with shape (#Samples, #MeasurementDimensions)
+
+    Returns:
+        jnp.ndarray: kernel width for each data dimension, shape: (#MeasurementDimensions,)
+
+    """
+    num_data_points, data_dim = data.shape
+    stdevs = jnp.std(data, axis=0, ddof=1)
+
+    # Silvermans rule
+    return stdevs * (num_data_points * (data_dim + 2) / 4.0) ** (
+        -1.0 / (data_dim + 4)
+    )
---
+++ packages/eulerpi/eulerpi-0.7.0-py3-none-any.whl/eulerpi/examples/corona/corona.py
@@ -0,0 +1,88 @@
+from typing import Optional
+
+import diffrax as dx
+import jax.numpy as jnp
+import numpy as np
+
+from eulerpi import logger
+from eulerpi.core.model import ArtificialModelInterface, JaxModel
+
+
+class Corona(JaxModel):
+    """ """
+
+    param_dim = 3
+    data_dim = 4
+
+    PARAM_LIMITS = np.array([[-4.5, 0.5], [-2.0, 3.0], [-2.0, 3.0]])
+    CENTRAL_PARAM = np.array([-1.8, 0.0, 0.7])
+
+    def __init__(
+        self,
+        central_param: np.ndarray = CENTRAL_PARAM,
+        param_limits: np.ndarray = PARAM_LIMITS,
+        name: Optional[str] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(central_param, param_limits, name=name, **kwargs)
+
+    @classmethod
+    def forward(cls, log_param):
+        param = jnp.power(10, log_param)
+        xInit = jnp.array([999.0, 0.0, 1.0, 0.0])
+
+        def rhs(t, x, param):
+            return jnp.array(
+                [
+                    -param[0] * x[0] * x[2],
+                    param[0] * x[0] * x[2] - param[1] * x[1],
+                    param[1] * x[1] - param[2] * x[2],
+                    param[2] * x[2],
+                ]
+            )
+
+        term = dx.ODETerm(rhs)
+        solver = dx.Kvaerno5()
+        saveat = dx.SaveAt(ts=[0.0, 1.0, 2.0, 5.0, 15.0])
+        stepsize_controller = dx.PIDController(rtol=1e-5, atol=1e-5)
+
+        try:
+            ode_sol = dx.diffeqsolve(
+                term,
+                solver,
+                t0=0.0,
+                t1=15.0,
+                dt0=0.1,
+                y0=xInit,
+                args=param,
+                saveat=saveat,
+                stepsize_controller=stepsize_controller,
+            )
+            return ode_sol.ys[1:5, 2]
+
+        except Exception as e:
+            logger.warning("ODE solution not possible!", exc_info=e)
+            return np.array([-np.inf, -np.inf, -np.inf, -np.inf])
+
+
+class CoronaArtificial(Corona, ArtificialModelInterface):
+    PARAM_LIMITS = np.array([[-2.5, -1.0], [-0.75, 0.75], [0.0, 1.5]])
+
+    def __init__(
+        self,
+        central_param: np.ndarray = Corona.CENTRAL_PARAM,
+        param_limits: np.ndarray = PARAM_LIMITS,
+        name: Optional[str] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(central_param, param_limits, name=name, **kwargs)
+
+    def generate_artificial_params(self, num_samples):
+        lower_bound = np.array([-1.9, -0.1, 0.6])
+        upper_bound = np.array([-1.7, 0.1, 0.8])
+
+        true_param_sample = lower_bound + (
+            upper_bound - lower_bound
+        ) * np.random.rand(num_samples, 3)
+
+        return true_param_sample

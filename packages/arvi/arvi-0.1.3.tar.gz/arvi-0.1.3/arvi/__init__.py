8c915a4139ca54d7ef9c5475c037a22bbc977ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:32 +0000

    Add package arvi/0.1.3/arvi-0.1.3.tar.gz

---
+++ packages/arvi/arvi-0.1.3.tar.gz/arvi-0.1.3/arvi/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.1.3'
+__all__ = []
+
+from .timeseries import RV
+
+_ran_once = False
+
+def __getattr__(name: str):
+    global _ran_once  # can't do it any other way :(
+    if _ran_once:
+        return RV(name)
+    else:
+        _ran_once = True

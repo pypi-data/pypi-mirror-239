82bb732fb61e49fed638eadf4ffabb229d0da4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:05:57 +0000

    Add package odoo14-addon-hr-entry-type-mitxelena/14.0.1.0.1.dev1/odoo14-addon-hr_entry_type_mitxelena-14.0.1.0.1.dev1.tar.gz

---
+++ packages/odoo14-addon-hr-entry-type-mitxelena/odoo14-addon-hr_entry_type_mitxelena-14.0.1.0.1.dev1.tar.gz/odoo14-addon-hr_entry_type_mitxelena-14.0.1.0.1.dev1/odoo/addons/hr_entry_type_mitxelena/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Entry Types Data for Mitxelena",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "sequence": 20,
+    "summary": "Entry Types Data Customizations for Mitxelena",
+    "website": "https://git.coopdevs.org/coopdevs/odoo/odoo-addons/odoo-mitxelena",
+    "author": "Coopdevs Treball SCCL",
+    "depends": [
+        "base",
+        "hr_entry_type",
+    ],
+    "data": [
+        "data/hr_entry_type.xml",
+        
+    ],
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}

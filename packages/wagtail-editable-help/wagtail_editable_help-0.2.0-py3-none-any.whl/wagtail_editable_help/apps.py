6203b308e730d1bdfe529430ae4da01d04ee1ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:07:22 +0000

    Add package wagtail-editable-help/0.2.0/wagtail_editable_help-0.2.0-py3-none-any.whl

---
+++ packages/wagtail-editable-help/wagtail_editable_help-0.2.0-py3-none-any.whl/wagtail_editable_help/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WagtailEditableHelpAppConfig(AppConfig):
+    label = "wagtail_editable_help"
+    name = "wagtail_editable_help"
+    verbose_name = "Wagtail Editable Help"
+    default_auto_field = "django.db.models.AutoField"

7439f5d54111145ed953cbffb44cdb327f3fa930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 15:05:17 +0000

    Add package imdbscraperdeepak/1.0/imdbscraperdeepak-1.0.tar.gz

---
+++ packages/imdbscraperdeepak/imdbscraperdeepak-1.0.tar.gz/imdbscraperdeepak-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="imdbscraperdeepak",
+    version="1.0",
+    py_modules=['imdbscraperdeepak'],
+    dependency_links=['https://github.com/deepakchoudhary101/https://github.com/DeepakChoudhary101/imdb_scraper.git/tarball/master#egg=imdbscraperdeepak'],
+
+)
+# pypi-AgEIcHlwaS5vcmcCJDQ3NTFmZTgxLTJhYTYtNDdjNy05M2FkLWVjMDYxYmEyNGFhYgACKlszLCJmYTA2MDQ0ZC1kNTgxLTRkMzctYjkzZi05ZjE0ODM1YzNkMWIiXQAABiA8JT2LOgmE1eati7EB4ZPie6u4t1wMTO0Gon_VnSIlbg
+# pypi-AgEIcHlwaS5vcmcCJDVmZmNlYzcxLTM2YWItNDA5My05MDMxLWQ5ZjdjMGY5ZDI4MwACKlszLCJmYTA2MDQ0ZC1kNTgxLTRkMzctYjkzZi05ZjE0ODM1YzNkMWIiXQAABiDbalVecauvV_CuHqMwUzbQt30Ykj0t74ZpD67Bh-HVQQ
+# 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

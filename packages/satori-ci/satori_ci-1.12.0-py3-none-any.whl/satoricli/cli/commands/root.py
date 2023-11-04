83f1c805f799dc0be63cef0393b9ae1571867d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 06:04:59 +0000

    Add package satori-ci/1.12.0/satori_ci-1.12.0-py3-none-any.whl

---
+++ packages/satori-ci/satori_ci-1.12.0-py3-none-any.whl/satoricli/cli/commands/root.py
@@ -0,0 +1,44 @@
+from argparse import ArgumentParser
+from importlib import metadata
+
+from ..arguments import json_arg, profile_arg
+from .base import BaseCommand
+from .config import ConfigCommand
+from .dashboard import DashboardCommand
+from .help import HelpCommand
+from .monitor import MonitorCommand
+from .outputs import OutputsCommand
+from .playbook import PlaybookCommand
+from .repo import RepoCommand
+from .report import ReportCommand
+from .run import RunCommand
+from .scan import ScanCommand
+from .team import TeamCommand
+from .update import UpdateCommand
+
+VERSION = metadata.version("satori-ci")
+
+
+class RootCommand(BaseCommand):
+    subcommands = (
+        ConfigCommand,
+        RunCommand,
+        ReportCommand,
+        MonitorCommand,
+        OutputsCommand,
+        RepoCommand,
+        ScanCommand,
+        PlaybookCommand,
+        DashboardCommand,
+        TeamCommand,
+        HelpCommand,
+        UpdateCommand,
+    )
+    name = "satori"
+    global_options = (profile_arg, json_arg)
+    default_subcommand = DashboardCommand
+
+    def register_args(self, parser: ArgumentParser):
+        parser.add_argument(
+            "-v", "--version", action="version", version=f"%(prog)s {VERSION}"
+        )

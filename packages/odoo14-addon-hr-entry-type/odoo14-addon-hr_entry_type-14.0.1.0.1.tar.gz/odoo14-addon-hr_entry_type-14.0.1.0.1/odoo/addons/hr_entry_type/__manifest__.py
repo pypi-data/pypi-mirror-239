8b176587bc0209c09b2768a327cbb962bb7103e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 05:03:02 +0000

    Add package odoo14-addon-hr-entry-type/14.0.1.0.1/odoo14-addon-hr_entry_type-14.0.1.0.1.tar.gz

---
+++ packages/odoo14-addon-hr-entry-type/odoo14-addon-hr_entry_type-14.0.1.0.1.tar.gz/odoo14-addon-hr_entry_type-14.0.1.0.1/odoo/addons/hr_entry_type/__manifest__.py
@@ -0,0 +1,22 @@
+# trunk-ignore(ruff/B018)
+{
+    "name": "HR Attendance Mitxelena Entry Types",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.1",
+    "category": "Human Resources",
+    "sequence": 20,
+    "summary": "Entry Types to Employee Attendances Customizations for Mitxelena",
+    "website": "https://git.coopdevs.org/coopdevs/odoo/odoo-addons/odoo-mitxelena",
+    "author": "Coopdevs Treball SCCL",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/hr_entry_type.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}

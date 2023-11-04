107312030967023c87d02843ad850e3f45c96921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 14:06:01 +0000

    Add package MqttLibPy/1.1.5/mqttlibpy-1.1.5.tar.gz

---
+++ packages/MqttLibPy/mqttlibpy-1.1.5.tar.gz/mqttlibpy-1.1.5/test/test_serialize.py
@@ -0,0 +1,28 @@
+import unittest
+import threading
+
+from random import randbytes
+from src.MqttLibPy.client import MqttClient
+
+
+class TestSerialize(unittest.TestCase):
+
+    def test_serialize_bytes(self):
+        downloads_folder = "/path/to/downloads/folder"
+        file_path = "/path/to/my/file"
+        broker_url = "mybroker.com"
+
+        client = MqttClient(broker_url, 1883)
+
+        @client.endpoint("test_bytes", is_file=True)
+        def get_file(client, user_data, file):
+            with open(f"{downloads_folder}/{file['filename']}", 'wb+') as f:
+                f.write(file['bytes'])
+                f.close()
+
+        threading.Thread(target=client.listen).start()
+
+        client.send_file("test_bytes", file_path)
+
+    def _get_n_mb(self, mbs: int):
+        return randbytes(mbs * 1000 * 1000)

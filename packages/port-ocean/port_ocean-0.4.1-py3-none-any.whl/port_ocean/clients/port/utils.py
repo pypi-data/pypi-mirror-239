3c7ec49e39c181809ff141378fad62fbae9da585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:06:05 +0000

    Add package port-ocean/0.4.1/port_ocean-0.4.1-py3-none-any.whl

---
+++ packages/port-ocean/port_ocean-0.4.1-py3-none-any.whl/port_ocean/clients/port/utils.py
@@ -0,0 +1,28 @@
+import httpx
+from loguru import logger
+from werkzeug.local import LocalStack, LocalProxy
+
+_http_client: LocalStack[httpx.AsyncClient] = LocalStack()
+
+
+def _get_http_client_context() -> httpx.AsyncClient:
+    client = _http_client.top
+    if client is None:
+        client = httpx.AsyncClient()
+        _http_client.push(client)
+
+    return client
+
+
+async_client: httpx.AsyncClient = LocalProxy(lambda: _get_http_client_context())  # type: ignore
+
+
+def handle_status_code(
+    response: httpx.Response, should_raise: bool = True, should_log: bool = True
+) -> None:
+    if should_log and response.is_error:
+        logger.error(
+            f"Request failed with status code: {response.status_code}, Error: {response.text}"
+        )
+    if should_raise:
+        response.raise_for_status()

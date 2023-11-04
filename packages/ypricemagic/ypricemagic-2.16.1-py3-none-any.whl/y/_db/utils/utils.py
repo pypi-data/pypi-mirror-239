3eb8c9b97ab672431f0d8845adc19574b18c19b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 07:03:43 +0000

    Add package ypricemagic/2.16.1/ypricemagic-2.16.1-py3-none-any.whl

---
+++ packages/ypricemagic/ypricemagic-2.16.1-py3-none-any.whl/y/_db/utils/utils.py
@@ -0,0 +1,26 @@
+
+import logging
+
+from a_sync import a_sync
+from brownie import chain
+from pony.orm import db_session
+
+from y._db.common import token_attr_threads
+from y._db.entities import Block, Chain, insert, retry_locked
+
+logger = logging.getLogger(__name__)
+
+@a_sync(default='async', executor=token_attr_threads)
+@db_session
+@retry_locked
+def get_chain() -> Chain:
+    return Chain.get(id=chain.id) or insert(type=Chain, id=chain.id) or Chain.get(id=chain.id)
+
+@a_sync(default='async', executor=token_attr_threads)
+@db_session
+@retry_locked
+def get_block(number: int) -> Block:
+    chain = get_chain(sync=True)
+    if block := Block.get(chain=chain, number=number):
+        return block
+    return insert(type=Block, chain=chain, number=number) or get_block(number, sync=True)

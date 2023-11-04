1cb14d38eca1c4e3faa1a4869acee78b1f90e90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 04:05:04 +0000

    Add package edwh-pipcompile-plugin/0.3.0/edwh_pipcompile_plugin-0.3.0-py3-none-any.whl

---
+++ packages/edwh-pipcompile-plugin/edwh_pipcompile_plugin-0.3.0-py3-none-any.whl/edwh_pipcompile_plugin/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2023-present Remco Boerma <remco.b@educationwarehouse.nl>
+#
+# SPDX-License-Identifier: MIT
+
+from .pipcompile_plugin import compile_infile, install, remove, upgrade

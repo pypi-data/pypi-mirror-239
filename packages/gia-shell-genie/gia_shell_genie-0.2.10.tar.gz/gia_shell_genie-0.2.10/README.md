5f6434d8af5e0db694b159612ce374ca5de772f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 03:06:18 +0000

    Add package gia-shell-genie/0.2.10/gia_shell_genie-0.2.10.tar.gz

---
+++ packages/gia-shell-genie/gia_shell_genie-0.2.10.tar.gz/gia_shell_genie-0.2.10/README.md
@@ -0,0 +1,102 @@
+
+# Intentions
+
+## --@STCGoal Derive another bot with special instructions based on GuillaumeAI's visions and current projects. 
+
+----
+original readme
+----
+
+# 🧞‍♂️ GIA Shell Genie
+
+_Your wishes are my commands._
+
+Shell Genie is a command-line tool that lets you interact with the terminal in plain English. You ask the genie what you want to do and it will give you the command you need.
+
+## Installation
+
+The recommended way to install Shell Genie is using [pipx](https://pypa.github.io/pipx/):
+
+1. Install Python 3.10 or higher.
+2. Install [pipx](https://github.com/pypa/pipx#install-pipx).
+3. Install Shell Genie: `pipx install shell-genie`
+
+Alternatively, you can install it using pip:
+
+1. Install Python 3.10 or higher.
+2. Create a virtual environment in your preferred location: `python -m venv .venv`
+3. Activate the virtual environment: `source .venv/bin/activate`
+4. Install Shell Genie: `pip install shell-genie`
+
+## How to use
+
+1. First, you need to initialize the tool by running the following command:
+
+   ```shell
+   shell-genie init
+   ```
+
+   This will prompt you to select a backend (either `openai-gpt3.5-turbo` or `free-genie`) and provide any additional information that is required (e.g. your own [OpenAI API](https://openai.com/api/) key for `openai-gpt3.5-turbo`).
+
+   The `free-genie` backend is free to use. I'm hosting it, and as you can imagine I'm not a big corporation with unlimited money, so there's no guarantee that it will be available at all times. My goal is to generate a dataset of commands to fine-tune a model later on (this is mentioned during the initialization process).
+
+2. Once you have initialized the tool, you can start asking the genie what you want to do. For example, you may ask it to find all the `json` files in the current directory that are larger than 1MB:
+
+   ```shell
+   shell-genie ask "find all json files in the current directory that are larger than 1MB"
+   ```
+
+   You'll see an output similar to this:
+
+   ```shell
+   Command: find . -name "*.json" -size +1M
+   Do you want to run this command? [y/n]:
+   ```
+
+   If you have questions about how the command works, you can ask the genie to explain it:
+
+   ```shell
+   shell-genie ask "find all json files in the current directory that are larger than 1MB" --explain
+   ```
+
+   And you'll see an output similar to this:
+
+   ```shell
+   Command: find . -name "*.json" -size +1M
+   Description: This command will search the current directory for all... (shortened for brevity)
+   Do you want to run the command? [y/n]:
+   ```
+
+   You can ask for commands in English or other languages, and the genie will try to provide you with a explanation in the same language.
+
+3. Run the command if you want to. If you're using `free-genie`, and you want to help improve the tool, you can provide feedback after you've run the command.
+
+### Using an alias
+
+If you find that writing `shell-genie ask` is too verbose, you can create an alias for the tool:
+
+```shell
+echo "alias please='shell-genie ask'" >> ~/.bashrc
+source ~/.bashrc
+```
+
+And now you can ask the genie using `please`:
+
+```shell
+please "find all json files in the current directory that are larger than 1MB"
+```
+
+## Examples
+
+Here are two short videos showing how to use the tool:
+
+- [Ask genie for a command](https://youtu.be/QM-fwgnGzDc)
+- [Ask genie to explain a command](https://youtu.be/Qi3w3abI4oE)
+
+## Limitations
+
+As you can imagine, not all the commands provided by the genie work as expected. If you request them, you might receive commands that could wreck your OS (e.g., rm -rf /). So, don't run commands if you don't understand their functions. If you choose to execute them despite not understanding them, do so at your own risk.
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ packages/gia-shell-genie/gia_shell_genie-0.2.10.tar.gz/gia_shell_genie-0.2.10/gia_shell_genie/main.py
@@ -0,0 +1,137 @@
+import json
+import os
+import subprocess
+from pathlib import Path
+
+import pyperclip
+import typer
+from rich import print
+from rich.prompt import Confirm, Prompt
+
+from .utils import get_backend, get_os_info
+
+APP_NAME = ".gia_shell_genie"
+app = typer.Typer()
+
+
+@app.command()
+def init():
+
+    backend = Prompt.ask(
+        "Select backend:", choices=["openai-gpt-3.5-turbo", "free-genie"]
+    )
+    additional_params = {}
+
+    if backend == "openai-gpt-3.5-turbo":
+        additional_params["openai_api_key"] = Prompt.ask("Enter a OpenAI API key")
+
+    if backend == "free-genie":
+        print(
+            "[yellow]Note that this server will store the requested command, OS, and shell version to improve the model. Also, I cannot guarantee that the server will be up and running 24/7.[/yellow]"
+        )
+        if not Confirm.ask("Do you want to continue?"):
+            return
+        additional_params["training-feedback"] = Confirm.ask(
+            "Do you want to provide feedback about the generated commands to improve the models?"
+        )
+    os_family, os_fullname = get_os_info()
+
+    if os_family:
+        if not Confirm.ask(f"Is your OS {os_fullname}?"):
+            os_fullname = Prompt.ask("What is your OS and version? (e.g. MacOS 13.1)")
+    else:
+        os_fullname = Prompt.ask("What is your OS and version? (e.g. MacOS 13.1)")
+
+    if os_family == "Windows":
+        shell = Prompt.ask(
+            "What shell are you using?",
+            choices=["cmd", "powershell"],
+        )
+
+    if os_family in ("Linux", "MacOS"):
+        shell_str = os.environ.get("SHELL") or ""
+        if "bash" in shell_str:
+            shell = "bash"
+        elif "zsh" in shell_str:
+            shell = "zsh"
+        elif "fish" in shell_str:
+            shell = "fish"
+        else:
+            typer.prompt("What shell are you using?")
+
+    config = {
+        "backend": backend,
+        "os": os_family,
+        "os_fullname": os_fullname,
+        "shell": shell,
+    } | additional_params
+
+    app_dir = typer.get_app_dir(APP_NAME)
+    config_path: Path = Path(app_dir) / "config.json"
+
+    print("The following configuration will be saved:")
+    print(config)
+
+    config_path.parent.mkdir(parents=True, exist_ok=True)
+
+    if config_path.exists():
+        overwrite = Confirm.ask(
+            "A config file already exists. Do you want to overwrite it?"
+        )
+        if not overwrite:
+            print("Did not overwrite config file.")
+            return
+
+    with open(config_path, "w") as f:
+        json.dump(config, f)
+
+    print(f"[bold green]Config file saved at {config_path}[/bold green]")
+
+
+@app.command()
+def ask(
+    wish: str = typer.Argument(..., help="What do you want to do?"),
+    explain: bool = False,
+):
+    app_dir = typer.get_app_dir(APP_NAME)
+    config_path = Path(app_dir) / "config.json"
+
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    genie = get_backend(**config)
+    try:
+        command, description = genie.ask(wish, explain)
+    except Exception as e:
+        print(f"[red]Error: {e}[/red]")
+        return
+
+    print(f"[bold]Command:[/bold] [yellow]{command}[/yellow]")
+
+    if description:
+        print(f"[bold]Description:[/bold] {description}")
+
+    if config["os"] == "Windows" and config["shell"] == "powershell":
+        pyperclip.copy(command)
+        print("[green]Command copied to clipboard.[/green]")
+    else:
+        execute = Confirm.ask("Do you want to run the command?")
+        if execute:
+            subprocess.run(command, shell=True)
+            feedback = False
+            try:
+                if config["training-feedback"]:
+                    feedback = Confirm.ask("Did the command work?")
+            except KeyError:
+                pass
+            genie.post_execute(
+                wish=wish,
+                explain=explain,
+                command=command,
+                description=description,
+                feedback=feedback,
+            )
+
+
+if __name__ == "__main__":
+    app()

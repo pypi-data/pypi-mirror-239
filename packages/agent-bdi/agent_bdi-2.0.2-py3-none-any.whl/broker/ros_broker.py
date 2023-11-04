0300adeebcc519b37e7240c8435c1ad14d1ccc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-04 16:05:42 +0000

    Add package agent-bdi/2.0.2/agent_bdi-2.0.2-py3-none-any.whl

---
+++ packages/agent-bdi/agent_bdi-2.0.2-py3-none-any.whl/broker/ros_broker.py
@@ -0,0 +1,36 @@
+import logging
+
+from broker.message_broker import MessageBroker
+from broker.notifier import BrokerNotifier
+
+
+logger = logging.getLogger("ABDI")
+
+
+class RosBroker(MessageBroker):
+    def __init__(self, notifier:BrokerNotifier):
+        logger.info(f"Initialize...")
+        
+        super().__init__(notifier=notifier)
+
+
+
+    ###################################
+    # Implementation of MessageBroker #
+    ###################################
+    
+    
+    def start(self, options:dict):
+        logger.info(f"Ros broker is starting...")
+       
+
+    def stop(self):
+        logger.info(f"Ros broker is stopping...")
+
+
+    def publish(self, topic:str, payload):
+        logger.info(f"topic: {topic}, payload: {payload}")
+        
+    
+    def subscribe(self, topic:str, data_type):
+        logger.info(f"topic: {topic}")

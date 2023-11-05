0dc4d79f82414649c959601636db26da1484f3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:36 +0000

    Add package stemmer-madura/0.1.1/stemmer-madura-0.1.1.tar.gz

---
+++ packages/stemmer-madura/stemmer-madura-0.1.1.tar.gz/stemmer-madura-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name='stemmer-madura',
+  version='0.1.1',
+  description='Stemmer untuk bahasa Madura menggunakan rule based',
+  author='Mohammad Nazir Arifin',
+  author_email='ceylon.rizan@gmail.com',
+  packages=['stemmer_madura'],
+  license='MIT',
+  install_requires=[],
+  keywords=['stemmer', 'madura', 'bahasa madura', 'nlp'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Education',
+    'Programming Language :: Python :: 3.10',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+)

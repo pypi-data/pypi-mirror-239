593a49b271ceff80db05afb7cdf88b1f74d46084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:59 +0000

    Add package pykuda/1.0.1/pykuda-1.0.1.tar.gz

---
+++ packages/pykuda/pykuda-1.0.1.tar.gz/pykuda-1.0.1/src/pykuda/pykuda.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from pykuda.classes.service_type import ServiceType
+from pykuda.utils import check_envs_are_set
+
+load_dotenv()
+
+
+class PyKuda(ServiceType):
+    """
+    PyKuda Class.
+    """
+
+    def __init__(self):
+        response = check_envs_are_set()
+
+        # If all credentials were properly set, response will
+        # be a bool(True), else will be a string
+        if not isinstance(response, bool):
+            raise ValueError(response)

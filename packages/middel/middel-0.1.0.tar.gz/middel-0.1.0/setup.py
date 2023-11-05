1c722a84df754be4ab47a6242994f45af4eb82bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:03:31 +0000

    Add package middel/0.1.0/middel-0.1.0.tar.gz

---
+++ packages/middel/middel-0.1.0.tar.gz/middel-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import io
+import os
+from setuptools import find_packages, setup
+import middel
+
+
+def read(*paths, **kwargs):
+    content = ""
+    with io.open(
+            os.path.join(os.path.dirname(__file__), *paths),
+            encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+setup(
+    name="middel",
+    version=middel.__version__,
+    description="Middle/Modern English Translator CLI - Written in Python.",
+    url="https://github.com/johnnystarr/middel",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Johnny Starr",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=[
+
+    ],
+    entry_points={
+    },
+)
---
+++ packages/middel/middel-0.1.0.tar.gz/middel-0.1.0/test/test_middel.py
@@ -0,0 +1,6 @@
+import pytest
+import middel
+
+
+def test_middel_version():
+    assert middel.__version__ == '0.1.0'

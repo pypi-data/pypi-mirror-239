39cfda1c3b1c14029e866ee34a7d66e46f00dd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:04 +0000

    Add package easyros/0.0.3/easyros-0.0.3.tar.gz

---
+++ packages/easyros/easyros-0.0.3.tar.gz/easyros-0.0.3/easyros/__init__.py
@@ -0,0 +1,10 @@
+import os,sys
+script_path = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(script_path)
+from install import rosInstall
+def run():
+    if input('Do you want to auto install ros?  (y/n)') == 'y':
+        rosInstall()
+        print("自动安装已结束")
+if __name__ == "__main__":
+    run()

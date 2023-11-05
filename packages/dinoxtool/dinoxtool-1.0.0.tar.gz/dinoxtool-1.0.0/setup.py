a1746c3c1b871644f474975b4c939906b6d14d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:23 +0000

    Add package dinoxtool/1.0.0/dinoxtool-1.0.0.tar.gz

---
+++ packages/dinoxtool/dinoxtool-1.0.0.tar.gz/dinoxtool-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dinoxtool",
+    version="1.0.0",
+    author="Miki007",
+    description="A library for making multitools",
+    long_description="Check full description on nothing LOL",
+    url="http://example.com/",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+    ],
+)

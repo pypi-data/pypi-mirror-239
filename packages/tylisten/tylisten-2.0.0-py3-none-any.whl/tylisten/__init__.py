40b2f7108c94edbf1b14a2c0d86c37a1dd1f3672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 15:06:17 +0000

    Add package tylisten/2.0.0/tylisten-2.0.0-py3-none-any.whl

---
+++ packages/tylisten/tylisten-2.0.0-py3-none-any.whl/tylisten/__init__.py
@@ -0,0 +1,17 @@
+"""A tiny hook specification library with typing support."""
+
+from typing import Any
+
+from .futstore import FutureStore
+from .hookspec import HookSpec, StaticHookSpec
+
+__all__ = [
+    "StaticHookSpec",
+    "HookSpec",
+    "hookdef",
+    "null_emitter",
+    "FutureStore",
+]
+
+hookdef = StaticHookSpec
+null_emitter: HookSpec[Any, Any] = StaticHookSpec(lambda *args, **kwds: None).instantiate()

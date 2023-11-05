2bef8e6f9aba1110055e49ba09a83c5aea8e6b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:51 +0000

    Add package tortoise-api-model/0.3.8/tortoise_api_model-0.3.8-py3-none-any.whl

---
+++ packages/tortoise-api-model/tortoise_api_model-0.3.8-py3-none-any.whl/tortoise_api_model/enums.py
@@ -0,0 +1,35 @@
+from enum import IntEnum
+
+class FieldType(IntEnum):
+    # str = 1
+    # txt = 2
+    # int = 3
+    # float = 4
+    # date = 8
+    # time = 9
+    # dt = 10
+    input = 1
+    checkbox = 2
+    # one = 11
+    # many = 12
+    select = 3
+    textarea = 4
+    collection = 5
+    list = 6
+
+class UserStatus(IntEnum):
+    Inactive = 0
+    Wait = 1  # waiting for approve
+    Test = 2  # trial
+    Active = 3
+
+class UserRole(IntEnum):
+    Client = 1
+    Agent = 2
+    Manager = 3
+    Admin = 4
+
+class Scope(IntEnum):
+    Read = 1
+    Write = 2
+    All = 3 # not only my

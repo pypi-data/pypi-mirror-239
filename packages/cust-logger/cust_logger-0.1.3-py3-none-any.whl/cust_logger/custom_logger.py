792e426a14893649f3a3a45ef05a0ee9367813be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:08:59 +0000

    Add package cust-logger/0.1.3/cust_logger-0.1.3-py3-none-any.whl

---
+++ packages/cust-logger/cust_logger-0.1.3-py3-none-any.whl/cust_logger/custom_logger.py
@@ -0,0 +1,53 @@
+from logging.handlers import RotatingFileHandler
+from typing import Callable
+import logging
+import os.path
+
+
+class CustomLogger:
+    def __init__(
+        self,
+        name: str,
+        logging_level: str = "DEBUG",
+        logs_dir: str = "logs",
+        max_MB: int = 5,
+        max_count: int = 10,
+        format_string: str = "%(asctime)s.%(msecs)03d [%(levelname)s] (%(module)s.%(funcName)s:%(lineno)d) - %(message)s",
+        args_separator: str = " | "
+    ):
+        logging_level = logging.getLevelName(logging_level.upper())
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+        logs_path = os.path.join(logs_dir, f"{name}.log")
+        self._args_separator = args_separator
+
+        handler = RotatingFileHandler(logs_path, maxBytes=max_MB * 1024 * 1024, backupCount=max_count)
+        # handler = logging.FileHandler(f"{NAME}.log", mode="w")
+        formatter = logging.Formatter(format_string, datefmt='%d-%m-%Y %H:%M:%S')
+
+        handler.setFormatter(formatter)
+        self._logger = logging.getLogger(name)
+        self._logger.setLevel(logging_level)
+        self._logger.addHandler(handler)
+
+    def debug(self, message: str, *args, tag: str = "") -> None:
+        self._log(self._logger.debug, message, *args, tag=tag)
+
+    def info(self, message: str, *args, tag: str = "") -> None:
+        self._log(self._logger.info, message, *args, tag=tag)
+
+    def warning(self, message: str, *args, tag: str = "") -> None:
+        self._log(self._logger.warning, message, *args, tag=tag)
+
+    def error(self, message: str, *args, tag: str = "") -> None:
+        self._log(self._logger.error, message, *args, tag=tag)
+
+    def critical(self, message: str, *args, tag: str = "") -> None:
+        self._log(self._logger.critical, message, *args, tag=tag)
+
+    def _log(self, level_func: Callable, message: str, *args, tag: str = "") -> None:
+        if args:
+            message += self._args_separator + self._args_separator.join(*args)
+        if tag:
+            message = tag + self._args_separator + message
+        level_func(message)

8a603b9855b6a4f87336edda309c991fa0ec95be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 15:06:09 +0000

    Add package ptspy/0.0.3/ptspy-0.0.3-py3-none-any.whl

---
+++ packages/ptspy/ptspy-0.0.3-py3-none-any.whl/ptspy/constants.py
@@ -0,0 +1,32 @@
+# Represents an arbitrary very small number. It is set as 0.00001 here.
+epsilon = 0.00001
+
+# π radian (180 deg)
+pi = 3.141592653589793
+
+# Two π radian (360deg)
+two_pi = 6.283185307179586
+
+# Half π radian (90deg)
+half_pi = 1.5707963267948966
+
+# π/4 radian (45deg)
+quarter_pi = 0.7853981633974483
+
+# π/180 or 1 degree in radian
+one_degree = 0.017453292519943295
+
+# Multiply this constant with a radian to get a degree
+rad_to_deg = 57.29577951308232
+
+# Multiply this constant with a degree to get a radian
+deg_to_rad = 0.017453292519943295
+
+# Gravity acceleration (unit=m/s^2) and gravity force (unit=Newton) on 1kg of mass.
+gravity = 9.81
+
+# 1 Newton=0.10197 Kilogram-force
+newton = 0.10197
+
+# Gaussian constant (1 / sqrt(2 * pi))
+gaussian = 0.3989422804014327

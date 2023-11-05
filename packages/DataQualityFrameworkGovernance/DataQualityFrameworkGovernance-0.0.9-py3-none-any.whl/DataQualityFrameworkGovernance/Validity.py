ef2269864443b1d7858b0cb05644a362b9991130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:03:32 +0000

    Add package DataQualityFrameworkGovernance/0.0.9/DataQualityFrameworkGovernance-0.0.9-py3-none-any.whl

---
+++ packages/DataQualityFrameworkGovernance/DataQualityFrameworkGovernance-0.0.9-py3-none-any.whl/DataQualityFrameworkGovernance/Validity.py
@@ -0,0 +1,33 @@
+#Validity
+
+def validate_age(location, age_column, min_age, max_age):
+    import pandas as pd
+
+    df = pd.DataFrame(location)
+
+    # Check the validity of the 'Age' column
+    df['Age Validity'] = (df[age_column] >= min_age) & (df[age_column] <= max_age)
+
+    return df
+
+def validate_age_count(location, age_column, min_age, max_age):
+    import pandas as pd
+
+    df = pd.DataFrame(location)
+
+    # Check the validity of the 'Age' column
+    validity_check = (df[age_column] >= min_age) & (df[age_column] <= max_age)
+
+    # Count the number of valid and invalid rows
+    valid_count = validity_check.sum()
+    invalid_count = len(df) - valid_count
+
+    data = pd.DataFrame({
+        'Count of dataset': [len(df)],
+        'Valid count': [valid_count],
+        'Invalid count': [invalid_count],
+        'Valid count (%)': [(valid_count / len(df))*100],
+        'Invalid count (%)': [(invalid_count / len(df))*100]
+        })
+
+    return data

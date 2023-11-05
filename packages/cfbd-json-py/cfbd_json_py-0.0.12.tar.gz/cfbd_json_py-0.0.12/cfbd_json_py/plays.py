5d7885f6bc51c3f5231bfd1b1b0957c1e6cbcf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:38 +0000

    Add package cfbd-json-py/0.0.12/cfbd_json_py-0.0.12.tar.gz

---
+++ packages/cfbd-json-py/cfbd_json_py-0.0.12.tar.gz/cfbd_json_py-0.0.12/cfbd_json_py/plays.py
@@ -0,0 +1,97 @@
+# Creation Date: 08/30/2023 01:13 EDT
+# Last Updated Date: 10/06/2023 07:53 PM EDT
+# Author: Joseph Armstrong (armstrongjoseph08@gmail.com)
+# File Name: plays.py
+# Purpose: Houses functions pertaining to CFB play data within the CFBD API.
+####################################################################################################
+
+def get_cfbd_pbp_data(
+        season: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+        week: int = None,
+        # required if team, offense, or defense, not specified
+        team: str = None,
+        offensive_team: str = None,
+        defensive_team: str = None,
+        conference_abv: str = None,
+        offensive_conference_abv: str = None,
+        defensive_conference_abv: str = None,
+        play_type: int = None,
+        ncaa_division: str = "fbs",
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_pbp_play_types(
+        api_key: str = None,
+        api_key_dir: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_pbp_stats(
+        season: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+        week: int = None,
+        team: str = None,
+        game_id: int = None,
+        athlete_id: int = None,
+        stat_type_id: int = None,
+        season_type: str = "regular",  # "regular", "postseason", or "both"
+        conference_abv: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_pbp_stat_types(
+        api_key: str = None,
+        api_key_dir: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+####################################################################################################
+# Patreon Only Functions.
+#   No cacheing, because the entire point of these functions are to get people
+#   data ASAP, and right before kickoff.
+####################################################################################################
+
+
+def get_cfbd_live_pbp_data(
+        game_id: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
---
+++ packages/cfbd-json-py/cfbd_json_py-0.0.12.tar.gz/cfbd_json_py-0.0.12/cfbd_json_py/rankings.py
@@ -0,0 +1,21 @@
+# Creation Date: 08/30/2023 01:13 EDT
+# Last Updated Date: 10/06/2023 07:53 PM EDT
+# Author: Joseph Armstrong (armstrongjoseph08@gmail.com)
+# File Name: rankings.py
+# Purpose: Houses functions pertaining to CFB poll data within the CFBD API.
+####################################################################################################
+
+def get_cfbd_poll_rankings(
+        season: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+        week: int = None,
+        season_type: str = "regular",  # "regular" or "postseason"
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )

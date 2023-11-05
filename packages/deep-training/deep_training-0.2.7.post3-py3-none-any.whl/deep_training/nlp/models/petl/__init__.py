7973e1c3e4e4f221588b610b8d725970564dbf7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:34 +0000

    Add package deep-training/0.2.7.post3/deep_training-0.2.7.post3-py3-none-any.whl

---
+++ packages/deep-training/deep_training-0.2.7.post3-py3-none-any.whl/deep_training/nlp/models/petl/__init__.py
@@ -0,0 +1,6 @@
+# @Time    : 2023/4/7 21:50
+# @Author  : tk
+# @FileName: __init__.py
+from ...layers.petl.utils import is_bnb_available # noqa
+from .lora.petl_model import PetlModel,LoraModule,LoraConfig,AdaLoraConfig,IA3Config,PetlArguments
+from .prompt import PromptModel,PromptLearningConfig,PromptArguments,get_prompt_model
---
+++ packages/deep-training/deep_training-0.2.7.post3-py3-none-any.whl/deep_training/nlp/models/petl/prompt/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time:  12:26
+# @Author: tk
+# @File：__init__.py
+
+
+from .prompt_model import PromptModel,PromptLearningConfig,get_prompt_model
+from .configuration import PromptArguments

cadf1a49e1c36e310ffff1624f3fc1bff3f972aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:15 +0000

    Add package PolyPhyPy/0.0.1/PolyPhyPy-0.0.1-py3-none-any.whl

---
+++ packages/PolyPhyPy/PolyPhyPy-0.0.1-py3-none-any.whl/polyphy/pipelines/common.py
@@ -0,0 +1,12 @@
+# PolyPhy
+# License: https://github.com/PolyPhyHub/PolyPhy/blob/main/LICENSE
+# Author: Oskar Elek
+# Maintainers:
+
+class PolyPhy:
+    def start_simulation(self):
+        # specific implementation has to create the following
+        # classes defined for that pipeline
+        # PPSimulation(self.ppInternalData,self.batch_mode,self.num_iterations)
+        # PPPostSimulation(self.ppInternalData)
+        pass

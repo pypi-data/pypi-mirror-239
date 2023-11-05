8031a479a59e29838474dae3e8086d3d5f91164b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:28 +0000

    Add package bluerpc/0.3.3/bluerpc-0.3.3.tar.gz

---
+++ packages/bluerpc/bluerpc-0.3.3.tar.gz/bluerpc-0.3.3/README.md
@@ -0,0 +1,1 @@
+# BlueRPC Python Worker Implementation
---
+++ packages/bluerpc/bluerpc-0.3.3.tar.gz/bluerpc-0.3.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bluerpc",
+    packages=find_packages(),
+    version="0.3.3",
+    author="drosocode",
+    license="MIT",
+    description="Python BlueRPC Worker",
+    url="https://github.com/BlueRPC/BlueRPC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "bleak",
+        "bluetooth-adapters",
+        "cryptography",
+        "grpcio",
+        "grpc-interceptor",
+        "protobuf",
+        "netifaces",
+        "zeroconf",
+    ],
+    python_requires=">=3.8",
+    project_urls={
+        "Documentation": "https://bluerpc.github.io/",
+        "Source": "https://github.com/BlueRPC/BlueRPC/tree/main/python",
+    },
+    entry_points={"console_scripts": ["bluerpc=bluerpc.cli:run"]},
+)

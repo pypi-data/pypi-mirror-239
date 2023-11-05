2bcefc20983adb5d54d1c2c1f0f22bb7c87044fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:07:25 +0000

    Add package SwiftNet/0.0.4/SwiftNet-0.0.4.tar.gz

---
+++ packages/SwiftNet/SwiftNet-0.0.4.tar.gz/SwiftNet-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="SwiftNet",
+    version="0.0.4",
+    description="Simple Package for creating multilayer Neural Nets ",
+    package_dir={"": "SwiftNet"},
+    packages=find_packages(where="SwiftNet"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/badapple21/SwiftNet",
+    author="Michael Noel",
+    author_email="mjn2024@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "flask", "python-mnist"],
+    python_requires=">=3.10",
+)

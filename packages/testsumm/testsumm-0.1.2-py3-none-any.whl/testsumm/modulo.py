3797ebb9c227b201cad1788d6d769d6ff87cad0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:27 +0000

    Add package testsumm/0.1.2/testsumm-0.1.2-py3-none-any.whl

---
+++ packages/testsumm/testsumm-0.1.2-py3-none-any.whl/testsumm/modulo.py
@@ -0,0 +1,17 @@
+class aritmetica:
+    """
+    Esta calse suma
+    """
+    
+    def __init__(self,a,b):
+        """
+        Esta funcion inicializa la clase suma
+        """
+        self.a = a
+        self.b = b
+        
+    def suma(self):
+        """
+        Esta funcion suma dos numeros
+        """
+        return self.a + self.b

9df49b68b11b2ba833900687a64dfabdb131082b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:08:42 +0000

    Add package testsumm/0.2.4/testsumm-0.2.4.tar.gz

---
+++ packages/testsumm/testsumm-0.2.4.tar.gz/testsumm-0.2.4/testsumm/__init__.py
@@ -0,0 +1,8 @@
+from .modulo import aritmetica
+
+__version__ = "0.1.3"  # Puedes definir la versión de tu librería aquí
+
+__author__ = "M Luciano"  # Puedes especificar el autor de la librería
+
+__all__ = ["aritmetica"]
+

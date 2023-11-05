e424a2bd1cc10dce8858dcd3926792b84aa8f708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:29 +0000

    Add package zrb/0.0.108/zrb-0.0.108.tar.gz

---
+++ packages/zrb/zrb-0.0.108.tar.gz/zrb-0.0.108/docs/tutorials/README.md
@@ -0,0 +1,12 @@
+🔖 [Table of Contents](../README.md)
+
+# Tutorials
+
+- [Preparing your machine for development](preparing-your-machine-for-development.md)
+- [Development to deployment: Low code](development-to-deployment-low-code.md)
+- [Run task programmatically](run-task-programmatically.md)
+- [Define task dynamically](define-task-dynamically.md)
+- [Copy task](copy-task.md)
+- [Extending CmdTask: Sending Message to Slack](extending-cmd-task.md)
+
+🔖 [Table of Contents](../README.md)
---
+++ packages/zrb/zrb-0.0.108.tar.gz/zrb-0.0.108/src/zrb/helper/accessories/name.py
@@ -0,0 +1,35 @@
+from zrb.helper.typecheck import typechecked
+import random
+import string
+
+
+@typechecked
+def get_random_name(
+    separator: str = '-',
+    add_random_digit: bool = True,
+    digit_count: int = 4
+) -> str:
+    prefixes = [
+        "aurum", "argentum", "platinum", "mercurius", "sulfur", "sal",
+        "luna", "sol", "ferrum", "cuprum", "argent", "aurora", "citrin",
+        "coral", "diamond", "dragon", "emerald", "garnet", "jade", "onyx",
+        "opal", "pearl", "ruby", "sapphire", "topaz", "turquoise", "verde",
+        "zircon"
+    ]
+    suffixes = [
+        "philosophorum", "spiritus", "tinctura", "essentia", "elixir",
+        "praeparatum", "aether", "vitae", "lapis", "metallum", "aureum",
+        "caelestis", "chrysopoeia", "cosmicum", "deum", "draconis",
+        "elementorum", "hermetica", "illuminationis", "magnum", "mysticum",
+        "occultum", "omnipotentis", "philosophia", "praestantissimum",
+        "quintessentia", "regeneratio", "universalis"
+    ]
+    prefix = random.choice(prefixes)
+    suffix = random.choice(suffixes)
+    parts = [prefix, suffix]
+    if add_random_digit:
+        random_digit = ''.join(
+            random.choices(string.digits, k=digit_count)
+        )
+        parts.append(random_digit)
+    return separator.join(parts)

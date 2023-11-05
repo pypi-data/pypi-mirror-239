52886eeb472f1f81f4f5ce47fdadbc4947e72034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:43 +0000

    Add package ioh/0.3.13/ioh-0.3.13.tar.gz

---
+++ packages/ioh/ioh-0.3.13.tar.gz/ioh-0.3.13/include/ioh/problem.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "problem/transformation.hpp"
+#include "problem/problem.hpp"
+#include "problem/single.hpp"
+#include "problem/wrap_function.hpp"
+#include "problem/bbob.hpp"
+#include "problem/pbo.hpp"
+#include "problem/wmodel.hpp"
+#include "problem/submodular.hpp"
+
+#ifdef USING_MKLANDSCAPE
+#include "problem/mklandscape/cliqueTreeC.hpp"
+#endif
+
+#include "problem/star_discrepancy/real.hpp"
+#include "problem/star_discrepancy/integer.hpp"

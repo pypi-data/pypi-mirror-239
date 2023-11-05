22a4fa2d502b10e46890bd87dc10a545338b919e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:03:46 +0000

    Add package bookio-fetchfox/1.2.1/bookio_fetchfox-1.2.1.tar.gz

---
+++ packages/bookio-fetchfox/bookio_fetchfox-1.2.1.tar.gz/bookio_fetchfox-1.2.1/fetchfox/dtos/__init__.py
@@ -0,0 +1,9 @@
+from .asset import AssetDTO
+from .campaign import CampaignDTO
+from .floor import FloorDTO
+from .holding import HoldingDTO
+from .listing import ListingDTO
+from .pair_stats import PairStatsDTO, StatsDTO
+from .order import OrderDTO, OrderType
+from .rank import RankDTO
+from .sale import SaleDTO, SaleType

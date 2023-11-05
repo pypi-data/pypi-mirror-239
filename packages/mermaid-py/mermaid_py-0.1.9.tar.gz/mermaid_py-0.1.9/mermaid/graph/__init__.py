4132d212df01347140bfb784efc2eab3585914be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:47 +0000

    Add package mermaid-py/0.1.9/mermaid_py-0.1.9.tar.gz

---
+++ packages/mermaid-py/mermaid_py-0.1.9.tar.gz/mermaid_py-0.1.9/mermaid/graph/__init__.py
@@ -0,0 +1,20 @@
+import os
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+
+@dataclass
+class Graph:
+    title: str
+    script: str
+
+    def save(self, path: Optional[Path] = None) -> None:
+        file_path: Path = path if path else Path(f'./{self.title}.txt')
+
+        with open(file_path, 'w') as file:
+            file.write(self.script)
+
+    def _build_script(self) -> None:
+        script: str = f'---\ntitle: {self.title}\n---' + self.script
+        self.script = script

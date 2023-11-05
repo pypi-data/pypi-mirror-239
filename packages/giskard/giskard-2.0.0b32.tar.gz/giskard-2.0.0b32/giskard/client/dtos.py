7064ee01f62718a01c4ddcaef290c062f79aa574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:38 +0000

    Add package giskard/2.0.0b32/giskard-2.0.0b32.tar.gz

---
+++ packages/giskard/giskard-2.0.0b32.tar.gz/giskard-2.0.0b32/giskard/client/dtos.py
@@ -0,0 +1,70 @@
+from typing import Any, Dict, List, Optional
+
+from giskard.core.validation import ConfiguredBaseModel
+
+
+class TestInputDTO(ConfiguredBaseModel):
+    name: str
+    value: str
+    type: str
+    params: List["TestInputDTO"] = list()
+    is_alias: bool = False
+
+
+class SuiteTestDTO(ConfiguredBaseModel):
+    testUuid: str
+    functionInputs: Dict[str, TestInputDTO]
+    displayName: Optional[str] = None
+
+
+class TestSuiteDTO(ConfiguredBaseModel):
+    name: Optional[str]
+    project_key: str
+    tests: List[SuiteTestDTO]
+    function_inputs: List[TestInputDTO]
+
+
+class ServerInfo(ConfiguredBaseModel):
+    instanceId: Optional[str] = None
+    serverVersion: Optional[str] = None
+    instanceLicenseId: Optional[str] = None
+    user: Optional[str] = None
+
+
+class TestInfo(ConfiguredBaseModel):
+    testUuid: str
+    functionInputs: Dict[str, Any]
+
+
+class SuiteInfo(ConfiguredBaseModel):
+    name: str
+    tests: List[TestInfo]
+
+
+class ModelMetaInfo(ConfiguredBaseModel):
+    id: str
+    name: str
+    modelType: str
+    featureNames: List[str]
+    threshold: Optional[float] = None
+    description: Optional[str] = None
+    classificationLabels: Optional[List[str]] = None
+    classificationLabelsDtype: Optional[str] = None
+    languageVersion: str
+    language: str
+    createdDate: str
+    size: int
+    projectId: int
+
+
+class DatasetMetaInfo(ConfiguredBaseModel):
+    target: Optional[str] = None
+    columnTypes: Dict[str, str]
+    columnDtypes: Dict[str, str]
+    numberOfRows: int
+    categoryFeatures: Dict[str, List[str]]
+    name: Optional[str] = None
+    originalSizeBytes: int
+    compressedSizeBytes: int
+    createdDate: str
+    id: str
---
+++ packages/giskard/giskard-2.0.0b32.tar.gz/giskard-2.0.0b32/giskard/scanner/performance/__init__.py
@@ -0,0 +1,3 @@
+from .performance_bias_detector import PerformanceBiasDetector
+
+__all__ = ["PerformanceBiasDetector"]

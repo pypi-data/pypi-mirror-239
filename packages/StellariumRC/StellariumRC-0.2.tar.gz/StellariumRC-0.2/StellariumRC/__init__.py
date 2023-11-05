590061d3feb68d2c729e9b2fdb682a041e9aa12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:04:12 +0000

    Add package StellariumRC/0.2/StellariumRC-0.2.tar.gz

---
+++ packages/StellariumRC/StellariumRC-0.2.tar.gz/StellariumRC-0.2/StellariumRC/__init__.py
@@ -0,0 +1,17 @@
+from . import main,objects,scripts,simbad,stelaction,stelproperty,location,locationsearch,view
+
+class Stellarium:
+    def __init__(self, ip="127.0.0.1", port=8090, password="") -> None:
+        self.password = password
+        self.ip = ip
+        self.port = port
+        self.main = main.Main(self.ip, self.port, self.password)
+        self.objects = objects.Objects(self.ip, self.port, self.password)
+        self.scripts = scripts.Scripts(self.ip, self.port, self.password)
+        self.simbad = simbad.Simbad(self.ip, self.port, self.password)
+        self.stelaction = stelaction.StelAction(self.ip, self.port, self.password)
+        self.stelproperty = stelproperty.StelProperty(self.ip, self.port, self.password)
+        self.location = location.Location(self.ip, self.port, self.password)
+        self.locationsearch = locationsearch.LocationSearch(self.ip, self.port, self.password)
+        self.view = view.View(self.ip, self.port, self.password)
+        

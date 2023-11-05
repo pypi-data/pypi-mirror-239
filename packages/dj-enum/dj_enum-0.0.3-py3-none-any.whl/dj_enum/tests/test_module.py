eecb33c171c08a5f4c8059b4e870cfa083c71cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:22 +0000

    Add package dj-enum/0.0.3/dj_enum-0.0.3-py3-none-any.whl

---
+++ packages/dj-enum/dj_enum-0.0.3-py3-none-any.whl/dj_enum/tests/test_module.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from dj_enum.exceptions import InvalidStateEnumError, InvalidTransitionError
+
+from .models import Orders, PaymentStatus
+
+
+class DJEnumStateChoiceFieldTest(TestCase):
+    def test_default_value(self):
+        order = Orders.objects.create(product_name="Test Product")
+        self.assertEqual(order.status, PaymentStatus.NOT_STARTED)
+
+    def test_valid_value(self):
+        order = Orders.objects.create(
+            product_name="Test Product", status=PaymentStatus.COMPLETED
+        )
+        self.assertEqual(order.status, PaymentStatus.COMPLETED)
+
+    def test_valid_value_as_string(self):
+        order = Orders.objects.create(product_name="Test Product", status="completed")
+        self.assertEqual(order.status, PaymentStatus.COMPLETED)
+
+    def test_invalid_value(self):
+        with self.assertRaises(InvalidStateEnumError):
+            Orders.objects.create(product_name="Test Product", status="invalid")
+
+    def test_invalid_state_change(self):
+        order = Orders.objects.create(
+            product_name="Test Product", status=PaymentStatus.COMPLETED
+        )
+        with self.assertRaises(InvalidTransitionError):
+            order.status = PaymentStatus.NOT_STARTED
+            order.save()

d87a4de63991f39fa81848ea65c314bb737e9b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:06:26 +0000

    Add package clonewall/0.0.2/clonewall-0.0.2.tar.gz

---
+++ packages/clonewall/clonewall-0.0.2.tar.gz/clonewall-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='clonewall',
+    version='0.0.2',
+    description='A placeholder for PyPI package name reservation.',
+    long_description='This is a placeholder package for PyPI name reservation.',
+    author='Reach Morrison',
+    author_email='reachmorrison@gmail.com',
+    url='https://github.com/yourusername/unique_package_name',
+    packages=[],
+)

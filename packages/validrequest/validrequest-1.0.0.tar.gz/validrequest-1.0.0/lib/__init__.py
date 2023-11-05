d9bfbef98b1fb418b7b5428c333eac24e22b34a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:14 +0000

    Add package validrequest/1.0.0/validrequest-1.0.0.tar.gz

---
+++ packages/validrequest/validrequest-1.0.0.tar.gz/validrequest-1.0.0/lib/__init__.py
@@ -0,0 +1,5 @@
+from lib.validator import validate, validator
+from lib.exceptions import ValidationError
+from lib.version import __version__
+
+__all__ = ['validate', 'validator', 'ValidationError', '__version__']

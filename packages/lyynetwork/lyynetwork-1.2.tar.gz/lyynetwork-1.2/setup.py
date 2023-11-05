386caf29cbb9f7ea8a2dec8f53c9b5ce8000ed52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 15:06:35 +0000

    Add package lyynetwork/1.2/lyynetwork-1.2.tar.gz

---
+++ packages/lyynetwork/lyynetwork-1.2.tar.gz/lyynetwork-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='lyynetwork',
+    version='1.2',
+    author='lyy',
+    author_email='',
+    description='network application for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+
+)

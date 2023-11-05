bb308d732eed9a1b4f62c95910e7568509961305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:10:00 +0000

    Add package my-example-app/0.4/my_example_app-0.4.tar.gz

---
+++ packages/my-example-app/my_example_app-0.4.tar.gz/my_example_app-0.4/my_example_module/run.py
@@ -0,0 +1,17 @@
+# run.py
+
+from my_example_module import my_module
+
+a = 10
+b = 5
+
+# 패키지의 함수를 사용
+result_add = my_module.add(a, b)
+result_subtract = my_module.subtract(a, b)
+result_multiply = my_module.multiply(a, b)
+result_divide = my_module.divide(a, b)
+
+print(f"Addition: {a} + {b} = {result_add}")
+print(f"Subtraction: {a} - {b} = {result_subtract}")
+print(f"Multiplication: {a} * {b} = {result_multiply}")
+print(f"Division: {a} / {b} = {result_divide}")
---
+++ packages/my-example-app/my_example_app-0.4.tar.gz/my_example_app-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="my_example_app",
+    version="0.4",
+    description="An example Python package",
+    author="Your Name",
+    author_email="youremail@example.com",
+    packages=["my_example_module"],
+)
+

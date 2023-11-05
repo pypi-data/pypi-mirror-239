2eb990b27b82e4f71575672aff7dd68078b07763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:10:04 +0000

    Add package my-example-app/0.5/my_example_app-0.5.tar.gz

---
+++ packages/my-example-app/my_example_app-0.5.tar.gz/my_example_app-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_example_app",
+    version="0.5",
+    description="An example Python package",
+    author="Your Name",
+    author_email="youremail@example.com",
+    packages=find_packages(),
+)
+

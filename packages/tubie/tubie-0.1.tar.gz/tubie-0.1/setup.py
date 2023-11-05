66a7433184d021c37df3f8459c16e88c45af6efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:03:42 +0000

    Add package tubie/0.1/tubie-0.1.tar.gz

---
+++ packages/tubie/tubie-0.1.tar.gz/tubie-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tubie',
+    version='0.1',
+    description='A simple youtube video downloader',
+    author='keyaedisa',
+    packages=['tubie'],
+)
+

a6ae894024d2cd82d49aef4818724be8d15d2e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:47 +0000

    Add package pyqcolur/1.1/pyqcolur-1.1.tar.gz

---
+++ packages/pyqcolur/pyqcolur-1.1.tar.gz/pyqcolur-1.1/pyqcolur/test.py
@@ -0,0 +1,75 @@
+#https://github.com/smithshawo/pyqcolur
+#from pyqcolur import *
+#u can use it like this
+
+#red,blue,orange,etc
+
+#from pyqcolur import Colors, Colorate
+
+#print(Colorate.Horizontal(Colors.yellow_to_red, "Hello", 1))
+
+#from pyqcolur import Colors, Colorate
+
+#text = "Hello world!"
+
+#print(Colors.blue + text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/pyqcolur/pyqcolur-1.1.tar.gz/pyqcolur-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="pyqcolur",
+    version='1.1',
+    license='Eclipse Public License 2.0',
+    authors=["johnblay"],
+    author_email="yksick@gmail.com",
+    description="python library for colors",
+    long_description='Documentation: https://github.com/smithshawo/pyqcolur',
+    packages=['pyqcolur']
+)
+

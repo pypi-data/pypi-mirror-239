ee4e65b54bed5715799a3d69be00c0579d229498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:26 +0000

    Add package feffery-antd-components/0.2.11/feffery_antd_components-0.2.11.tar.gz

---
+++ packages/feffery-antd-components/feffery_antd_components-0.2.11.tar.gz/feffery_antd_components-0.2.11/feffery_antd_components/AntdBadge.py
@@ -0,0 +1,71 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class AntdBadge(Component):
+    """An AntdBadge component.
+
+
+Keyword arguments:
+
+- children (a list of or a singular dash component, string or number; optional)
+
+- id (string; optional)
+
+- className (string | dict; optional)
+
+- color (string; optional)
+
+- count (number; optional)
+
+- dot (boolean; default False)
+
+- key (string; optional)
+
+- loading_state (dict; optional)
+
+    `loading_state` is a dict with keys:
+
+    - component_name (string; optional):
+        Holds the name of the component that is loading.
+
+    - is_loading (boolean; optional):
+        Determines if the component is loading or not.
+
+    - prop_name (string; optional):
+        Holds which property is loading.
+
+- nClicks (number; default 0)
+
+- offset (list of numbers; optional)
+
+- overflowCount (number; default 99)
+
+- showZero (boolean; default False)
+
+- size (a value equal to: 'default', 'small'; default 'default')
+
+- status (a value equal to: 'success', 'processing', 'default', 'error', 'warning'; optional)
+
+- style (dict; optional)
+
+- text (string; optional)
+
+- title (string; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'feffery_antd_components'
+    _type = 'AntdBadge'
+    @_explicitize_args
+    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, key=Component.UNDEFINED, color=Component.UNDEFINED, count=Component.UNDEFINED, dot=Component.UNDEFINED, showZero=Component.UNDEFINED, overflowCount=Component.UNDEFINED, offset=Component.UNDEFINED, status=Component.UNDEFINED, text=Component.UNDEFINED, title=Component.UNDEFINED, size=Component.UNDEFINED, nClicks=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'className', 'color', 'count', 'dot', 'key', 'loading_state', 'nClicks', 'offset', 'overflowCount', 'showZero', 'size', 'status', 'style', 'text', 'title']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id', 'className', 'color', 'count', 'dot', 'key', 'loading_state', 'nClicks', 'offset', 'overflowCount', 'showZero', 'size', 'status', 'style', 'text', 'title']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        super(AntdBadge, self).__init__(children=children, **args)

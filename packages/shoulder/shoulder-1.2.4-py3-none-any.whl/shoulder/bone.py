bfd086ebeae04f5be9d614c8700119475b955a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:04:01 +0000

    Add package shoulder/1.2.4/shoulder-1.2.4-py3-none-any.whl

---
+++ packages/shoulder/shoulder-1.2.4-py3-none-any.whl/shoulder/bone.py
@@ -0,0 +1,174 @@
+from . import utils
+from .humerus import mesh
+from .humerus import canal
+from .humerus import epicondyle
+from .humerus import surgical_neck
+from .humerus import anatomic_neck
+from .humerus import bicipital_groove
+from .humerus import slice
+from .base import Bone
+
+from abc import ABC, abstractmethod
+from pathlib import Path
+import plotly.graph_objects as go
+import numpy as np
+
+# ignore warnings
+# This is bad practice but one of the libraries has an issue that generates a nonsense warning
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class Humerus(Bone):
+    def __init__(self, stl_file):
+        self.transform = np.identity(4)
+        self._obb = mesh.FullObb(stl_file)
+        self.stl_file = self._obb.file
+        self.mesh = self._obb.mesh_ct
+        self._full_slices = slice.FullSlices(self._obb)
+        self._distal_slices = slice.DistalSlices(self._obb)
+
+        self.surgical_neck = surgical_neck.SurgicalNeck(self._full_slices)
+        self._proximal_slices = slice.ProximalSlices(
+            self._obb, self.surgical_neck, return_odd=False
+        )
+        self.canal = canal.Canal(self._full_slices)
+        self.trans_epiconylar = epicondyle.TransEpicondylar(self._distal_slices)
+        self.bicipital_groove = bicipital_groove.DeepGroove(
+            self._proximal_slices, self.canal
+        )
+        self.anatomic_neck = anatomic_neck.AnatomicNeck(
+            self._proximal_slices, self.bicipital_groove
+        )
+
+    def apply_csys_canal_transepiconylar(self) -> np.ndarray:
+        """applies a coordinate system constructed from the canal axis (z+) and transepicondylar axis (+y) to previously calculated landmarks"""
+        self.transform = construct_csys(self.canal.axis(), self.trans_epiconylar.axis())
+        self._update_landmark_data(self.transform)
+        self.mesh = self._obb.mesh_ct.copy().apply_transform(self.transform)
+        return self.transform
+
+    def apply_csys_obb(self) -> np.ndarray:
+        """applies a coordinate system constructed from an oriented bounding box to previously calculated landmarks"""
+        self.transform = self._obb.transform
+        self._update_landmark_data(self.transform)
+        self.mesh = self._obb.mesh.copy()
+        return self.transform
+
+    def apply_csys_ct(self) -> np.ndarray:
+        """applies a the native CT coordinate system to previously calculated landmarks"""
+        self.transform = np.identity(4)
+        self._update_landmark_data(self.transform)
+        self.mesh = self._obb.mesh_ct.copy()
+        return self.transform
+
+    def apply_csys_custom(self, transform, from_ct=True):
+        """applies a user defined coordinate system defined as a transformation matrix between the CT coordinate system and the user defined coordiante system to previously calculated landmarks"""
+        if from_ct:
+            self.transform = transform
+            self._update_landmark_data(self.transform)
+            self.mesh = self._obb.mesh_ct.copy().apply_transform(self.transform)
+        else:
+            self.transform = np.dot(transform, self.transform)
+            self._update_landmark_data(self.transform)
+            self.mesh = self.mesh.apply_transform(self.transform)
+
+    def apply_translation(self, translation):
+        """applies a user defined translation in the CT coordinate system to previously calculated landmarks"""
+        _transform = utils.translate_transform(translation)
+        self.transform = np.dot(_transform, self.transform)
+        self._update_landmark_data(self.transform)
+        self.mesh = self.mesh.apply_transform(self.transform)
+
+
+class ProximalHumerus(Bone):
+    def __init__(self, stl_file):
+        self.transform = np.identity(4)
+        self._obb = mesh.ProxObb(stl_file)
+        self.stl_file = self._obb.file
+        self.mesh = self._obb.mesh_ct
+        self._full_slices = slice.FullSlices(self._obb)
+        self.surgical_neck = surgical_neck.SurgicalNeck(
+            self._full_slices,
+            only_proximal=True,
+        )
+        self._proximal_slices = slice.ProximalSlices(
+            self._obb,
+            self.surgical_neck,
+            return_odd=False,
+        )
+        self.canal = canal.Canal(self._full_slices, proximal=True)
+        self.bicipital_groove = bicipital_groove.DeepGroove(
+            self._proximal_slices, self.canal
+        )
+        self.anatomic_neck = anatomic_neck.AnatomicNeck(
+            self._proximal_slices, self.bicipital_groove
+        )
+
+    def apply_csys_canal_articular(self, articular) -> np.ndarray:
+        """applies a coordinate system constructed from the canal axis (+z) and the head central axis(+y) to previously calculated landmarks"""
+
+        self.transform = construct_csys(self.canal.axis(), articular)
+        self._update_landmark_data(self.transform)
+        self.mesh = self._obb.mesh_ct.copy().apply_transform(self.transform)
+        return self.transform
+
+    def apply_csys_obb(self) -> np.ndarray:
+        """applies a coordinate system constructed from an oriented bounding box to previously calculated landmarks"""
+
+        self.transform = self._obb.transform
+        self._update_landmark_data(self.transform)
+        self.mesh = self._obb.mesh.copy()
+        return self.transform
+
+    def apply_csys_ct(self) -> np.ndarray:
+        """applies a the native CT coordinate system to previously calculated landmarks"""
+
+        self.transform = np.identity(4)
+        self._update_landmark_data(self.transform)
+        self.mesh = self._obb.mesh_ct.copy()
+        return self.transform
+
+    def apply_csys_custom(self, transform, from_ct=True):
+        """applies a user defined coordinate system defined as a transformation matrix between the CT coordinate system and the user defined coordiante system to previously calculated landmarks"""
+        if from_ct:
+            self.transform = transform
+            self._update_landmark_data(self.transform)
+            self.mesh = self._obb.mesh_ct.copy().apply_transform(self.transform)
+        else:
+            self.transform = np.dot(transform, self.transform)
+            self._update_landmark_data(self.transform)
+            self.mesh = self.mesh.apply_transform(self.transform)
+
+
+def construct_csys(vec_z, vec_y):
+    # define center and two axes
+    pos = np.average(vec_z, axis=0)
+    pos = pos.flatten()
+    z_hat = utils.unit_vector(vec_z[0], vec_z[1])
+    x_hat = utils.unit_vector(vec_y[0], vec_y[1])
+
+    # calculate remaing axis
+    y_hat = np.cross(x_hat, z_hat)
+    y_hat /= np.linalg.norm(y_hat)
+
+    # transepicondylar axis is not quite perpendicular so do it again
+    # this is but a temporary fix, maybe switchinf back the transepi to
+    # being dependent on the canal would be wise
+    x_hat = np.cross(y_hat, z_hat)
+    x_hat /= np.linalg.norm(x_hat)
+
+    # construct transform
+    transform = np.c_[x_hat, y_hat, z_hat, pos]
+    transform = np.r_[transform, np.array([0, 0, 0, 1]).reshape(1, 4)]
+
+    # if the determinant is 0 then this is a reflection, to undo that the direciton of the
+    # epicondylar axis should be switched
+
+    if np.round(np.linalg.det(transform)) == -1:
+        transform[:, 0] *= -1
+
+    # return transform for CT csys -> canal-epi csys
+    transform = utils.inv_transform(transform)
+    return transform
---
+++ packages/shoulder/shoulder-1.2.4-py3-none-any.whl/shoulder/humerus/anatomic_neck.py
@@ -0,0 +1,148 @@
+from shoulder.humerus import slice
+from shoulder.base import Landmark
+from shoulder import utils
+from shoulder.humerus import bicipital_groove
+
+import plotly.graph_objects as go
+import numpy as np
+import skspatial.objects
+
+from sklearn.preprocessing import MinMaxScaler
+import importlib.resources
+import onnxruntime as rt
+
+
+class AnatomicNeck(Landmark):
+    def __init__(self, slc: slice.Slices, bcptl: bicipital_groove.DeepGroove):
+        self._slc = slc
+        self._bcptl = bcptl
+        self._points_ct = None
+        self._points = None
+        self._plane = None
+        self._plane_ct = None
+
+    def points(self):
+        if self._points is None:
+            cutoff = (0.0, 0.852)  # not changeable
+            itr = self._slc.itr_start(cutoff)
+            zs = self._slc.zs(cutoff)
+
+            image = np.zeros((itr.shape[0], itr.shape[2]))
+            itr_shft = np.zeros(itr.shape)
+            for i, (z, tr) in enumerate(zip(zs, itr)):
+                # interpolate on even theta
+                # sometimes the last is also the first which creates artifacts
+                t_sampling = np.linspace(tr[0][0], tr[0][-2], tr.shape[1])
+                tr = np.c_[t_sampling, np.interp(t_sampling, tr[0, :-1], tr[1, :-1])].T
+
+                # shift to starting at bicipital groove
+                self._bcptl.axis()  # force the calculation of the bicipital groove if not done yet
+                closest_bg_idx = np.argmin(np.abs(tr[0] - self._bcptl.bg_theta))
+                tr = np.c_[tr[:, closest_bg_idx:], tr[:, :closest_bg_idx]]
+
+                # add to image
+                image[i] = tr[1]
+                # add to itr shifted
+                itr_shft[i] = tr
+
+            image_shape = image.shape
+            image = MinMaxScaler().fit_transform(image.reshape(-1, 1))
+            image = image.reshape(image_shape)
+
+            # open random forest saved in onnx
+            with open(
+                importlib.resources.files("shoulder")
+                / "humerus/models/anp512_hausdorf.onnx",
+                "rb",
+            ) as file:
+                unet = rt.InferenceSession(
+                    file.read(), providers=["CPUExecutionProvider"]
+                )
+
+            # get mask prediction
+            input_name = unet.get_inputs()[0].name
+            input_image = image.astype(np.float32).reshape(1, 1, 512, 512)
+            mask = unet.run(None, {input_name: input_image})[0]
+
+            # extract mask edge
+            mask = np.squeeze(mask)
+            mask = (mask > 0).astype(int)
+            mask_edge = np.abs(np.diff(mask, prepend=0))
+            mask_edge = mask_edge.astype(bool)
+
+            # pull out theta and radius
+            t = itr_shft[:, 0, :]
+            r = itr_shft[:, 1, :]
+            # setup zs to be same shape as t and r
+            zs = np.repeat(zs.reshape(-1, 1), t.shape[1], axis=1)
+            zs = zs[mask_edge]
+            # grab the radial values that correspond to the edge of the mask
+            t = t[mask_edge]
+            r = r[mask_edge]
+            x = r * np.cos(t)
+            y = r * np.sin(t)
+
+            # create array of points in obb space and transform to CT
+            anp_points = np.c_[x, y, zs]
+            # needed to calculate derivative methods (axis, plane etc.)
+            self._points_obb = anp_points
+
+            anp_points = utils.transform_pts(
+                anp_points, utils.inv_transform(self._slc.obb.transform)
+            )
+            self._points = anp_points
+            self._points_ct = anp_points
+
+        return self._points
+
+    def plane(self):
+        if self._points is None:
+            self.points()  # calculate landmark if not yet calculated
+
+        plane = skspatial.objects.Plane.best_fit(self._points_obb)
+
+        plane_pts = np.array(
+            self._slc._mesh_oriented_uobb.section(
+                plane_origin=plane.point, plane_normal=plane.normal
+            ).vertices
+        )
+        plane_pts = utils.transform_pts(
+            plane_pts, utils.inv_transform(self._slc.obb.transform)
+        )
+        self._plane_ct = plane_pts
+        self._plane = plane_pts
+
+        return self._plane
+
+    # TODO: impliment features
+    # def axis_central(self):
+    #     if self._points is None:
+    #         self.points()  # calculate landmark if not yet calculated
+
+    # def axis_normal(self):
+    #     if self._points is None:
+    #         self.points()  # calculate landmark if not yet calculated
+
+    def transform_landmark(self, transform) -> None:
+        if self._points is not None:
+            self._points = utils.transform_pts(self._points_ct, transform)
+
+        if self._plane is not None:
+            self._plane = utils.transform_pts(self._plane_ct, transform)
+
+    def _graph_obj(self):
+        if self._points is None:
+            return None
+        else:
+            plot = [
+                go.Scatter3d(
+                    x=self._points[:, 0],
+                    y=self._points[:, 1],
+                    z=self._points[:, 2],
+                    mode="markers",
+                    showlegend=True,
+                    name="Anatomic Neck",
+                ),
+            ]
+
+            return plot

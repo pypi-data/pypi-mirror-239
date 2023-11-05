082ce6805443ae75571e31f3fd545166252004b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:07:16 +0000

    Add package numina/0.32.3/numina-0.32.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/numina/numina-0.32.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/numina/user/tests/test_helpers.py
@@ -0,0 +1,26 @@
+
+import os.path
+
+from ..helpers import WorkEnvironment
+
+
+def test_work1(tmpdir):
+    """Test default definitions"""
+    obsid = 100
+    base = 'base'
+    basedir = str(tmpdir.dirpath(base))
+
+    work = WorkEnvironment(obsid, basedir=basedir)
+    work.sane_work()
+
+    assert work.workdir == os.path.join(basedir, f"obsid{obsid}_work")
+    assert work.basedir == basedir
+    assert work.resultsdir == os.path.join(basedir, f"obsid{obsid}_results")
+
+    index_base = "index.pkl"
+    assert work.index_file == os.path.join(work.workdir, index_base)
+
+    assert os.path.isdir(work.workdir)
+    assert os.path.isdir(work.resultsdir)
+    assert os.path.isdir(work.basedir)
+    assert os.path.isfile(work.index_file)

db8ac077df1f7e8a58383171fc26d51bf770e7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:08:59 +0000

    Add package agent-bdi/2.0.3/agent_bdi-2.0.3-py3-none-any.whl

---
+++ packages/agent-bdi/agent_bdi-2.0.3-py3-none-any.whl/__init__.py
@@ -0,0 +1,8 @@
+import logging
+
+
+__logger = logging.getLogger("ABDI")
+
+
+def get_logger():        
+    return __logger
---
+++ packages/agent-bdi/agent_bdi-2.0.3-py3-none-any.whl/broker/empty_broker.py
@@ -0,0 +1,36 @@
+import logging
+
+from broker.message_broker import MessageBroker
+from broker.notifier import BrokerNotifier
+
+
+logger = logging.getLogger("ABDI")
+
+
+class EmptyBroker(MessageBroker):
+    def __init__(self, notifier:BrokerNotifier):
+        logger.info(f"Initialize...")
+        
+        super().__init__(notifier=notifier)
+
+
+
+    ###################################
+    # Implementation of MessageBroker #
+    ###################################
+    
+    
+    def start(self, options:dict):
+        logger.info(f"Empty broker is starting. options:{options}")
+       
+
+    def stop(self):
+        logger.info(f"Empty broker is stopping...")
+
+
+    def publish(self, topic:str, payload):
+        logger.info(f"topic: {topic}, payload: {payload}")
+        
+    
+    def subscribe(self, topic:str, data_type):
+        logger.info(f"topic: {topic}, data_type: {data_type}")

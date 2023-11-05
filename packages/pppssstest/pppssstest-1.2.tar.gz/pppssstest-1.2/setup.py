6b2cd8a15ee559c0c44624b04c8ab521e107e114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:35 +0000

    Add package pppssstest/1.2/pppssstest-1.2.tar.gz

---
+++ packages/pppssstest/pppssstest-1.2.tar.gz/pppssstest-1.2/setup.py
@@ -0,0 +1,37 @@
+
+
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setuptools.setup(
+name="pppssstest",
+version="1.2",
+author="author",
+author_email="author@email.com",
+install_requires=[""],
+description="Desc here",
+long_description=readme(),
+long_description_content_type="text/markdown",
+license='Free To Use But Restricted',
+keywords="pp ss tt",
+url="",
+packages=setuptools.find_packages(),
+python_requires="",
+classifiers=[
+],
+package_data={"": 
+[""]
+        },
+entry_points={'gui_scripts': [
+"pptest=pppssstest.pppssstest:main",
+    ]
+    },
+)
+

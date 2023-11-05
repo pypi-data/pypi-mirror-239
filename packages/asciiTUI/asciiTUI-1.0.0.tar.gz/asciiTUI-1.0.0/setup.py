c9b131989a9909a7faf950b10f11143ab5d2b500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:19 +0000

    Add package asciiTUI/1.0.0/asciiTUI-1.0.0.tar.gz

---
+++ packages/asciiTUI/asciiTUI-1.0.0.tar.gz/asciiTUI-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         = 'asciiTUI',
+    version      = '1.0.0',
+    author       = 'azzammuhyala',
+    author_email = 'azzammuhyala@gmail.com',
+    description  = 'This is a library of tools for you to use with your needs for an attractive type of terminal (console) display.',
+    url          = 'https://github.com/azzammuhyala/asciiTUI',
+)

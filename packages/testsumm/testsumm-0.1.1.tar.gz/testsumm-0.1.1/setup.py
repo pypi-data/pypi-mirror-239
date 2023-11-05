5ae1b94fefd55508ecb8b3382629c418316a4a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:20 +0000

    Add package testsumm/0.1.1/testsumm-0.1.1.tar.gz

---
+++ packages/testsumm/testsumm-0.1.1.tar.gz/testsumm-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+with open('README.md',"r") as fh:
+    description = fh.read() 
+
+setup(
+    name='testsumm',
+    version='0.1.1',
+    packages=find_packages(exclude=['testsumm']),
+    description='library ML',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author='Luciano M', 
+    license='MIT',
+    install_requires=["numpy"],
+    python_requires='>=3.10.12',
+    author_email='luciano.munoz1@udea.edu.co'
+    #url = ''
+)
+
+
+    
+    

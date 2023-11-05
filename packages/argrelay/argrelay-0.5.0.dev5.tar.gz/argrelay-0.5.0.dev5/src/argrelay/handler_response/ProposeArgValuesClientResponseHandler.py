5cce4ffe4f86a18386201e49eace713186abb086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:04:05 +0000

    Add package argrelay/0.5.0.dev5/argrelay-0.5.0.dev5.tar.gz

---
+++ packages/argrelay/argrelay-0.5.0.dev5.tar.gz/argrelay-0.5.0.dev5/src/argrelay/handler_response/ProposeArgValuesClientResponseHandler.py
@@ -0,0 +1,17 @@
+from argrelay.handler_response.AbstractClientResponseHandler import AbstractClientResponseHandler
+from argrelay.schema_response.ArgValuesSchema import arg_values_
+
+
+class ProposeArgValuesClientResponseHandler(AbstractClientResponseHandler):
+
+    def __init__(
+        self,
+    ):
+        super().__init__(
+        )
+
+    def handle_response(
+        self,
+        response_dict: dict,
+    ):
+        print("\n".join(response_dict[arg_values_]))

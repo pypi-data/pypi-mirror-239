4a9ecad09af8c9830d53aa8f03573dbb3d598727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:22 +0000

    Add package quarrel-solver/1.11/quarrel-solver-1.11.tar.gz

---
+++ packages/quarrel-solver/quarrel-solver-1.11.tar.gz/quarrel-solver-1.11/setup.py
@@ -0,0 +1,35 @@
+version = '1.11'
+
+from setuptools import setup
+
+setup(
+	name='quarrel-solver',
+	version=version,
+	description='Tool for Quarrel (video game)',
+	long_description=open('README.md', 'r').read(),
+	long_description_content_type='text/markdown',
+	url='https://github.com/silvncr/quarrel',
+	author='silvncr',
+	include_package_data=True,
+	license='MIT',
+	packages=['quarrel_solver'],
+	package_data={
+		'quarrel_solver': [
+			'*.json'
+		]
+	},
+	setup_requires=['pytest_runner'],
+	python_requires='>=3.6',
+	scripts=[],
+	tests_require=['pytest'],
+	entry_points={},
+	zip_safe=True,
+	classifiers=[
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Topic :: Games/Entertainment :: Board Games',
+		'Topic :: Games/Entertainment :: Puzzle Games',
+		'Topic :: Software Development :: Libraries',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+)

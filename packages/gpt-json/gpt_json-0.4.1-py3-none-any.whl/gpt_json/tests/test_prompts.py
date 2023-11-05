acd69c4ac57e1e125da580b83c7d258813fc1618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:03:44 +0000

    Add package gpt-json/0.4.1/gpt_json-0.4.1-py3-none-any.whl

---
+++ packages/gpt-json/gpt_json-0.4.1-py3-none-any.whl/gpt_json/tests/test_prompts.py
@@ -0,0 +1,61 @@
+from re import sub
+
+import pytest
+
+from gpt_json.generics import resolve_generic_model
+from gpt_json.gpt import ListResponse
+from gpt_json.prompts import generate_schema_prompt
+from gpt_json.tests.shared import LiteralSchema, MySchema
+
+
+def strip_whitespace(input_string: str):
+    return sub(r"\s+", "", input_string)
+
+
+@pytest.mark.parametrize(
+    "schema_definition,expected",
+    [
+        (
+            MySchema,
+            """
+            {{
+                "text": str,
+                "items": str[],
+                "numerical": int | float,
+                "sub_element": {{
+                    "name": str
+                }},
+                "reason": bool // Explanation
+            }}
+            """,
+        ),
+        (
+            ListResponse[MySchema],
+            """
+            {{
+            "items": {{
+                "text": str,
+                "items": str[],
+                "numerical": int | float,
+                "sub_element": {{
+                    "name": str
+                }},
+                "reason": bool // Explanation            
+            }}[] // Repeat for as many objects as are relevant
+            }}
+            """,
+        ),
+        (
+            LiteralSchema,
+            """
+            {{
+            "work_format": "REMOTE" | "OFFICE" | "ANY" // One of the given values
+            }}
+            """,
+        ),
+    ],
+)
+def test_generate_schema_prompt(schema_definition, expected: str):
+    assert strip_whitespace(
+        generate_schema_prompt(resolve_generic_model(schema_definition))
+    ) == strip_whitespace(expected)

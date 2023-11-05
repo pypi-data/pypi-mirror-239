5f11b68f67cd03a70da8d1d6d6ecf5d4d4d06195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:04:05 +0000

    Add package MeowerBot/3.1.0/meowerbot-3.1.0-py3-none-any.whl

---
+++ packages/MeowerBot/meowerbot-3.1.0-py3-none-any.whl/MeowerBot/__init__.py
@@ -0,0 +1,16 @@
+"""
+MeowerBot.py
+
+MIT License
+
+"""
+
+__version__ = "3.0.3"
+
+
+# Public library imports
+from . import Bot as botm
+
+from .Bot import Bot, cbids
+
+__all__ = ["__version__", "Bot", "botm", "cbids"]

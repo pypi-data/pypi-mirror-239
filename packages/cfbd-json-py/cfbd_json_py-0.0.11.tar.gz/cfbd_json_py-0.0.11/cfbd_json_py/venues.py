8ea5b5af1dacb1c275edc4cfa6d24489e32082cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:07:12 +0000

    Add package cfbd-json-py/0.0.11/cfbd_json_py-0.0.11.tar.gz

---
+++ packages/cfbd-json-py/cfbd_json_py-0.0.11.tar.gz/cfbd_json_py-0.0.11/cfbd_json_py/venues.py
@@ -0,0 +1,17 @@
+# Creation Date: 08/30/2023 01:13 EDT
+# Last Updated Date: 10/15/2023 06:57 PM EDT
+# Author: Joseph Armstrong (armstrongjoseph08@gmail.com)
+# File Name: venues.py
+# Purpose: Houses functions pertaining to CFB team venues/stadium data within the CFBD API.
+####################################################################################################
+
+def get_cfbd_venues(
+        api_key: str = None,
+        api_key_dir: str = None,
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )

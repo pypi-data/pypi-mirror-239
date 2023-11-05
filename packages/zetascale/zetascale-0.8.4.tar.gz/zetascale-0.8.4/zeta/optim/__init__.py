04cb6fd429cfcd4d74ee35125d581d28652cd6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:05:43 +0000

    Add package zetascale/0.8.4/zetascale-0.8.4.tar.gz

---
+++ packages/zetascale/zetascale-0.8.4.tar.gz/zetascale-0.8.4/zeta/optim/__init__.py
@@ -0,0 +1,28 @@
+from zeta.optim.batched_optimizer import (
+    BatchedOptimizer,
+    Eden,
+    Eve,
+    LRScheduler,
+    ScaledAdam,
+    _test_scaled_adam,
+)
+from zeta.optim.decoupled_lion import DecoupledLionW
+from zeta.optim.decoupled_optimizer import decoupled_optimizer
+from zeta.optim.decoupled_sophia import SophiaG
+from zeta.optim.stable_adam import StableAdamWUnfused
+from zeta.optim.gradient_ascent import GradientAscent
+
+
+__all__ = [
+    "BatchedOptimizer",
+    "Eden",
+    "Eve",
+    "LRScheduler",
+    "ScaledAdam",
+    "_test_scaled_adam",
+    "DecoupledLionW",
+    "decoupled_optimizer",
+    "SophiaG",
+    "StableAdamWUnfused",
+    "GradientAscent",
+]

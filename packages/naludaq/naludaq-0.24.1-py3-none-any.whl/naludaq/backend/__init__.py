ed4d87d5867e2d25ada1be175ea8e4626654e840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 14:03:45 +0000

    Add package naludaq/0.24.1/naludaq-0.24.1-py3-none-any.whl

---
+++ packages/naludaq/naludaq-0.24.1-py3-none-any.whl/naludaq/backend/__init__.py
@@ -0,0 +1,21 @@
+from .context import Context
+from .managers import (
+    AcquisitionManager,
+    BoardIoManager,
+    ConfigManager,
+    ConnectionManager,
+)
+from .models import (
+    AvailableD2xxDevice,
+    AvailableSerialDevice,
+    D2xxDevice,
+    Device,
+    DeviceType,
+    DiskAcquisition,
+    DiskChunk,
+    LocalAcquisition,
+    RemoteAcquisition,
+    SerialDevice,
+    TemporaryAcquisition,
+    UdpDevice,
+)

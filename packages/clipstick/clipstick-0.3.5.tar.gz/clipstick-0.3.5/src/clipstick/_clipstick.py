7e46e0de150564b3c363172e0b0df583fb4de383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:07:14 +0000

    Add package clipstick/0.3.5/clipstick-0.3.5.tar.gz

---
+++ packages/clipstick/clipstick-0.3.5.tar.gz/clipstick-0.3.5/src/clipstick/_clipstick.py
@@ -0,0 +1,47 @@
+import sys
+
+from clipstick._exceptions import ClipStickError
+from clipstick._help import console
+from clipstick._parse import tokenize, validate_model
+from clipstick._tokens import Command, TPydanticModel
+
+
+def parse(model: type[TPydanticModel], args: list[str] | None = None) -> TPydanticModel:
+    try:
+        validate_model(model)
+    except ClipStickError as err:
+        console.print(err)
+        sys.exit(1)
+    if args is None:
+        entry_point, args = sys.argv[0], sys.argv[1:]
+    else:
+        # Normally the first item in your sys.argv is the command/entrypoint you've entered.
+        # During testing you don't provide that (only the actual arguments you enter after that).
+        entry_point = "dummy-entrypoint"
+
+    root_node = Command(key=entry_point, cls=model, parent=None)
+    try:
+        tokenize(model=model, sub_command=root_node)
+    except ClipStickError as err:
+        console.print(err)
+        sys.exit(1)
+    try:
+        success, idx = root_node.match(0, args)
+    except ClipStickError as err:
+        console.print(err)
+        sys.exit(1)
+    if not idx == len(args):
+        console.print("Unable to consume all provided arguments.")
+        sys.exit(1)
+    if not success:
+        console.print("Unable to resolve the arguments into a usable command line.")
+        sys.exit(1)
+
+    try:
+        parsed = root_node.parse(args)
+    except ClipStickError as err:
+        console.print(err)
+        sys.exit(1)
+    else:
+        return parsed[entry_point]
+        return parsed[entry_point]

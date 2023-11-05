fbec42633a6f427eb7f0f7872c61e19c956fa1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 04:06:54 +0000

    Add package pyorc/0.9.0/pyorc-0.9.0.tar.gz

---
+++ packages/pyorc/pyorc-0.9.0.tar.gz/pyorc-0.9.0/src/_pyorc/Writer.h
@@ -0,0 +1,52 @@
+#ifndef WRITER_H
+#define WRITER_H
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "orc/OrcFile.hh"
+
+#include "Converter.h"
+
+namespace py = pybind11;
+
+class Writer
+{
+  private:
+    std::unique_ptr<orc::OutputStream> outStream;
+    std::unique_ptr<orc::Writer> writer;
+    std::unique_ptr<orc::ColumnVectorBatch> batch;
+    std::unique_ptr<Converter> converter;
+    uint64_t batchSize;
+    uint64_t batchItem;
+
+  public:
+    uint64_t currentRow;
+
+    Writer(py::object,
+           py::object,
+           uint64_t = 1024,
+           uint64_t = 67108864,
+           uint64_t = 10000,
+           int = 1,
+           int = 0,
+           uint64_t = 65536,
+           std::set<uint64_t> = {},
+           double = 0.05,
+           py::object = py::none(),
+           unsigned int = 0,
+           py::object = py::none(),
+           double = 0.0,
+           double = 0.0,
+           py::object = py::none());
+    void addUserMetadata(py::str, py::bytes);
+    void write(py::object);
+    uint64_t writerows(py::iterable);
+#if !(ORC_VERSION_MAJOR <= 1 && ORC_VERSION_MINOR < 9)
+    uint64_t writeIntermediateFooter();
+#endif
+    void close();
+    ~Writer(){};
+};
+
+#endif

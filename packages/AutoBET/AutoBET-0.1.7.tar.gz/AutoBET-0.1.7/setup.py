eb9f03c9dd95170ba08fece948f2da07410d5a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-05 03:09:14 +0000

    Add package AutoBET/0.1.7/AutoBET-0.1.7.tar.gz

---
+++ packages/AutoBET/AutoBET-0.1.7.tar.gz/AutoBET-0.1.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AutoBET",
+    version="0.1.7",
+    author="Jiameng Liu",
+    author_email="JiamengLiu.PRC@gmail.com",
+    description="Automatic Brain Extraction Tools",
+    long_description=long_description,
+    long_description_content_type="README.md",
+    url="https://github.com/SaberPRC/Auto-BET",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # exapmle
+        'antspyx',
+        'numpy',
+        'pandas',
+        'torch',
+        'SimpleITK',
+        'MIDP',
+        # 'Django >= 1.11, != 1.11.1, <= 2',
+    ],
+    include_package_data=True,
+)

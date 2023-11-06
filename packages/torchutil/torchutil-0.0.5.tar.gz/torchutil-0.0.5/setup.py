16d0f1a58983737d4493f6c8fce66f0f70152699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:30 +0000

    Add package torchutil/0.0.5/torchutil-0.0.5.tar.gz

---
+++ packages/torchutil/torchutil-0.0.5.tar.gz/torchutil-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+with open('README.md') as file:
+    long_description = file.read()
+
+
+setup(
+    name='torchutil',
+    description='PyTorch utilities for developing deep learning frameworks',
+    version='0.0.5',
+    author='Max Morrison',
+    author_email='maxrmorrison@gmail.com',
+    url='https://github.com/maxrmorrison/torchutil',
+    install_requires=['accelerate', 'apprise', 'torch'],
+    packages=['torchutil'],
+    package_data={'torchutil': ['assets/*', 'assets/*/*']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['pytorch', 'utility', 'training'],
+    classifiers=['License :: OSI Approved :: MIT License'],
+    license='MIT')

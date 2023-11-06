f6ff95194dab622af9255f7861304329266f77c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:33 +0000

    Add package datasetter/0.1.0/datasetter-0.1.0.tar.gz

---
+++ packages/datasetter/datasetter-0.1.0.tar.gz/datasetter-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datasetter',
+    version='0.1.0',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'datasetter = datasetter.hello:say_hello',
+        ],
+    },
+)

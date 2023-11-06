750667f3e3e7a3d66726f9fd959423acf108afd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:23 +0000

    Add package imspy-connector/0.2.2/imspy_connector-0.2.2.tar.gz

---
+++ packages/imspy-connector/imspy_connector-0.2.2.tar.gz/imspy_connector-0.2.2/src/lib.rs
@@ -0,0 +1,25 @@
+mod py_handle;
+mod py_mz_spectrum;
+mod py_tims_frame;
+mod py_tims_slice;
+
+use pyo3::prelude::*;
+use crate::py_handle::PyTimsDataHandle;
+use crate::py_mz_spectrum::{PyMzSpectrum, PyIndexedMzSpectrum, PyTimsSpectrum, PyMzSpectrumVectorized};
+use crate::py_tims_frame::{PyTimsFrame, PyTimsFrameVectorized};
+use crate::py_tims_slice::{PyTimsPlane, PyTimsSlice, PyTimsSliceVectorized};
+
+#[pymodule]
+fn imspy_connector(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyTimsDataHandle>()?;
+    m.add_class::<PyMzSpectrum>()?;
+    m.add_class::<PyMzSpectrumVectorized>()?;
+    m.add_class::<PyIndexedMzSpectrum>()?;
+    m.add_class::<PyTimsSpectrum>()?;
+    m.add_class::<PyTimsFrame>()?;
+    m.add_class::<PyTimsFrameVectorized>()?;
+    m.add_class::<PyTimsSlice>()?;
+    m.add_class::<PyTimsSliceVectorized>()?;
+    m.add_class::<PyTimsPlane>()?;
+    Ok(())
+}

491306daeadb97bff475c6ed0d71826a7dbffc63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:03:59 +0000

    Add package ddc-api-sdk/1.0.3/ddc_api_sdk-1.0.3-py3-none-any.whl

---
+++ packages/ddc-api-sdk/ddc_api_sdk-1.0.3-py3-none-any.whl/DDCSDK/sdk.py
@@ -0,0 +1,6 @@
+class DDCAPISDK:
+    def __init__(self):
+        self.APIHost = ""
+
+    def test(self):
+        print('print test')
---
+++ packages/ddc-api-sdk/ddc_api_sdk-1.0.3-py3-none-any.whl/DDCSDK/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='ddc_api_sdk',
+      version='1.0.0',
+      description='Ding Dang Cats HTTP API Python DDCCore',
+      author='ddc',
+      author_email='',
+      url='',
+      license='MIT',
+      keywords='',
+      project_urls={},
+      packages=['DDCCore'],
+      python_requires='>=3.9'
+ )

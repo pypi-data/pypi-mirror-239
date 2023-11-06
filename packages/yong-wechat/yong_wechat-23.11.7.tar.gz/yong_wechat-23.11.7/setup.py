78d1b2e93a7c580696a6547fd548a35c4f33edd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:05:13 +0000

    Add package yong-wechat/23.11.7/yong_wechat-23.11.7.tar.gz

---
+++ packages/yong-wechat/yong_wechat-23.11.7.tar.gz/yong_wechat-23.11.7/setup.py
@@ -0,0 +1,34 @@
+'''
+FilePath: setup.py
+Author: yongze.chen
+Date: 2023-11-05 20:32:59 +0800
+LastEditors: yongze.chen~home yongze@dingtalk.com
+Email: yongze@dingtalk.com
+LastEditTime: 2023-11-05 21:21:16 +0800
+Copyright: 2023 Yong CO.,LTD. All Rights Reserved.
+Descripttion: 
+'''
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='yong_wechat',
+    version='23.11.7',
+    author='yongze',
+    author_email='yongze@dingtalk.com',
+    url='https://github.com/chenyongze',
+    license='MIT',
+    keywords=['yong', 'utils'],
+    long_description='https://github.com/chenyongze',
+    description=u'yong_wechat',
+    packages=['yong_wechat'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'jujube=yong_wechat:jujube',
+            'pill=yong_wechat:pill'
+        ]
+    }
+)
---
+++ packages/yong-wechat/yong_wechat-23.11.7.tar.gz/yong_wechat-23.11.7/yong_wechat/__init__.py
@@ -0,0 +1,20 @@
+'''
+FilePath: __init__.py
+Author: yongze.chen
+Date: 2023-11-05 20:33:46 +0800
+LastEditors: yongze.chen~home yongze@dingtalk.com
+Email: yongze@dingtalk.com
+LastEditTime: 2023-11-05 20:33:48 +0800
+Copyright: 2023 Yong CO.,LTD. All Rights Reserved.
+Descripttion: 
+'''
+#!/usr/bin/env python
+# encoding=utf-8
+
+
+def jujube():
+    print u'吃枣'
+ 
+
+def pill():
+    print u'药丸'

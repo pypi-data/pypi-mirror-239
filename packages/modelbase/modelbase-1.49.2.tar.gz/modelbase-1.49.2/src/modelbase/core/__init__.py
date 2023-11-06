ddd2ce66b24082193455899bd4d36da79908a4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:06:40 +0000

    Add package modelbase/1.49.2/modelbase-1.49.2.tar.gz

---
+++ packages/modelbase/modelbase-1.49.2.tar.gz/modelbase-1.49.2/src/modelbase/core/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+__all__ = [
+    "AlgebraicMixin",
+    "BaseModel",
+    "CompoundMixin",
+    "ParameterMixin",
+    "RateMixin",
+    "StoichiometricMixin",
+]
+
+from .algebraicmixin import AlgebraicMixin
+from .basemodel import BaseModel
+from .compoundmixin import CompoundMixin
+from .parametermixin import ParameterMixin
+from .ratemixin import RateMixin
+from .stoichiometricmixin import StoichiometricMixin

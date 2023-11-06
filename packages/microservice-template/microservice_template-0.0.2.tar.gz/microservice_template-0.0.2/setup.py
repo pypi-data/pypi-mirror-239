851a149fec5db5050bc15d8d422d41e54f2c27de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 05:03:30 +0000

    Add package microservice-template/0.0.2/microservice_template-0.0.2.tar.gz

---
+++ packages/microservice-template/microservice_template-0.0.2.tar.gz/microservice_template-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Microservicetemplate'
+LONG_DESCRIPTION = 'Template for Simple Flask Microservices'
+
+# Setting up
+setup(
+    name="microservice_template",
+    version=VERSION,
+    author="Pixelfehler",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['flask', 'flask-cors'],
+    keywords=['python', 'microservice'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

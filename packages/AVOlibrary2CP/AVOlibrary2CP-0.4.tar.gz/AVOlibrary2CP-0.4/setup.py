e8a0635d58611c93d5be4b015844cf0cba18ded2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:04 +0000

    Add package AVOlibrary2CP/0.4/AVOlibrary2CP-0.4.tar.gz

---
+++ packages/AVOlibrary2CP/AVOlibrary2CP-0.4.tar.gz/AVOlibrary2CP-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AVOlibrary2CP',
+    version='0.4',
+    packages=find_packages(),
+    description='Curve fitting for COVID-19 data using Gaussian kernel smoother',
+    author='Alejandro Valencia',
+    author_email='alejandro.valenciao1@udea.edu.co',
+    license="MIT",
+    install_requires=['numpy','scipy', 'matplotlib'],
+)

9deb210c6b9057ee27f4063c8b4badc1eeb58361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:04:18 +0000

    Add package ymdown/0.1.1/ymdown-0.1.1.tar.gz

---
+++ packages/ymdown/ymdown-0.1.1.tar.gz/ymdown-0.1.1/README.md
@@ -0,0 +1,11 @@
+# ymdown
+A utility build on top of ytmusicapi and yt-dlp for easy search-and-download in YouTube Music.
+
+## Installation
+`pip install ymdown`
+
+## Usages
+`ymdown <input_song_list_path> <output_directory_path>`
+
+You can change the format by using `-f`, the default format is .mp3.
+`ymdown -f wav <input_song_list_path> <output_directory_path>`
---
+++ packages/ymdown/ymdown-0.1.1.tar.gz/ymdown-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='ymdown',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'ymdown = ymdown:main',
+        ],
+    },
+    # Additional metadata about your package.
+    author='Chan Yat Fu',
+    author_email='chanyatfu0616@google.com',
+    description='A utility build on top of ytmusicapi and yt-dlp for esay search-and-download in YouTube Music.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/chanyatfu/ymdown',
+)

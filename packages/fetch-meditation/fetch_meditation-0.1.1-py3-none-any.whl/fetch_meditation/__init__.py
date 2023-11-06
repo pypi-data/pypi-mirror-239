3159ffa1aad90d0aa1eb8f521dbc4fcbe5363f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:08:08 +0000

    Add package fetch-meditation/0.1.1/fetch_meditation-0.1.1-py3-none-any.whl

---
+++ packages/fetch-meditation/fetch_meditation-0.1.1-py3-none-any.whl/fetch_meditation/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = '0.1.1'
+
+from fetch_meditation.JFTLanguage import JFTLanguage
+from fetch_meditation.JFTSettings import JFTSettings
+from fetch_meditation.JFT import JFT
+from fetch_meditation.SPADLanguage import SPADLanguage
+from fetch_meditation.SPADSettings import SPADSettings
+from fetch_meditation.SPAD import SPAD

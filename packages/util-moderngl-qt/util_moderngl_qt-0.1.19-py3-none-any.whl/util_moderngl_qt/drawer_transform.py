4c6585e37ff8a598f3359b9701074f46b48ff569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:07:53 +0000

    Add package util-moderngl-qt/0.1.19/util_moderngl_qt-0.1.19-py3-none-any.whl

---
+++ packages/util-moderngl-qt/util_moderngl_qt-0.1.19-py3-none-any.whl/util_moderngl_qt/drawer_transform.py
@@ -0,0 +1,19 @@
+from pyrr import Matrix44
+import numpy
+import moderngl
+
+
+class DrawerTransform:
+
+    def __init__(self, drawer):
+        self.transform = Matrix44.identity(dtype=numpy.float32)
+        self.is_visible = True
+        self.drawer = drawer
+
+    def init_gl(self, ctx: moderngl.Context):
+        self.drawer.init_gl(ctx)
+
+    def paint_gl(self, mvp: Matrix44):
+        if not self.is_visible:
+            return
+        self.drawer.paint_gl(mvp * self.transform)

6c6b76fa68f04c67b0b96195c786a78c634653f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:32 +0000

    Add package leet-daily/0.1.0/leet_daily-0.1.0-py3-none-any.whl

---
+++ packages/leet-daily/leet_daily-0.1.0-py3-none-any.whl/leet_daily/leet.py
@@ -0,0 +1,73 @@
+import subprocess
+from .config import Config
+from pathlib import Path
+import httpx
+import shlex
+from selectolax.parser import HTMLParser
+import itertools, textwrap
+
+
+class Leet:
+    def __init__(self):
+        self.config = Config()
+        self.daily_qn_link = get_daily_qn_link()
+        self.leet_file = (self.config.leet_dir 
+            / Path(self.daily_qn_link).with_suffix('.py').name)
+        self.armor = lambda x: shlex.quote(str(x))
+
+    def open_in_browser(self):
+        subprocess.run([self.armor(s)
+            for s in (self.config.browser, self.daily_qn_link)])
+
+    def open_in_editor(self):
+        subprocess.run([self.armor(s)
+            for s in (self.config.editor, self.leet_file)])
+
+    def gen_leet_file(self):
+        if self.leet_file.exists(): return
+        self.leet_file.parent.mkdir(parents=True, exist_ok=True)
+        question = self.get_daily_question()
+        leet_file_content = self.config.template_file.read_text().format(
+            today=self.config.today.strftime('%Y-%m-%d'),
+            question=question,
+            daily_qn_link=self.daily_qn_link
+        )
+        self.leet_file.write_text(leet_file_content)
+
+    def get_daily_question(self) -> str | None:
+        res = httpx.get(self.daily_qn_link)
+        html = HTMLParser(res.text)
+        content = (html
+            .css_first('meta[name="description"]')
+            .attributes['content']
+        )
+        if content is None: return None
+
+        # getting only the question content
+        content_gen = itertools.takewhile(
+            lambda x: not x.startswith('Example'),
+            content.splitlines()
+        )
+
+        # wrapping the content to 79 characters
+        content_gen = (textwrap.fill(
+            d,
+            initial_indent='    ',
+            subsequent_indent='    ',
+            width=79,
+        ) for d in content_gen)
+        return '\n'.join(content_gen).strip()
+
+def get_daily_qn_link() -> str:
+    base_url = 'https://leetcode.com/graphql/'
+    query = {
+        'query': 'query questionOfToday {\n\tactiveDailyCodingChallengeQuestion {\n\t\tdate\n\t\tlink\n\t}\n}\n',
+        'operationName': 'questionOfToday'
+    }
+    res = httpx.post(base_url, json=query)
+    relative_url = (res
+        .raise_for_status()
+        .json()['data']['activeDailyCodingChallengeQuestion']['link']
+    )
+    return base_url.rstrip('/graphql/') + relative_url
+

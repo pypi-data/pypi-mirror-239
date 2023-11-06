23d0001bcbf08869e9bc6d4804e9bf9a07b071bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:18 +0000

    Add package shieldb/0.1.0/shieldb-0.1.0.tar.gz

---
+++ packages/shieldb/shieldb-0.1.0.tar.gz/shieldb-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shieldb',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'SQLAlchemy~=2.0.22',
+        'Flask~=3.0.0',
+        'psycopg2-binary~=2.9.1'
+    ],
+    data_files=[('', ['script/app.py', 'README.md', 'requirements.txt'])],
+    entry_points={
+        'console_scripts': [
+            'shieldb=script.app:main',
+        ],
+    },
+)

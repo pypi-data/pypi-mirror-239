817b16236623e8d52876649c8c1eee83ab649b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:04:32 +0000

    Add package seleniumGoogleLogin/0.0.3/seleniumGoogleLogin-0.0.3.tar.gz

---
+++ packages/seleniumGoogleLogin/seleniumGoogleLogin-0.0.3.tar.gz/seleniumGoogleLogin-0.0.3/setup.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+
+#from setuptools import setup
+from setuptools import setup, Extension
+# or
+#from distutils.core import setup  
+
+with open('README.md',encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+        name='seleniumGoogleLogin',   
+        version='0.0.3',   
+        description='auto login google account by selenium chrome driver ',#long_description=foruser,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='KuoYuan Li',  
+        author_email='funny4875@gmail.com',  
+        url='https://pypi.org/project/seleniumGoogleLogin',      
+        packages=['seleniumGoogleLogin'],   
+        include_package_data=True,
+        keywords = ['selenium', 'google auth' , 'chromedriver'],   # Keywords that define your package best
+          install_requires=[            
+          'selenium',
+          'webdriver_manager'
+          ],
+      classifiers=[
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',
+      ]
+)

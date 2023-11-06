d7fefc19d6a21a0986aa250cb3d59bd52d910bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:19 +0000

    Add package kadita/0.0.8/kadita-0.0.8.tar.gz

---
+++ packages/kadita/kadita-0.0.8.tar.gz/kadita-0.0.8/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("kadita/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().split("\n")
+
+setuptools.setup(
+    name="kadita",
+    version="0.0.8",
+    author="Firza Ichlasul Amal Ariansyah",
+    author_email="napija25@gmail.com",
+    description="Kadita Computer Vision",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify it's Markdown format
+    url="https://github.com/Kastara-Digital-Technology/KaditaCV",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": ["kadita = kadita.DeepFace:cli"],
+    },
+    python_requires=">=3.5.5",
+    install_requires=requirements,  # Include your dependencies from requirements.txt
+)

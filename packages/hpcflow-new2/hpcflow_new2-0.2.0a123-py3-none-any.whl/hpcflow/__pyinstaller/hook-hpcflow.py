2710a7dce8f8377c7128e10f26402ac02ad0d43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:32 +0000

    Add package hpcflow-new2/0.2.0a123/hpcflow_new2-0.2.0a123-py3-none-any.whl

---
+++ packages/hpcflow-new2/hpcflow_new2-0.2.0a123-py3-none-any.whl/hpcflow/__pyinstaller/hook-hpcflow.py
@@ -0,0 +1,35 @@
+from PyInstaller.utils.hooks import collect_data_files
+
+from hpcflow.sdk import sdk_classes
+
+
+# most of the modules in `sdk_classes` are imported on-demand via the app object:
+hiddenimports = list(sdk_classes.values())
+
+hiddenimports += [
+    "hpcflow.sdk.data",
+    "hpcflow.sdk.data.template_components",
+    "hpcflow.sdk.demo.scripts",
+    "hpcflow.sdk.demo.data",
+    "hpcflow.sdk.demo.workflows",
+    "hpcflow.sdk.core.test_utils",
+    "click.testing",
+    "requests",  # for GitHub fsspec file system
+    "fsspec.implementations.github",  # for GitHub fsspec file system
+]
+
+datas = collect_data_files("hpcflow.sdk.data")
+datas += collect_data_files("hpcflow.sdk.demo")
+datas += collect_data_files("hpcflow.sdk.demo.data")
+datas += collect_data_files("hpcflow.sdk.demo.workflows")
+datas += collect_data_files("hpcflow.sdk.data.template_components")
+datas += collect_data_files(
+    "hpcflow.tests",
+    include_py_files=True,
+    excludes=("**/__pycache__",),
+)
+datas += collect_data_files(
+    "hpcflow.sdk.demo.scripts",
+    include_py_files=True,
+    excludes=("**/__pycache__",),
+)

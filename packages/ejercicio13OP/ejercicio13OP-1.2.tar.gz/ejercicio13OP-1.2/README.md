4025cd97e7fc67ecf07464266d07d3f316479900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 16:04:13 +0000

    Add package ejercicio13OP/1.2/ejercicio13OP-1.2.tar.gz

---
+++ packages/ejercicio13OP/ejercicio13OP-1.2.tar.gz/ejercicio13OP-1.2/README.md
@@ -0,0 +1,31 @@
+# Librería matemática
+ 
+##Funcionalidades básicas
+ 
+###1. Método sumar(valor,valor)
+Devuelve la suma de los dos valores
+ 
+###2. Método restar(valor,valor)
+Devuelve la resta de los dos valores
+
+###3. Método dividir(valor,valor)
+Devuelve la division de los dos valores
+
+###4. Método multiplicacion(valor,valor)
+Devuelve la multiplicacion de los dos valores
+
+	
+###La forma de instalarlo es con:
+pip install ejercicio13
+ 
+###Ejemplos de importación en proyecto cliente:
+from ejercicio13.operaciones import suma, resta, multiplicacion, division
+ 
+###Ejemplo de uso en proyecto cliente:
+print(suma(2, 3)) 
+ 
+5
+ 
+print(resta(2, 3))
+ 
+-1
---
+++ packages/ejercicio13OP/ejercicio13OP-1.2.tar.gz/ejercicio13OP-1.2/Setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+    # Breve descripción de la librería
+    description = 'Ejercicio 13 Operaciones',
+
+    # Nombre del autor
+
+    author = 'Oscar Caraballero Galvez',
+
+    # Email del autor
+
+    author_email='xxx@gmail.com',
+    package='operaciones 2.0',
+    license='MIT',
+    version='1.2',
+
+    # Palabras claves de la librería
+    keywords = ['mates', 'suma', 'resta','multiplicacion','division'],
+
+    classifiers = [
+
+        'Development Status :: 3 - Alpha',
+         # Se define el público de la librería
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
+)

e79f4aac5aef4374d61ba30dcaad7662ddb49779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:07:41 +0000

    Add package graph-datasets/0.6.0/graph_datasets-0.6.0.tar.gz

---
+++ packages/graph-datasets/graph_datasets-0.6.0.tar.gz/graph_datasets-0.6.0/.ci/install-dev.sh
@@ -0,0 +1,21 @@
+rm -rf .venv
+
+# # install python 3.7.16 use pyenv
+# pyenv install 3.7.16
+# pyenv local 3.7.16
+
+# create and activate virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+# update pip
+# add a source if prefered: -i https://pypi.tuna.tsinghua.edu.cn/simple/
+python -m pip install -U pip
+
+# install requirements
+# add a source if prefered: -i https://pypi.tuna.tsinghua.edu.cn/simple/
+python -m pip install -r requirements-dev.txt
+
+pre-commit install
+
+echo install DEV requirements successfully!

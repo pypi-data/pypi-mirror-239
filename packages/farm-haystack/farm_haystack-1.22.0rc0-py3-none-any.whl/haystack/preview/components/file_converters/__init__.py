fdc8c0e26ab0cb2291074061fdf0ca60a50f4999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:33 +0000

    Add package farm-haystack/1.22.0rc0/farm_haystack-1.22.0rc0-py3-none-any.whl

---
+++ packages/farm-haystack/farm_haystack-1.22.0rc0-py3-none-any.whl/haystack/preview/components/file_converters/__init__.py
@@ -0,0 +1,4 @@
+from haystack.preview.components.file_converters.txt import TextFileToDocument
+from haystack.preview.components.file_converters.tika import TikaDocumentConverter
+
+__all__ = ["TextFileToDocument", "TikaDocumentConverter"]

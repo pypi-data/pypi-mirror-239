149193c40db0c63c2b65a4e7752d20e170b6224b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:35 +0000

    Add package beamshapy/0.1.7/beamshapy-0.1.7-py3-none-any.whl

---
+++ packages/beamshapy/beamshapy-0.1.7-py3-none-any.whl/beamshapy/intensity_generation/functions_intensity_profile.py
@@ -0,0 +1,26 @@
+from beamshapy.mask_generation.functions_masks_generation import *
+from beamshapy.spatial_profiles.functions_basic_shapes import *
+
+def fresnel_lens(GridPositionMatrix_X_out, GridPositionMatrix_Y_out, radius,parabola_coef,hyper_gauss_order):
+    """
+    Function to generate a Fresnel lens intensity profile
+
+    Args:
+        GridPositionMatrix_X_out (np.ndarray): X coordinates of the target grid
+        GridPositionMatrix_Y_out (np.ndarray): Y coordinates of the target grid
+        radius (float): Radius of the Fresnel lens (in m)
+        parabola_coef (float): Coefficient of the parabola profile (no units)
+        hyper_gauss_order (float): Order of the hyper-gaussian profile
+
+    Returns:
+        np.ndarray: Fresnel lens intensity profile
+    """
+
+
+    parabola = ParabolaMask(GridPositionMatrix_X_out, GridPositionMatrix_Y_out, parabola_coef)
+    wrap_parabola = parabola % 1
+
+    x_array_out = GridPositionMatrix_X_out[0,:]
+    fresnel_lens = wrap_parabola * supergaussian2D(x_array_out, hyper_gauss_order, radius)
+
+    return fresnel_lens

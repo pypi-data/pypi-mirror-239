2ef51f6d899e1b9eda9756cacc904415a491ee43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:06:38 +0000

    Add package libprg/3/libprg-3-py3-none-any.whl

---
+++ packages/libprg/libprg-3-py3-none-any.whl/prg/disk.py
@@ -0,0 +1,131 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C0112,C0115,C0116,W0105,R0903,E0402,C0209,R1710,C0413
+
+
+"storage"
+
+
+import datetime
+import inspect
+import os
+
+
+from .object import Object, cdir, read, write
+
+
+"defines"
+
+
+def  __dir__():
+    return (
+            'Storage',
+            'fetch',
+            'fqn',
+            'ident',
+            'strip',
+            'sync'
+           )
+
+
+"storage"
+
+
+class Storage:
+
+    classes = {}
+    wd = ""
+
+    @staticmethod
+    def add(clz) -> None:
+        if not clz:
+            return
+        name = str(clz).split()[1][1:-2]
+        Storage.classes[name] = clz
+
+    @staticmethod
+    def files() -> []:
+        return os.listdir(Storage.store())
+
+    @staticmethod
+    def long(name) -> str:
+        split = name.split(".")[-1].lower()
+        res = name
+        for named in Storage.classes:
+            if split in named.split(".")[-1].lower():
+                res = named
+                break
+        if "." not in res:
+            for fnm in Storage.files():
+                claz = fnm.split(".")[-1]
+                if fnm == claz.lower():
+                    res = fnm
+        return res
+
+    @staticmethod
+    def mods() -> str:
+        pth =  Storage.path("mods")
+        cdir(pth)
+        return pth
+
+    @staticmethod
+    def path(pth) -> str:
+        if not pth:
+            pth = ""
+        pth2 =  os.path.join(Storage.wd, pth)
+        cdir(pth2)
+        return pth2
+
+    @staticmethod
+    def store(pth="") -> str:
+        pth = os.path.join(Storage.wd, "store", pth)
+        pth2 = os.path.dirname(pth)
+        cdir(pth2)
+        return pth
+
+    @staticmethod
+    def scan(mod) -> None:
+        for key, clz in inspect.getmembers(mod, inspect.isclass):
+            if key.startswith("cb"):
+                continue
+            if not issubclass(clz, Object):
+                continue
+            Storage.add(clz)
+
+
+"utility"
+
+
+def strip(pth, nmr=3) -> str:
+    return os.sep.join(pth.split(os.sep)[-nmr:])
+
+
+"methods"
+
+
+def fqn(obj) -> str:
+    kin = str(type(obj)).split()[-1][1:-2]
+    if kin == "type":
+        kin = obj.__name__
+    return kin
+
+
+def fetch(obj, pth) -> None:
+    pth2 = Storage.store(pth)
+    read(obj, pth2)
+    return strip(pth)
+
+
+def ident(obj) -> str:
+    return os.path.join(
+                        fqn(obj),
+                        os.path.join(*str(datetime.datetime.now()).split())
+                       )
+
+
+def sync(obj, pth=None) -> str:
+    if pth is None:
+        pth = ident(obj)
+    pth2 = Storage.store(pth)
+    write(obj, pth2)
+    return pth
---
+++ packages/libprg/libprg-3-py3-none-any.whl/prg/mods/__init__.py
@@ -0,0 +1,8 @@
+# This file is placed in the Public Domain.
+#
+#
+
+"modules"
+
+
+from . import cmd, err, fnd, log, mbx, mod, shp, tdo, thr

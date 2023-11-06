fdfdbf997700e39b356c06089d3c71c9a75383eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:04:21 +0000

    Add package tableapple/0.1/tableapple-0.1.tar.gz

---
+++ packages/tableapple/tableapple-0.1.tar.gz/tableapple-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tableapple',
+    version='0.1',
+    description='Print and copy greetings based on user input',
+    author='Your Name',
+    packages=['tableapple'],
+    install_requires=['pyperclip'],
+)

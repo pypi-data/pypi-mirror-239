de08fe8f99aac2ceb8bcbb731ba0df531dbd8bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:05:04 +0000

    Add package tablemango23/0.2/tablemango23-0.2-py3-none-any.whl

---
+++ packages/tablemango23/tablemango23-0.2-py3-none-any.whl/tablemango/tablemango.py
@@ -0,0 +1,15 @@
+import pyperclip
+
+def generate_and_copy_text():
+    choice = input("Enter 1 or 2: ")
+    if choice == "1":
+        text = "hello1"
+    elif choice == "2":
+        text = "hello2"
+    else:
+        text = "Invalid choice"
+    
+    print(text)
+    pyperclip.copy(text)
+    print("Text copied to clipboard!")
+    generate_and_copy_text()

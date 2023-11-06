a493ca9183dd6af78e90570049f4f81f92d0847a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:07:20 +0000

    Add package meilisearch-fastapi/0.19.0/meilisearch_fastapi-0.19.0.tar.gz

---
+++ packages/meilisearch-fastapi/meilisearch_fastapi-0.19.0.tar.gz/meilisearch_fastapi-0.19.0/meilisearch_fastapi/models/search_parameters.py
@@ -0,0 +1,24 @@
+from typing import List, Optional, Union
+
+from camel_converter.pydantic_base import CamelBase
+
+
+class SearchParameters(CamelBase):
+    uid: str
+    query: Optional[str] = None
+    offset: int = 0
+    limit: int = 20
+    filter: Optional[Union[str, List[Union[str, List[str]]]]] = None
+    facets: Optional[List[str]] = None
+    attributes_to_retrieve: List[str] = ["*"]
+    attributes_to_crop: Optional[List[str]] = None
+    sort: Optional[List[str]] = None
+    crop_length: int = 200
+    attributes_to_highlight: Optional[List[str]] = None
+    show_matches_position: bool = False
+    highlight_pre_tag: str = "<em>"
+    highlight_post_tag: str = "</em>"
+    crop_marker: str = "..."
+    matching_strategy: str = "all"
+    hits_per_page: Optional[int] = None
+    page: Optional[int] = None
---
+++ packages/meilisearch-fastapi/meilisearch_fastapi-0.19.0.tar.gz/meilisearch_fastapi-0.19.0/meilisearch_fastapi/models/settings.py
@@ -0,0 +1,5 @@
+from meilisearch_python_sdk.models.settings import MeilisearchSettings
+
+
+class MeilisearchIndexSettings(MeilisearchSettings):
+    uid: str

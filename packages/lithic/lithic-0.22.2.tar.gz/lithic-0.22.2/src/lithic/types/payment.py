80055c9b02bf652b1dff0c312212189d4e7289c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:07:44 +0000

    Add package lithic/0.22.2/lithic-0.22.2.tar.gz

---
+++ packages/lithic/lithic-0.22.2.tar.gz/lithic-0.22.2/src/lithic/types/payment.py
@@ -0,0 +1,31 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+from .financial_transaction import FinancialTransaction
+
+__all__ = ["Payment", "PaymentMethodAttributes"]
+
+
+class PaymentMethodAttributes(BaseModel):
+    sec_code: Literal["PPD", "CCD", "WEB"]
+
+    retries: Optional[int] = None
+
+    return_reason_code: Optional[str] = None
+
+
+class Payment(FinancialTransaction):
+    direction: Literal["CREDIT", "DEBIT"]
+
+    method: Literal["ACH_NEXT_DAY", "ACH_SAME_DAY"]
+
+    method_attributes: PaymentMethodAttributes
+
+    source: Literal["LITHIC", "CUSTOMER"]
+
+    external_bank_account_token: Optional[str] = None
+
+    user_defined_id: Optional[str] = None

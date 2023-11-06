d765c5ecf135183f9a7380d9207ac29c50332064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:06:50 +0000

    Add package latentcor/0.2.5/latentcor-0.2.5-py2.py3-none-any.whl

---
+++ packages/latentcor/latentcor-0.2.5-py2.py3-none-any.whl/latentcor-0.2.5.dist-info/AUTHORS.rst
@@ -0,0 +1,14 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Mingze Huang, Christian L. Müller, Irina Gaynanova <mingzehuang@gmail.com, christian.mueller@stat.uni-muenchen.de, irinag@stat.tamu.edu>
+
+Contributors
+------------
+
+* Mingze Huang, Christian L. Müller, Irina Gaynanova <mingzehuang@gmail.com, christian.mueller@stat.uni-muenchen.de, irinag@stat.tamu.edu>
+
---
+++ packages/latentcor/latentcor-0.2.5-py2.py3-none-any.whl/latentcor/__init__.py
@@ -0,0 +1,15 @@
+"""
+Fast Computation of Latent Correlations for Mixed Data
+======================================================
+
+"""
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+from latentcor.latentcor import gen_data, get_tps, latentcor
+
+__author__ = 'Mingze Huang, Christian L. Müller, Irina Gaynanova'
+__email__ = 'mingzehuang@gmail.com, christian.mueller@stat.uni-muenchen.de, irinag@stat.tamu.edu'
+__version__ = '0.2.5'
+

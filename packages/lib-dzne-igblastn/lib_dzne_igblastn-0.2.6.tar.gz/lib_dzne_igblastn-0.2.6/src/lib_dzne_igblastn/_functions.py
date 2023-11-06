9f93418602d35b29f0bcd07acb96cebb1a099ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:05:35 +0000

    Add package lib-dzne-igblastn/0.2.6/lib_dzne_igblastn-0.2.6.tar.gz

---
+++ packages/lib-dzne-igblastn/lib_dzne_igblastn-0.2.6.tar.gz/lib_dzne_igblastn-0.2.6/src/lib_dzne_igblastn/_functions.py
@@ -0,0 +1,50 @@
+
+
+import contextlib as _contextlib
+import os as _os
+import shutil as _shutil
+import tempfile as _tempfile
+
+import lib_dzne_filedata as _fd
+
+import lib_dzne_igblastn._parsers as _prs
+
+
+def get_cline(
+    prog,
+    *, 
+    query, 
+    out, 
+    auxiliary_data,
+    germline_db_V,
+    germline_db_D,
+    germline_db_J,
+):
+    return [
+        prog,
+        '-auxiliary_data', auxiliary_data,
+        '-germline_db_V', germline_db_V,
+        '-germline_db_D', germline_db_D,
+        '-germline_db_J', germline_db_J,
+        '-domain_system', 'imgt',
+        '-num_alignments_V', '1',
+        '-num_alignments_J', '1',
+        '-num_alignments_D', '1',
+        '-outfmt', '7 std qseq sseq btop',
+        '-query', query, 
+        '-out', out,
+    ]
+
+
+def igdata(IGDATA, *, directory):
+    src = _os.path.join(IGDATA, 'internal_data')
+    dest = _os.path.join(directory, 'internal_data')
+    _shutil.copytree(src, dest)
+    _os.environ['IGDATA'] = directory
+
+def parse(text):
+    return _fd.TOMLData(_prs.Parser.parse_text(text))
+
+
+
+    

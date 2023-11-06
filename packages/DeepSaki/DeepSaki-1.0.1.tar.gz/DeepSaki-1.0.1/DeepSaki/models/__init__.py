3ed79e72989a76c8dd18c241ca8a287fc82810f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:04:35 +0000

    Add package DeepSaki/1.0.1/DeepSaki-1.0.1.tar.gz

---
+++ packages/DeepSaki/DeepSaki-1.0.1.tar.gz/DeepSaki-1.0.1/DeepSaki/models/__init__.py
@@ -0,0 +1,14 @@
+from DeepSaki.models.discriminators import LayoutContentDiscriminator
+from DeepSaki.models.discriminators import PatchDiscriminator
+from DeepSaki.models.discriminators import UNetDiscriminator
+
+from DeepSaki.models.autoencoders import UNet
+from DeepSaki.models.autoencoders import ResNet
+
+__all__ = [
+    "LayoutContentDiscriminator",
+    "PatchDiscriminator",
+    "UNetDiscriminator",
+    "UNet",
+    "ResNet",
+]

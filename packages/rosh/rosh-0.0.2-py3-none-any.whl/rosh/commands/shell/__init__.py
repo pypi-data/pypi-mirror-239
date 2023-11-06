87a8eb65688872660fcf4fcb6f346a69672db80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 05:03:28 +0000

    Add package rosh/0.0.2/rosh-0.0.2-py3-none-any.whl

---
+++ packages/rosh/rosh-0.0.2-py3-none-any.whl/rosh/commands/shell/__init__.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+from rosh.commands import RoshSystemCommand
+
+shell_exe = os.environ.get('SHELL', shutil.which('sh'))
+
+class RoshShellCommand(RoshSystemCommand):
+    description = 'launch a interactive system shell'
+
+    def __init__(self, rosh):
+        super().__init__(rosh, shell_exe)
+
+is_rosh_command = shell_exe is not None
+rosh_command = RoshShellCommand

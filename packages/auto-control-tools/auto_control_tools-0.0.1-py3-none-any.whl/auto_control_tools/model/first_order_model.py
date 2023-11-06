acc7d0d9efac7feed9c4164fb40b3da38245cd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:05:13 +0000

    Add package auto-control-tools/0.0.1/auto_control_tools-0.0.1-py3-none-any.whl

---
+++ packages/auto-control-tools/auto_control_tools-0.0.1-py3-none-any.whl/auto_control_tools/model/first_order_model.py
@@ -0,0 +1,141 @@
+from typing import Union
+
+import control
+import pandas as pd
+import sympy as sp
+
+from .model import Model
+
+
+class FirstOrderModel(Model):
+    """
+    Classe para modelos clássicos de primeira ordem com atraso.
+
+    Essa classe é voltada um :term:`Modelo` paramétrico da dinâmica de um processo comumente encontrado na indústria,
+    caracterizado pela seguinte :term:`Função de Transferência` :footcite:p:`CoelhoChapter4`:
+
+    .. math::
+        \\frac{K}{\\tau s + 1}e^{-\\theta s}
+
+    Onde:
+
+    * :math:`K` é o ganho do :term:`Sistema`, representando a amplificação do sinal.
+    * :math:`\\tau` é uma constante de tempo, que indica o quão rápido o :term:`Sistema` responde a uma mudança.
+    * :math:`\\theta` é um termo relacionado ao atraso ou tempo morto no :term:`Sistema`.
+
+    Essa forma específica da :term:`Função de Transferência` é comumente usada para representar sistemas de primeira ordem
+    com um atraso de tempo. Mas também pode ser usada para casos tem atraso, quando :math:`\\theta` for zero.
+
+    Sendo uma subclasse de :class:`Model`, essa classe adiciona suporte a definição de um modelo com os parâmetros
+    :paramref:`K` (:math:`K`), :paramref:`tau` (:math:`\\tau`) e :paramref:`theta` (:math:`\\theta`)
+    mas ainda mantém todas as funcionalidades da classe pai.
+
+    Parameters
+    ----------
+    K : float
+        Ganho do :term:`Sistema`.
+
+        O termo :math:`K`, se refere ao ganho do :term:`Sistema`, ele descreve a relação de amplificação entre a entrada
+        e a saída de um :term:`Sistema` dinâmico. É um parâmetro crucial que determina a escala da resposta do
+        :term:`Sistema` às mudanças na entrada.
+    tau : float
+        Constante de tempo de reação do :term:`Sistema`.
+
+        A constante de tempo :math:`\\tau` representa a velocidade com que o :term:`Sistema` atinge sua resposta
+        estacionária ou valor de regime após uma perturbação. Em sistemas de primeira ordem, :math:`\\tau` indica o
+        tempo necessário para que a  resposta atinja cerca de 63,2% de sua mudança total. Uma constante de tempo menor
+        denota uma resposta mais rápida.
+    theta : float
+        Termo de atraso do :term:`Sistema`.
+
+        O termo :math:`\\theta` está associado a um atraso de tempo no :term:`Sistema`. Ele representa o tempo adicional
+        que o :term:`Sistema` leva para responder a uma mudança na entrada, introduzindo uma componente de defasagem
+        temporal na resposta.
+    pade_degree : int, optional
+        Grau do denominador de aproximação de atraso.
+
+        Valor utilizado na aproximação do atributo :attr:`pade`, caso o :term:`Sistema` possua atraso
+        (:math:`\\theta` != 0). Quanto maior o grau mais próxima a aproximação, e consequentemente mais termos são
+        adicionados a aproximação.
+
+        Para mais detalhes sopre a aproximação do atraso verificar atributo :attr:`pade`.
+    source_data : pd.Series, optional
+        Conjunto de dados representando a variação da saida em relação ao tempo.
+        Deve um objeto do tipo :class:`pandas.Series` da bilbioteca
+        :mod:`pandas`, sendo os valores representativos da saida
+        e os valores de index representativos do tempo.
+
+        Essa classe não faz nenhuma análise desses dados por si só, porém eles ficam salvos, e podem ser utilizados
+        posteriormente, para plotagem de gráficos, por exemplo.
+
+    Attributes
+    ----------
+    pade : control.TransferFunction
+        :term:`Função de Transferência` representante do atraso do :term:`Sistema`.
+
+        Caso haja atraso no :term:`Sistema` (:math:`\\theta` != 0), é calculada a :term:`Função de Transferência` que
+        representa o termo de atraso:
+
+        .. math:: e^{-\\theta s}
+
+        A aproximação é feita através do método de Padé, utilizando a função :func:`control.pade` da biblioteca de
+        :mod:`control`, que resulta nos coeficientes de
+        numerador e denominador de uma função de trasnferência que aproxima o atraso desejado.
+
+        O parâmetro :paramref:`theta` representa o atraso de tempo, enquanto o parâmetro
+        :paramref:`pade_degree` representa o grau do denominador da :term:`Função de Transferência` utilizada para
+        a aproximação.
+
+    Notes
+    -----
+    O atributo :attr:`pade` é mantido em separado do atributo :attr:`tf`, visto que o aumento nos graus do numerador
+    e denominador interfere nos cálculos de alguns métodos aproximação de controlador.
+
+    Referencias:
+
+        .. footbibliography::
+
+    Examples
+    --------
+    Uso básico:
+
+    >>> model = act.FirstOrderModel(K=1, tau=2, theta=0.5)
+    >>> model.view.print_tf()
+
+    .. math::
+
+        \\frac{1}{2s + 1}e^{-0.5s}
+
+    Diferenças de :paramref:`pade_degree`:
+
+    >>> model = act.FirstOrderModel(K=1, tau=2, theta=0.5, pade_degree=2)
+    >>> model.view.plot_model_step_response_graph()
+
+    .. image:: ../image_resources/first_order_model_pade_deg_2.png
+
+
+    >>> model = act.FirstOrderModel(K=1, tau=2, theta=0.5, pade_degree=10)
+    >>> model.view.plot_model_step_response_graph()
+
+    .. image:: ../image_resources/first_order_model_pade_deg_10.png
+
+    """
+    def __init__(
+            self,
+            K: float,
+            tau: float,
+            theta: float = 0,
+            pade_degree: int = 5,
+            source_data: Union[pd.Series, None] = None,
+            step_signal: float = 1,
+    ):
+        super().__init__(([K], [tau, 1]), source_data=source_data, step_signal=step_signal)
+        self.K = K
+        self.tau = tau
+        self.theta = theta
+
+        if theta != 0 and pade_degree > 0:
+            self.pade = control.tf(*control.pade(theta, pade_degree))
+
+            s = sp.symbols('s')
+            self.tf_symbolic = self.tf_symbolic * sp.exp(-theta * s)
---
+++ packages/auto-control-tools/auto_control_tools-0.0.1-py3-none-any.whl/auto_control_tools/utils/data_input.py
@@ -0,0 +1,190 @@
+import os
+from typing import List, Union
+from copy import copy
+
+import pandas as pd
+
+
+class DataInputUtils:
+    """
+    Classe utilitária para a entrada de dados.
+
+    Aqui são implementados diversos métodos para criação e leitura de tabelas referentes a entrada dos dados utilizados
+    para :term:`Identificação` de plantas de sistemas de controle.
+
+    Attributes
+    ----------
+
+    default_table_name : str
+        Nome padrão para as tabelas criadas, default: ``'data_input'``
+    standard_fields : List[str]
+        Campos padrão para as tabelas criadas default: ``['time', 'input', 'output']``.
+    allowed_file_type : List[str]
+        Tipos de arquivos de tabela permitidos e suportados: ``['csv', 'xlsx']``.
+
+    """
+
+    default_table_name = 'data_input'  # update BaseModelIdentification.get_data_input_layout docstring if changed
+    standard_fields = ['time', 'input', 'output']
+    allowed_file_type = ['csv', 'xlsx']
+
+    @classmethod
+    def create_table_with_fields(
+            cls,
+            path: str,
+            fields: List[str],
+            table_name: str = default_table_name,
+            save_as: str = 'csv'
+    ) -> str:
+        """
+        Cria um arquivo contendo uma tabela vazia com os campos especificados.
+
+        Cria um arquivo (CSV ou Excel) contendo uma tabela vazia com os campos especificados.
+        O arquivo é salvo no caminho fornecido e seu nome é determinado pelo parâmetro :paramref:`table_name`.
+
+        Parameters
+        ----------
+        path : str
+            Caminho onde o arquivo será salvo.
+        fields : List[str]
+            Lista dos campos da tabela.
+        table_name : str, optional
+            Nome da tabela. O padrão é ``'data_input'``.
+        save_as : str, optional
+            Formato do arquivo a ser salvo (``'csv'`` ou ``'xlsx'``). O padrão é ``'csv'``.
+
+        Returns
+        -------
+        str
+            Caminho completo para o arquivo criado.
+
+        Raises
+        ------
+        ValueError
+            Se o formato de arquivo fornecido não estiver entre os permitidos.
+
+        Examples
+        --------
+        >>> file_path = act.DataInputUtils.create_table_with_fields('/path/to/save', ['time', 'input', 'output'])
+        """
+        if save_as not in cls.allowed_file_type:
+            raise ValueError(f'{save_as} is not an allowed file type')
+
+        file_path = os.path.join(path, f'{table_name}.{save_as}')
+        df = pd.DataFrame(columns=fields)
+
+        if save_as == 'csv':
+            df.to_csv(file_path, index=False)
+        elif save_as == 'xlsx':
+            df.to_excel(file_path, index=False)
+        else:
+            raise ValueError(f'{save_as} is not an allowed file type')
+
+        return file_path
+
+    @classmethod
+    def read_table_with_fields(cls, path: str, fields: Union[List[str], None] = None) -> pd.DataFrame:
+        """
+        Lê um arquivo contendo uma tabela e retorna um
+        :class:`pandas.DataFrame` com **apenas** os campos
+        especificados.
+
+        Lê um arquivo (CSV ou Excel) contendo uma tabela e retorna um
+        :class:`pandas.DataFrame` com apenas
+        os campos especificados.
+        O caminho do arquivo e os campos desejados são fornecidos como parâmetros.
+
+        Parameters
+        ----------
+        path : str
+            Caminho do arquivo a ser lido.
+        fields : List[str], optional
+            Lista dos campos desejados. Se None, utiliza os campos padrão da classe.
+            O padrão é ``['time', 'input', 'output']``.
+
+        Returns
+        -------
+        pandas.DataFrame
+            :class:`pandas.DataFrame`
+            contendo os dados da tabela e os campos especificados.
+
+        Raises
+        ------
+        ValueError
+            Se o formato de arquivo fornecido não estiver entre os permitidos.
+
+        Examples
+        --------
+        >>> df = act.DataInputUtils.read_table_with_fields('/path/to/table.xlsx', ['input', 'output'])
+        """
+        if fields is None:
+            fields = cls.standard_fields
+
+        if path.lower().endswith(".xlsx"):
+            df = pd.read_excel(path)
+        elif path.lower().endswith(".csv"):
+            df = pd.read_csv(path)
+        else:
+            raise ValueError(f'{os.path.splitext(path)[-1]} is not an allowed file type')
+
+        df = df[[f for f in fields if f in df.columns]]
+
+        return df
+
+    @classmethod
+    def expected_fields(cls, sample_time: Union[float, bool, None] = None,
+                        step_signal: Union[float, bool, None] = None) -> List[str]:
+        """
+        Retorna os campos esperados.
+
+        Retorna a lista :attr:`standard_fields` com exeção dos campos :paramref:`sample_time` ou
+        :paramref:`step_signal` caso sejam
+        informados nos parâmetros.
+        """
+        fields = copy(cls.standard_fields)
+
+        if sample_time is not None:
+            fields.remove('time')
+
+        if step_signal is not None:
+            fields.remove('input')
+
+        return fields
+
+    @classmethod
+    def get_model_data_default(cls, path: str, sample_time: Union[None, float] = None,
+                               step_signal: Union[float, None] = None) -> pd.DataFrame:
+        """
+        Salva planilha de leiaute para preenchimento.
+
+        Obtém os campos esperados, lê os dados do arquivo indicado através de
+        :meth:`DataInputUtils.read_table_with_fields` verifica se não existem campos faltantes
+        e retorna o pandas.DataFrame resultante.
+
+        Parameters
+        ----------
+        path : str
+            Caminho até o arquivo a ser lido. O leiaute pode ser obtido através de
+            :meth:`~BaseModelIdentification.get_data_input_layout`.
+
+        sample_time : float, optional
+            Valor do invervalo de amostragem. Caso informado, o intervalo de amostragem é considerado constante e
+            igual ao valor fornecido.
+
+        step_signal : float, optional
+            Valor do sinal degrau de entrada. Se informado é considerado que o sinal está ativo em todos os momentos
+            nos dados recebidos.
+
+        Returns
+        -------
+        pandas.DataFrame
+            os dados do arquivo recebido e as colunas esperadas.
+        """
+        expected_fields = cls.expected_fields(sample_time, step_signal)
+        df = cls.read_table_with_fields(path, expected_fields)
+
+        if any(f not in df.columns for f in expected_fields):
+            missing_fields = [f for f in expected_fields if f not in df.columns]
+            raise ValueError(f'The fields {missing_fields} are required and were informed in the input data')
+
+        return df

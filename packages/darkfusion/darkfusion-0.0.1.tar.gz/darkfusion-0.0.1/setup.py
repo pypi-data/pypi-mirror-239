4bc8271ce3d0f03d387d485e222a5904aab69fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:06:46 +0000

    Add package darkfusion/0.0.1/darkfusion-0.0.1.tar.gz

---
+++ packages/darkfusion/darkfusion-0.0.1.tar.gz/darkfusion-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='darkfusion',
+    version='0.0.1',
+    description='A placeholder for PyPI package name reservation.',
+    long_description='This is a placeholder package for PyPI name reservation.',
+    author='LORD OF KILLZ',
+    author_email='reachmorrison@gmail.com',
+    url='https://github.com/yourusername/unique_package_name',
+    packages=[],
+)

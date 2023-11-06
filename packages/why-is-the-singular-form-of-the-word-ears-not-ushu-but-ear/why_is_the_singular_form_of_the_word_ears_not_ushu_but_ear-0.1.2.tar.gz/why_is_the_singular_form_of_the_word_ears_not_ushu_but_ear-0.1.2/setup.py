8dbf2c6c6375d0b45e2afd186fbb899807617c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 16:04:14 +0000

    Add package why-is-the-singular-form-of-the-word-ears-not-ushu-but-ear/0.1.2/why_is_the_singular_form_of_the_word_ears_not_ushu_but_ear-0.1.2.tar.gz

---
+++ packages/why-is-the-singular-form-of-the-word-ears-not-ushu-but-ear/why_is_the_singular_form_of_the_word_ears_not_ushu_but_ear-0.1.2.tar.gz/why_is_the_singular_form_of_the_word_ears_not_ushu_but_ear-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+def readme():
+    return "I don't know"
+
+setup(
+    name='why_is_the_singular_form_of_the_word_ears_not_ushu_but_ear',
+    version='0.1.2',
+    author='Nodus',
+    description='Mem modul',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='',
+    project_urls={},
+    python_requires='>=3.11'
+)

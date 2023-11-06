0db8676728811f3c24fa18fe6ea599a107b2e8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:03:56 +0000

    Add package Flask-Admix/1.6.1.1/Flask-Admix-1.6.1.1.tar.gz

---
+++ packages/Flask-Admix/Flask-Admix-1.6.1.1.tar.gz/Flask-Admix-1.6.1.1/setup.py
@@ -0,0 +1,88 @@
+# Fix for older setuptools
+import re
+import os
+
+from setuptools import setup, find_packages
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+def desc():
+    info = read('README.rst')
+    try:
+        return info + '\n\n' + read('doc/changelog.rst')
+    except IOError:
+        return info
+
+
+# grep flask_admin/__init__.py since python 3.x cannot import it before using 2to3
+file_text = read(fpath('flask_admin/__init__.py'))
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+extras_require = {
+    'aws': ['boto'],
+    'azure': ['azure-storage-blob']
+}
+
+
+install_requires = [
+    'Flask>=1.1.1',
+    'wtforms>=3.1.0'
+]
+
+
+setup(
+    name='Flask-Admix',
+    version=grep('__version__'),
+    url='https://github.com/flask-admix/flask-admix/',
+    license='BSD',
+    python_requires='>=3.8',
+    author=grep('__author__'),
+    description='Simple and extensible admin interface framework for Flask',
+    long_description=desc(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    extras_require=extras_require,
+    install_requires=install_requires,
+    tests_require=[
+        'pytest',
+        'pillow>=9.1.0',
+        'pymongo',
+        'wtf-peewee',
+        'sqlalchemy',
+        'flask-sqlalchemy',
+        'flask-babelex',
+        'shapely',
+        'geoalchemy2',
+        'psycopg2',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    test_suite='flask_admin.tests'
+)

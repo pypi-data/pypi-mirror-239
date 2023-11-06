fca267ce60867a9309539a5afc58b07ae934bed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:06:05 +0000

    Add package kernelmch/0.1.1/kernelmch-0.1.1-py3-none-any.whl

---
+++ packages/kernelmch/kernelmch-0.1.1-py3-none-any.whl/kernelmch/__init__.py
@@ -0,0 +1,7 @@
+from .modulo import KernelGaussiano
+
+__version__ = "0.1.1"     # Puedes definir la versión de tu librería aquí
+
+__author__  = "M Luciano"  # Puedes especificar el autor de la librería
+
+__all__     = ["KernelGaussiano"]

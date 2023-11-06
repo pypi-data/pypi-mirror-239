1c6063311930310186258c7d9fa0aa589168b458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:06:41 +0000

    Add package django-super-inlines/0.2.2/django-super-inlines-0.2.2.tar.gz

---
+++ packages/django-super-inlines/django-super-inlines-0.2.2.tar.gz/django-super-inlines-0.2.2/super_inlines/templatetags/super_inlines.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+from django.template import Library
+
+from ..admin import SuperInlineModelAdmin
+
+
+register = Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_sub_inline_formsets(context, inline, original, index, is_template):
+    if not isinstance(inline, SuperInlineModelAdmin):
+        return ()
+    request = context['request']
+    formsets, inline_instances = inline._create_formsets(
+        request, obj=original, change=original is not None, index=index,
+        is_template=is_template)
+    return inline.get_inline_formsets(request, formsets, inline_instances,
+                                      obj=original)

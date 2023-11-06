39e508423e507256401d3ea3ae492728835d1cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 05:03:31 +0000

    Add package microservice-template/0.0.5/microservice_template-0.0.5.tar.gz

---
+++ packages/microservice-template/microservice_template-0.0.5.tar.gz/microservice_template-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Microservicetemplate'
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="microservice_template",
+    version=VERSION,
+    author="Pixelfehler",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['flask', 'flask-cors'],
+    keywords=['python', 'microservice'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

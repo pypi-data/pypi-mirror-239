f1418c2b771868b1acd0b5ad3a06e549904c57e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:05:12 +0000

    Add package fruitgenerator/0.1/fruitgenerator-0.1.tar.gz

---
+++ packages/fruitgenerator/fruitgenerator-0.1.tar.gz/fruitgenerator-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='fruitgenerator',
+    version='0.1',
+    description='Generate fruit names based on user input',
+    author='Your Name',
+    packages=['fruitgenerator'],
+)

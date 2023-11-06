8be9f4b0ecac58c38afb2b9d98f0067a710372df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:08:23 +0000

    Add package AutoOpenApi/1.0.0/AutoOpenApi-1.0.0.tar.gz

---
+++ packages/AutoOpenApi/AutoOpenApi-1.0.0.tar.gz/AutoOpenApi-1.0.0/README.md
@@ -0,0 +1,18 @@
+# AutoOpenApi
+
+make change
+
+Update version in setup.cfg
+
+
+# WINDOWS
+py -m build : build command
+
+py -m twine upload --repository pypi dist/*
+
+# UBUNTU
+
+python3 -m build
+
+python3 -m twine upload --repository pypi dist/*
+
---
+++ packages/AutoOpenApi/AutoOpenApi-1.0.0.tar.gz/AutoOpenApi-1.0.0/tests/test_schema.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestSchema(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass

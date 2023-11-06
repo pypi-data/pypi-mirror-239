36578177bbc214950282a29c7221c91872cf352d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 05:03:30 +0000

    Add package console-games/0.1.22/console_games-0.1.22-py3-none-any.whl

---
+++ packages/console-games/console_games-0.1.22-py3-none-any.whl/games/number_crush/__init__.py
@@ -0,0 +1,19 @@
+from games.controller import Controller
+from games.objects import One, Two, Plus, Stickman, ScreenObjectGroup, Player
+from games.number_crush.scenes import Crush, Intro
+
+
+class NumberCrush(Controller):
+    name = "Number Crush"
+
+    def init(self):
+        self.scenes = [Intro, Crush]
+        self.logo = ScreenObjectGroup(0, 0, objects=[
+            One(0, 0),
+            Plus(0, 0),
+            Two(0, 0)
+        ])
+        self.player = Player('Jon',
+                             Stickman(int(self.screen.width / 2) + 1, self.screen.height - 2),
+                             controller=self,
+                             score_title='Crushed')

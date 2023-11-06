4421b729b2353870a9ac59e75a950518879ab046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:04:15 +0000

    Add package apsisocr/0.0.3/apsisocr-0.0.3-py3-none-any.whl

---
+++ packages/apsisocr/apsisocr-0.0.3-py3-none-any.whl/apsisocr/__init__.py
@@ -0,0 +1,5 @@
+#-*- coding: utf-8 -*-
+import warnings
+warnings.filterwarnings('ignore')
+from .ocr import ApsisOCR,ApsisNet,SVTRLCNet,PaddleDBNet,DenseNet121BnEnClassifier
+from .base import BaseOCR 

c69c8b8ec41d734a5b116fd6f2ba101c19174136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:08:21 +0000

    Add package python-upbit-api/1.2.0/python_upbit_api-1.2.0-py3-none-any.whl

---
+++ packages/python-upbit-api/python_upbit_api-1.2.0-py3-none-any.whl/upbit/__init__.py
@@ -0,0 +1,33 @@
+# A library that provides a Python interface to the Upbit API
+# Copyright 2023
+# 이지혜 Lee Jihye <ghe.lee19@gmail.com>
+"""A library that provides a Python interface to the Upbit API"""
+
+__all__ = [
+]
+
+from .upbit import Upbit
+
+from .exceptions import (
+    UpbitHTTPError,
+    UpbitClientError,
+    UpbitServerError,
+    TooManyRequests,
+    CreateAskError,
+    CreateBidError,
+    InsufficientFundsAsk,
+    InsufficientFundsBid,
+    UnderMinTotalAsk,
+    UnderMinTotalBid,
+    WithdrawAddressNotRegistered,
+    InvalidParameterError,
+    ValidationError,
+    InvalidQueryPayload,
+    JwtVerification,
+    ExpiredAccessKey,
+    NonceUsed,
+    NoAuthorizationIP,
+    OutOfScope,
+    ApiKeyError,
+    InvalidRemainingReq,
+)

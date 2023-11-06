71474a4a182f8ded9eb7bce37e0b06710a415315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:08:01 +0000

    Add package opencopilot-db/0.1.0/opencopilot_db-0.1.0.tar.gz

---
+++ packages/opencopilot-db/opencopilot_db-0.1.0.tar.gz/opencopilot_db-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="opencopilot_db",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "sqlalchemy",
+        "pymysql"
+    ],
+)

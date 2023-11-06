dc4f30644b2d9c21bf9360081f9a1a4d62e05ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 14:07:41 +0000

    Add package precinct/0.1.0/precinct-0.1.0.tar.gz

---
+++ packages/precinct/precinct-0.1.0.tar.gz/precinct-0.1.0/README.md
@@ -0,0 +1,157 @@
+# Precinct
+
+## About
+
+Precinct is a SQL query LLM copilot that helps analyze your queries, pick up indices and other optimizations and provide other actionable feedback. Currently only Postgres is supported.
+
+## Features
+
+- Input from a file or plain text.
+- Checks for relevant tables.
+- Retrieves table indices.
+- Runs `EXPLAIN ANALYZE` for query diagnosis.
+- Offers concise, actionable feedback.
+
+See [Usage](#usage) for more details.
+
+## Setup
+
+### Requirements
+
+- Python >=3.7
+- PostgreSQL (More databases support coming soon)
+
+### Installation
+
+```bash
+pip install precinct
+```
+
+For developers, you can install locally in edit mode with optional extras:
+
+```bash
+pip install -e .[dev]
+```
+
+## Usage
+
+Here is the `--help` output:
+
+```bash
+Usage: precinct [OPTIONS] QUERY
+
+  Precinct: A SQL query LLM copilot for analyzing queries, suggesting indices,
+  and providing optimizations. Currently supports PostgreSQL.
+
+  Examples:
+
+      precinct "SELECT * FROM table;"
+
+      precinct "path/to/your/file.sql"
+
+Options:
+  --uri TEXT                     PostgreSQL connection URI, ie. 'postgresql://
+                                 username:password@host:port/database'.
+                                 Mutually exclusive with --service.
+  --service TEXT                 PostgreSQL service name as located in
+                                 ~/.pg_service.conf or at specified path.
+                                 Mutually exclusive with --uri.
+  --service-file PATH            Path to PGSERVICEFILE. Optionally provide in
+                                 conjunction with --service.
+  --model [gpt-4|gpt-3.5-turbo]  Model to use.
+  --rows INTEGER                 Number of rows to return from query at most.
+                                 Typically used for previewing query results.
+  --json                         Enable VSCode optimized JSON I/O mode.
+  --help                         Show this message and exit.
+```
+
+### Getting started
+
+To analyze a query from a file:
+
+```bash
+python precinct.py "path/to/your/file.sql"
+```
+
+To analyze a plaintext query:
+
+```bash
+python precinct.py "SELECT * FROM table;"
+```
+
+### Specify intent
+
+Precinct will then analyze your query and extract an intent, which you can either accept or clarify. For example:
+
+```bash
+Query: select * from businesses
+Intent: The intent of the query is simply to retrieve all data from all columns in the 'businesses' table.
+Issue clarification to intent (y) to proceed, or (q) to quit (y/q/[intent]): y
+```
+
+To specify another intent, just type it in and press enter:
+
+```bash
+Issue clarification to intent (y) to proceed, or (q) to quit (y/q/[intent]): I want to find all businesses in the 'restaurants' category.
+```
+
+### Optimize
+
+Precinct will then analyze your query and provide feedback on how to optimize it. For example:
+
+```bash
+Optimized query: select * from businesses limit 10
+Explanation: The current query is already highly efficient, retrieving only 10 rows of data in a matter of milliseconds. Since the 'LIMIT' clause restricts the number of records returned, the query ensures minimal data transfer from the database to the application. Optimization refinements like adding WHERE clause or ordering of data will not make significant improvements in this specific scenario. Indices won't have an impact as the data fetched is minimal and not filtered or sorted. Therefore, optimizing this query is not necessary.
+Run query now with `r`, copy to clipboard with `c`, or cancel with `q` (r/c/q): c
+```
+
+From here, you can run the query and preview some rows (the `--rows` parameter can be used to specify the max number to preview, although there is only rudimentary support for this at the moment). You can also copy the query to your clipboard and paste it into your editor.
+
+## Authentication
+
+For database authentication, Precinct supports the following methods:
+
+- [Connection Service File](#connection-service-file)
+- [Connection URI](#connection-uri)
+- [Environment Variables](#environment-variables)
+
+### Connection Service File
+
+PostgreSQL's Connection Service File is the recommended method. It allows for secure and easily configurable database connections. To use it, create a `.pg_service.conf` file in your home directory or specify its location using the `PGSERVICEFILE` environment variable or via the `--service-file` command line argument.
+
+In this file, you can define your database connection parameters like so:
+
+```ini
+[my_service]
+dbname=mydb
+user=myuser
+password=mypassword
+host=localhost
+port=5432
+```
+
+You can then connect to the database using the service name:
+
+```bash
+precinct --service my_service "SELECT * FROM table;"
+```
+
+### Connection URI
+
+You can also connect using a connection URI. This is a commonly used method among database clients. To connect using a URI, use the `--uri` command line argument:
+
+```bash
+precinct --uri "postgresql://myuser:mypassword@localhost:5432/mydb" "SELECT * FROM table;"
+```
+
+### Environment Variables
+
+Finally, you can connect using environment variables. To connect using environment variables, just set them and Precinct will default if a service file or URI is not available.
+
+```bash
+precinct "SELECT * FROM table;"
+```
+
+## License
+
+GPL-3.0-or-later
---
+++ packages/precinct/precinct-0.1.0.tar.gz/precinct-0.1.0/precinct/models.py
@@ -0,0 +1,374 @@
+import yaml
+import psycopg2
+import psycopg2.sql
+import psycopg2.extensions
+import sqlparse
+import sqlparse.sql
+import openai
+import os
+import instructor
+
+from enum import Enum
+from typing import List, Dict, Set, Optional, Tuple, Union
+
+from pydantic import BaseModel
+from cachetools import cached, LRUCache
+from cachetools.keys import hashkey
+
+from precinct.logging import get_logger
+
+logger = get_logger()
+
+# Support openai function calling with pydantic base model
+instructor.patch()
+
+
+class GPTModel(str, Enum):
+    GPT_4 = "gpt-4"
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
+
+
+def load_prompts(
+    file_path: Optional[str] = None,
+) -> Dict[str, Union[str, Dict[str, str]]]:
+    """Load prompts from prompts.yml file.
+
+    Args:
+        file_path: The path to the prompts.yml file.
+    Returns:
+        Dict[str, Union[str, Dict[str, str]]]: The prompts.
+    """
+    if file_path is None:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, "prompts.yml")
+
+    with open(file_path, "r") as prompts_file:
+        return yaml.safe_load(prompts_file)
+
+
+PROMPTS = load_prompts()
+
+
+class ExplainQueryInput(BaseModel):
+    """Input to the explain query prompt.
+
+    Attributes:
+        query: The SQL query as provided.
+        clarification: Optional clarification on the nature or functionality of the query.
+    """
+
+    query: str
+    clarification: Optional[str]
+
+    def __str__(self) -> str:
+        base = f"<query>{self.query}</query>"
+        if self.clarification:
+            return base + f"\n<clarification>{self.clarification}</clarification>"
+        return base
+
+
+class ExplainQueryOutput(BaseModel):
+    """Output from the explain query prompt, in natural language.
+
+    Attributes:
+        structure: Comments on the structure of the query
+        intent: The intent of the query explained succinctly
+    """
+
+    structure: str
+    intent: str
+
+
+class OptimizeQueryOutput(BaseModel):
+    """Output from the optimize query prompt.
+
+    Attributes:
+        query: The optimized SQL query.
+        explanation: Comments on the newly optimized query
+    """
+
+    query: str
+    explanation: str
+
+
+class OptimizeQueryInput(BaseModel):
+    """Input to the optimize query prompt.
+
+    Attributes:
+        query: The SQL query as provided.
+        intent: The intent of the query.
+        indices: The indices on the tables referenced by the query.
+        columns: The properties of the columns referenced by the query.
+        analyze: The output of the EXPLAIN ANALYZE on the query.
+    """
+
+    query: str
+    intent: str
+    indices: Dict[str, Dict[str, str]]
+    columns: Dict[str, Dict[str, str]]
+    analyze: List[str]
+
+    def __str__(self) -> str:
+        return f"<query>{self.query}</query>\n<intent>{self.intent}</intent>\n<indices>{self.indices}</indices>\n<columns>{self.columns}</columns>\n<analyze>{self.analyze}</analyze>"
+
+
+class PrecinctTable:
+    """Represents a table referenced by a query and related information for the optimization process.
+
+    Attributes:
+        table_name: The name of the table.
+        conn: The connection to the database.
+        indices: The indices on the table.
+        column_properties: The properties of the columns in the table.
+    """
+
+    def __init__(self, table_name: str, conn: psycopg2.extensions.connection) -> None:
+        self.table_name = table_name
+        self.conn = conn
+
+        logger.info(f"Fetching indices for table {self.table_name}")
+        self.indices = self.fetch_indices()
+
+        logger.info(f"Fetching columns for table {self.table_name}")
+        self.column_properties = self.fetch_columns()
+
+    def fetch_indices(self) -> Dict[str, str]:
+        """Fetch indices on the table from the database.
+
+        Returns:
+            A dictionary of index names and definitions.
+        """
+        query = """
+        SELECT
+            indexname AS index_name,
+            indexdef AS index_definition
+        FROM pg_indexes
+        WHERE tablename = %s;
+        """
+
+        indices: Dict[str, str] = {}
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(query, (self.table_name,))
+                for index in cursor.fetchall():
+                    indices[index[0]] = index[1]
+            except Exception as e:
+                logger.error(f"Error fetching indices: {str(e)}")
+                raise
+
+        logger.debug(f"Indices: {indices}")
+        return indices
+
+    def fetch_columns(self) -> Dict[str, str]:
+        """Fetch column names and types from database.
+
+        Returns:
+            A list of dictionaries keyed by column name whose values are the type.
+        """
+        query = """
+        SELECT column_name, data_type 
+        FROM information_schema.columns 
+        WHERE table_name = %s;
+        """
+        columns = {}
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(query, (self.table_name,))
+                for col in cursor.fetchall():
+                    columns[col[0]] = col[1]
+            except Exception as e:
+                logger.error(f"Error fetching columns: {str(e)}")
+                raise
+
+        logger.debug(f"Columns: {columns}")
+        return columns
+
+
+class PrecinctQuery:
+    """Represents a query and related information for the optimization process.
+
+    Attributes:
+        query_str: The SQL query string.
+        analysis: The output of the EXPLAIN ANALYZE on the query.
+        tables: The tables referenced by the query.
+    """
+
+    def __init__(
+        self, query_str: str, conn: psycopg2.extensions.connection, model: GPTModel
+    ) -> None:
+        self.query_str = query_str
+        self.conn = conn
+        self.model = model
+
+        if not self.validate():
+            logger.error("Invalid query.")
+            raise ValueError("Invalid query.")
+
+        self.analysis = self.execute_query_analysis()
+        self.tables = self.get_tables(query_str)
+
+    def validate(self) -> bool:
+        """
+        Validate query by attempting to prepare it.
+
+        Returns:
+            True if query is valid, False otherwise.
+        """
+        parsed = sqlparse.parse(self.query_str)
+        if not parsed:
+            logger.warning("Failed to parse query.")
+            return False
+
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(
+                    psycopg2.sql.SQL("PREPARE stmt AS {0}").format(
+                        psycopg2.sql.SQL(self.query_str)
+                    )
+                )
+                cursor.execute("DEALLOCATE stmt")
+            except psycopg2.Error as e:
+                logger.error(f"Failed to prepare statement: {str(e)}")
+                return False
+        return True
+
+    def get_tables(self, query: str) -> List[PrecinctTable]:
+        """Parse SQL query and extract referenced table names.
+
+        Args:
+            query: The SQL query string.
+        Returns:
+            A list of PrecinctTable objects, representing the tables referenced by the query.
+        """
+        table_names = self.extract_table_names(query)
+        return [self.get_table(table_name) for table_name in table_names]
+
+    @cached(
+        cache=LRUCache(maxsize=128),
+        key=lambda self, table_name: hashkey(table_name),
+    )
+    def get_table(self, table_name: str) -> PrecinctTable:
+        """Fetch table information from database.
+
+        Caches the table information to avoid repeated queries.
+        Args:
+            table_name: The name of the table.
+        Returns:
+            A PrecinctTable object, representing the table referenced by the query.
+        """
+        return PrecinctTable(table_name, self.conn)
+
+    def extract_table_names(self, query: str) -> Set[str]:
+        """Parse SQL query and extract referenced table names.
+
+        Args:
+            query: The SQL query string.
+        Returns:
+            A set of table names referenced by the query.
+        """
+        table_names = set()
+
+        parsed = sqlparse.parse(query)
+        for statement in parsed:
+            for token in statement.tokens:
+                if isinstance(
+                    token, (sqlparse.sql.Identifier, sqlparse.sql.IdentifierList)
+                ):
+                    if isinstance(token, sqlparse.sql.Identifier):
+                        table_names.add(token.get_real_name())
+                    else:  # token is IdentifierList
+                        for identifier in token.get_identifiers():
+                            table_names.add(identifier.get_real_name())
+
+        logger.info(f"Extracted table names: {table_names}")
+        return table_names
+
+    def execute_query_analysis(self) -> List[str]:
+        """
+        Run an EXPLAIN ANALYZE on the query and return the output.
+
+        Returns:
+            A list of analysis output strings.
+        """
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(f"EXPLAIN ANALYZE {self.query_str}")
+                return [plan[0] for plan in cursor.fetchall()]
+            except psycopg2.Error as e:
+                logger.error(f"Failed to execute query analysis: {str(e)}")
+                return []
+
+    def get_query_summary(
+        self, clarification: Optional[str] = None
+    ) -> ExplainQueryOutput:
+        """
+        Get a natural language summary of the query and its intent.
+
+        Args:
+            clarification: Optional clarification on the nature or functionality of the query.
+        Returns:
+            An ExplainQueryOutput object.
+        """
+        try:
+            query_input = ExplainQueryInput(
+                query=self.query_str, clarification=clarification
+            )
+            query_out = openai.ChatCompletion.create(
+                model=self.model,
+                response_model=ExplainQueryOutput,
+                messages=[
+                    {"role": "system", "content": PROMPTS["explain"]},
+                    {"role": "user", "content": str(query_input)},
+                ],
+            )
+
+            if not isinstance(query_out, ExplainQueryOutput):
+                raise ValueError("Failed to get query summary.")
+
+            return query_out
+        except Exception as e:
+            logger.error(f"Failed to get query summary: {str(e)}")
+            raise e
+
+    def get_optimized_query(
+        self,
+        prior_clarification: Optional[str] = None,
+    ) -> Tuple["PrecinctQuery", str]:
+        """
+        Get an optimized query based on the original query and related information
+
+        Args:
+            prior_clarification: Optional clarification on the nature or functionality of the query.
+        Returns:
+            A tuple of the optimized PrecinctQuery object and the explanation of the optimization.
+        """
+        try:
+            query_input = OptimizeQueryInput(
+                query=self.query_str,
+                intent=self.get_query_summary(prior_clarification).intent,
+                indices={table.table_name: table.indices for table in self.tables},
+                columns={
+                    table.table_name: table.column_properties for table in self.tables
+                },
+                analyze=self.analysis,
+            )
+
+            optimized_query_out = openai.ChatCompletion.create(
+                model=self.model,
+                response_model=OptimizeQueryOutput,
+                messages=[
+                    {"role": "system", "content": PROMPTS["optimize"]},
+                    {"role": "user", "content": str(query_input)},
+                ],
+            )
+            if not isinstance(optimized_query_out, OptimizeQueryOutput):
+                raise ValueError("Failed to optimize query.")
+
+            optimized_query = PrecinctQuery(
+                optimized_query_out.query, conn=self.conn, model=self.model
+            )
+            return optimized_query, optimized_query_out.explanation
+
+        except Exception as e:
+            logger.error(f"Failed to get query summary: {str(e)}")
+            raise e

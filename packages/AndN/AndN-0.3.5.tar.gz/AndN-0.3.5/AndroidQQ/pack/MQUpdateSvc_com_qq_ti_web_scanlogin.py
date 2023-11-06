b243c078784015b49cf3c7ef926437712f4f5939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:05:37 +0000

    Add package AndN/0.3.5/AndN-0.3.5.tar.gz

---
+++ packages/AndN/AndN-0.3.5.tar.gz/AndN-0.3.5/AndroidQQ/pack/MQUpdateSvc_com_qq_ti_web_scanlogin.py
@@ -0,0 +1,24 @@
+from AndroidQQ.im.oidb.scanlogin import ScanLoginReq, ScanLoginRsp
+from AndroidQQ.package import PackHeadNoToken, Pack_
+
+
+def web_scanlogin(info, service_type: int, token: str):
+    """
+        uint32_service_type int 4 扫码 5 确认登录
+        bytes_token str
+
+    """
+    scan_login_req = ScanLoginReq(
+        service_type=service_type,
+        token=token.encode('utf-8')
+    )
+    buffer = scan_login_req.SerializeToString()
+    buffer = PackHeadNoToken(info, buffer, 'MQUpdateSvc_com_qq_ti.web.scanlogin')
+    buffer = Pack_(info, buffer, Types=11, encryption=1, sso_seq=info.seq)
+    return buffer
+
+
+def web_scanlogin_req(buffer):
+    scan_login_rsp = ScanLoginRsp()
+    scan_login_rsp.ParseFromString(buffer)
+    return scan_login_rsp.field4.decode()

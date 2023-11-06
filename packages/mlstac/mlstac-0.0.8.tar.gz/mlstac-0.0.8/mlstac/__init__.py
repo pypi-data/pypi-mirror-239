e3966872cb9908f79a0257dce1fe06fda8164d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 03:04:14 +0000

    Add package mlstac/0.0.8/mlstac-0.0.8.tar.gz

---
+++ packages/mlstac/mlstac-0.0.8.tar.gz/mlstac-0.0.8/mlstac/__init__.py
@@ -0,0 +1,9 @@
+from mlstac.sample.datamodel import Sample, SampleMetadata, SampleTensor
+from mlstac.collection.datamodel import Collection, Provider, Providers, Split
+from mlstac.api.main import load, download
+from mlstac.api.datasets import LocalDataset, StreamDataset
+
+from mlstac.api.nest_asyncio import apply as nest_asyncio_apply
+
+# Patch asyncio to make its event loop reentrant.
+nest_asyncio_apply()

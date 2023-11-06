ac4640feeb1bf8c370b057b80448b93e3c4b597a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:35 +0000

    Add package Agently/3.0.0a7/Agently-3.0.0a7.tar.gz

---
+++ packages/Agently/Agently-3.0.0a7.tar.gz/Agently-3.0.0a7/Agently/__init__.py
@@ -0,0 +1,11 @@
+from .Request import Request
+from .Agent import AgentFactory
+from .Facility import FacilityManager
+from .WebSocket import WebSocketServer, WebSocketClient
+from ._global import global_plugin_manager, global_storage
+from .utils import *
+
+def create_agent(*args, **kwargs):
+    return AgentFactory().create_agent(*args, **kwargs)
+
+facility = FacilityManager()

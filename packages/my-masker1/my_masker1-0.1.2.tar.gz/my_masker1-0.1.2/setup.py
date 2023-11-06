47475bcdd20604b4967c82731100730f25a65d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:16 +0000

    Add package my-masker1/0.1.2/my_masker1-0.1.2.tar.gz

---
+++ packages/my-masker1/my_masker1-0.1.2.tar.gz/my_masker1-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_masker1',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'SQLAlchemy~=2.0.22',
+        'Flask~=3.0.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_script_name=test.app:main',
+        ],
+    },
+)

60dc9e2bfeac60c84c0d21a824ae71956147f83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:20 +0000

    Add package nfn/0.1.2/nfn-0.1.2.tar.gz

---
+++ packages/nfn/nfn-0.1.2.tar.gz/nfn-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nfn',
+    version='0.1.2',
+    description='Neural Functional Network (NFN) layers',
+    url='https://github.com/AllanYangZhou/nfn',
+    author='Allan Zhou',
+    author_email='ayz@cs.stanford.edu',
+    license='MIT',
+    packages=find_packages(exclude=["examples", "experiments", "tests"]),
+    install_requires=['einops', 'torch'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ]
+)

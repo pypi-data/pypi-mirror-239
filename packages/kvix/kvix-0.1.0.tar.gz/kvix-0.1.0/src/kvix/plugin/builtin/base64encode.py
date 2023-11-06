4b193b0b87199f70b1f72a579a8387dca3f326bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 04:04:31 +0000

    Add package kvix/0.1.0/kvix-0.1.0.tar.gz

---
+++ packages/kvix/kvix-0.1.0.tar.gz/kvix-0.1.0/src/kvix/plugin/builtin/base64encode.py
@@ -0,0 +1,21 @@
+import base64
+
+import kvix
+from kvix import ActionType
+from kvix.impl import BaseActionType, BasePlugin
+from kvix.plugin.builtin.machinist import BaseMachinist
+
+
+class Action(BaseMachinist):
+    def _get_text(self) -> str:
+        return base64.b64encode(self.action_type.context.ui.paste_from_clipboard()).decode("UTF-8")
+
+
+class Plugin(BasePlugin):
+    def _create_single_action_type(self) -> ActionType:
+        return BaseActionType(
+            self.context, "base64-encode", "Base64 encode", action_factory=Action
+        )
+
+    def get_actions(self) -> list[kvix.Action]:
+        return [Action(self._single_action_type, "Base64 encode", "encode base64")]

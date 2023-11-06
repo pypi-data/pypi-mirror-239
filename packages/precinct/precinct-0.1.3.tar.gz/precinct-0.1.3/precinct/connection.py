37a6e569a3d87a05cde1eda05d801ca70121eb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-06 15:05:36 +0000

    Add package precinct/0.1.3/precinct-0.1.3.tar.gz

---
+++ packages/precinct/precinct-0.1.3.tar.gz/precinct-0.1.3/precinct/connection.py
@@ -0,0 +1,180 @@
+import os
+import click
+import configparser
+import psycopg2
+import psycopg2.extensions
+from typing import Optional
+from pydantic import BaseModel
+
+from precinct.logging import get_logger
+logger = get_logger()
+
+
+class PostgresConnection(BaseModel):
+    """A PostgreSQL connection object.
+
+    Attributes:
+        host (str): The host of the PostgreSQL database.
+        port (int): The port of the PostgreSQL database.
+        db_name (str): The name of the PostgreSQL database.
+        user (str): The user of the PostgreSQL database.
+        password (str): The password of the PostgreSQL database.
+    """
+
+    host: str
+    port: int
+    db_name: str
+    user: str
+    password: str
+
+    def get_connection(self) -> psycopg2.extensions.connection:
+        """Get and return a connection to a PostgreSQL database using the attributes of this object.
+
+        Returns:
+            psycopg2.extensions.connection: A connection to a PostgreSQL database.
+        """
+        conn_str = f"dbname='{self.db_name}' user='{self.user}' host='{self.host}' port='{self.port}' password='{self.password}'"
+        conn = psycopg2.connect(conn_str)
+        return conn
+
+    @classmethod
+    def from_connection_string(cls, conn_str: str) -> "PostgresConnection":
+        """Create a PostgresConnection object from a connection string.
+
+        Args:
+            conn_str (str): A connection string to use.
+        Returns:
+            PostgresConnection: A PostgresConnection object.
+        Raises:
+            ValueError: If the connection string is invalid.
+        """
+        try:
+            _, uri = conn_str.split("://")
+            credentials, hostinfo = uri.split("@")
+            user, password = credentials.split(":")
+            host, port_db = hostinfo.split("/")
+            port, db_name = port_db.split(":")
+            return cls(
+                host=host, port=int(port), db_name=db_name, user=user, password=password
+            )
+        except ValueError:
+            raise ValueError(
+                "Invalid connection string format. Expected format: postgresql://username:password@host:port/database"
+            )
+
+    @classmethod
+    def get_from_env(cls) -> "PostgresConnection":
+        """Create a PostgresConnection object from environment variables.
+
+        Returns:
+            PostgresConnection: A PostgresConnection object.
+        Raises:
+            ValueError: If any of the required environment variables are missing.
+        """
+        host = os.getenv("PGHOST")
+        port = os.getenv("PGPORT")
+        db_name = os.getenv("PGDATABASE")
+        user = os.getenv("PGUSER")
+        password = os.getenv("PGPASSWORD")
+
+        if not host or not port or not db_name or not user or not password:
+            missing_vars = [
+                "PGHOST" if not host else None,
+                "PGPORT" if not port else None,
+                "PGDATABASE" if not db_name else None,
+                "PGUSER" if not user else None,
+                "PGPASSWORD" if not password else None,
+            ]
+
+            raise ValueError(
+                f"Missing environment variables for connection: {[var for var in missing_vars if var]}"
+            )
+
+        return cls(
+            host=host, port=int(port), db_name=db_name, user=user, password=password
+        )
+
+
+def get_connection(service_name, connection_string) -> psycopg2.extensions.connection:
+    """Get and return a connection to a PostgreSQL database.
+
+    Args:
+        service_name (str): The name of the service to use.
+        connection_string (str): A connection string to use.
+    Returns:
+        psycopg2.extensions.connection: A connection to a PostgreSQL database.
+    Raises:
+        ValueError: If both service_name and connection_string are provided.
+        click.BadParameter: If neither service_name nor connection_string are provided.
+    """
+    if not connection_string:
+        try:
+            conn = get_service_connection(service_name)
+            return conn
+        except Exception:
+            pass
+
+    if connection_string:
+        logger.info(f"Using connection string {connection_string}...")
+        postgres_connection = PostgresConnection.from_connection_string(
+            connection_string
+        )
+        return postgres_connection.get_connection()
+
+    try:
+        postgres_connection = PostgresConnection.get_from_env()
+        logger.info("Using environment variables...")
+        return postgres_connection.get_connection()
+    except ValueError:
+        raise click.BadParameter(
+            "You must provide either --service, --connection-string, or set the appropriate environment variables."
+        )
+
+
+def get_service_connection(
+    service_name: Optional[str] = None,
+) -> psycopg2.extensions.connection:
+    """Checks for and parses a connection from PostgreSQL's service file specification
+
+    Args:
+        service_name (str): The name of the service to use.
+    Returns:
+        psycopg2.extensions.connection: A connection to a PostgreSQL database.
+    Raises:
+        FileNotFoundError: If the service file is not found.
+        ValueError: If the service name is invalid.
+    """
+    service_file_path = os.getenv(
+        "PGSERVICEFILE", os.path.expanduser("~/.pg_service.conf")
+    )
+
+    # Check if service file exists
+    if not os.path.exists(service_file_path):
+        logger.error(f"Service file not found: {service_file_path}")
+        raise FileNotFoundError(f"Service file not found: {service_file_path}")
+
+    config = configparser.ConfigParser()
+    config.read(service_file_path)
+
+    # Use the first service name if none is provided
+    if not service_name:
+        if config.sections():
+            service_name = config.sections()[0]
+            logger.info(
+                f"No service name provided. Using the first service: {service_name}"
+            )
+        else:
+            logger.error("No service sections found in the service file.")
+            raise ValueError("No service sections found in the service file.")
+
+    if service_name not in config.sections():
+        logger.error(f"Invalid service name: {service_name}")
+        raise ValueError(f"Invalid service name: {service_name}")
+
+    try:
+        conn = psycopg2.connect(service=service_name)
+        logger.info(f"Connection established using service: {service_name}")
+        return conn
+    except Exception as e:
+        logger.error(f"Failed to establish connection: {str(e)}")
+        raise
---
+++ packages/precinct/precinct-0.1.3.tar.gz/precinct-0.1.3/precinct/main.py
@@ -0,0 +1,161 @@
+import pyperclip
+import click
+import json
+import os
+import openai
+
+from precinct.models import PrecinctQuery, GPTModel
+from precinct.connection import get_connection
+from precinct.logging import get_logger
+
+logger = get_logger()
+
+
+def get_query_string(query_input: str):
+    """Determine if query_input is a query string or file path."""
+    if os.path.isfile(query_input):
+        with open(query_input, "r") as file:
+            return file.read()
+    return query_input
+
+
+@click.command(
+    help=(
+        "\nPrecinct: A SQL query LLM copilot for analyzing queries, suggesting indices, "
+        "and providing optimizations. Currently supports PostgreSQL.\n\n"
+        "Examples:\n\n"
+        '    precinct "SELECT * FROM table;"\n\n'
+        '    precinct "path/to/your/file.sql"\n\n'
+    )
+)
+@click.argument("query", type=str, required=True, nargs=1)
+@click.option(
+    "--uri",
+    type=str,
+    help="PostgreSQL connection URI, ie. 'postgresql://username:password@host:port/database'. Mutually exclusive with --service.",
+)
+@click.option(
+    "--service",
+    type=str,
+    help="PostgreSQL service name as located in ~/.pg_service.conf or at specified path. Mutually exclusive with --uri.",
+)
+@click.option(
+    "--service-file",
+    type=click.Path(exists=True),
+    default=os.path.expanduser("~/.pg_service.conf"),
+    help="Path to PGSERVICEFILE. Optionally provide in conjunction with --service.",
+    show_default="~/.pg_service.conf",
+)
+@click.option(
+    "--openai-api-key",
+    type=str,
+    default=os.environ.get('OPENAI_API_KEY', ''),
+    help="OpenAI API key for authentication.",
+    show_default="from environment variable OPENAI_API_KEY",
+)
+@click.option(
+    "--model",
+    type=click.Choice([GPTModel.GPT_4, GPTModel.GPT_3_5_TURBO]),
+    default=GPTModel.GPT_4,
+    help="Model to use.",
+)
+@click.option(
+    "--rows",
+    type=int,
+    default=10,
+    help="Number of rows to return from query at most. Typically used for previewing query results.",
+)
+@click.option(
+    "--json", "json_io", is_flag=True, help="Enable VSCode optimized JSON I/O mode."
+)
+def main(query, uri, service, service_file, openai_api_key, model, rows, json_io):
+    if service and uri:
+        raise click.UsageError("Options --service and --uri are mutually exclusive.")
+
+    query = get_query_string(query)
+
+    if openai_api_key:
+        openai.api_key = openai_api_key
+
+    if service_file:
+        os.environ["PGSERVICEFILE"] = service_file
+
+    conn = get_connection(service, uri)
+
+    # Special input mode for VSCode extension
+    if json_io:
+        # Switch to JSON I/O mode for interaction with VSCode
+        precinct_query = PrecinctQuery(query, conn, model)
+        explanation = precinct_query.get_query_summary()
+        # Output initial explanation in JSON
+        print(json.dumps({"query": query, "intent": explanation.intent}))
+
+        # Wait for JSON input from VSCode
+        input_json = json.loads(input())
+        user_modified_intent = input_json.get("intent")
+
+        # Perform optimization based on the modified intent
+        new_query, explanation = precinct_query.get_optimized_query(
+            user_modified_intent
+        )
+        print(
+            json.dumps(
+                {"optimized_query": new_query.query_str, "explanation": explanation}
+            )
+        )
+        return
+
+    # Extract tables, indices, column properties, and analysis
+    try:
+        precinct_query = PrecinctQuery(query, conn, model)
+    except ValueError:
+        print("Invalid query.")
+        return
+
+    # Gather any clarifications on query intent
+    do_proceed = False
+    clarification = None
+    while not do_proceed:
+        explanation = precinct_query.get_query_summary(clarification)
+        print(f"Query: {query}")
+        print(f"Intent: {explanation.intent}")
+        confirm = input(
+            "Accept intent with `y`, quit with `q` or type out clarification to intent (y/q/[intent]): "
+        )
+        if confirm.lower() == "y":
+            do_proceed = True
+        if confirm.lower() == "q":
+            return
+        else:
+            clarification = confirm
+
+    # Synthesize query, indices, and plan into a query to get the diff
+    attempts = 0
+    while attempts < 3:
+        try:
+            new_query, explanation = precinct_query.get_optimized_query()
+            break
+        except Exception:
+            attempts += 1
+            logger.warn("Failed to optimize query. Retrying...")
+    else:
+        logger.error("Unable to optimize query. Exiting")
+        return
+
+    print(f"\nOptimized query: {new_query.query_str}\nExplanation: {explanation}")
+    action = input(
+        "Run query now with `r`, copy to clipboard with `c`, or cancel with `q` (r/c/q): "
+    )
+
+    if action.lower() == "r":
+        with conn.cursor() as cursor:
+            cursor.execute(new_query.query_str)
+            print(cursor.fetchmany(rows))
+    elif action.lower() == "c":
+        pyperclip.copy(new_query.query_str)
+    else:
+        print("Operation cancelled.")
+
+
+if __name__ == "__main__":
+    main()

c506ecd8b26b870404f35648532243e5082cb07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:07:44 +0000

    Add package SegRunLib/0.0.3/SegRunLib-0.0.3-py3-none-any.whl

---
+++ packages/SegRunLib/SegRunLib-0.0.3-py3-none-any.whl/SegRunLib/run.py
@@ -0,0 +1,8 @@
+from RunnerClass import Runner
+
+def run(in_path_nifty: str, out_path_nifty: str):
+    runner = Runner()
+    runner.predict_and_save(in_path_nifty, out_path_nifty)
+    
+    
+    

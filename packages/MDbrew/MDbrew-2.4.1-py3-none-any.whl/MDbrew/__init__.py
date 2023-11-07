b99313f9d43812323aeba7210d9a83d335b292e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:04:15 +0000

    Add package MDbrew/2.4.1/MDbrew-2.4.1-py3-none-any.whl

---
+++ packages/MDbrew/MDbrew-2.4.1-py3-none-any.whl/MDbrew/__init__.py
@@ -0,0 +1,8 @@
+from MDbrew.spatial import *
+from MDbrew.analysis import *
+from MDbrew.main.brewery import Brewery
+from MDbrew.tool import plot
+
+__version__ = "2.4.1"
+__name__ = "MDbrew"
+__all__ = ["MSD", "RDF", "Brewery", "plot"]

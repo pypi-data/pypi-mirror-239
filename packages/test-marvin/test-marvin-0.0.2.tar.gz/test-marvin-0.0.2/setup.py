fb82b9c5fd0c8dbc565e7e5e52116655813e84c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:07:05 +0000

    Add package test-marvin/0.0.2/test-marvin-0.0.2.tar.gz

---
+++ packages/test-marvin/test-marvin-0.0.2.tar.gz/test-marvin-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import re
+
+import setuptools
+
+
+setuptools.setup(
+    name="test-marvin",
+    description="",
+    keywords="",
+    long_description="""Dummy package""",
+    include_package_data=True,
+    version="v0.0.2",
+    url="",
+    author="Greenhouse AI team",
+    author_email="ai@greenhousegroup.com",
+    package_dir={"test-marvin": "src/test-marvin"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest>=4"],
+    packages=setuptools.find_packages("src"),
+)

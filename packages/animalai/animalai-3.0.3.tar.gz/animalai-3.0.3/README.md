3156207b4e63fcea57c64594e5b5c8a62b57c4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:14 +0000

    Add package animalai/3.0.3/animalai-3.0.3.tar.gz

---
+++ packages/animalai/animalai-3.0.3.tar.gz/animalai-3.0.3/README.md
@@ -0,0 +1,19 @@
+# AnimalAI
+![steampunkFOURcrop](https://github.com/Kinds-of-Intelligence-CFI/animal-ai/assets/65875290/df798f4a-cb2c-416f-a150-093b9382a621)
+Animal-AI (3) Python Project
+
+This repository is responsible for managing and updating primarily the PyPI package (https://pypi.org/project/animalai/) for Animal-AI project package dependencies. 
+
+The main project is located here: https://github.com/Kinds-of-Intelligence-CFI/animal-ai.
+
+The Animal-AI (3) Unity project repository is here: https://github.com/Kinds-of-Intelligence-CFI/animal-ai-unity-project.
+
+## Version History
+
+- v3.0.3
+  - Asserted python version to be 3.6.1 or higher, but less than 3.10.0 (exclusive).
+  - Added package description to setup.py and package metadata.
+- v3.0.2
+  - Fixed major pacakge dependency issues, related to mlagents 0.30.0, protopuf, and shimy.
+  - Updated project setup.py to accomodate the latest version of Animal-AI package dependencies.
+    - Users can now use # pip install animalai # to install from PyPI directly (previously had to be done via locally). 
---
+++ packages/animalai/animalai-3.0.3.tar.gz/animalai-3.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="animalai",
+    version="3.0.3",
+    description="Animal AI 3 Python API",
+    long_description= open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url="https://github.com/Kinds-of-Intelligence-CFI/animalai-package",
+    author="Matt Crosby; Ibrahim Alhas; K. Voudouris; W. Schellaert",
+    author_email="kindsofintelligence.cfi@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    zip_safe=False,
+    install_requires=["mlagents==0.30.0",
+                      "numpy==1.21.2",
+                      "scipy==1.7.2",
+                      "pandas== 1.3.2",
+                      "protobuf== 3.20.3",
+                      "stable-baselines3== 2.1.0",
+                      "shimmy== 1.3.0",
+                      "notebook"],
+    python_requires=">=3.9, <3.10",
+)

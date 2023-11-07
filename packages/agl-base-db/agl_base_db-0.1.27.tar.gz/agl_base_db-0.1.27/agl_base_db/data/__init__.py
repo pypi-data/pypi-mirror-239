6055f16606d6971de1ebd0a7cffdfe9e0fab9011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 17:04:10 +0000

    Add package agl-base-db/0.1.27/agl_base_db-0.1.27.tar.gz

---
+++ packages/agl-base-db/agl_base_db-0.1.27.tar.gz/agl_base_db-0.1.27/agl_base_db/data/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from tkinter import E
+
+DATA_DIR = Path(__file__).parent
+
+INFORMATION_DATA_DIR = DATA_DIR / "information_source"
+UNIT_DATA_DIR = DATA_DIR / "unit"
+CENTER_DATA_DIR = DATA_DIR / "center"
+EXAMINATION_DATA_DIR = DATA_DIR / "examination"
+LABEL_DATA_DIR = DATA_DIR / "label"
+MODEL_TYPE_DATA_DIR = DATA_DIR / "model_type"

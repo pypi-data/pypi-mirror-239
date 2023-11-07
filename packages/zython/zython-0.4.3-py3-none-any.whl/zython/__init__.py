55dfda0e3abb3d3ccda380f10719c815357d50ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 05:04:40 +0000

    Add package zython/0.4.3/zython-0.4.3-py3-none-any.whl

---
+++ packages/zython/zython-0.4.3-py3-none-any.whl/zython/__init__.py
@@ -0,0 +1,13 @@
+import zython.var_par.types
+from zython.solver.console_wrappers import available_solver_tags
+from zython.var_par.var import var
+from zython.var_par.par import par
+from zython.var_par.collections.array import Array
+from zython.var_par.collections.set import Set
+from zython.operations.functions_and_predicates import exists, forall, sum, alldifferent, circuit, count, min, max,\
+    allequal, ndistinct, increasing, decreasing, cumulative, disjunctive, table
+from zython.model import Model
+from zython.result import as_original
+
+
+range = zython.var_par.types._range

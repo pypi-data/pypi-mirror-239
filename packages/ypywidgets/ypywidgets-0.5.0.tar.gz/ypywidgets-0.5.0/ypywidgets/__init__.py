c29518f3254c61764654be5dc20e006e7f85719b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 05:04:47 +0000

    Add package ypywidgets/0.5.0/ypywidgets-0.5.0.tar.gz

---
+++ packages/ypywidgets/ypywidgets-0.5.0.tar.gz/ypywidgets-0.5.0/ypywidgets/__init__.py
@@ -0,0 +1,5 @@
+from .ypywidgets import Widget  # noqa
+from .reactive import reactive  # noqa
+
+
+__version__ = "0.5.0"
---
+++ packages/ypywidgets/ypywidgets-0.5.0.tar.gz/ypywidgets-0.5.0/ypywidgets/reactive.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import Callable
+
+from reacttrs.reactive import Reactive, ReactiveType
+
+
+def set_attr(obj, name, value):
+    if obj._attrs is None:
+        return
+
+    try:
+        obj._attrs[name] = value
+    except RuntimeError:
+        # Already mutably borrowed
+        pass
+
+
+class reactive(Reactive[ReactiveType]):
+    def __init__(
+        self,
+        default: ReactiveType | Callable[[], ReactiveType],
+        *,
+        init: bool = True,
+        always_update: bool = False,
+        sync: bool = True,
+    ) -> None:
+        on_set = set_attr if sync else None
+        super().__init__(
+            default,
+            init=init,
+            always_update=always_update,
+            on_set=on_set,
+        )

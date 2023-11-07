d2721cba23d3b87f7fbfc9c920130992c6d7fb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:04:34 +0000

    Add package chinesenamewangjing2023/0.0.5/chinesenamewangjing2023-0.0.5.tar.gz

---
+++ packages/chinesenamewangjing2023/chinesenamewangjing2023-0.0.5.tar.gz/chinesenamewangjing2023-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chinesenamewangjing2023',
+    version='0.0.5',
+    # keywords = ('chinesename',),
+    description='get a chinesename by random',
+    license='MIT License',
+    install_requires=[],
+    packages=['chinesenamewangjing2023'],  # 要打包的项目文件夹
+    include_package_data=True,  # 自动打包文件夹内所有数据
+    author='wj',
+    author_email='120551025@qq.com',
+    url='https://github.com/mouday/chinesename',
+    # packages = find_packages(include=("*"),),
+)
+

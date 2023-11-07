053cf51aca5103b2193a2b4384eab489f0f685a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:04:30 +0000

    Add package otrs-somconnexio/0.4.60/otrs_somconnexio-0.4.60-py2.py3-none-any.whl

---
+++ packages/otrs-somconnexio/otrs_somconnexio-0.4.60-py2.py3-none-any.whl/otrs_somconnexio/otrs_models/coverage/vdf_fibre.py
@@ -0,0 +1,9 @@
+class VdfFibreCoverage:
+    VALUES = [
+        ("fibraCoaxial", "Fibra (coaxial)"),
+        ("fibraFTTH", "Fibra (FTTH)"),
+        ("NEBAFTTH", "Fibra (NEBA)"),
+        ("asociatel", "Asociatel VDF"),
+        ("NoFibraVdf", "No fibra"),
+        ("NoRevisat", "No revisat"),
+    ]

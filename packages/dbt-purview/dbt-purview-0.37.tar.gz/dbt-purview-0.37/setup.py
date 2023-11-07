78cb0e45a16c12db99ba6fba45519990d3c093d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:05:38 +0000

    Add package dbt-purview/0.37/dbt-purview-0.37.tar.gz

---
+++ packages/dbt-purview/dbt-purview-0.37.tar.gz/dbt-purview-0.37/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbt-purview',
+    version='0.37',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'databricks-sql-connector',
+        'requests',
+        'pytz',
+        'typing',
+        'datetime',
+        'apache-airflow',
+        'snowflake-sqlalchemy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dbtpurview = dbtpurview.main:dbtpurview',
+        ],
+    },
+)

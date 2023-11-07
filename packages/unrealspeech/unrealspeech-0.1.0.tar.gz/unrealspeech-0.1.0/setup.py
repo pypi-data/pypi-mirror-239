b200141ec100352e8f9c4cced9ddf1f8fa5f3ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:07:40 +0000

    Add package unrealspeech/0.1.0/unrealspeech-0.1.0.tar.gz

---
+++ packages/unrealspeech/unrealspeech-0.1.0.tar.gz/unrealspeech-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# Package name
+name = 'unrealspeech'
+
+# Version number
+version = '0.1.0'
+
+# Package description
+description = 'A Python package for interacting with the Unreal Speech API.'
+
+# Author information
+author = 'Ajaga Abdulbasit'
+author_email = 'basitng2004@gmail.com'
+
+# URL for the package's homepage or repository
+url = 'https://github.com/basitng/unrealspeech'
+
+# License information
+license = 'MIT'
+
+install_requires = [
+    'requests',
+]
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    author=author,
+    packages=find_packages(),
+    author_email=author_email,
+    url=url,
+    license=license,
+    install_requires=install_requires,
+)

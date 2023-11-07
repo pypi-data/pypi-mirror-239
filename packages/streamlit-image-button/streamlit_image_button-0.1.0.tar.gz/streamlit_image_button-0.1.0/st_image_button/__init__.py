4f2b239af4d7b68d59b5362a69d783d9526aba22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:10 +0000

    Add package streamlit-image-button/0.1.0/streamlit_image_button-0.1.0.tar.gz

---
+++ packages/streamlit-image-button/streamlit_image_button-0.1.0.tar.gz/streamlit_image_button-0.1.0/st_image_button/__init__.py
@@ -0,0 +1,28 @@
+import os
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "st_image_button",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("st_image_button", path=build_dir)
+
+
+def st_image_button(title, image, key=None):
+
+    _ = _component_func(title = title, image = image, key = None)
+
+if __name__ == "__main__":
+    
+    import streamlit as st
+
+    st.subheader("Test 1")
+
+    num_clicks = st_image_button("Title", "icon.png")
+    st.markdown("---")

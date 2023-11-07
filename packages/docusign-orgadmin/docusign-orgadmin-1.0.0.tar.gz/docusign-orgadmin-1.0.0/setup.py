fe9a8f3258eed5cadc1b80013f622c22b4374ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:05:26 +0000

    Add package docusign-orgadmin/1.0.0/docusign-orgadmin-1.0.0.tar.gz

---
+++ packages/docusign-orgadmin/docusign-orgadmin-1.0.0.tar.gz/docusign-orgadmin-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup 
+
+setup(name='docusign-orgadmin', #package name
+      version='1.0.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False)

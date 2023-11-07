4f6243cc67cecd0ad6448fd9fa5f89bd8842b131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 05:04:00 +0000

    Add package malevich-space/0.2.6b0/malevich_space-0.2.6b0.tar.gz

---
+++ packages/malevich-space/malevich_space-0.2.6b0.tar.gz/malevich_space-0.2.6b0/malevich_space/gql/component/add_comp_in_flow.py
@@ -0,0 +1,40 @@
+from gql import gql
+
+
+add_comp_to_flow = gql(
+    """
+    mutation AddComponentToFlow(
+        $flow_id: String!,
+        $target_comp_version_id: String!,
+        $flow_comp_status: String,
+        $comp_flow_comp_status: String,
+        $offset_x: Float,
+        $offset_y: Float,
+        $version_id: String,
+        $selected_op: [InFlowSelectedOp!]) {
+      flow(uid: $flow_id) {
+        addComponent(
+          input: {
+            node: {
+              offsetX: $offset_x,
+              offsetY: $offset_y
+            },
+            rel: {
+              status: $flow_comp_status,
+              versionId: $version_id
+            },
+            componentVersionId: $target_comp_version_id,
+            componentRel: {
+              status: $comp_flow_comp_status
+            }
+            selectedOp: $selected_op
+          }
+        ) {
+          details {
+            uid
+          }
+        }
+      }
+    }
+    """
+)

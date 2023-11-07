70896b45a895f2e48e2f4bf5e7569a53eade010b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 15:05:08 +0000

    Add package jacksung/0.0.2.43/jacksung-0.0.2.43-py3-none-any.whl

---
+++ packages/jacksung/jacksung-0.0.2.43-py3-none-any.whl/jacksung/utils/time.py
@@ -0,0 +1,24 @@
+import time
+from datetime import datetime
+import pytz
+
+
+class RemainTime:
+    def __init__(self, epoch):
+        self.start_time = time.time()
+        self.epoch = epoch
+        self.now_epoch = 0
+
+    def update(self, log_temp='[ Epochs Remaining:{}\tFinished in {} ]', print_log=True, update_step=1):
+        epoch_time = time.time() - self.start_time
+        epoch_remaining = self.epoch - self.now_epoch
+        time_remaining = epoch_time * epoch_remaining
+        pytz.timezone('Asia/Shanghai')  # 东八区
+        t = datetime.fromtimestamp(int(time.time()) + time_remaining,
+                                   pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
+        log = log_temp.format(epoch_remaining, t)
+        if print_log:
+            print(log)
+        self.start_time = time.time()
+        self.now_epoch += update_step
+        return epoch_remaining, t

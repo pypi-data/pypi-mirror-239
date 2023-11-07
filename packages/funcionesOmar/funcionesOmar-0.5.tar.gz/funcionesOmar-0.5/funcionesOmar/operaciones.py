1d3850d109b554b2a4601d8e1559268f29b4766f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:06:54 +0000

    Add package funcionesOmar/0.5/funcionesOmar-0.5.tar.gz

---
+++ packages/funcionesOmar/funcionesOmar-0.5.tar.gz/funcionesOmar-0.5/funcionesOmar/operaciones.py
@@ -0,0 +1,12 @@
+
+def suma(num1: float, num2: float) -> float:
+    return num1 + num2
+
+def resta(num1: float, num2: float) -> float:
+    return num1 - num2
+
+def producto(num1: float, num2: float) -> float:
+    return num1 * num2
+
+def division(num1: float, num2: float) -> float:
+    return num1 / num2 if num2 > 0 else 0

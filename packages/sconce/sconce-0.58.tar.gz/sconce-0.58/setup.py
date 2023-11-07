2de50f22e766e58d810e4bb43972419e3bddcce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:07:45 +0000

    Add package sconce/0.58/sconce-0.58.tar.gz

---
+++ packages/sconce/sconce-0.58.tar.gz/sconce-0.58/setup.py
@@ -0,0 +1,69 @@
+from setuptools import setup, find_packages
+from distutils.core import setup, Extension
+import os
+
+
+with open("README.rst", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+# fmt: off
+__version__ = '0.57'
+# fmt: on
+
+requirements = [
+    "torch>=1.1.0",
+    "pandas",
+    "matplotlib",
+    "numpy>=1.17",
+    "nir",
+    "nirtorch",
+]
+
+
+test_requirements = ["pytest>=6"]
+
+version = __version__
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []
+
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    author="Sathyaprakash Narayanan",
+    author_email="snaray17@ucsc.edu",
+    python_requires=">=3.7",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    description="Model Compresion Made Easy",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    license="MIT License",
+    include_package_data=True,
+    keywords="sconce",
+    name="sconce",
+    packages=find_packages(include=["sconce"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/satabios/sconce",
+    version=__version__,
+    zip_safe=False,
+)
+

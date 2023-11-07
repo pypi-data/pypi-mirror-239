247376fc367a23418e1cb4fe71f574ac7eec2442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:04:56 +0000

    Add package pybiolib/1.1.1512/pybiolib-1.1.1512.tar.gz

---
+++ packages/pybiolib/pybiolib-1.1.1512.tar.gz/pybiolib-1.1.1512/biolib/jobs/types.py
@@ -0,0 +1,43 @@
+from biolib.typing_utils import TypedDict, Optional, Literal, List
+
+JobState = Literal['in_progress', 'completed', 'failed', 'cancelled']
+
+
+class _BaseCloudJobDict(TypedDict):
+    created_at: str
+    finished_at: Optional[str]
+    uuid: str
+    error_code: int
+
+
+class CloudJobDict(_BaseCloudJobDict):
+    started_at: Optional[str]
+    compute_node_url: Optional[str]
+
+
+class CloudJobStartedDict(_BaseCloudJobDict):
+    started_at: str
+    compute_node_url: str
+
+
+class JobDict(TypedDict):
+    app_uri: str
+    auth_token: str
+    created_at: str
+    ended_at: Optional[str]
+    runtime_seconds: int
+    started_at: str
+    state: JobState
+    uuid: str
+    cloud_job: Optional[CloudJobDict]
+
+
+class BasePaginatedResponse(TypedDict):
+    current_page_number: int
+    object_count: int
+    page_count: int
+    page_size: int
+
+
+class JobsPaginatedResponse(BasePaginatedResponse):
+    results: List[JobDict]

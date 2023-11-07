c367f5e63aeb974efc4b28e87ef06ff66a517aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 15:06:19 +0000

    Add package adtcls/0.0.20/adtcls-0.0.20.tar.gz

---
+++ packages/adtcls/adtcls-0.0.20.tar.gz/adtcls-0.0.20/adtcls/src/__init__.py
@@ -0,0 +1,5 @@
+from .utils.cfg_utils import ConfigUtils
+from .utils.utils import get_classes
+from .base_train import BaseTrain
+from .evaluate import Evaluate
+from .train import main as train_pipeline_main

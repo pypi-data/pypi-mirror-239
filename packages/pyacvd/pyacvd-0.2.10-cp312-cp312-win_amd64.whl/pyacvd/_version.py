62149cf73e2a92bd6bfdb7f618ca67c3ff7ebf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 15:06:29 +0000

    Add package pyacvd/0.2.10/pyacvd-0.2.10-cp312-cp312-win_amd64.whl

---
+++ packages/pyacvd/pyacvd-0.2.10-cp312-cp312-win_amd64.whl/pyacvd/_version.py
@@ -0,0 +1,11 @@
+"""Version number for pyacvd
+
+On the ``main`` branch, use 'dev0' to denote a development version.
+For example:
+
+version_info = 0, 27, 'dev0'
+
+"""
+# major, minor, patch
+version_info = 0, 2, 10
+__version__ = ".".join(map(str, version_info))

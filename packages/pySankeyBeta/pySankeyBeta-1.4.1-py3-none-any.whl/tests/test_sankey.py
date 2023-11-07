3677aeb3452651d99e6b5aa982050b9ac39babde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:22 +0000

    Add package pySankeyBeta/1.4.1/pySankeyBeta-1.4.1-py3-none-any.whl

---
+++ packages/pySankeyBeta/pySankeyBeta-1.4.1-py3-none-any.whl/tests/test_sankey.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+from pysankey import sankey
+from tests.generic_test import TestFruit
+
+
+class TestSankey(TestFruit):
+    def test_right_color(self) -> None:
+        ax = sankey(self.data["true"], self.data["predicted"], rightColor=True)
+        self.assertIsInstance(ax, plt.Axes)

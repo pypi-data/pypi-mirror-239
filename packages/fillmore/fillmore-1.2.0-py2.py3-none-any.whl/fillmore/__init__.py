486dbbcbb23b9ee51884cf419da4edac25349720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:57 +0000

    Add package fillmore/1.2.0/fillmore-1.2.0-py2.py3-none-any.whl

---
+++ packages/fillmore/fillmore-1.2.0-py2.py3-none-any.whl/fillmore/__init__.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+"""Fillmore is a Python library for scrubbing Sentry events."""
+
+# x.y.z or x.y.z.dev0
+__version__ = "1.2.0"
+
+
+SCRUBBER_MODULE_NAME = __name__

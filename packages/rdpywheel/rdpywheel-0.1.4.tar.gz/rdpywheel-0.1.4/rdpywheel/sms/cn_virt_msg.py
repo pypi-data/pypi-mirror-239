ba0f1aac53377d93a6ca31054c2ce73be0b674de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:05:16 +0000

    Add package rdpywheel/0.1.4/rdpywheel-0.1.4.tar.gz

---
+++ packages/rdpywheel/rdpywheel-0.1.4.tar.gz/rdpywheel-0.1.4/rdpywheel/sms/cn_virt_msg.py
@@ -0,0 +1,37 @@
+import os
+from urllib.parse import urljoin, urlencode
+import requests
+
+sms_token = os.environ.get("CN_VIRTUAL_TOKEN")
+sms_base_url = os.environ.get("CN_VIRTUAL_BASE_URL")
+
+
+class CnVirtMsg:
+    def get_sms_phone_number(token: str):
+        try:
+            params = {
+                "code": "getPhone",
+                "token": token,
+                "cardType": "实卡"
+            }
+            url = urljoin(sms_base_url, "?" + urlencode(params))
+            response = requests.get(url=url, timeout=10)
+            data = response.json()
+            return data["data"]
+        except Exception as e:
+            print(e)
+
+    def get_sms_msg(token: str, phone_no: str, key_word: str):
+        try:
+            params = {
+                "code": "getMsg",
+                "token": token,
+                "phone": phone_no,
+                "keyWord": key_word
+            }
+            url = urljoin(sms_base_url, "?" + urlencode(params))
+            response = requests.get(url=url, timeout=10)
+            data = response.json()
+            return data["data"]
+        except Exception as e:
+            print(e)

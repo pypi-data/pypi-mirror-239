1f3486d3c4ae316fc3e461b7a8d3a7687fba3bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:35 +0000

    Add package dlplan/0.3.6/dlplan-0.3.6.tar.gz

---
+++ packages/dlplan/dlplan-0.3.6.tar.gz/dlplan-0.3.6/include/dlplan/policy/parsers/semantic/context.hpp
@@ -0,0 +1,38 @@
+#ifndef DLPLAN_INCLUDE_DLPLAN_POLICY_PARSERS_SEMANTIC_CONTEXT_HPP_
+#define DLPLAN_INCLUDE_DLPLAN_POLICY_PARSERS_SEMANTIC_CONTEXT_HPP_
+
+#include <unordered_map>
+
+#include "include/dlplan/policy.h"
+#include "include/dlplan/core.h"
+
+#include "include/dlplan/policy/parsers/syntactic/ast.hpp"
+
+
+namespace dlplan::policy {
+
+template<typename Node, typename Result>
+struct Data {
+    Node node;
+    Result result;
+};
+
+using NamedBooleanData = Data<ast::BooleanDefinition, std::shared_ptr<const NamedBoolean>>;
+using NamedNumericalData = Data<ast::NumericalDefinition, std::shared_ptr<const NamedNumerical>>;
+using NamedConceptData = Data<ast::ConceptDefinition, std::shared_ptr<const NamedConcept>>;
+using NamedRoleData = Data<ast::RoleDefinition, std::shared_ptr<const NamedRole>>;
+
+
+struct Context {
+    PolicyFactory& policy_factory;
+    std::unordered_map<std::string, NamedBooleanData> booleans;
+    std::unordered_map<std::string, NamedNumericalData> numericals;
+    std::unordered_map<std::string, NamedConceptData> concepts;
+    std::unordered_map<std::string, NamedRoleData> roles;
+
+    explicit Context(PolicyFactory& policy_factory);
+};
+
+}
+
+#endif

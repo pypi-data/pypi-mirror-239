2b6ce0a1b49ad30f0f6f5d794ab7bde55f5392a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:07:16 +0000

    Add package mwcliparser/1.0.1/mwcliparser-1.0.1.tar.gz

---
+++ packages/mwcliparser/mwcliparser-1.0.1.tar.gz/mwcliparser-1.0.1/mwcliparser/cli.py
@@ -0,0 +1,26 @@
+import argparse
+
+def get_word():
+    parser = argparse.ArgumentParser(
+        description='MW_CLI is a command line tool to get word definition using Merrian-webster API.', 
+        add_help='This command line tool will help to get the description of passed words'
+        )
+    parser.add_argument(
+        'word', 
+        help='The word to get the definition from Merrian-Webster',
+        )
+    return parser.parse_args()
+
+
+def main():
+    import sys, json
+    from mwcliparser import api
+    args=get_word()
+    if args.word:
+        word=args.word
+        api.getData(word)
+    else:
+        print(f'Enter a word to get its definition from Merrian-Websters')
+
+if __name__ == '__main__':
+    main()

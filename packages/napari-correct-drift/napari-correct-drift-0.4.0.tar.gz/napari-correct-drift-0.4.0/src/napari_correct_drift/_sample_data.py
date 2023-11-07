7c22535dcf0dbc0b277bc5bd42b1811238cbfabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:21 +0000

    Add package napari-correct-drift/0.4.0/napari-correct-drift-0.4.0.tar.gz

---
+++ packages/napari-correct-drift/napari-correct-drift-0.4.0.tar.gz/napari-correct-drift-0.4.0/src/napari_correct_drift/_sample_data.py
@@ -0,0 +1,149 @@
+"""
+This module is an example of a barebones sample data provider for napari.
+
+It implements the "sample data" specification.
+see: https://napari.org/stable/plugins/guides.html?#sample-data
+
+Replace code below according to your needs.
+"""
+from __future__ import annotations
+
+import numpy as np
+from skimage.draw import disk
+from skimage.morphology import ball, binary_dilation
+
+
+class Example_TZYX:
+    def __init__(self):
+        deltas_3d = (
+            [[1, 6, 4]] * 4
+            + [[-1, 4, 1]] * 6
+            + [[1, 2, -3]] * 6
+            + [[0, -2, 7]] * 4
+            + [[0, 2, -5]] * 4
+            + [[0, 7, 0]] * 4
+            + [[0, 0, -7]] * 3
+        )
+
+        shifts_3d = np.cumsum(deltas_3d, 0)
+        shifts_3d = np.vstack(
+            [
+                [0] * 3,
+                shifts_3d,
+            ]
+        )
+
+        self.shape = (32, 16, 128, 80)
+        self.start_pos = (8, 24, 32)
+        self.deltas = deltas_3d
+        self.shifts = shifts_3d
+
+    def create(self, noise_level=0):
+        deltas = np.asarray(self.deltas)
+
+        T, Z, Y, X = self.shape
+        img = np.zeros((T, Z, Y, X), dtype="bool")
+
+        t = 0
+        img[t][self.start_pos] = 1
+        img[t] = binary_dilation(img[t], ball(3))
+
+        pos = np.array(self.start_pos, dtype="int32")
+
+        for d in deltas:
+            t += 1
+            pos += d
+            img[t][tuple(pos)] = 1
+            img[t] = binary_dilation(img[t], ball(3))
+
+        img = img.astype("uint8") * (255 - noise_level)
+
+        for t in range(T):
+            img[t] += (np.random.rand(Y, X) * noise_level).astype("uint8")
+
+        return img
+
+
+class Example_TYX:
+    def __init__(self):
+        deltas_2d = [[8, 8]] * 10
+        deltas_2d += [[-2, 6]] * 5
+        deltas_2d += [[6, 3]] * 10
+        deltas_2d += [[0, 7]] * 3
+        deltas_2d += [[11, -3]] * 3
+
+        deltas_2d = [(0, y, x) for y, x in deltas_2d]
+
+        shifts_2d = np.cumsum(deltas_2d, 0)
+        shifts_2d = np.vstack([[0, 0, 0], shifts_2d])
+
+        self.shape = (32, 256, 256)
+        self.start_pos = (32, 32)
+        self.deltas = deltas_2d
+        self.shifts = shifts_2d
+
+    def create(self, noise_level=0):
+        deltas = np.asarray(self.deltas)
+
+        T, Y, X = self.shape
+        img = np.zeros((T, Y, X), dtype="uint8")
+
+        for t in range(T):
+            img[t] += (np.random.rand(Y, X) * noise_level).astype("uint8")
+
+        t = 0
+        rr, cc = disk(self.start_pos, 8, shape=(256, 256))
+        img[t, rr, cc] = 255
+
+        pos = np.array(self.start_pos)
+
+        for d in deltas:
+            t += 1
+            pos += d[1:]
+            rr, cc = disk(pos, 8, shape=(256, 256))
+            img[t, rr, cc] = 255
+
+        return img
+
+
+class Example_TCYX(Example_TYX):
+    def create(self, noise_level=0):
+        img = super().create(noise_level=noise_level)
+
+        img = np.stack([img, img[::-1], np.zeros_like(img)], axis=1)
+        self.shape = img.shape
+
+        return img
+
+
+def sample_2d():
+    """Creates 2D + time example data"""
+    return [
+        (
+            Example_TYX().create(noise_level=16),
+            {"name": "test_TYX", "contrast_limits": (0, 64)},
+        ),
+    ]
+
+
+def sample_3d():
+    """Creates 3D + time example data"""
+    return [
+        (
+            Example_TZYX().create(noise_level=16),
+            {"name": "test_TZYX", "contrast_limits": (0, 64)},
+        ),
+    ]
+
+
+def sample_3d_ch():
+    """Creates 2D + channel + time example data"""
+    return [
+        (
+            Example_TCYX().create(noise_level=16),
+            {
+                "name": "test_TCYX",
+                "contrast_limits": (0, 64),
+            },
+        ),
+    ]
---
+++ packages/napari-correct-drift/napari-correct-drift-0.4.0.tar.gz/napari-correct-drift-0.4.0/src/napari_correct_drift/_tests/test_core.py
@@ -0,0 +1,269 @@
+import numpy as np
+
+from napari_correct_drift._core import CorrectDrift, ROIRect
+from napari_correct_drift._sample_data import Example_TYX, Example_TZYX
+
+
+def test_tyx():
+    example = Example_TYX()
+    img_2d = example.create()
+    ist_2d = CorrectDrift(img_2d, "tyx")
+
+    for t0 in [0, 17]:
+        ofs = ist_2d._estimate_drift_absolute(t0)
+        ok = ofs - example.shifts + example.shifts[t0]
+        assert np.allclose(ok, 0), "Shift estimation mismatch. Diff:\n"
+
+
+def test_tzyx():
+    example = Example_TZYX()
+    img_3d = example.create()
+    ist_3d = CorrectDrift(img_3d, "tzyx")
+
+    for t0 in [0, 16]:
+        ofs = ist_3d._estimate_drift_absolute(t0)
+        ok = ofs - example.shifts + example.shifts[t0]
+        assert np.allclose(ok, 0), "Shift estimation mismatch. Diff:\n"
+
+
+def test_tcyx():
+    example = Example_TYX()
+    img_2d = example.create()
+    img_2d = np.stack([img_2d, img_2d // 2], axis=1)
+    ist_2d = CorrectDrift(img_2d, "tcyx")
+
+    for t0 in [0, 7]:
+        ofs = ist_2d._estimate_drift_absolute(t0, channel=0)
+        ok = ofs - example.shifts + example.shifts[t0]
+        assert np.allclose(ok, 0), "Shift estimation mismatch. Diff:\n"
+
+
+def test_tczyx():
+    example = Example_TZYX()
+    img_3d = example.create()
+
+    img_3d = np.stack([img_3d, img_3d // 2], axis=1)
+    ist_3d = CorrectDrift(img_3d, "tczyx")
+
+    for t0 in [0, 16]:
+        ofs = ist_3d._estimate_drift_absolute(t0, channel=0)
+        ok = ofs - example.shifts + example.shifts[t0]
+        assert np.allclose(ok, 0), "Shift estimation mismatch."
+
+
+def test_tyx_roi():
+    example = Example_TYX()
+    img_2d = example.create()
+    ist_2d = CorrectDrift(img_2d, "tyx")
+
+    poly2d = np.array(
+        [
+            [23.0, 122.57642757, 142.27071085],
+            [23.0, 122.57642757, 190.77701484],
+            [23.0, 181.65929409, 190.77701484],
+            [23.0, 181.65929409, 142.27071085],
+        ]
+    )
+
+    roi = ROIRect.from_shape_poly(poly2d, "tyx", 0, 1)
+    t0 = roi.t0
+
+    ofs = ist_2d._estimate_drift_absolute(t0, roi=roi)
+    ok = ofs - example.shifts + example.shifts[t0]
+    assert np.allclose(ok, 0), "Shift estimation mismatch."
+
+
+def test_tzyx_roi():
+    example = Example_TZYX()
+    img_3d = example.create()
+    ist_3d = CorrectDrift(img_3d, "tzyx")
+
+    poly_3d = np.array(
+        [
+            [11.0, 7.0, 59.46602839, 35.94993883],
+            [11.0, 7.0, 59.46602839, 66.41732531],
+            [11.0, 7.0, 89.637615, 66.41732531],
+            [11.0, 7.0, 89.637615, 35.94993883],
+        ]
+    )
+
+    roi = ROIRect.from_shape_poly(poly_3d, "tzyx", 3, 10)
+    t0 = roi.t0
+
+    ofs = ist_3d._estimate_drift_absolute(t0, roi=roi)
+    ok = ofs - example.shifts + example.shifts[t0]
+    assert np.allclose(ok, 0), "Shift estimation mismatch."
+
+
+def test_tczyx_roi():
+    example = Example_TZYX()
+    img_3d = example.create()
+
+    img_3d = np.stack([img_3d, img_3d[::-1]], axis=1)
+
+    ist_3d = CorrectDrift(img_3d, "tczyx")
+
+    poly_3d = np.array(
+        [
+            [11.0, 0, 2.0, 59.46602839, 35.94993883],
+            [11.0, 0, 7.0, 59.46602839, 66.41732531],
+            [11.0, 0, 7.0, 89.637615, 66.41732531],
+            [11.0, 0, 11.0, 89.637615, 35.94993883],
+        ]
+    )
+
+    roi = ROIRect.from_shape_poly(poly_3d, "tzyx", 0, 15)
+    t0 = roi.t0
+    c0 = roi.c0
+    ofs = ist_3d._estimate_drift_absolute(t0, roi=roi)
+
+    ok = ofs - example.shifts + example.shifts[t0]
+    assert np.allclose(ok, 0)
+
+
+def test_tyx_rel():
+    example = Example_TYX()
+    img_2d = example.create()
+    ist_2d = CorrectDrift(img_2d, "tyx")
+
+    for t0 in [0, 6, 13]:
+        ofs = ist_2d._estimate_drift_relative(t0)
+        ok = ofs - example.shifts + example.shifts[t0]
+        assert np.allclose(ok, 0)
+
+
+def test_tczyx_rel():
+    example = Example_TZYX()
+    img_3d = example.create()
+
+    img_3d = np.stack([img_3d, img_3d[:, ::-1] // 2], axis=1)
+
+    ist_3d = CorrectDrift(img_3d, "tczyx")
+
+    for t0 in [0, 16]:
+        ofs = ist_3d._estimate_drift_relative(t0, channel=0)
+        ok = ofs - example.shifts + example.shifts[t0]
+        assert np.allclose(ok, 0), str(ok)
+
+
+def test_tyx_roi_rel():
+    example = Example_TYX()
+    img_2d = example.create()
+    ist_2d = CorrectDrift(img_2d, "tyx")
+
+    poly2d = np.array(
+        [
+            [23.0, 122.57642757, 142.27071085],
+            [23.0, 122.57642757, 190.77701484],
+            [23.0, 181.65929409, 190.77701484],
+            [23.0, 181.65929409, 142.27071085],
+        ]
+    )
+
+    start_roi = ROIRect.from_shape_poly(poly2d, "tyx", 0, 1)
+    t0 = start_roi.t0
+
+    ofs = ist_2d._estimate_drift_relative(t0, roi=start_roi)
+    ok = ofs - example.shifts + example.shifts[t0]
+    assert np.allclose(ok, 0), str(ok)
+
+
+def test_tyx_roi_rel_no_win():
+    example = Example_TYX()
+    img_2d = example.create()
+    ist_2d = CorrectDrift(img_2d, "tyx")
+
+    poly2d = np.array(
+        [
+            [23.0, 122.57642757, 142.27071085],
+            [23.0, 122.57642757, 190.77701484],
+            [23.0, 181.65929409, 190.77701484],
+            [23.0, 181.65929409, 142.27071085],
+        ]
+    )
+
+    start_roi = ROIRect.from_shape_poly(poly2d, "tyx", 0, 1)
+    t0 = start_roi.t0
+
+    ofs = ist_2d._estimate_drift_relative(t0, roi=start_roi, windowing=False)
+    ok = ofs - example.shifts + example.shifts[t0]
+    assert np.allclose(ok, 0), str(ok)
+
+
+def test_zcyx_roi_rel():
+    example = Example_TZYX()
+    img_3d = example.create()
+
+    img_3d = np.stack([img_3d, img_3d[::-1]], axis=1)
+    ist_3d = CorrectDrift(img_3d, "tczyx")
+
+    poly_3d = np.array(
+        [
+            [11.0, 0, 2.0, 59.46602839, 35.94993883],
+            [11.0, 0, 7.0, 59.46602839, 66.41732531],
+            [11.0, 0, 7.0, 89.637615, 66.41732531],
+            [11.0, 0, 11.0, 89.637615, 35.94993883],
+        ]
+    )
+
+    roi = ROIRect.from_shape_poly(poly_3d, "tzyx", 4, 15)
+    t0 = roi.t0
+    c0 = roi.c0
+    assert t0 == 11
+    assert c0 == 0
+    assert roi.z_min == 4
+    assert roi.z_max == 15
+
+    ofs = ist_3d._estimate_drift_relative(t0, roi=roi)
+
+    ok = ofs - example.shifts + example.shifts[t0]
+
+    assert np.allclose(ok, 0), str(ok)
+
+
+def test_tyx_big_roi_rel():
+    example = Example_TYX()
+    img_2d = example.create()
+    ist_2d = CorrectDrift(img_2d, "tyx")
+
+    poly2d = np.array(
+        [
+            [23.0, 2.57642757, 4.27071085],
+            [23.0, 2.57642757, 250.77701484],
+            [23.0, 250.65929409, 250.77701484],
+            [23.0, 250.65929409, 4.27071085],
+        ]
+    )
+
+    start_roi = ROIRect.from_shape_poly(poly2d, "tyx", 0, 1)
+    t0 = start_roi.t0
+
+    ofs = ist_2d._estimate_drift_relative(t0, roi=start_roi)
+
+    ok = ofs - example.shifts + example.shifts[t0]
+    assert np.allclose(ok, 0)
+
+
+def test_roirect():
+    poly_3d = np.array(
+        [
+            [11.0, 3.0, 11.0, 59.46602839, 35.94993883],
+            [11.0, 3.0, 11.0, 59.46602839, 66.41732531],
+            [11.0, 3.0, 11.0, 89.637615, 66.41732531],
+            [11.0, 3.0, 11.0, 89.637615, 35.94993883],
+        ]
+    )
+
+    roi = ROIRect.from_shape_poly(poly_3d, "tczyx", 7, 15)
+    t0 = roi.t0
+    c0 = roi.c0
+    assert t0 == 11
+    assert c0 == 3
+    assert roi.z_min == 7
+    assert roi.z_max == 15
+    assert roi.y_max == 90
+    assert roi.x_min == 36
+
+    roi_clone = ROIRect.from_bbox(roi.bbox, t0, c0)
+    assert np.allclose(roi.bbox, roi_clone.bbox)
+    assert np.allclose(roi.c0, roi_clone.c0)

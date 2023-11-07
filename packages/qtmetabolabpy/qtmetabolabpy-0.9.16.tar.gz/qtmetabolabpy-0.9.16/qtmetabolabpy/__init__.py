cb4122f602faa55517c33247139a049fd43bcf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:08:14 +0000

    Add package qtmetabolabpy/0.9.16/qtmetabolabpy-0.9.16.tar.gz

---
+++ packages/qtmetabolabpy/qtmetabolabpy-0.9.16.tar.gz/qtmetabolabpy-0.9.16/qtmetabolabpy/__init__.py
@@ -0,0 +1,5 @@
+import metabolabpy.__init__ as ml_version
+__author__ = 'Christian Ludwig (C.Ludwig@bham.ac.uk)'
+__credits__ = 'Christian Ludwig (C.Ludwig@bham.ac.uk)'
+__version__ = '0.9.16'
+__license__ = 'GPLv3'

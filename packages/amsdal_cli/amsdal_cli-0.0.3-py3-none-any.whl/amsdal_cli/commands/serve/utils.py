1dc375cd6bd2204c30cc9e7a7bee3f12fd307a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 05:03:55 +0000

    Add package amsdal_cli/0.0.3/amsdal_cli-0.0.3-py3-none-any.whl

---
+++ packages/amsdal_cli/amsdal_cli-0.0.3-py3-none-any.whl/amsdal_cli/commands/serve/utils.py
@@ -0,0 +1,16 @@
+import shutil
+from pathlib import Path
+
+
+def cleanup_app(output_path: Path) -> None:
+    """
+    Cleanup the generated models and files after stopping.
+    """
+
+    for path in (
+        (output_path / 'models'),
+        (output_path / 'schemas'),
+        (output_path / 'fixtures'),
+        (output_path / 'warehouse'),
+    ):
+        shutil.rmtree(str(path.resolve()))

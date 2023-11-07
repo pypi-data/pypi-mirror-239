3490fdca91eb3838aa361bca35cf10c27e988dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 15:06:18 +0000

    Add package vectorizer-ai/2.0.2/vectorizer_ai-2.0.2.tar.gz

---
+++ packages/vectorizer-ai/vectorizer_ai-2.0.2.tar.gz/vectorizer_ai-2.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vectorizer_ai",
+    version="2.0.2",
+    python_requires=">3.6",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    author="Mitchell Bregman",
+    author_email="mitch@mitchbregs.com",
+    description="Python SDK for vectorizer.ai",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    keywords="vectorizer.ai svg vectorize image",
+    url="https://github.com/mitchbregs/vectorizer-ai",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+)
---
+++ packages/vectorizer-ai/vectorizer_ai-2.0.2.tar.gz/vectorizer_ai-2.0.2/vectorizer_ai/__init__.py
@@ -0,0 +1,3 @@
+from .client import VectorizerAI
+
+__all__ = ["VectorizerAI"]

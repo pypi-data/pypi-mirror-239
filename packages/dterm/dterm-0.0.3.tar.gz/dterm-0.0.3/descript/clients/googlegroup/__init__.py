7d55e8774e657f99cf2659d98ada6d838d9703d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 06:04:13 +0000

    Add package dterm/0.0.3/dterm-0.0.3.tar.gz

---
+++ packages/dterm/dterm-0.0.3.tar.gz/dterm-0.0.3/descript/clients/googlegroup/__init__.py
@@ -0,0 +1,16 @@
+import csv
+
+GROUP_FILE = 'data/google-groups'
+
+class GoogleGroupTSV:
+    def __init__(self):
+        self.entries = {}
+
+        with open(GROUP_FILE, 'r') as fp:
+            reader = csv.reader(fp, delimiter='\t')
+            for line in reader:
+                group, name, email = line
+                self.entries[name.lower().strip()] = group
+
+    def group_lookup(self, name):
+        return self.entries.get(name.lower().strip())

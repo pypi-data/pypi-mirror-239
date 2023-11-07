cba19df9eb712221142da5b425f99c0ad39e18b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:07:44 +0000

    Add package SegRunLib/0.0.3/SegRunLib-0.0.3.tar.gz

---
+++ packages/SegRunLib/SegRunLib-0.0.3.tar.gz/SegRunLib-0.0.3/README.md
@@ -0,0 +1,40 @@
+# **SegRunLib** #
+
+This is simple library 
+
+----------
+
+
+## How to install ##
+To install, you can use the command:
+
+    pip install SegRunLib
+
+Or download the repository from [GitHub](https://github.com/NotYourLady)
+
+----------
+
+## Using ##
+lala
+
+### lala ###
+lala
+> lala
+> lala
+
+  
+lala  
+  
+- lala
+  
+- lala
+  
+
+
+----------
+
+
+## From the developer ##
+
+> Hello. lala
+  
---
+++ packages/SegRunLib/SegRunLib-0.0.3.tar.gz/SegRunLib-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='SegRunLib',
+    version='0.0.3',
+    author='msst',
+    author_email='mihailshutov105@gmail.com',
+    description='This is the module for segmentator running',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NotYourLady',
+    packages=find_packages(),
+    install_requires=['torch',
+                      'torchio',
+                      'nibabel',
+                      'tqdm'],
+    classifiers=[
+    'Programming Language :: Python :: 3.10',
+    'Operating System :: OS Independent'
+    ],
+    keywords='ml nn cnn',
+    project_urls={
+    'GitHub': 'https://github.com/NotYourLady'
+    },
+    python_requires='>=3.6'
+)

844e75567f384ad86c003344d6e453c38572d69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 05:06:19 +0000

    Add package pyflowline/0.3.2/pyflowline-0.3.2-py2.py3-none-any.whl

---
+++ packages/pyflowline/pyflowline-0.3.2-py2.py3-none-any.whl/pyflowline/algorithms/direction/correct_flowline_direction.py
@@ -0,0 +1,110 @@
+import sys
+sys.setrecursionlimit(10000)
+import numpy as np 
+from pyflowline.algorithms.auxiliary.check_head_water import check_head_water
+lFlowlineIndex=0
+def correct_flowline_direction(aFlowline_in, pVertex_outlet_in):    
+    """_summary_ This function should expect the flowline may not be ordered, so the stream order info is not available.
+
+    Args:
+        aFlowline_in (_type_): _description_
+        pVertex_outlet_in (_type_): _description_
+
+    Returns:
+        _type_: List of flowline, ordered from outlet to headwater
+    """
+    
+    aFlowline_out= list()     
+    #we have to go reversely    
+    aFlag_process=None
+    global lFlowlineIndex    
+    nFlowline = len(aFlowline_in)
+    aFlag_process=np.full(nFlowline, 0, dtype =int)
+    iFlag_first = 1
+    for i in range(nFlowline):
+        pFlowline = aFlowline_in[i]
+        iFlag_dam = pFlowline.iFlag_dam
+        iStream_order = pFlowline.iStream_order
+        pVertex_start = pFlowline.pVertex_start
+        pVertex_end = pFlowline.pVertex_end
+        dDiatance = pVertex_end.calculate_distance( pVertex_outlet_in )   
+        if iFlag_first == 1:
+            dDiatance_min = dDiatance                
+            lIndex_outlet = i            
+            iFlag_first=0    
+        else:            
+            if  dDiatance < dDiatance_min:
+                dDiatance_min = dDiatance
+                #found it
+                lIndex_outlet = i      
+                pass    
+            else:
+          
+                pass
+        
+    lFlowlineIndex = 0 
+    pFlowline = aFlowline_in[lIndex_outlet]
+    pFlowline.lFlowlineIndex = lFlowlineIndex
+    pFlowline.iFlag_dam = 1 # this one is not dam, but it should be preserved
+    aFlowline_out.append(pFlowline)
+    pVertex_start = pFlowline.pVertex_start
+    pVertex_end = pFlowline.pVertex_end
+    lFlowlineIndex = lFlowlineIndex + 1    
+    #we might find more than 1 upstream    
+    def find_upstream_flowline(pVertex_start_in, pVertex_end_in):
+        nUpstream = 0 
+        aUpstream=list()
+        aReverse=list()
+        for i in range(nFlowline):
+            pFlowline = aFlowline_in[i]
+            pVerter_start = pFlowline.pVertex_start
+            pVerter_end = pFlowline.pVertex_end
+            if pVerter_end == pVertex_start_in  and pVerter_start!=pVertex_end_in:
+                if aFlag_process[i] != 1:
+                    nUpstream = nUpstream + 1
+                    aUpstream.append(i)
+                    aReverse.append(0)
+                    aFlag_process[i] = 1
+                    pass
+            else:
+                if pVerter_start == pVertex_start_in and pVerter_end !=pVertex_end_in :
+                    if aFlag_process[i] != 1:
+                        nUpstream = nUpstream + 1
+                        aUpstream.append(i)
+                        aReverse.append(1)
+                        aFlag_process[i] = 1
+                        pass
+                pass
+
+            pass
+        return nUpstream, aUpstream, aReverse
+    
+    
+    def tag_upstream(pVertex_start_in, pVertex_end_in):
+        
+        global lFlowlineIndex
+        if(check_head_water(aFlowline_in, pVertex_start_in)==1):            
+            pass
+        else:
+            nUpstream, aUpstream, aReverse = find_upstream_flowline(pVertex_start_in, pVertex_end_in)
+            if nUpstream > 0:
+                
+                for j in range(nUpstream):
+                    pFlowline = aFlowline_in[ aUpstream[j] ] 
+                    if (aReverse[j]==1):             
+                        pFlowline.reverse()                    
+                        pass
+                    else:
+                        pass
+                    pFlowline.lFlowlineIndex = lFlowlineIndex
+                    aFlowline_out.append(pFlowline)
+                    lFlowlineIndex = lFlowlineIndex + 1
+                    tag_upstream(  pFlowline.pVertex_start, pFlowline.pVertex_end  )            
+
+                pass
+            else:
+                pass
+
+    tag_upstream(pVertex_start, pVertex_end)
+    
+    return aFlowline_out
---
+++ packages/pyflowline/pyflowline-0.3.2-py2.py3-none-any.whl/pyflowline/algorithms/merge/merge_flowline.py
@@ -0,0 +1,135 @@
+
+import numpy as np
+
+
+import importlib
+iFlag_cython = importlib.util.find_spec("cython") 
+if iFlag_cython is not None:
+    from pyflowline.algorithms.cython.kernel import find_vertex_in_list
+else:
+    from pyflowline.algorithms.auxiliary.find_vertex_in_list import find_vertex_in_list
+
+lID = 0
+
+def merge_flowline(aFlowline_in, 
+                   aVertex_in, 
+                    pVertex_outlet_in, 
+                    aIndex_headwater_in,
+                    aIndex_middle_in, 
+                    aIndex_confluence_in ):
+    """Merge the flowline feature to its simplist format
+
+    Args:
+        aFlowline_in (_type_): _description_
+        aVertex_in (_type_): _description_
+        pVertex_outlet_in (_type_): _description_
+        aIndex_headwater_in (_type_): _description_
+        aIndex_middle_in (_type_): _description_
+        aIndex_confluence_in (_type_): _description_
+
+    Returns:
+        List: The flowline are strictly ordered from outlet to headwater
+    """
+
+    nVertex=len(aVertex_in)
+    nFlowline = len(aFlowline_in)
+    aFlowline_out=list()               
+    aVertex = np.array(aVertex_in)
+    aIndex_headwater = np.array(aIndex_headwater_in)
+    aIndex_middle = np.array(aIndex_middle_in)
+    aIndex_confluence = np.array(aIndex_confluence_in)
+    if aIndex_middle.size == 0:
+        return aFlowline_in
+    
+    aVertex_headwater=aVertex[aIndex_headwater]    
+    aVertex_middle=aVertex[aIndex_middle]   
+    if aIndex_confluence.size > 0:        
+        iFlag_confluence = 1
+        aVertex_confluence=aVertex[aIndex_confluence]    
+    else:
+        iFlag_confluence = 0
+        pass
+    
+    def merge_flowline_reach(lIndex_in, pVertex_start_in, pVertex_end_in):
+        global lID
+        pFlowline = aFlowline_in[lIndex_in]
+        iSegment = pFlowline.iStream_segment
+        pVertex_current = pVertex_start_in
+        
+        while (find_vertex_in_list(aVertex_middle.tolist(), pVertex_current)[0] ==1):            
+            for j in range(0, nFlowline):      
+                pFlowline2 = aFlowline_in[j]                
+                pVertex_start = pFlowline2.pVertex_start
+                pVertex_end = pFlowline2.pVertex_end
+                if pVertex_end == pVertex_current:
+                    pFlowline = pFlowline.merge_upstream(pFlowline2)
+                    pVertex_current = pVertex_start                    
+                    break
+                else:
+                    pass
+
+               
+        #go to next 
+        if find_vertex_in_list(aVertex_headwater.tolist(), pVertex_current)[0] ==1: 
+            pFlowline.iStream_segment = iSegment      
+            pFlowline.iStream_order = 1           
+            pFlowline.lFlowlineIndex = lID
+            aFlowline_out.append(pFlowline)        
+            lID = lID + 1 
+            return
+        else:
+            pFlowline.iStream_segment = iSegment              
+            pFlowline.lFlowlineIndex = lID
+            aFlowline_out.append(pFlowline)        
+            lID = lID + 1 
+            #confluence
+            if find_vertex_in_list(aVertex_confluence.tolist(), pVertex_current)[0] ==1: 
+                for k in range(0, nFlowline):                      
+                    pFlowline3 = aFlowline_in[k]                
+                    pVertex_start = pFlowline3.pVertex_start
+                    pVertex_end = pFlowline3.pVertex_end
+                    if pVertex_end == pVertex_current:
+                        merge_flowline_reach(k, pVertex_start, pVertex_end)
+                                
+    
+    #find outlet
+    iFlag_first=1
+    for i in range(nFlowline):        
+        pFlowline = aFlowline_in[i]                
+        pVertex_start = pFlowline.pVertex_start
+        pVertex_end = pFlowline.pVertex_end
+        dDiatance = pVertex_end.calculate_distance( pVertex_outlet_in)
+        if iFlag_first ==1:
+            dDiatance_min = dDiatance                
+            lIndex_outlet = i            
+            iFlag_first=0    
+        else:            
+            if  dDiatance < dDiatance_min:
+                dDiatance_min = dDiatance           
+                lIndex_outlet = i                
+                pass    
+            else:
+                pass
+            
+    pFlowline = aFlowline_in[lIndex_outlet]            
+    pVertex_start = pFlowline.pVertex_start
+    pVertex_end = pFlowline.pVertex_end   
+
+    #now start from outlet
+    if iFlag_confluence == 1:
+        #check whether outlet is a confluence
+        if  (find_vertex_in_list(aVertex_confluence.tolist(), pVertex_end)[0] ==1):
+            for i in range(nFlowline):
+                pFlowline = aFlowline_in[i]  
+                pVertex_start_dummy = pFlowline.pVertex_start
+                pVertex_end_dummy = pFlowline.pVertex_end
+
+                if pVertex_end == pVertex_end_dummy:
+                    #this is 
+                    merge_flowline_reach(i, pVertex_start_dummy, pVertex_end_dummy)  
+            pass
+        else:
+            merge_flowline_reach(lIndex_outlet, pVertex_start, pVertex_end)   
+    else:
+        merge_flowline_reach(lIndex_outlet, pVertex_start, pVertex_end)   
+    return aFlowline_out

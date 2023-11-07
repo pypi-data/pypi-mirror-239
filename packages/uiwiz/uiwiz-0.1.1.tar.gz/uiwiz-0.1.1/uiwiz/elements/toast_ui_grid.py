f7dc9c6dac0e5d0c25f80a8206b4fbbc6bd9a391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 05:05:29 +0000

    Add package uiwiz/0.1.1/uiwiz-0.1.1.tar.gz

---
+++ packages/uiwiz/uiwiz-0.1.1.tar.gz/uiwiz-0.1.1/uiwiz/elements/toast_ui_grid.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from uiwiz.element import Element
+import json
+
+class ToastUIGrid(Element):
+    def __init__(self, df: pd.DataFrame, id: str = None) -> None:
+        super().__init__("div")
+        self.classes("toastgrid")
+        cols = []
+        rows = []
+        if df is not None:
+            cols = [{"header": item, "name": item} for item in df.columns.to_list()]
+            rows = json.dumps(df.to_dict("records"), default=str)
+        self.script = f"""
+        var Grid = tui.Grid;
+            var instance = new Grid({{
+              columnOptions: {{
+                minWidth: 180
+            }},
+            el: document.getElementById('{self.id if id is None else id}'),
+            columns: {cols},
+            data: {rows},
+            usageStatistics: false
+        }});
+
+        Grid.applyTheme('striped');
+        """

cb3d2276b276ed03fe8b88828324b32f79705204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:06:45 +0000

    Add package proalgotrader-strategy-runner/0.0.1/proalgotrader_strategy_runner-0.0.1.tar.gz

---
+++ packages/proalgotrader-strategy-runner/proalgotrader_strategy_runner-0.0.1.tar.gz/proalgotrader_strategy_runner-0.0.1/README.md
@@ -0,0 +1,1 @@
+ProAlgoTrader Strategy Runner
---
+++ packages/proalgotrader-strategy-runner/proalgotrader_strategy_runner-0.0.1.tar.gz/proalgotrader_strategy_runner-0.0.1/proalgotrader_strategy_runner/strategy_runner.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+
+
+def run_strategy():
+    try:
+        args = " ".join(sys.argv)
+
+        subprocess.run(
+            f"trader_core {args}",
+            shell=True,
+            check=True,
+        )
+
+        subprocess.run(
+            "uvicorn proalgotrader_strategy_runner.main:app --host 127.0.0.1 --port 5555",
+            shell=True,
+            check=True,
+        )
+    except Exception as e:
+        print(e)
+        sys.exit()

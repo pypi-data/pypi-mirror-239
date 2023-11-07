ce99b49a0b8b2761bb698191e065df58b0ec9e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:05:34 +0000

    Add package bizhi-framework/1.0.12/bizhi-framework-1.0.12.tar.gz

---
+++ packages/bizhi-framework/bizhi-framework-1.0.12.tar.gz/bizhi-framework-1.0.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bizhi-framework',
+    version='1.0.12',
+    author='huangwenbin',
+    description='bizhi project base framework',
+    packages=['bizhi_framework','bizhi_framework.bizhi_web'],
+    install_requires=[
+        'seven-framework>=1.1.32'
+    ],
+    password='pypi-AgEIcHlwaS5vcmcCJGMzODEzYWZhLTUyY2EtNDU5OC1iZWNkLTJhNDgxNWViYzE2OQACKlszLCI3N2M3MWVlMy05NGQwLTRiZDEtYWI3YS00OTQ1ZTdlNmQwNzciXQAABiDFZYU2iKKyTt3EdZykU7KO7YnNwiPujkQ-q8m1q8Kvrg'
+)
+
+#python setup.py sdist bdist_wheel
+#twine upload --username __token__ --password pypi-AgEIcHlwaS5vcmcCJDA4NTVkOTQ2LTIzMDktNDFmOS1hYzk5LTI0NjFjZmU2ZjgxMAACF1sxLFsiYml6aGktZnJhbWV3b3JrIl1dAAIsWzIsWyIwOWIwOWNlOC01YjkwLTQ2ZTAtOGVlNS1jYTUwODA3OTViMWYiXV0AAAYgftFLJuAlJj39FlLn_HFcqMBz8bbcbgUe2ioto84w-nA dist/*

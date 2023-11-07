1ce0bb477b5cc13d8b2f6aaeba69f795aef56a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:08:09 +0000

    Add package sigProfilerPlotting/1.3.17/sigProfilerPlotting-1.3.17-py3-none-any.whl

---
+++ packages/sigProfilerPlotting/sigProfilerPlotting-1.3.17-py3-none-any.whl/sigProfilerPlotting/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILERPLOTTING SETUP.PY
+short_version = '1.3.17'
+version = '1.3.17'
+update = 'Upgrade v1.3.17: Update input handling to use deepcopy'
+    
+    

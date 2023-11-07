d31018d1988250ac5f48ecf111afb9f23e344281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:04:42 +0000

    Add package run-stardist/0.0.1/run_stardist-0.0.1-py3-none-any.whl

---
+++ packages/run-stardist/run_stardist-0.0.1-py3-none-any.whl/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+# __version__ = open('plantseg/__version__.py', encoding='utf-8').read()
+long_description = open("README.md", 'r', encoding='utf-8').read()
+
+setup(
+    name="run-stardist",  # Replace with your own username
+    version='0.1.0',
+    author="Qin Yu",
+    author_email="qin.yu@embl.de",
+    license="bsd-3-clause",
+    description="Train or use StarDist models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qin-yu/ovules-instance-segmentation",
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+        'stardist',
+        'wandb',
+        'pydantic==1.*',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Image Processing",
+    ],
+    keywords="stardist image-processing deep-learning bioimage-analysis",
+    python_requires='>=3.10, <3.12',  # >=3.10 for pydantic and <3.12 for tensorflow
+    entry_points={
+        'console_scripts': [
+            'train-stardist=runstardist.train:main',
+            'predict-stardist=runstardist.predict:main',
+        ],
+    },
+)
+
+# bump-my-version bump --current-version 0.1.0 patch setup.py

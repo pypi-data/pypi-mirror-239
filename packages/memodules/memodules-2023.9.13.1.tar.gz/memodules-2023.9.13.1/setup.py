676b61f25dab7624a636e5e02da4b291fe90f62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 15:06:39 +0000

    Add package memodules/2023.9.13.1/memodules-2023.9.13.1.tar.gz

---
+++ packages/memodules/memodules-2023.9.13.1.tar.gz/memodules-2023.9.13.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='memodules',
+    version='2023.09.13.01',
+    author='mie31',
+    author_email='mie.mey.master@icloud.com',
+    description='local use functions',
+    keywords='No redistribution!',
+    packages=find_packages(),  # インポートするパッケージのリスト
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+)

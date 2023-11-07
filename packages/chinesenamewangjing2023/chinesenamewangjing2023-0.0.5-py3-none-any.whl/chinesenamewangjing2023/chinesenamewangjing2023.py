ff5c0fd1fa207492287ac54e27048405ca1a4576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 03:04:34 +0000

    Add package chinesenamewangjing2023/0.0.5/chinesenamewangjing2023-0.0.5-py3-none-any.whl

---
+++ packages/chinesenamewangjing2023/chinesenamewangjing2023-0.0.5-py3-none-any.whl/chinesenamewangjing2023/chinesenamewangjing2023.py
@@ -0,0 +1,15 @@
+
+def prit_lol(the_list, level):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            prit_lol(each_item, level + 1)
+        else:
+            for i in range(level):
+                print('\t', end='')
+            print(each_item)
+
+    return each_item
+
+#
+# the_list = ['A', 'a', 'B', 'b', 'C', 'c', ['1', '2', '3']]
+# prit_lol(the_list, 0)

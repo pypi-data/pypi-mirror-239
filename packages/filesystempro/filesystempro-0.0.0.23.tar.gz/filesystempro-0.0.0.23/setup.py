6c71db410684675218f290d2a52235a882c93431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 06:04:35 +0000

    Add package filesystempro/0.0.0.23/filesystempro-0.0.0.23.tar.gz

---
+++ packages/filesystempro/filesystempro-0.0.0.23.tar.gz/filesystempro-0.0.0.23/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name = 'filesystempro',
+    version = '0.0.0.23',
+    url = 'https://github.com/hbisneto/FileSystemPro',
+    license = 'MIT License',
+    
+    author = 'Heitor Bisneto',
+    author_email = 'heitor.bardemaker@live.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    description = u'FileSystem Pro is designed to identify the operating system (OS) on which it`s running and define the paths to various user directories based on the OS.',
+    install_requires = [''],
+    long_description = readme,
+    long_description_content_type = "text/markdown",
+    keywords = ['FileSystem', 'Linux', 'macOS', 'Windows', 'File', 'System'],
+    packages = ['filesystem', 'filesystem/wrapper'],
+    platforms = 'any',
+    python_requires= '>=3.8',
+)

6ef07b990bdf93e745e2e6ec5d3b8f628b07c6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 06:04:23 +0000

    Add package totemlib/0.1.0.23/totemlib-0.1.0.23.tar.gz

---
+++ packages/totemlib/totemlib-0.1.0.23.tar.gz/totemlib-0.1.0.23/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='totemlib',
+    version='0.1.0.23',
+    packages=find_packages(),
+    install_requires=[
+        # lista de dependencias
+        'jproperties==2.1.1',
+    ],
+    author='Totem Bear',
+    author_email='info@totembear.com',
+    description='Base library for general uses',
+)

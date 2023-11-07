e37b7b7474a99d6e743c04bd4e4fc4c0ad185485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 14:07:46 +0000

    Add package kent/1.1.0/kent-1.1.0.tar.gz

---
+++ packages/kent/kent-1.1.0.tar.gz/kent-1.1.0/src/kent/cli_server.py
@@ -0,0 +1,29 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import socket
+
+os.environ["FLASK_APP"] = "kent.app"
+
+from flask.cli import main as flask_main  # noqa
+from werkzeug import serving  # noqa
+
+
+def mock_get_interface_ip(family):
+    # NOTE(willkg): we do this weird thing becauwe werkzeug tries really hard
+    # to be helpful when the host is 0.0.0.0 or something like that and if
+    # you're running it in a docker container, it picks up the ip address of
+    # the container which is really unhelpful.
+    if family == socket.AF_INET:
+        return "localhost"
+    if family == socket.AF_INET6:
+        return "::1"
+
+
+serving.get_interface_ip = mock_get_interface_ip
+
+
+def main():
+    flask_main()

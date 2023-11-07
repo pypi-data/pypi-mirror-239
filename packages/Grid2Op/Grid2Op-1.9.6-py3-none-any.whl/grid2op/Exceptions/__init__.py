c861f88258cce85bd9667f7994c7d07763600361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:05:34 +0000

    Add package Grid2Op/1.9.6/Grid2Op-1.9.6-py3-none-any.whl

---
+++ packages/Grid2Op/Grid2Op-1.9.6-py3-none-any.whl/grid2op/Exceptions/__init__.py
@@ -0,0 +1,142 @@
+__all__ = [
+    "Grid2OpException",
+    "EnvError",
+    "IncorrectNumberOfLoads",
+    "IncorrectNumberOfGenerators",
+    "IncorrectNumberOfLines",
+    "IncorrectNumberOfSubstation",
+    "IncorrectNumberOfStorages",
+    "IncorrectNumberOfElements",
+    "IncorrectPositionOfLoads",
+    "IncorrectPositionOfGenerators",
+    "IncorrectPositionOfLines",
+    "IncorrectPositionOfStorages",
+    "UnknownEnv",
+    "MultiEnvException",
+    "IllegalAction",
+    "OnProduction",
+    "VSetpointModified",
+    "ActiveSetPointAbovePmax",
+    "ActiveSetPointBelowPmin",
+    "OnLoad",
+    "OnLines",
+    "InvalidReconnection",
+    "UnitCommitorRedispachingNotAvailable",
+    "NotEnoughGenerators",
+    "GeneratorTurnedOffTooSoon",
+    "GeneratorTurnedOnTooSoon",
+    "InvalidRedispatching",
+    "InvalidBusStatus",
+    "InvalidNumberOfObjectEnds",
+    "InvalidNumberOfLines",
+    "InvalidNumberOfGenerators",
+    "InvalidNumberOfLoads",
+    "UnrecognizedAction",
+    "InvalidLineStatus",
+    "InvalidStorage",
+    "InvalidCurtailment",
+    "AmbiguousAction",
+    "NonFiniteElement",
+    "AmbiguousActionRaiseAlert",
+    "DivergingPowerFlow",
+    "BaseObservationError",
+    "NoForecastAvailable",
+    "SimulateError",
+    "SimulateUsedTooMuchThisStep",
+    "SimulateUsedTooMuchThisEpisode",
+    "ChronicsError",
+    "ChronicsNotFoundError",
+    "InsufficientData",
+    "BackendError",
+    "DivergingPowerflow",
+    "IslandedGrid",
+    "IsolatedElement",
+    "DisconnectedLoad",
+    "DisconnectedGenerator",
+    "PlotError",
+    "OpponentError",
+    "UsedRunnerError",
+    "NotEnoughAttentionBudget",
+    "AgentError",
+    "SimulatorError",
+    "HandlerError"
+]
+
+from grid2op.Exceptions.Grid2OpException import Grid2OpException
+
+from grid2op.Exceptions.EnvExceptions import (EnvError,
+                                              IncorrectNumberOfLoads,
+                                              IncorrectNumberOfGenerators,
+                                              IncorrectNumberOfLines,
+                                              IncorrectNumberOfSubstation,
+                                              IncorrectNumberOfStorages,
+                                              IncorrectNumberOfElements,
+                                              IncorrectPositionOfLoads,
+                                              IncorrectPositionOfGenerators,
+                                              IncorrectPositionOfLines,
+                                              IncorrectPositionOfStorages,
+                                              UnknownEnv,
+                                              MultiEnvException)
+
+from grid2op.Exceptions.IllegalActionExceptions import (IllegalAction,
+                                                        OnProduction,
+                                                        VSetpointModified,
+                                                        ActiveSetPointAbovePmax,
+                                                        ActiveSetPointBelowPmin,
+                                                        OnLoad,
+                                                        OnLines,
+                                                        InvalidReconnection,
+                                                        UnitCommitorRedispachingNotAvailable,
+                                                        )
+
+from grid2op.Exceptions.AmbiguousActionExceptions import (NotEnoughGenerators,
+                                                          GeneratorTurnedOffTooSoon,
+                                                          GeneratorTurnedOnTooSoon,
+                                                          InvalidRedispatching,
+                                                          InvalidBusStatus,
+                                                          InvalidNumberOfObjectEnds,
+                                                          InvalidNumberOfLines,
+                                                          InvalidNumberOfGenerators,
+                                                          InvalidNumberOfLoads,
+                                                          InvalidCurtailment,
+                                                          UnrecognizedAction,
+                                                          InvalidLineStatus,
+                                                          InvalidStorage,
+                                                          AmbiguousAction,
+                                                          NonFiniteElement,
+                                                          AmbiguousActionRaiseAlert)
+
+from grid2op.Exceptions.PowerflowExceptions import DivergingPowerFlow
+
+from grid2op.Exceptions.ObservationExceptions import (BaseObservationError,
+                                                      NoForecastAvailable,
+                                                      SimulateError,
+                                                      SimulateUsedTooMuchThisStep,
+                                                      SimulateUsedTooMuchThisEpisode)
+
+from grid2op.Exceptions.ChronicsExceptions import (ChronicsError,
+                                                   ChronicsNotFoundError,
+                                                   InsufficientData,
+                                                   )
+from grid2op.Exceptions.handlers_exceptions import (HandlerError,
+                                                    )
+
+from grid2op.Exceptions.BackendExceptions import (BackendError,
+                                                  DivergingPowerflow,
+                                                  IslandedGrid,
+                                                  IsolatedElement,
+                                                  DisconnectedLoad,
+                                                  DisconnectedGenerator,
+                                                  )
+
+from grid2op.Exceptions.PlotExceptions import PlotError
+
+from grid2op.Exceptions.OpponentError import OpponentError
+
+from grid2op.Exceptions.RunnerError import UsedRunnerError
+
+from grid2op.Exceptions.AttentionBudgetExceptions import NotEnoughAttentionBudget
+
+from grid2op.Exceptions.agentError import AgentError
+
+from grid2op.Exceptions.simulatorExceptions import SimulatorError

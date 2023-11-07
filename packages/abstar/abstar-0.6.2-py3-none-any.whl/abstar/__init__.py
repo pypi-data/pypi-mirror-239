7049f295c8fe93d6f4c812953860abdbf53737e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 16:06:51 +0000

    Add package abstar/0.6.2/abstar-0.6.2-py3-none-any.whl

---
+++ packages/abstar/abstar-0.6.2-py3-none-any.whl/abstar/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+from Bio import BiopythonWarning
+
+warnings.simplefilter("ignore", BiopythonWarning)
+
+from .core.abstar import run, run_standalone, main, create_parser, validate_args
+from .utils.preprocess import fastqc, adapter_trim, quality_trim
+
+from .version import __version__

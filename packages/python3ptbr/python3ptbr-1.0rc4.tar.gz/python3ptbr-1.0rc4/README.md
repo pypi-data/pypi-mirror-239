34f811c296b2af446281592053d3d533830e2b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-07 04:06:11 +0000

    Add package python3ptbr/1.0rc4/python3ptbr-1.0rc4.tar.gz

---
+++ packages/python3ptbr/python3ptbr-1.0rc4.tar.gz/python3ptbr-1.0rc4/README.md
@@ -0,0 +1,3 @@
+# Python3-ptbr
+
+Python3 library, write python codes in Portuguese.
---
+++ packages/python3ptbr/python3ptbr-1.0rc4.tar.gz/python3ptbr-1.0rc4/python3ptbr/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa: F401
+# noreorder
+"""
+Pytubefix: a very serious Python library for downloading YouTube Videos.
+"""
+__title__ = "python3-ptbr"
+__author__ = "Juan Bindez"
+__license__ = "MIT License"
+__js__ = None
+__js_url__ = None
+
+from python3ptbr.__main__ import *

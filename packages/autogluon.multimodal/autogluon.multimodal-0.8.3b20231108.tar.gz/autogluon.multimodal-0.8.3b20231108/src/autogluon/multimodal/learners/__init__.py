460fd2601f34d98458bbc18e666b3285e099baed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 17:07:58 +0000

    Add package autogluon.multimodal/0.8.3b20231108/autogluon.multimodal-0.8.3b20231108.tar.gz

---
+++ packages/autogluon.multimodal/autogluon.multimodal-0.8.3b20231108.tar.gz/autogluon.multimodal-0.8.3b20231108/src/autogluon/multimodal/learners/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseLearner
+from .matching import MultiModalMatcher
+from .ner import NERLearner
+from .object_detection import ObjectDetectionLearner
+from .semantic_segmentation import SemanticSegmentationLearner

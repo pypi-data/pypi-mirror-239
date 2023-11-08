dad2be23efe235783fbdcd191ea5eeb398a59109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:36 +0000

    Add package sstgame/0.2.1/sstgame-0.2.1.tar.gz

---
+++ packages/sstgame/sstgame-0.2.1.tar.gz/sstgame-0.2.1/tests/test_wyr.py
@@ -0,0 +1,40 @@
+import pytest
+import tempfile
+from src.sstgame import wyr
+from io import StringIO
+
+def test_wouldYouRatherWithFileChoiceOne(capfd, monkeypatch):
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        tmp_file.write('Would you rather eat food or drink water')
+
+    inputString = StringIO("1")
+    monkeypatch.setattr('sys.stdin', inputString)
+
+    result = wyr.wouldYouRather(tmp_file.name)
+
+    captured = capfd.readouterr()
+
+    assert "Would you rather eat food or drink water\nPlease enter 1 for choice 1 or 2 for choice 2: "==captured.out
+    assert result == 1
+
+def test_wouldYouRatherWithFileChoiceTwo(capfd, monkeypatch):
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        tmp_file.write('Would you rather eat food or drink water')
+
+    inputString = StringIO("2")
+    monkeypatch.setattr('sys.stdin', inputString)
+
+    result = wyr.wouldYouRather(tmp_file.name)
+
+    captured = capfd.readouterr()
+
+    assert "Would you rather eat food or drink water\nPlease enter 1 for choice 1 or 2 for choice 2: "==captured.out
+    assert result == 1
+
+def test_wouldYouRatherWithOutFile(monkeypatch):
+    inputString = StringIO("drink water")
+    monkeypatch.setattr('sys.stdin', inputString)
+
+    result = wyr.wouldYouRather()
+    assert result == 1
+

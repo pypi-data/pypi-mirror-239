dba8f6b05f30edf4c4d56fab651f8ac463466c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:05:59 +0000

    Add package cml-pam/0.3.1/cml_pam-0.3.1-py3-none-any.whl

---
+++ packages/cml-pam/cml_pam-0.3.1-py3-none-any.whl/pam/__init__.py
@@ -0,0 +1,41 @@
+__version__ = "0.3.1"
+
+
+class PAMValidationError(Exception):
+    """Custom exception raised for an Activity Plan validation Error."""
+
+
+class PAMSequenceValidationError(PAMValidationError):
+    """Custom exception raised for an Activity Plan sequence validation Error."""
+
+
+class PAMTimesValidationError(PAMValidationError):
+    """Custom exception raised for an Activity Plan Time validation Error."""
+
+
+class PAMInvalidStartTimeError(PAMTimesValidationError):
+    """Custom exception raised for an Activity Plan Time validation Error."""
+
+
+class PAMInvalidTimeSequenceError(PAMTimesValidationError):
+    """Custom exception raised for an Activity Plan Time validation Error."""
+
+
+class PAMInvalidEndTimeError(PAMTimesValidationError):
+    """Custom exception raised for an Activity Plan Time validation Error."""
+
+
+class PAMValidationLocationsError(PAMValidationError):
+    """Custom exception raised for an Activity Plan Locations validation Error."""
+
+
+class PAMVehicleIdError(PAMValidationError):
+    """Custom exception raised for Vehicle not matching Person ID."""
+
+
+class PAMVehicleTypeError(PAMValidationError):
+    """Custom exception raised for Vehicle type error."""
+
+
+class InvalidMATSimError(PAMValidationError):
+    """Custom exception raised for invalid MATSim."""

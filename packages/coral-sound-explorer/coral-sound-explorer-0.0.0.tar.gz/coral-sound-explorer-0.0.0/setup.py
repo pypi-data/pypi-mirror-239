18bc5a109affbc24d6b1366a30083aa602685c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:05:23 +0000

    Add package coral-sound-explorer/0.0.0/coral-sound-explorer-0.0.0.tar.gz

---
+++ packages/coral-sound-explorer/coral-sound-explorer-0.0.0.tar.gz/coral-sound-explorer-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="coral-sound-explorer",
+    version="0.0.0",
+    description="CoralSoundExplorer",
+    author="Bamdad Sabbagh",
+    author_email="hi@bamdad.fr",
+    url="",
+    packages=["src"],
+)

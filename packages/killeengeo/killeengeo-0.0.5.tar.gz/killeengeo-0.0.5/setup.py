3bbfa37ba120e0c6ea6c01998045af78f63e05dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:11:09 +0000

    Add package killeengeo/0.0.5/killeengeo-0.0.5.tar.gz

---
+++ packages/killeengeo/killeengeo-0.0.5.tar.gz/killeengeo-0.0.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="killeengeo",
+    version="0.0.5",
+    description="2D and 3D geometry in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Benjamin D. Killeen",
+    author_email="killeen@jhu.edu",
+    url="https://killeengeo.readthedocs.io/en/latest/",
+    python_requires=">=3.10",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "typing_extensions",
+    ],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)

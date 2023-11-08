f81f56f60ebf36489b046f619a6df766aa64b122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:43 +0000

    Add package langchain-qa-with-references/0.0.330/langchain_qa_with_references-0.0.330-py3-none-any.whl

---
+++ packages/langchain-qa-with-references/langchain_qa_with_references-0.0.330-py3-none-any.whl/langchain_qa_with_references/chains/qa_with_references/__init__.py
@@ -0,0 +1,12 @@
+"""Load question answering with reference chains.
+
+We believe hallucinations pose a significant issue. Enabling straightforward referencing
+of what led to the answer's derivation is imperative.
+
+This chain extracts the information from the documents that was used to answer the
+question. The output `source_documents` contains only the documents that were used.
+"""
+from .base import QAWithReferencesChain
+from .retrieval import RetrievalQAWithReferencesChain
+
+__all__ = ["QAWithReferencesChain", "RetrievalQAWithReferencesChain"]

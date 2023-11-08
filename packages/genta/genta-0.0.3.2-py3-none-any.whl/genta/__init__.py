4e7eb495a8e525eb140421aab8eef2acbb2e0cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:04:52 +0000

    Add package genta/0.0.3.2/genta-0.0.3.2-py3-none-any.whl

---
+++ packages/genta/genta-0.0.3.2-py3-none-any.whl/genta/__init__.py
@@ -0,0 +1,11 @@
+"""
+genta-api
+
+Simple python interface to access models from Genta API.
+"""
+
+__version__ = "0.0.3.2"
+__author__ = 'Rifky Bujana bisri'
+__credits__ = 'Genta Technology'
+
+from .api import GentaAPI

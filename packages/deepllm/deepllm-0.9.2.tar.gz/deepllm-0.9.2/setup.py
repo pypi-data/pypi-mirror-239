6a64597de822e1ca54ecf7b3388a7bd9988ab4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:26 +0000

    Add package deepllm/0.9.2/deepllm-0.9.2.tar.gz

---
+++ packages/deepllm/deepllm-0.9.2.tar.gz/deepllm-0.9.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import setuptools
+
+import deepllm
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+version = deepllm.__version__
+setup(name='deepllm',
+      version=version,
+      description='Deep, recursive, goal-driven LLM explorer',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ptarau/recursors.git',
+      author='Paul Tarau',
+      author_email='paul.tarau@gmail.com',
+      license='GPL-3',
+      packages=setuptools.find_packages(),
+      package_data={
+          'deepllm': [
+              'tests/*.*',
+              'apps/*.*',
+              'demos/*.*',
+              'local_llms/*.*'
+          ]
+      },
+      include_package_data=True,
+      install_requires=required,
+      zip_safe=False
+      )

ba50088358a681b462061e38e4ffa4c414863ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 17:08:12 +0000

    Add package weaviate-client/4.1b1/weaviate-client-4.1b1.tar.gz

---
+++ packages/weaviate-client/weaviate-client-4.1b1.tar.gz/weaviate-client-4.1b1/weaviate/classes.py
@@ -0,0 +1,47 @@
+from weaviate.collections.classes.config import (
+    Configure,
+    Reconfigure,
+    DataType,
+    Multi2VecField,
+    Property,
+    ReferenceProperty,
+    ReferencePropertyMultiTarget,
+    Tokenization,
+    VectorDistance,
+)
+from weaviate.collections.classes.data import (
+    DataObject,
+)
+from weaviate.collections.classes.filters import Filter
+from weaviate.collections.classes.grpc import (
+    HybridFusion,
+    FromNested,
+    FromReference,
+    FromReferenceMultiTarget,
+    MetadataQuery,
+)
+from weaviate.collections.classes.internal import Nested, CrossReference, Reference
+from weaviate.collections.classes.tenants import Tenant
+
+__all__ = [
+    "Configure",
+    "Reconfigure",
+    "CrossReference",
+    "DataObject",
+    "DataType",
+    "Filter",
+    "HybridFusion",
+    "FromNested",
+    "FromReference",
+    "FromReferenceMultiTarget",
+    "MetadataQuery",
+    "Multi2VecField",
+    "Nested",
+    "Property",
+    "Reference",
+    "ReferenceProperty",
+    "ReferencePropertyMultiTarget",
+    "Tenant",
+    "Tokenization",
+    "VectorDistance",
+]

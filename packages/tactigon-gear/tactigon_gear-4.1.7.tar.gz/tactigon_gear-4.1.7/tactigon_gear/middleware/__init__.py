8ad2769a2ac61be8f1b9a535d48d120a4c844f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:05:18 +0000

    Add package tactigon-gear/4.1.7/tactigon_gear-4.1.7.tar.gz

---
+++ packages/tactigon-gear/tactigon_gear-4.1.7.tar.gz/tactigon_gear-4.1.7/tactigon_gear/middleware/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['Tactigon_Gesture', 'Tactigon_Recorder']
+
+from tactigon_gear.middleware.Tactigon_Gesture import Tactigon_Gesture
+from tactigon_gear.middleware.Tactigon_Recorder import Tactigon_Recorder
---
+++ packages/tactigon-gear/tactigon_gear-4.1.7.tar.gz/tactigon_gear-4.1.7/tactigon_gear/middleware/utilities/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['Tactigon_RT_Computing', 'Data_Preprocessor']
+
+from tactigon_gear.middleware.utilities.Data_Preprocessor import Data_Preprocessor
+from tactigon_gear.middleware.utilities.Tactigon_RT_Computing import Tactigon_RT_Computing

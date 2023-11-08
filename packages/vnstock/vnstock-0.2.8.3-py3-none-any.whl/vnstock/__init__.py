d9c192bf869cec114b1cb984fce486e320773d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:41 +0000

    Add package vnstock/0.2.8.3/vnstock-0.2.8.3-py3-none-any.whl

---
+++ packages/vnstock/vnstock-0.2.8.3-py3-none-any.whl/vnstock/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Thinh Vu @ GitHub
+# See LICENSE for details.
+
+__author__ = "Thinh Vu @thinh-vu in GitHub"
+__version__ = "0.2.0"
+
+from .config import *
+from .derivative import *
+from .fundamental import *
+from .technical import *
+from .trading import *
+from .market import *
+# from .chart import *
+from .utils import *
+from .integration import *

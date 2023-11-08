41bed9f27eca920aa586c6d53aadef54a22a01f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:11:03 +0000

    Add package lilit/1.2.8/lilit-1.2.8-py3-none-any.whl

---
+++ packages/lilit/lilit-1.2.8-py3-none-any.whl/lilit/__init__.py
@@ -0,0 +1,13 @@
+"""# Welcome to LiLit!
+
+A Python package encoding the likelihood for LiteBIRD.
+
+.. include:: ../README.md
+"""
+
+from .likelihood import LiLit
+from .functions import *
+
+__author__ = "Giacomo Galloni"
+__version__ = "1.2.8"
+__docformat__ = "numpy"

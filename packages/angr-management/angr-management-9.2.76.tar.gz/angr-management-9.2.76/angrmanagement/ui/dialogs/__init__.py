84d0d3e5a2ec3e4a38b6b5385ba0fe5ef83c28b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:15 +0000

    Add package angr-management/9.2.76/angr-management-9.2.76.tar.gz

---
+++ packages/angr-management/angr-management-9.2.76.tar.gz/angr-management-9.2.76/angrmanagement/ui/dialogs/__init__.py
@@ -0,0 +1,19 @@
+from .analysis_options import AnalysisOptionsDialog
+from .assemble_patch import AssemblePatchDialog
+from .breakpoint import BreakpointDialog
+from .function import FunctionDialog
+from .load_binary import LoadBinary
+from .load_docker_prompt import LoadDockerPrompt
+from .load_plugins import LoadPlugins
+from .preferences import Preferences
+
+__all__ = [
+    "AnalysisOptionsDialog",
+    "AssemblePatchDialog",
+    "BreakpointDialog",
+    "FunctionDialog",
+    "LoadBinary",
+    "LoadDockerPrompt",
+    "LoadPlugins",
+    "Preferences",
+]

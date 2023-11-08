81aad19db058281dda8edcc7da425839a03e9fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 15:05:10 +0000

    Add package pweb-form-rest/0.0.4/pweb_form_rest-0.0.4-py3-none-any.whl

---
+++ packages/pweb-form-rest/pweb_form_rest-0.0.4-py3-none-any.whl/pweb_form_rest/schema/pweb_rest_schema.py
@@ -0,0 +1,66 @@
+from marshmallow import Schema, fields
+from marshmallow_sqlalchemy import SQLAlchemySchema
+
+
+class APIBase(Schema):
+    class Meta:
+        ordered = True
+
+
+class APIBaseResponse(APIBase):
+    class Meta:
+        ordered = True
+
+    status = fields.String()
+    code = fields.String()
+
+
+class APIMessageResponse(APIBaseResponse):
+    message = fields.String()
+
+
+class APIErrorResponse(APIBaseResponse):
+    error = fields.Dict(keys=fields.String(), values=fields.String())
+
+
+class APIDataResponse(APIBaseResponse):
+    message = fields.String()
+    data = fields.Dict()
+
+
+class APIDataListResponse(APIBaseResponse):
+    data = fields.List(fields.Dict)
+
+
+class Pagination(APIBase):
+    page = fields.Integer()
+    itemPerPage = fields.Integer()
+    total = fields.Integer()
+    totalPage = fields.Integer()
+
+
+class APIPaginateResponse(APIDataListResponse):
+    pagination = fields.Nested(Pagination())
+
+
+class PWebDataDTO(APIBase, object):
+    pass
+
+
+class PWebOrmDTO(PWebDataDTO, SQLAlchemySchema):
+
+    def set_meta_model(self, model):
+        if hasattr(self, "Meta") and hasattr(self, "opts"):
+            setattr(self.Meta, "model", model)
+            setattr(self.opts, "model", model)
+        return self
+
+
+class PWebRestDTO(PWebOrmDTO):
+    class Meta:
+        ordered = True
+
+    id = fields.Integer(dump_only=True)
+    created = fields.DateTime(dump_only=True)
+    updated = fields.DateTime(dump_only=True)
+    uuid = fields.UUID(dump_only=True)

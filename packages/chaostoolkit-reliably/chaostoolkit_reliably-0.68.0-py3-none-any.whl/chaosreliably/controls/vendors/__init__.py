33611ab749fe3d22d28083335417db6b6cc4a1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:43 +0000

    Add package chaostoolkit-reliably/0.68.0/chaostoolkit_reliably-0.68.0-py3-none-any.whl

---
+++ packages/chaostoolkit-reliably/chaostoolkit_reliably-0.68.0-py3-none-any.whl/chaosreliably/controls/vendors/__init__.py
@@ -0,0 +1,33 @@
+from logzero import logger
+
+from chaosreliably.controls.vendors.honeycomb import HoneycombVendorHandler
+
+__all__ = ["apply_vendors", "register_vendors"]
+VENDORS = []
+
+
+def register_vendors() -> None:
+    if HoneycombVendorHandler.is_on():
+        VENDORS.append(HoneycombVendorHandler())
+
+
+def apply_vendors(method: str, **kwargs) -> None:  # type: ignore
+    for v in VENDORS:
+        if method == "started":
+            try:
+                v.started(**kwargs)
+            except Exception:
+                logger.debug(
+                    "failed to apply 'started' method on vendor "
+                    f"class {v.__class__.__name__}",
+                    exc_infp=True,
+                )
+        elif method == "finished":
+            try:
+                v.finished(**kwargs)
+            except Exception:
+                logger.debug(
+                    "failed to apply 'finished' method on vendor "
+                    f"class {v.__class__.__name__}",
+                    exc_infp=True,
+                )

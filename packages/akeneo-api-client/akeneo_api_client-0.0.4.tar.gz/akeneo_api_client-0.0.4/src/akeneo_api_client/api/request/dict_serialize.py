87577b347904955248243ad87de97ab4f49b0a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:45 +0000

    Add package akeneo-api-client/0.0.4/akeneo_api_client-0.0.4.tar.gz

---
+++ packages/akeneo-api-client/akeneo_api_client-0.0.4.tar.gz/akeneo_api_client-0.0.4/src/akeneo_api_client/api/request/dict_serialize.py
@@ -0,0 +1,11 @@
+import json
+from api.request.json_serializable import JsonSerializable
+
+
+class DictSerialize(JsonSerializable):
+
+    def __init__(self, obj: dict):
+        self.obj = obj
+
+    def serialize(self) -> str:
+        return json.dumps(self.obj)

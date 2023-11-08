2b6ab7dcfd2aca0ab30c15900ec3c777e50b068d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:40 +0000

    Add package mutwo.common/0.11.1/mutwo.common-0.11.1.tar.gz

---
+++ packages/mutwo.common/mutwo.common-0.11.1.tar.gz/mutwo.common-0.11.1/mutwo/common_generators/__init__.py
@@ -0,0 +1,19 @@
+from . import constants
+
+from .brown import *
+from .brun import *
+from .chomksy import *
+from .edwards import *
+from .gray import *
+from .koenig import *
+from .lehmer import *
+from .toussaint import *
+
+from mutwo import core_utilities
+
+__all__ = core_utilities.get_all(
+    brown, brun, chomksy, edwards, gray, koenig, lehmer, toussaint
+)
+
+# Force flat structure
+del brown, brun, core_utilities, chomksy, edwards, gray, koenig, lehmer, toussaint
---
+++ packages/mutwo.common/mutwo.common-0.11.1.tar.gz/mutwo.common-0.11.1/mutwo/common_version/__init__.py
@@ -0,0 +1,6 @@
+MAJOR, MINOR, PATCH = 0, 11, 1
+
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+"""The version of the package ``mutwo.common``."""
+
+del MAJOR, MINOR, PATCH

67d10c159595e08e20333c72af8ee16487adf7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:00 +0000

    Add package phixpy/0.0.2/phixpy-0.0.2-py3-none-any.whl

---
+++ packages/phixpy/phixpy-0.0.2-py3-none-any.whl/phixpy/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+# Frontend of the 'phystuff'
+
+from .backend.vector.vector import vector
+from .backend.compressor import compressor  # Example, replace with your actual class names
+from .backend.math_funcs_utils import math_funcs
+from .backend.math_funcs_utils import uid
+from .backend.visualization.vector_plot import VectorPlot

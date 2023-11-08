a2d51a2f01aea9bfe436f06e1df25e706afb4b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:33 +0000

    Add package runtimepy/3.0.0/runtimepy-3.0.0-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-3.0.0-py3-none-any.whl/runtimepy/net/apps/__init__.py
@@ -0,0 +1,18 @@
+"""
+A module aggregating commonly used network applications (that use the
+connection-arbiter technology).
+"""
+
+# internal
+from runtimepy.net.arbiter import AppInfo, init_only
+
+__all__ = ["AppInfo", "init_only", "wait_for_stop"]
+
+
+async def wait_for_stop(app: AppInfo) -> int:
+    """Waits for the stop signal to be set."""
+
+    await app.all_finalized()
+    await app.stop.wait()
+
+    return 0

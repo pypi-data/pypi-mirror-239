a981c1a06ebe65240271eb89dab1b4a477849be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 06:04:48 +0000

    Add package eth-typing/3.5.2/eth-typing-3.5.2.tar.gz

---
+++ packages/eth-typing/eth-typing-3.5.2.tar.gz/eth-typing-3.5.2/setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+extras_require = {
+    "test": [
+        "pytest>=7.0.0",
+        "pytest-xdist>=2.4.0",
+    ],
+    "lint": [
+        "flake8==6.0.0",  # flake8 claims semver but adds new warnings at minor releases, leave it pinned.
+        "flake8-bugbear==23.3.23",  # flake8-bugbear does not follow semver, leave it pinned.
+        "isort>=5.10.1",
+        "mypy==0.971",  # mypy does not follow semver, leave it pinned.
+        "pydocstyle>=6.0.0",
+        "black>=23",
+        "types-setuptools",
+    ],
+    "docs": [
+        "sphinx>=5.0.0",
+        "sphinx_rtd_theme>=1.0.0",
+        "towncrier>=21,<22",
+    ],
+    "dev": [
+        "bumpversion>=0.5.3",
+        "pytest-watch>=4.1.0",
+        "tox>=4.0.0",
+        "build>=0.9.0",
+        "wheel",
+        "twine",
+        "ipython",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"]
+    + extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["docs"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="eth-typing",
+    # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
+    version="3.5.2",
+    description="""eth-typing: Common type annotations for ethereum python packages""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/eth-typing",
+    include_package_data=True,
+    install_requires=[
+        # remove typing_extensions after python_requires>=3.8
+        "typing-extensions>=4.0.1",
+    ],
+    python_requires=">=3.7.2, <4",
+    extras_require=extras_require,
+    py_modules=["eth_typing"],
+    license="MIT",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"eth_typing": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)

604ec1976e5a4b354a95d2d0b98df0011d2f90b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:10:51 +0000

    Add package eyes-images/5.20.8/eyes_images-5.20.8-py2.py3-none-any.whl

---
+++ packages/eyes-images/eyes_images-5.20.8-py2.py3-none-any.whl/applitools/images/runner.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+from applitools.common.runner import ClassicEyesRunner
+
+
+class ClassicRunner(ClassicEyesRunner):
+    BASE_AGENT_ID = "eyes.images.python"

cf40467d6c60cce242859713d253b4514917c54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:23 +0000

    Add package iparapheur-internal/1.10.0/iparapheur-internal-1.10.0.tar.gz

---
+++ packages/iparapheur-internal/iparapheur-internal-1.10.0.tar.gz/iparapheur-internal-1.10.0/iparapheur_internal/apis/paths/api_internal_admin_tenant_tenant_id_signature_templates_template_type_example.py
@@ -0,0 +1,7 @@
+from iparapheur_internal.paths.api_internal_admin_tenant_tenant_id_signature_templates_template_type_example.post import ApiForpost
+
+
+class ApiInternalAdminTenantTenantIdSignatureTemplatesTemplateTypeExample(
+    ApiForpost,
+):
+    pass

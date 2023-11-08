0212909734a39459f406ba16b44cec6db9a27657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:22 +0000

    Add package python-tosspayments/1.1.0/python-tosspayments-1.1.0.tar.gz

---
+++ packages/python-tosspayments/python-tosspayments-1.1.0.tar.gz/python-tosspayments-1.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-tosspayments",
+    version="1.1.0",
+    description="Toss Payments SDK for python. 토스페이먼츠가 아닌 제 3자가 만든 SDK입니다.",
+    author="jsonlee",
+    author_email="dudegs.py@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GisangLee/python-tosspayment",
+    license="MIT",
+    install_requires=["requests"],
+    keywords=["toss", "toss payment", "payment"],
+    python_requires=">=3.8",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)

37d6aaaba9b1a3c2f3f419700f1227e7dd2d76fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 07:05:26 +0000

    Add package geopmpy/3.0.0/geopmpy-3.0.0.tar.gz

---
+++ packages/geopmpy/geopmpy-3.0.0.tar.gz/geopmpy-3.0.0/geopmpy/__init__.py
@@ -0,0 +1,28 @@
+#
+#  Copyright (c) 2015 - 2023, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+"""The geopm python package: launcher, error, endpoint, io, pio, plotter,
+policy_store, topo, agent, and version.
+
+"""
+
+import os
+
+try:
+    from geopmpy.version import __version__
+    from geopmpy.version import __beta__
+except ImportError:
+    try:
+        # Look for VERSION file in git repository
+        file_path = os.path.abspath(__file__)
+        src_version_path = os.path.join(
+                           os.path.dirname(
+                           os.path.dirname(
+                           os.path.dirname(file_path))),
+                           'VERSION')
+        with open(src_version_path) as fid:
+            __version__ = fid.read().strip()
+    except IOError:
+        __version__ = '0.0.0'

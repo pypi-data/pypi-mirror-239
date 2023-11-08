6798955975626e36cd2e54ab8282fd2b0a866375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:05:41 +0000

    Add package opsml/1.4.11/opsml-1.4.11.tar.gz

---
+++ packages/opsml/opsml-1.4.11.tar.gz/opsml-1.4.11/opsml/registry/data/types.py
@@ -0,0 +1,52 @@
+# Copyright (c) Shipt, Inc.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Dict, Mapping, Optional, Union
+
+import numpy as np
+import pyarrow as pa
+from polars.datatypes.classes import DataType, DataTypeClass
+from pydantic import BaseModel, ConfigDict
+
+POLARS_SCHEMA = Mapping[str, Union[DataTypeClass, DataType]]  # pylint: disable=invalid-name
+
+
+@dataclass
+class DataCardUris:
+    """Data uri holder for DataCardMetadata
+
+    Args:
+        data_uri:
+            Location where converted data is stored
+        datacard_uri:
+            Location where DataCard is stored
+        profile_uri:
+            Location where profile is stored
+        profile_html_uri:
+            Location where profile html is stored
+    """
+
+    data_uri: Optional[str] = None
+    datacard_uri: Optional[str] = None
+    profile_uri: Optional[str] = None
+    profile_html_uri: Optional[str] = None
+
+
+class AllowedTableTypes(str, Enum):
+    NDARRAY = "ndarray"
+    ARROW_TABLE = "Table"
+    PANDAS_DATAFRAME = "PandasDataFrame"
+    POLARS_DATAFRAME = "PolarsDataFrame"
+    DICTIONARY = "Dictionary"
+    IMAGE_DATASET = "ImageDataset"
+
+
+class ArrowTable(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    table: Union[pa.Table, np.ndarray]
+    table_type: AllowedTableTypes
+    storage_uri: Optional[str] = None
+    feature_map: Optional[Union[Dict[str, Any], POLARS_SCHEMA]] = None

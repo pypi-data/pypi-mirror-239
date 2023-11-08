0ded3099f11e36272f5e2d8d75ae14c0adde6679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:10:27 +0000

    Add package magnelPy/0.2.2/magnelPy-0.2.2.tar.gz

---
+++ packages/magnelPy/magnelPy-0.2.2.tar.gz/magnelPy-0.2.2/magnelPy/SFE/MaterialProperties/__init__.py
@@ -0,0 +1,18 @@
+"""
+magnelPy.SFE.MaterialProperties
+============
+Provides
+  1. Temperature-dependent mechanical properties (functions)
+
+=============== =================================================================
+Steel Properties ~ Steel
+=================================================================================
+ky_EC3          Steel yield stress retention factor according to EN 1993-1-2:2005
+kE_EC3          Steel modulus of elasticity retention factor according to EN 1993-1-2:2005
+=============== =================================================================
+
+"""
+
+### get submodules ###
+from magnelPy.SFE.MaterialProperties import Steel
+

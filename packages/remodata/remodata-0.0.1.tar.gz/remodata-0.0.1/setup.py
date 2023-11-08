12fe6d10bba8bc09d5153c6626996e7ad537fd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:53 +0000

    Add package remodata/0.0.1/remodata-0.0.1.tar.gz

---
+++ packages/remodata/remodata-0.0.1.tar.gz/remodata-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='remodata',
+    version='0.0.1',
+    description='',
+    author='tkms',
+    author_email='tkmnet@users.noreply.github.com',
+    url='https://github.com/tkmnet/remodata',
+    packages=find_packages(),
+)

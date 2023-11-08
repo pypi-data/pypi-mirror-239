d981d3b25d0620f91257dcc2a0aca6240a8a5126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:39 +0000

    Add package eo-grow/1.6.3/eo_grow-1.6.3-py3-none-any.whl

---
+++ packages/eo-grow/eo_grow-1.6.3-py3-none-any.whl/eogrow/types.py
@@ -0,0 +1,32 @@
+""" Includes custom types used in schemas
+"""
+
+import datetime
+import sys
+from enum import Enum
+from typing import Any, Dict, List, Literal, Tuple, Union
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+from eolearn.core import EONode
+from sentinelhub import BBox
+
+PatchList: TypeAlias = List[Tuple[str, BBox]]
+ExecKwargs: TypeAlias = Dict[str, Dict[EONode, Dict[str, object]]]
+
+ImportPath: TypeAlias = str
+TimePeriod: TypeAlias = Tuple[datetime.date, datetime.date]
+
+BoolOrAuto: TypeAlias = Union[Literal["auto"], bool]
+
+JsonDict: TypeAlias = Dict[str, Any]
+RawSchemaDict: TypeAlias = Dict[str, Any]
+
+
+class ProcessingType(Enum):
+    RAY = "ray"
+    SINGLE = "single"
+    MULTI = "multi"

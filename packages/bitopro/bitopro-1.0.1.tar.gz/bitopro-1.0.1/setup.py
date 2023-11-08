14a098d485c21729bfc4c7ab93e2fe25df3de3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:33 +0000

    Add package bitopro/1.0.1/bitopro-1.0.1.tar.gz

---
+++ packages/bitopro/bitopro-1.0.1.tar.gz/bitopro-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().split("\n")
+
+setuptools.setup(
+    name="bitopro",
+    version="1.0.1",
+    author="bitopro",
+    author_email="twfxjjbw@gmail.com",
+    description="bitopro-api-python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+)

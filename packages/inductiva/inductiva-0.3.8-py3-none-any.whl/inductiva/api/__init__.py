7a2e7ee4d9ce3f7fed68b74f7031f1b0a87fe2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:19 +0000

    Add package inductiva/0.3.8/inductiva-0.3.8-py3-none-any.whl

---
+++ packages/inductiva/inductiva-0.3.8-py3-none-any.whl/inductiva/api/__init__.py
@@ -0,0 +1,4 @@
+"""Functionality to interact with the Web API."""
+from .methods import download_output, invoke_async_api, get_task_status
+from .methods import validate_api_key
+from .methods import get_client

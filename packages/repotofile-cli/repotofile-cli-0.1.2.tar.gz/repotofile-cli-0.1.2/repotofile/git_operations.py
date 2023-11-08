d6e76c939f14a5f3ade53f0054f727864db4a84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:10:27 +0000

    Add package repotofile-cli/0.1.2/repotofile-cli-0.1.2.tar.gz

---
+++ packages/repotofile-cli/repotofile-cli-0.1.2.tar.gz/repotofile-cli-0.1.2/repotofile/git_operations.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def clone_repo(url, branch='master', output_dir='repo_dir'):
+    # Clone the repository into the specified directory
+    clone_command = ['git', 'clone', '--depth', '1']
+    if branch:  # If a specific branch or tag is given, use it
+        clone_command += ['--branch', branch]
+    clone_command += [url, output_dir]
+
+    try:
+        subprocess.run(clone_command, check=True)
+        print(f'Repository cloned into {output_dir}')
+    except subprocess.CalledProcessError as e:
+        print(f'Error cloning repository: {e}')
+        return False
+
+    return True

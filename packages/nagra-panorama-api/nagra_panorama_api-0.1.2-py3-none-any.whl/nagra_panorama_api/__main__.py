7dbcc8c4a4812cc0405991a7d8a5c242bc9033ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 17:08:12 +0000

    Add package nagra-panorama-api/0.1.2/nagra_panorama_api-0.1.2-py3-none-any.whl

---
+++ packages/nagra-panorama-api/nagra_panorama_api-0.1.2-py3-none-any.whl/nagra_panorama_api/__main__.py
@@ -0,0 +1,21 @@
+import logging
+
+import click
+from nagra_network_misc_utils.logger import set_default_logger  # noqa: F401
+
+from .dump_dir import dump_dir
+
+# set_default_logger()  # Already called somewhere
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(dump_dir)
+
+if __name__ == '__main__':
+    cli()

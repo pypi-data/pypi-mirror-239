4b8bb666577fa14f629583e71c0280804813cd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 06:04:49 +0000

    Add package ringity/0.3a5/ringity-0.3a5-py3-none-any.whl

---
+++ packages/ringity/ringity-0.3a5-py3-none-any.whl/ringity/tda/phomology.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from ringity.tda.pdiagram.pdiagram import PDiagram
+from gtda.homology import VietorisRipsPersistence # CHANGE FOR FUTURE: OPTIONAL IMPORT
+
+# =============================================================================
+#  ----------------------- VIETORIS-RIPS PERSISTENCE -------------------------
+# =============================================================================
+
+def vietoris_rips_from_point_cloud(X,
+                            metric = 'euclidean',
+                            metric_params = {},
+                            homology_dimensions = (0, 1),
+                            collapse_edges = False,
+                            coeff = 2,
+                            max_edge_length = np.inf,
+                            infinity_values = None,
+                            reduced_homology = True,
+                            n_jobs = None):
+    VR = VietorisRipsPersistence(
+                            metric = metric,
+                            metric_params = metric_params,
+                            homology_dimensions = homology_dimensions,
+                            collapse_edges = collapse_edges,
+                            coeff = coeff,
+                            max_edge_length = max_edge_length,
+                            infinity_values = infinity_values,
+                            reduced_homology = reduced_homology,
+                            n_jobs = n_jobs)
+    dgm = VR.fit_transform([X])[0]
+    return PDiagram.from_gtda(dgm)
+

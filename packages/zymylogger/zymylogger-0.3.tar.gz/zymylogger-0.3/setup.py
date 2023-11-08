1569cf3fc497644caea61792059b9005fcf5914b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:25 +0000

    Add package zymylogger/0.3/zymylogger-0.3.tar.gz

---
+++ packages/zymylogger/zymylogger-0.3.tar.gz/zymylogger-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zymylogger',
+    version='0.03',
+    author='zhang yue',
+    packages=['zymylogger'],
+    install_requires=[
+        '',
+    ],
+)

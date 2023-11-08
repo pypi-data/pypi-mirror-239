518ab22475d28181c55f465437eb1dd2b23c950d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:05:41 +0000

    Add package tfacon/1.1.3/tfacon-1.1.3.tar.gz

---
+++ packages/tfacon/tfacon-1.1.3.tar.gz/tfacon-1.1.3/setup.py
@@ -0,0 +1,13 @@
+import os
+import re
+
+from setuptools import setup
+
+
+setup(
+    name='tfacon',
+    version="1.1.3",
+    description="tfacon",
+    author="Red Hat Inc",
+)
+os.system('source scripts/install_tfacon.sh')

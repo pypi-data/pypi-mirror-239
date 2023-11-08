44cdf5be9c98c4615b9653a54a7b65f3e27d5114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:51 +0000

    Add package paradocx/1.5.0/paradocx-1.5.0.tar.gz

---
+++ packages/paradocx/paradocx-1.5.0.tar.gz/paradocx-1.5.0/docs/index.rst
@@ -0,0 +1,36 @@
+Welcome to |project| documentation!
+===================================
+
+.. sidebar-links::
+   :home:
+   :pypi:
+
+.. toctree::
+   :maxdepth: 1
+
+   history
+
+
+.. automodule:: paradocx
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: paradocx.document
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: paradocx.styles
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ packages/paradocx/paradocx-1.5.0.tar.gz/paradocx-1.5.0/tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+import tempfile
+
+import pytest
+
+
+@pytest.fixture
+def sample_stream():
+    with open('tests/resources/sample.docx', mode='rb') as stream:
+        yield stream
+
+
+@pytest.fixture
+def sample_filename():
+    return 'tests/resources/sample.docx'
+
+
+@pytest.fixture
+def table_data():
+    data = [
+        ['Name', 'Age'],
+        ['Christian', 29],
+        ['Sarah', 28],
+        ['Curtis', 4],
+        ['Eli', 2],
+    ]
+    return data
+
+
+@pytest.fixture
+def writable_filename(request):
+    """
+    Whenever a function needs a 'writable_filename', create one, but
+    be sure it's cleaned up afterward.
+    """
+    fobj, name = tempfile.mkstemp()
+    os.close(fobj)
+    os.remove(name)
+
+    def remove_if_exists():
+        if os.path.exists(name):
+            os.remove(name)
+
+    request.addfinalizer(remove_if_exists)
+    return name

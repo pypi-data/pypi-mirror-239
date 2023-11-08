1842b1e478b3ae6dbd7b5dffbbf479d058af7863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:01 +0000

    Add package geneius/1.3/geneius-1.3.tar.gz

---
+++ packages/geneius/geneius-1.3.tar.gz/geneius-1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geneius',
+    version='1.3',
+    packages=['src'],
+    url='https://github.com/aaronwtr/geneius',
+    description='A tool for disease-gene evidence search and explanation',
+    long_description='More information on the GitHub repository: https://github.com/aaronwtr/geneius',
+    license='MIT',
+    author='Aaron Wenteler',
+    email='a.wenteler@qmul.ac.uk',
+    install_requires=[
+        'anthropic',
+        'biopython',
+    ],
+    entry_points={
+        'console_scripts': [
+            'geneius = src.geneius:main'
+        ]
+    }
+)

67043dc4dc905854f19c28c84dbffb15af7feb77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:38 +0000

    Add package zymysql/0.3/zymysql-0.3.tar.gz

---
+++ packages/zymysql/zymysql-0.3.tar.gz/zymysql-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zymysql',
+    version='0.03',
+    author='zhang yue',
+    packages=['zymysql'],
+    install_requires=[
+        'pymysql',
+    ],
+)

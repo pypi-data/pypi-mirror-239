3819fd276646f983d7c355e32127ac3c58515c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:40 +0000

    Add package HttpSwirl3/0.0.0/HttpSwirl3-0.0.0.tar.gz

---
+++ packages/HttpSwirl3/HttpSwirl3-0.0.0.tar.gz/HttpSwirl3-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+# :)
+VERSION = '1.0.0'
+
+# Setting up
+setup(
+    name='HttpSwirl3', 
+    author="",
+    author_email="",
+    packages=find_packages(),
+)

a23fdebc2bd972f75846f08903162f092397c1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:29 +0000

    Add package events-sdk-python/0.0.3/events_sdk_python-0.0.3-py2.py3-none-any.whl

---
+++ packages/events-sdk-python/events_sdk_python-0.0.3-py2.py3-none-any.whl/hightouch/htevents/test/constants.py
@@ -0,0 +1,6 @@
+# Test suite was originally written to run against the production backend.
+# Therefore, this is a real working write key.
+# See the event-sdk-tests-e15uy workspace as the HTTP source DO_NOT_DELETE_python_sdk_tests
+# TODO: mock requests to hightouch's v1/batch endpoint
+# See hightouch.htevents.test.oauth for examples mocking requests.Session
+TEST_WRITE_KEY = 'bad602e1b5f02b47f1768fd95ca6ca03039da01b56b3346ae9028873879ed8ae'

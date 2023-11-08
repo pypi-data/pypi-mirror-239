f2d09c6acb74c8f6f57b24400a16b3f352ab88b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 17:07:40 +0000

    Add package fuck-game/0.0.7a0/fuck-game-0.0.7a0.tar.gz

---
+++ packages/fuck-game/fuck-game-0.0.7a0.tar.gz/fuck-game-0.0.7a0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os 
+
+
+  
+setup(
+    name ='fuck-game',
+    version ='0.0.7a',
+    author ='4sushi',
+    url ='https://github.com/4sushi/fuck',
+    description ='Python game, made with curses',
+    license ='MIT',
+    packages = find_packages(),
+    entry_points ={
+        'console_scripts': [
+            'fuck-game = src.fuck_game.main:main'
+        ]
+    },
+    classifiers =(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires = [],
+    zip_safe = False
+)

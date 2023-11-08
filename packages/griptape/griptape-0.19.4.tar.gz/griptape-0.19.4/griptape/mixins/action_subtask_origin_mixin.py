5aa6019e492d0b829abf0e65165582e46812fdb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:23 +0000

    Add package griptape/0.19.4/griptape-0.19.4.tar.gz

---
+++ packages/griptape/griptape-0.19.4.tar.gz/griptape-0.19.4/griptape/mixins/action_subtask_origin_mixin.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
+from abc import abstractmethod
+from attr import define
+
+if TYPE_CHECKING:
+    from griptape.memory import ToolMemory
+    from griptape.tools import BaseTool
+    from griptape.tasks import ActionSubtask
+
+
+@define(slots=False)
+class ActionSubtaskOriginMixin:
+    @property
+    @abstractmethod
+    def action_types(self) -> list[str]:
+        ...
+
+    @abstractmethod
+    def find_tool(self, tool_name: str) -> Optional[BaseTool]:
+        ...
+
+    @abstractmethod
+    def find_memory(self, memory_name: str) -> Optional[ToolMemory]:
+        ...
+
+    @abstractmethod
+    def find_subtask(self, subtask_id: str) -> Optional[ActionSubtask]:
+        ...
+
+    @abstractmethod
+    def add_subtask(self, subtask: ActionSubtask) -> ActionSubtask:
+        ...
---
+++ packages/griptape/griptape-0.19.4.tar.gz/griptape-0.19.4/griptape/tokenizers/bedrock_jurassic_tokenizer.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+from attr import define, field, Factory
+from typing import Any
+from griptape.tokenizers import BaseTokenizer
+
+
+@define(frozen=True)
+class BedrockJurassicTokenizer(BaseTokenizer):
+    DEFAULT_MODEL = "ai21.j2-ultra-v1"
+    DEFAULT_MAX_TOKENS = 8192
+
+    session: boto3.Session = field(
+        default=Factory(lambda: boto3.Session()), kw_only=True
+    )
+    model: str = field(kw_only=True)
+    bedrock_client: Any = field(
+        default=Factory(
+            lambda self: self.session.client("bedrock-runtime"), takes_self=True
+        ),
+        kw_only=True,
+    )
+
+    @property
+    def max_tokens(self) -> int:
+        return self.DEFAULT_MAX_TOKENS
+
+    def count_tokens(self, text: str) -> int:
+        payload = {"prompt": text}
+
+        response = self.bedrock_client.invoke_model(
+            body=json.dumps(payload),
+            modelId=self.model,
+            accept="application/json",
+            contentType="application/json",
+        )
+        response_body = json.loads(response.get("body").read())
+
+        return len(response_body["prompt"]["tokens"])

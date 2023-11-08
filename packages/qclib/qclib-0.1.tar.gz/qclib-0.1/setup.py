620832d9ee827bdeef5919f217698937c979ab63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:19 +0000

    Add package qclib/0.1/qclib-0.1.tar.gz

---
+++ packages/qclib/qclib-0.1.tar.gz/qclib-0.1/setup.py
@@ -0,0 +1,33 @@
+"""
+pypi setup
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qclib",
+    version="0.1",
+    author="qclib team",
+    author_email="ajsilva@cin.ufpe.br",
+    description="A quantum computing library using qiskit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qclib/qclib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'scipy>=1.7.1',
+        'qiskit>=0.39.5',
+        'deprecation',
+        'tensorly>=0.8.0',
+        'keras',
+        'graphviz'
+    ]
+)

1a77c17e526640fc0e9d696a17b9aac732678e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:19 +0000

    Add package quos/0.0.1/quos-0.0.1.tar.gz

---
+++ packages/quos/quos-0.0.1.tar.gz/quos-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quos",
+    version="0.0.1",
+    author="Lalit Patel",
+    author_email="llsr@att.net",
+    description="Quos package for simulating quantum computing based on oscillatory quota",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lapyl/quos",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'networkx',
+        'matplotlib',
+    ],
+)

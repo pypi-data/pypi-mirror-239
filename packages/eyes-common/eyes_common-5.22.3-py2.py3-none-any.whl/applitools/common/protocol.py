42836132ca213a6c5d6980e289219b4df66ccb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:10:51 +0000

    Add package eyes-common/5.22.3/eyes_common-5.22.3-py2.py3-none-any.whl

---
+++ packages/eyes-common/eyes_common-5.22.3-py2.py3-none-any.whl/applitools/common/protocol.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, division, print_function
+
+from typing import TYPE_CHECKING
+
+from .command_context import CommandContext
+from .object_registry import ObjectRegistry
+
+if TYPE_CHECKING:
+    from typing import List, Optional, Union
+
+    from .connection import USDKConnection
+
+
+class USDKProtocol(object):
+    _CommandContext = CommandContext
+    _ObjectRegistry = ObjectRegistry
+
+    @classmethod
+    def object_registry(cls):
+        return cls._ObjectRegistry()
+
+    @classmethod
+    def context(cls, connection, object_registry=None):
+        # type: (USDKConnection, Optional[ObjectRegistry]) -> CommandContext
+        return cls._CommandContext(connection, object_registry or cls.object_registry())
+
+    @classmethod
+    def commands_or_kind(cls):
+        # type: () -> Union[str, List[str]]
+        return cls._CommandContext.commands_or_kind()

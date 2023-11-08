c868bc55d797bd15e1a14d5e694e7d48d0bc80c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:53 +0000

    Add package scdiffeq/0.0.48rc4/scdiffeq-0.0.48rc4.tar.gz

---
+++ packages/scdiffeq/scdiffeq-0.0.48rc4.tar.gz/scdiffeq-0.0.48rc4/scdiffeq/__init__.py
@@ -0,0 +1,30 @@
+
+__module_name__ = "__init__.py"
+__doc__ = """Top-level __init__ for the scDiffEq package."""
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu"])
+
+
+import os as _os
+
+
+from ._version in import _PackageVersion
+
+_package_version = _PackageVersion()
+__version__ = __VERSION__ = __Version__ = _package_version()
+
+_os.environ["KEOPS_VERBOSE"] = "0"
+
+
+# -- import model API: -------------------------------------------------------------------
+from .core._scdiffeq import scDiffEq
+
+
+# -- import sub-packages: ----------------------------------------------------------------
+from . import core
+from . import io
+# from . import plotting as pl
+from . import tools as tl
+from . import datasets
+
+from . import _backend_utilities as utils
---
+++ packages/scdiffeq/scdiffeq-0.0.48rc4.tar.gz/scdiffeq-0.0.48rc4/setup.py
@@ -0,0 +1,34 @@
+
+# -- import packages: ---------------------------------------------------------
+import setuptools
+import re
+import os
+import sys
+
+
+# -- fetch requirements packages: ---------------------------------------------
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+# -- run setup: ---------------------------------------------------------------
+setuptools.setup(
+    name="scdiffeq",
+    version="0.0.48rc4",
+    python_requires=">3.9.0",
+    author="Michael E. Vinyard",
+    author_email="mvinyard.ai@gmail.com",
+    url="https://github.com/scDiffEq/scDiffEq",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="scDiffEq: modeling single-cell dynamics using neural differential equations.",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)

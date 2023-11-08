1a9730427d5e6a587c9a5dfee331a1f629759f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:25 +0000

    Add package aaz-dev/2.2.0/aaz_dev-2.2.0-py3-none-any.whl

---
+++ packages/aaz-dev/aaz_dev-2.2.0-py3-none-any.whl/aaz_dev/utils/plane.py
@@ -0,0 +1,35 @@
+
+
+class PlaneEnum:
+    Mgmt = "mgmt-plane"
+    Data = "data-plane"
+
+    _config = {
+        Mgmt: {
+            "displayName": "Control plane",
+            "client": "MgmtClient",  # MgmtClient is implemented in azure.cli.core.aaz._client
+        },
+        Data: {
+            "displayName": "Data plane",
+            # DataPlaneClient is implemented as the default client for data plane using AAD Auth
+            "client": "DataPlaneClient",
+        }
+    }
+
+    @classmethod
+    def choices(cls):
+        return tuple(v for k, v in vars(cls).items() if not k.startswith('_') and isinstance(v, str))
+
+    @classmethod
+    def is_valid_swagger_module(cls, plane, module_name):
+        swagger_modules = cls._config[plane].get('swagger_modules', None)
+        if not swagger_modules or module_name in swagger_modules:
+            return True
+        return False
+
+    @classmethod
+    def http_client(cls, plane):
+        return cls._config[plane]['client']
+
+
+__all__ = ["PlaneEnum"]

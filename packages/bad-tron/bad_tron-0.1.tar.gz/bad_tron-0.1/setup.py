1771f4afadb1d0543c6331b0139400a6d85f49a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 15:05:34 +0000

    Add package bad-tron/0.1/bad_tron-0.1.tar.gz

---
+++ packages/bad-tron/bad_tron-0.1.tar.gz/bad_tron-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bad_tron',
+    version='0.1',
+    description='A library ',
+    author='Knox26',
+    author_email='knoxgaming7558@gmail.com',
+    packages=find_packages(),
+     classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

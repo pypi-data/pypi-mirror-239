0e12a3a7a6e2d84ee8b7bade39b8b1fcf5e00cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 15:05:23 +0000

    Add package spider-brew-kit/0.1.2/spider_brew_kit-0.1.2.tar.gz

---
+++ packages/spider-brew-kit/spider_brew_kit-0.1.2.tar.gz/spider_brew_kit-0.1.2/spider_brew_kit/contants.py
@@ -0,0 +1,6 @@
+ENCODINGS = [
+    'latin1', 'cp1252', 'iso-8859-1', 'gbk', 'ascii', 'gb2312', 'gb18030',
+    'big5', 'big5hkscs', 'hz', 'iso2022_jp', 'iso2022_jp_2',
+    'iso2022_jp_2004', 'iso2022_jp_3', 'iso2022_jp_ext', 'iso2022_kr',
+    'utf-8'
+]

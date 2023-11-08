febb7f37f5f5dc3ef2138be79bf5dff2d5065e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:05:22 +0000

    Add package sound-scape-explorer/0.0.0/sound-scape-explorer-0.0.0.tar.gz

---
+++ packages/sound-scape-explorer/sound-scape-explorer-0.0.0.tar.gz/sound-scape-explorer-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sound-scape-explorer",
+    version="0.0.0",
+    description="SoundScapeExplorer",
+    author="Bamdad Sabbagh",
+    author_email="hi@bamdad.fr",
+    url="https://github.com/sound-scape-explorer/sound-scape-explorer",
+    packages=["src"],
+)
---
+++ packages/sound-scape-explorer/sound-scape-explorer-0.0.0.tar.gz/sound-scape-explorer-0.0.0/src/__init__.py
@@ -0,0 +1,1 @@
+print("hello sound-scape-explorer")

9bc841267476fc1917f897199f944047b03f2c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:05 +0000

    Add package hshmeng/2023.11.7/hshmeng-2023.11.7.tar.gz

---
+++ packages/hshmeng/hshmeng-2023.11.7.tar.gz/hshmeng-2023.11.7/README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+This is a simple example package. You can use
+[GitHub-flavored Markdown](WO Mei You Zhe Ge/)
+to write your content.
---
+++ packages/hshmeng/hshmeng-2023.11.7.tar.gz/hshmeng-2023.11.7/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='hshmeng',
+    version='2023.11.07',
+    description='猛写的汉化',
+    author='hshmeng',
+    author_email='hshmeng@foxmail.com',
+    py_modules=['hshmeng']
+)

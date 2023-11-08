56b6a444a5cf6b1b60a4cf2750ac3b270dc2130b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:13 +0000

    Add package ecoinvent-interface/2.3/ecoinvent_interface-2.3.tar.gz

---
+++ packages/ecoinvent-interface/ecoinvent_interface-2.3.tar.gz/ecoinvent_interface-2.3/ecoinvent_interface/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    "__version__",
+    "CachedStorage",
+    "EcoinventRelease",
+    "EcoinventProcess",
+    "permanent_setting",
+    "ProcessFileType",
+    "ProcessMapping",
+    "ReleaseType",
+    "Settings",
+]
+
+__version__ = "2.3"
+
+from .storage import CachedStorage
+from .settings import Settings, permanent_setting
+from .release import EcoinventRelease, ReleaseType
+from .process_interface import EcoinventProcess, ProcessFileType
+from .mapping import ProcessMapping

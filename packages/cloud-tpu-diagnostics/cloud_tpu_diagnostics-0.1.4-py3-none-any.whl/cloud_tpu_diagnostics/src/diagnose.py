e97caf10080a07419cf2e4ff90b673b970fcf04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:31 +0000

    Add package cloud-tpu-diagnostics/0.1.4/cloud_tpu_diagnostics-0.1.4-py3-none-any.whl

---
+++ packages/cloud-tpu-diagnostics/cloud_tpu_diagnostics-0.1.4-py3-none-any.whl/cloud_tpu_diagnostics/src/diagnose.py
@@ -0,0 +1,16 @@
+import contextlib
+
+from cloud_tpu_diagnostics.src.debug import start_debugging
+from cloud_tpu_diagnostics.src.debug import stop_debugging
+
+
+@contextlib.contextmanager
+def diagnose(config):
+  """Context manager to debug and identify errors."""
+  if config is not None and config.debug_config is not None:
+    start_debugging(config.debug_config)
+  try:
+    yield
+  finally:
+    if config is not None and config.debug_config is not None:
+      stop_debugging(config.debug_config)

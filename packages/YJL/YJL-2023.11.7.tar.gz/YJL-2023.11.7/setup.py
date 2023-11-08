b0d26d7228e6ff528c7ae093195c5aac0df50dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:12:20 +0000

    Add package YJL/2023.11.7/YJL-2023.11.7.tar.gz

---
+++ packages/YJL/YJL-2023.11.7.tar.gz/YJL-2023.11.7/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='YJL',
+    version='2023.11.07',
+    description='A simple module for Base64 encoding and decoding, and printing "LiMeng Love YangJiaLing!"',
+    author='hshmeng',
+    author_email='hshmeng@foxmail.com',
+    py_modules=['YJL']
+)

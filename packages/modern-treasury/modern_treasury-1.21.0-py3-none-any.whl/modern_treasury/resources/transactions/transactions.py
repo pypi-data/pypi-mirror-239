f1cd6e46313fd0cf8457e867450e9ce5dcb086dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:36 +0000

    Add package modern-treasury/1.21.0/modern_treasury-1.21.0-py3-none-any.whl

---
+++ packages/modern-treasury/modern_treasury-1.21.0-py3-none-any.whl/modern_treasury/resources/transactions/transactions.py
@@ -0,0 +1,399 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, Union, Optional
+from datetime import date
+
+import httpx
+
+from ...types import Transaction, transaction_list_params, transaction_update_params
+from ..._types import NOT_GIVEN, Body, Query, Headers, NotGiven
+from ..._utils import maybe_transform
+from .line_items import (
+    LineItems,
+    AsyncLineItems,
+    LineItemsWithRawResponse,
+    AsyncLineItemsWithRawResponse,
+)
+from ..._resource import SyncAPIResource, AsyncAPIResource
+from ..._response import to_raw_response_wrapper, async_to_raw_response_wrapper
+from ...pagination import SyncPage, AsyncPage
+from ..._base_client import AsyncPaginator, make_request_options
+
+if TYPE_CHECKING:
+    from ..._client import ModernTreasury, AsyncModernTreasury
+
+__all__ = ["Transactions", "AsyncTransactions"]
+
+
+class Transactions(SyncAPIResource):
+    line_items: LineItems
+    with_raw_response: TransactionsWithRawResponse
+
+    def __init__(self, client: ModernTreasury) -> None:
+        super().__init__(client)
+        self.line_items = LineItems(client)
+        self.with_raw_response = TransactionsWithRawResponse(self)
+
+    def retrieve(
+        self,
+        id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Transaction:
+        """
+        Get details on a single transaction.
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get(
+            f"/api/transactions/{id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Transaction,
+        )
+
+    def update(
+        self,
+        id: str,
+        *,
+        metadata: Dict[str, str] | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+        idempotency_key: str | None = None,
+    ) -> Transaction:
+        """
+        Update a single transaction.
+
+        Args:
+          metadata: Additional data in the form of key-value pairs. Pairs can be removed by passing
+              an empty string or `null` as the value.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+
+          idempotency_key: Specify a custom idempotency key for this request
+        """
+        return self._patch(
+            f"/api/transactions/{id}",
+            body=maybe_transform({"metadata": metadata}, transaction_update_params.TransactionUpdateParams),
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                idempotency_key=idempotency_key,
+            ),
+            cast_to=Transaction,
+        )
+
+    def list(
+        self,
+        *,
+        after_cursor: Optional[str] | NotGiven = NOT_GIVEN,
+        as_of_date_end: Union[str, date] | NotGiven = NOT_GIVEN,
+        as_of_date_start: Union[str, date] | NotGiven = NOT_GIVEN,
+        counterparty_id: str | NotGiven = NOT_GIVEN,
+        description: str | NotGiven = NOT_GIVEN,
+        direction: str | NotGiven = NOT_GIVEN,
+        internal_account_id: str | NotGiven = NOT_GIVEN,
+        metadata: Dict[str, str] | NotGiven = NOT_GIVEN,
+        payment_type: str | NotGiven = NOT_GIVEN,
+        per_page: int | NotGiven = NOT_GIVEN,
+        posted: bool | NotGiven = NOT_GIVEN,
+        transactable_type: str | NotGiven = NOT_GIVEN,
+        vendor_id: str | NotGiven = NOT_GIVEN,
+        virtual_account_id: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> SyncPage[Transaction]:
+        """
+        Get a list of all transactions.
+
+        Args:
+          as_of_date_end: Filters transactions with an `as_of_date` starting on or before the specified
+              date (YYYY-MM-DD).
+
+          as_of_date_start: Filters transactions with an `as_of_date` starting on or after the specified
+              date (YYYY-MM-DD).
+
+          description: Filters for transactions including the queried string in the description.
+
+          internal_account_id: Specify `internal_account_id` if you wish to see transactions to/from a specific
+              account.
+
+          metadata: For example, if you want to query for records with metadata key `Type` and value
+              `Loan`, the query would be `metadata%5BType%5D=Loan`. This encodes the query
+              parameters.
+
+          posted: Either `true` or `false`.
+
+          vendor_id: Filters for transactions including the queried vendor id (an identifier given to
+              transactions by the bank).
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get_api_list(
+            "/api/transactions",
+            page=SyncPage[Transaction],
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform(
+                    {
+                        "after_cursor": after_cursor,
+                        "as_of_date_end": as_of_date_end,
+                        "as_of_date_start": as_of_date_start,
+                        "counterparty_id": counterparty_id,
+                        "description": description,
+                        "direction": direction,
+                        "internal_account_id": internal_account_id,
+                        "metadata": metadata,
+                        "payment_type": payment_type,
+                        "per_page": per_page,
+                        "posted": posted,
+                        "transactable_type": transactable_type,
+                        "vendor_id": vendor_id,
+                        "virtual_account_id": virtual_account_id,
+                    },
+                    transaction_list_params.TransactionListParams,
+                ),
+            ),
+            model=Transaction,
+        )
+
+
+class AsyncTransactions(AsyncAPIResource):
+    line_items: AsyncLineItems
+    with_raw_response: AsyncTransactionsWithRawResponse
+
+    def __init__(self, client: AsyncModernTreasury) -> None:
+        super().__init__(client)
+        self.line_items = AsyncLineItems(client)
+        self.with_raw_response = AsyncTransactionsWithRawResponse(self)
+
+    async def retrieve(
+        self,
+        id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Transaction:
+        """
+        Get details on a single transaction.
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._get(
+            f"/api/transactions/{id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Transaction,
+        )
+
+    async def update(
+        self,
+        id: str,
+        *,
+        metadata: Dict[str, str] | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+        idempotency_key: str | None = None,
+    ) -> Transaction:
+        """
+        Update a single transaction.
+
+        Args:
+          metadata: Additional data in the form of key-value pairs. Pairs can be removed by passing
+              an empty string or `null` as the value.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+
+          idempotency_key: Specify a custom idempotency key for this request
+        """
+        return await self._patch(
+            f"/api/transactions/{id}",
+            body=maybe_transform({"metadata": metadata}, transaction_update_params.TransactionUpdateParams),
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                idempotency_key=idempotency_key,
+            ),
+            cast_to=Transaction,
+        )
+
+    def list(
+        self,
+        *,
+        after_cursor: Optional[str] | NotGiven = NOT_GIVEN,
+        as_of_date_end: Union[str, date] | NotGiven = NOT_GIVEN,
+        as_of_date_start: Union[str, date] | NotGiven = NOT_GIVEN,
+        counterparty_id: str | NotGiven = NOT_GIVEN,
+        description: str | NotGiven = NOT_GIVEN,
+        direction: str | NotGiven = NOT_GIVEN,
+        internal_account_id: str | NotGiven = NOT_GIVEN,
+        metadata: Dict[str, str] | NotGiven = NOT_GIVEN,
+        payment_type: str | NotGiven = NOT_GIVEN,
+        per_page: int | NotGiven = NOT_GIVEN,
+        posted: bool | NotGiven = NOT_GIVEN,
+        transactable_type: str | NotGiven = NOT_GIVEN,
+        vendor_id: str | NotGiven = NOT_GIVEN,
+        virtual_account_id: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AsyncPaginator[Transaction, AsyncPage[Transaction]]:
+        """
+        Get a list of all transactions.
+
+        Args:
+          as_of_date_end: Filters transactions with an `as_of_date` starting on or before the specified
+              date (YYYY-MM-DD).
+
+          as_of_date_start: Filters transactions with an `as_of_date` starting on or after the specified
+              date (YYYY-MM-DD).
+
+          description: Filters for transactions including the queried string in the description.
+
+          internal_account_id: Specify `internal_account_id` if you wish to see transactions to/from a specific
+              account.
+
+          metadata: For example, if you want to query for records with metadata key `Type` and value
+              `Loan`, the query would be `metadata%5BType%5D=Loan`. This encodes the query
+              parameters.
+
+          posted: Either `true` or `false`.
+
+          vendor_id: Filters for transactions including the queried vendor id (an identifier given to
+              transactions by the bank).
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get_api_list(
+            "/api/transactions",
+            page=AsyncPage[Transaction],
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform(
+                    {
+                        "after_cursor": after_cursor,
+                        "as_of_date_end": as_of_date_end,
+                        "as_of_date_start": as_of_date_start,
+                        "counterparty_id": counterparty_id,
+                        "description": description,
+                        "direction": direction,
+                        "internal_account_id": internal_account_id,
+                        "metadata": metadata,
+                        "payment_type": payment_type,
+                        "per_page": per_page,
+                        "posted": posted,
+                        "transactable_type": transactable_type,
+                        "vendor_id": vendor_id,
+                        "virtual_account_id": virtual_account_id,
+                    },
+                    transaction_list_params.TransactionListParams,
+                ),
+            ),
+            model=Transaction,
+        )
+
+
+class TransactionsWithRawResponse:
+    def __init__(self, transactions: Transactions) -> None:
+        self.line_items = LineItemsWithRawResponse(transactions.line_items)
+
+        self.retrieve = to_raw_response_wrapper(
+            transactions.retrieve,
+        )
+        self.update = to_raw_response_wrapper(
+            transactions.update,
+        )
+        self.list = to_raw_response_wrapper(
+            transactions.list,
+        )
+
+
+class AsyncTransactionsWithRawResponse:
+    def __init__(self, transactions: AsyncTransactions) -> None:
+        self.line_items = AsyncLineItemsWithRawResponse(transactions.line_items)
+
+        self.retrieve = async_to_raw_response_wrapper(
+            transactions.retrieve,
+        )
+        self.update = async_to_raw_response_wrapper(
+            transactions.update,
+        )
+        self.list = async_to_raw_response_wrapper(
+            transactions.list,
+        )
---
+++ packages/modern-treasury/modern_treasury-1.21.0-py3-none-any.whl/modern_treasury/types/payment_order_type.py
@@ -0,0 +1,32 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing_extensions import Literal
+
+__all__ = ["PaymentOrderType"]
+
+PaymentOrderType = Literal[
+    "ach",
+    "au_becs",
+    "bacs",
+    "book",
+    "card",
+    "chats",
+    "check",
+    "cross_border",
+    "eft",
+    "interac",
+    "masav",
+    "neft",
+    "nics",
+    "nz_becs",
+    "provxchange",
+    "rtp",
+    "se_bankgirot",
+    "sen",
+    "sepa",
+    "sg_giro",
+    "sic",
+    "signet",
+    "wire",
+    "zengin",
+]

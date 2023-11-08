a50bc8197cbf805495f71e3f31a1be72aa54a487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:05:59 +0000

    Add package reportseff/2.3.2/reportseff-2.3.2.tar.gz

---
+++ packages/reportseff/reportseff-2.3.2.tar.gz/reportseff-2.3.2/src/reportseff/db_inquirer.py
@@ -0,0 +1,487 @@
+"""Abstract and concrete implementations of scheduler databases."""
+from abc import ABC, abstractmethod
+import datetime
+import re
+import shlex
+import subprocess
+from typing import Callable, Dict, List, Optional, Set
+
+import click
+
+
+class BaseInquirer(ABC):
+    """Abstract interface for inquiring different schedulers."""
+
+    def __init__(self) -> None:
+        """Initialize a new inquirer."""
+
+    @abstractmethod
+    def get_valid_formats(self) -> List[str]:
+        """Get the valid formatting options supported by the inquirer.
+
+        Returns:
+            List of valid format options
+        """
+
+    @abstractmethod
+    def set_sacct_args(self, jobs: List[str]) -> List[str]:
+        """Set arguments of sacct query.
+
+        Args:
+            jobs: list of job names
+
+        Returns:
+            String of sacct arguments
+
+        Raises:
+            RuntimeError: if sacct doesn't return properly
+        """
+
+    @abstractmethod
+    def get_db_output(
+        self,
+        columns: List[str],
+        jobs: List[str],
+        debug_cmd: Optional[Callable],
+    ) -> List[Dict[str, str]]:
+        """Query the database with the supplied columns.
+
+        Args:
+            columns: validated format names as strings
+            jobs: list of job names
+            debug_cmd: If specified, the raw output will passed to this function
+
+        Returns:
+            List of rows, where each row is a dictionary
+            with the columns as keys and entries as values
+            Output order is not garunteed to match the jobs list
+
+        """
+
+    @abstractmethod
+    def set_user(self, user: str) -> None:
+        """Set the collection of jobs based on the provided user.
+
+        Args:
+            user: user name
+        """
+
+    @abstractmethod
+    def set_partition(self, partition: str) -> None:
+        """Set the collection of jobs based on the provided partition.
+
+        Args:
+            partition: partition name
+        """
+
+    @abstractmethod
+    def set_extra_args(self, extra_args: str) -> None:
+        """Set extra arguments to be forwarded to sacct.
+
+        Args:
+            extra_args: list of arguments
+        """
+
+    @abstractmethod
+    def all_users(self) -> None:
+        """Ignore provided jobs, query for all users."""
+
+    @abstractmethod
+    def set_state(self, state: str) -> None:
+        """Set the state to include output jobs.
+
+        Args:
+            state: comma separated list of state names or codes
+        """
+
+    @abstractmethod
+    def set_not_state(self, state: str) -> None:
+        """Set the state to exclude from output jobs.
+
+        Args:
+            state: comma separated list of state names or codes
+        """
+
+    @abstractmethod
+    def parse_date(self, d: str) -> str:
+        """Parse and convert custom string date format.
+
+        Args:
+            d: the string of date.
+
+        Returns:
+            converted string of date
+        """
+
+    @abstractmethod
+    def set_until(self, until: str) -> None:
+        """Set the filter for time of jobs to consider.
+
+        Args:
+            until: the string for filtering.  If specified as time=amount
+                will subtract that amount from the current time
+        """
+
+    @abstractmethod
+    def set_since(self, since: str) -> None:
+        """Set the filter for time of jobs to consider.
+
+        Args:
+            since: the string for filtering.  If specified as time=amount
+                will subtract that amount from the current time
+        """
+
+    @abstractmethod
+    def has_since(self) -> bool:
+        """Tests if `since` has been set.
+
+        Returns:
+            True if set_since has been called on this inquirer
+        """
+
+    @abstractmethod
+    def get_partition_timelimits(self) -> dict:
+        """Get partition time limits.
+
+        Returns:
+            dict mapping partition names to maximum timelimits.
+        """
+
+
+class SacctInquirer(BaseInquirer):
+    """Implementation of BaseInquirer for the sacct slurm function."""
+
+    def __init__(self) -> None:
+        """Initialize a new inquirer."""
+        self.default_args = "sacct -P -n --delimiter=^|^".split()
+        self.user: Optional[str] = None
+        self.state: Optional[Set] = None
+        self.not_state: Optional[Set] = None
+        self.since: Optional[str] = None
+        self.until: Optional[str] = None
+        self.query_all_users: bool = False
+        self.partition: Optional[str] = None
+        self.extra_args: Optional[str] = None
+
+    def get_valid_formats(self) -> List[str]:
+        """Get the valid formatting options supported by the inquirer.
+
+        Returns:
+            List of valid format options
+
+        Raises:
+            RuntimeError: if sacct raises an error
+        """
+        command_args = "sacct --helpformat".split()
+        cmd_result = subprocess.run(
+            args=command_args,
+            stdout=subprocess.PIPE,
+            encoding="utf8",
+            check=True,
+            universal_newlines=True,
+            shell=False,
+        )
+        if cmd_result.returncode != 0:
+            raise RuntimeError("Error retrieving sacct options with --helpformat")
+        result = cmd_result.stdout.split()
+        return result
+
+    def set_sacct_args(self, jobs: List[str]) -> List[str]:
+        """Set arguments of sacct query.
+
+        Args:
+            jobs: list of job names
+
+        Returns:
+            String of sacct arguments
+
+        """
+        args = []
+        if self.user:
+            if not self.since:
+                start_date = datetime.date.today() - datetime.timedelta(days=7)
+                self.since = start_date.strftime("%m%d%y")  # MMDDYY
+            args += [f"--user={self.user}"]
+        elif self.query_all_users:
+            args += ["--allusers"]
+        else:
+            args += ["--jobs=" + ",".join(jobs)]
+
+        if self.since:
+            args += [f"--starttime={self.since}"]
+        if self.partition:
+            args += [f"--partition={self.partition}"]
+        if self.until:
+            args += [f"--endtime={self.until}"]
+        if self.extra_args:
+            args += shlex.split(self.extra_args)
+        return args
+
+    def get_db_output(
+        self,
+        columns: List[str],
+        jobs: List[str],
+        debug_cmd: Optional[Callable] = None,
+    ) -> List[Dict[str, str]]:
+        """Query the database with the supplied columns.
+
+        Args:
+            columns: validated format names as strings
+            jobs: list of job names
+            debug_cmd: If specified, the raw output will passed to this function
+
+        Returns:
+            List of rows, where each row is a dictionary
+            with the columns as keys and entries as values
+            Output order is not guaranteed to match the jobs list
+
+        Raises:
+            RuntimeError: if sacct doesn't return properly
+        """
+        args = self.default_args + ["--format=" + ",".join(columns)]
+        args += self.set_sacct_args(jobs)
+        try:
+            cmd_result = subprocess.run(
+                args=args,
+                stdout=subprocess.PIPE,
+                encoding="utf8",
+                check=True,
+                universal_newlines=True,
+                shell=False,
+            )
+            cmd_result.check_returncode()
+
+        except subprocess.CalledProcessError as error:
+            raise RuntimeError(f"Error running sacct!\n{error.stderr}") from error
+
+        lines = cmd_result.stdout.split("\n")
+        if debug_cmd is not None:
+            debug_cmd("\n".join(lines))
+
+        sacct_split = re.compile(r"\^\|\^")
+        result = [dict(zip(columns, sacct_split.split(line))) for line in lines if line]
+
+        if self.state:
+            # split to get first word in entries like "CANCELLED BY X"
+            result = [r for r in result if r["State"].split()[0] in self.state]
+
+        if self.not_state:
+            # split to get first word in entries like "CANCELLED BY X"
+            result = [r for r in result if r["State"].split()[0] not in self.not_state]
+
+        return result
+
+    def set_user(self, user: str) -> None:
+        """Set the collection of jobs based on the provided user.
+
+        Args:
+            user: user name
+        """
+        self.user = user
+
+    def set_partition(self, partition: str) -> None:
+        """Set the collection of jobs based on the provided partition.
+
+        Args:
+            partition: partition name
+        """
+        self.partition = partition
+
+    def set_extra_args(self, extra_args: str) -> None:
+        """Set extra arguments to be forwarded to sacct.
+
+        Args:
+            extra_args: list of arguments
+        """
+        self.extra_args = extra_args
+
+    def all_users(self) -> None:
+        """Query for all users if `since` is set."""
+        self.query_all_users = True
+
+    def set_state(self, state: str) -> None:
+        """Set the state to include output jobs.
+
+        Args:
+            state: comma separated list of state names or codes
+        """
+        if not state:
+            return
+
+        self.state = get_states_as_set(state)
+        # add a single value if it's empty here
+        if not self.state:
+            click.secho("No valid states provided to include", fg="yellow", err=True)
+            self.state.add(None)
+
+    def set_not_state(self, state: str) -> None:
+        """Set the state to exclude from output jobs.
+
+        Args:
+            state: comma separated list of state names or codes
+
+        """
+        if not state:
+            return
+
+        self.not_state = get_states_as_set(state)
+        # add a single value if it's empty here
+        if not self.not_state:
+            click.secho("No valid states provided to exclude", fg="yellow", err=True)
+            self.not_state = None
+
+    def parse_date(self, d: str) -> str:
+        """Parse and convert custom string date format.
+
+        Args:
+            d: the string of date.
+
+        Returns:
+            converted string of date
+        """
+        abbrev_to_key = {
+            "w": "weeks",
+            "W": "weeks",
+            "d": "days",
+            "D": "days",
+            "h": "hours",
+            "H": "hours",
+            "m": "minutes",
+            "M": "minutes",
+        }
+        valid_args = ["weeks", "days", "hours", "minutes"]
+        date_args = {}
+
+        args = d.split(",")
+        for arg in args:
+            toks = arg.split("=")
+
+            # lines don't have an equal
+            if len(toks) < 2:
+                continue
+
+            # convert key to name
+            if toks[0] in abbrev_to_key:
+                toks[0] = abbrev_to_key[toks[0]]
+
+            toks[0] = toks[0].lower()
+
+            if toks[0] in valid_args:
+                try:
+                    date_args[toks[0]] = int(toks[1])
+                except ValueError:
+                    continue
+
+        date = datetime.datetime.today()
+        date -= datetime.timedelta(**date_args)
+        return date.strftime("%Y-%m-%dT%H:%M")  # MMDDYY
+
+    def set_until(self, until: str) -> None:
+        """Set the filter for time of jobs to consider.
+
+        Args:
+            until: the string for filtering. If specified as time=amount
+                will subtract that amount from the current time
+        """
+        if not until:
+            return
+        if "=" in until:  # handle custom format
+            self.until = self.parse_date(until)
+        else:
+            self.until = until
+
+    def set_since(self, since: str) -> None:
+        """Set the filter for time of jobs to consider.
+
+        Args:
+            since: the string for filtering.  If specified as time=amount
+                will subtract that amount from the current time
+        """
+        if not since:
+            return
+        if "=" in since:  # handle custom format
+            self.since = self.parse_date(since)
+        else:
+            self.since = since
+
+    def has_since(self) -> bool:
+        """Check if since has been set.
+
+        Returns:
+            True if since has been set properly
+        """
+        return bool(self.since)
+
+    def get_partition_timelimits(self) -> dict:
+        """Get partition time limits.
+
+        Returns:
+            dict mapping partition names to maximum timelimits.
+
+        Raises:
+            RuntimeError: if scontrol raises an error
+        """
+        command_args = "scontrol show partition".split()
+        cmd_result = subprocess.run(
+            args=command_args,
+            stdout=subprocess.PIPE,
+            encoding="utf8",
+            check=True,
+            universal_newlines=True,
+            shell=False,
+        )
+        if cmd_result.returncode != 0:
+            raise RuntimeError("Error retrieving information from scontrol")
+
+        partition_name = re.compile(r"^PartitionName=(?P<name>\S+)$")
+        time_limit = re.compile(r"MaxTime=(?P<time>\S+)")
+
+        partition = ""
+        result = {}
+        for line in cmd_result.stdout.split():
+            match = re.match(partition_name, line)
+            if match:
+                partition = match.group("name")
+            match = re.match(time_limit, line)
+            if match:
+                result[partition] = match.group("time")
+
+        return result
+
+
+def get_states_as_set(state_list: str) -> Set:
+    """Helper method to parse the state string.
+
+    Args:
+        state_list: comma separated string with codes and states
+
+    Returns:
+        Set with valid state names in upper case
+    """
+    codes_to_states = {
+        "BF": "BOOT_FAIL",
+        "CA": "CANCELLED",
+        "CD": "COMPLETED",
+        "DL": "DEADLINE",
+        "F": "FAILED",
+        "NF": "NODE_FAIL",
+        "OOM": "OUT_OF_MEMORY",
+        "PD": "PENDING",
+        "PR": "PREEMPTED",
+        "R": "RUNNING",
+        "RQ": "REQUEUED",
+        "RS": "RESIZING",
+        "RV": "REVOKED",
+        "S": "SUSPENDED",
+        "TO": "TIMEOUT",
+    }
+    possible_states = set(codes_to_states.values())
+    states = {
+        codes_to_states.get(state, state) for state in state_list.upper().split(",")
+    }
+
+    for state in states:
+        if state not in possible_states:
+            click.secho(f"Unknown state {state}", fg="yellow", err=True)
+
+    return states.intersection(possible_states)
---
+++ packages/reportseff/reportseff-2.3.2.tar.gz/reportseff-2.3.2/src/reportseff/job_collection.py
@@ -0,0 +1,219 @@
+"""Module representing a collection of jobs."""
+import os
+import re
+from typing import Dict, List, Optional, Tuple
+
+from .job import Job
+from .output_renderer import OutputRenderer
+
+
+class JobCollection:
+    """A group of jobs."""
+
+    def __init__(self) -> None:
+        """Create a new job collection with default options."""
+        self.columns = [
+            "JobIDRaw",
+            "JobID",
+            "State",
+            "AllocCPUS",
+            "TotalCPU",
+            "Elapsed",
+            "Timelimit",
+            "REQMEM",
+            "MaxRSS",
+            "NNodes",
+            "NTasks",
+            "Partition",
+        ]
+
+        self.job_file_regex = re.compile(
+            r"^.*?[_-](?P<jobid>(?P<job>[0-9]+)(_[0-9]+)?)(\.out)?$"
+        )
+        self.job_regex = re.compile(r"^(?P<jobid>(?P<job>[0-9]+)(_[][\-0-9]+)?)$")
+
+        self.jobs: Dict[str, Job] = {}
+        self.renderer: Optional[OutputRenderer] = None
+        self.dir_name = ""
+        self.partition_timelimits: dict = {}
+
+    def get_columns(self) -> List[str]:
+        """The list of columns requested from inquirer.
+
+        Returns:
+            The current columns of this collection.
+        """
+        return self.columns
+
+    def get_jobs(self) -> List[str]:
+        """List of jobs to get from inquirer.
+
+        Returns:
+            job names as a sorted list
+        """
+        return sorted({job.job for job in self.jobs.values()})
+
+    def set_out_dir(self, directory: str) -> None:
+        """Set this collection's directory to try parsing out jobs from output files.
+
+        If directory is empty, will use the current directory, otherwise will
+        try to get the full path name.
+
+        Args:
+            directory: the directory to search for job names.
+
+        Raises:
+            ValueError: if the directory does not exist
+            ValueError: if the directory contains no files
+            ValueError: if the directory contains no valid files
+        """
+        # set and validate working directory to full path
+        if directory == "":
+            working_directory = os.getcwd()
+        else:
+            working_directory = os.path.realpath(directory)
+
+        if not os.path.exists(working_directory):
+            raise ValueError(f"{working_directory} does not exist!")
+
+        # get files from directory
+        files = os.listdir(working_directory)
+        files = list(
+            filter(lambda x: os.path.isfile(os.path.join(working_directory, x)), files)
+        )
+        if len(files) == 0:
+            raise ValueError(f"{working_directory} contains no files!")
+
+        for file in files:
+            self.process_seff_file(file)
+
+        if len(self.jobs) == 0:
+            raise ValueError(
+                f"{working_directory} contains no valid output files!"
+                "\nDo you need to set a custom format with `--slurm-format`?"
+            )
+        self.dir_name = working_directory
+
+    def set_jobs(self, jobs: tuple) -> None:
+        """Set the collection jobs to the provided job ids.
+
+        if jobs is empty, use the current working directory
+        if jobs is a singleton and a directory, set to that directory
+        else, parse jobs as files or raw job ids
+
+        Args:
+            jobs: Tuple of job names to use for populating the collection
+
+        Raises:
+            ValueError: if no valid jobs are provided
+        """
+        if jobs == ():
+            # look in current directory for slurm outputs
+            self.set_out_dir("")
+            return
+        if len(jobs) == 1 and os.path.isdir(jobs[0]):
+            self.set_out_dir(jobs[0])
+            return
+        for job_id in jobs:
+            match = self.job_regex.match(job_id)
+
+            if match:
+                self.add_job(match.group("job"), match.group("jobid"))
+            else:
+                self.process_seff_file(job_id)
+
+        if len(self.jobs) == 0:
+            raise ValueError("No valid jobs provided!")
+
+    def process_seff_file(self, filename: str) -> None:
+        """Try to parse out job information from the supplied filename.
+
+        Args:
+            filename: the filename to try and match
+        """
+        match = self.job_file_regex.match(filename)
+        if match:
+            self.add_job(match.group("job"), match.group("jobid"), filename)
+
+    def add_job(self, job: str, jobid: str, filename: Optional[str] = None) -> None:
+        """Add a job to the collection.
+
+        Args:
+            job: the 'base' job number
+            jobid: equal to the job unless it is an array job
+            filename: the filename of the out file this job is derived from
+        """
+        self.jobs[jobid] = Job(job, jobid, filename)
+
+    def process_entry(self, entry: Dict, add_job: bool = False) -> None:
+        """Update the jobs collection with information from the provided entry.
+
+        Args:
+            entry: the account entry from a db inquirer
+            add_job: if true, will add the job to the collection if it doesn't exist
+        """
+        job_id = entry["JobID"].split(".")[0]
+        job_id_raw = entry["JobIDRaw"].split(".")[0]
+        if job_id not in self.jobs:
+            match = self.job_regex.match(job_id)
+            # job is in jobs
+            if match and (match.group("job") in self.jobs or add_job):
+                self.add_job(match.group("job"), job_id)
+            # check if the job_id is an array job
+            elif job_id_raw in self.jobs:
+                old_job = self.jobs.pop(job_id_raw)
+                self.add_job(old_job.job, job_id, old_job.filename)
+            else:
+                return
+
+        # handle partition limit for timelimit
+        if (
+            "Timelimit" in entry
+            and entry["Timelimit"] == "Partition_Limit"
+            and "Partition" in entry
+            and entry["Partition"] in self.partition_timelimits
+        ):
+            entry["Timelimit"] = self.partition_timelimits[entry["Partition"]]
+
+        self.jobs[job_id].update(entry)
+
+    def get_sorted_jobs(self, change_sort: bool) -> List[Job]:
+        """Sort the jobs.
+
+        Args:
+            change_sort: a switch to indicate the type of sorting
+                if true will sort by the modified time of the file or job number
+                if false will sort by the if the file exists,
+                the length, then the content
+
+        Returns:
+            sorted list of jobs to display
+        """
+
+        def get_time(job: Job) -> float:
+            # handle None and '', use numeric representation of name
+            idnum = float(re.sub("[^0-9.]", "", job.jobid.replace("_", ".")))
+            file = job.filename
+            if file and self.dir_name:
+                file = os.path.join(self.dir_name, file)
+            if file and os.path.exists(file):
+                return os.path.getmtime(file)
+            return idnum
+
+        def get_file_name(job: Job) -> Tuple[bool, int, str]:
+            file = job.name()
+            file = os.path.join(self.dir_name, file)
+            return (not os.path.exists(file), len(file), file)
+
+        if change_sort:
+            return sorted(self.jobs.values(), key=get_time, reverse=True)
+
+        return sorted(self.jobs.values(), key=get_file_name)
+
+    def set_partition_limits(self, limits: dict) -> None:
+        """Set partition limits from db inquirer.
+
+        Args:
+            limits: dict of partition to partition timelimit
+        """
+        self.partition_timelimits = limits

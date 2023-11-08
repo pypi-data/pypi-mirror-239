fe081bbb923077ab1e6262a4c4c05cb038c51bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 17:07:54 +0000

    Add package panstat/1.0.2/panstat-1.0.2.tar.gz

---
+++ packages/panstat/panstat-1.0.2.tar.gz/panstat-1.0.2/panstat/bin/_merge.py
@@ -0,0 +1,30 @@
+
+import os
+import click
+import textwrap
+
+from panstat.util.merge import merge_result
+
+
+__epilog__ = click.style('''\n
+\b
+examples:
+    panstat merge -h
+''', fg='green')
+
+
+@click.command(
+    name='merge',
+    no_args_is_help=True,
+    help=click.style('Merge statistics results', italic=True, fg='blue'),
+    epilog=__epilog__,
+)
+@click.argument('result_dir')
+@ click.option('-o', '--merge_dir',
+               help='The output directory to store the merged results',
+               default='merge', show_default=True)
+def main(**kwargs):
+    result_dir = kwargs['result_dir']
+    merge_dir = kwargs['merge_dir']
+
+    merge_result(result_dir=result_dir, merge_dir=merge_dir)

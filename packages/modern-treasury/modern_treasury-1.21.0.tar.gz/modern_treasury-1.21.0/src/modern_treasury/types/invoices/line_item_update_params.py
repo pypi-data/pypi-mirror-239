3a7081de7fd6af8e7e6a64259cce3ffd3a8d63d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:06:36 +0000

    Add package modern-treasury/1.21.0/modern_treasury-1.21.0.tar.gz

---
+++ packages/modern-treasury/modern_treasury-1.21.0.tar.gz/modern_treasury-1.21.0/src/modern_treasury/types/invoices/line_item_update_params.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["LineItemUpdateParams"]
+
+
+class LineItemUpdateParams(TypedDict, total=False):
+    invoice_id: Required[str]
+
+    description: str
+    """An optional free-form description of the line item."""
+
+    direction: str
+    """Either `debit` or `credit`.
+
+    `debit` indicates that a client owes the business money and increases the
+    invoice's `total_amount` due. `credit` has the opposite intention and effect.
+    """
+
+    name: str
+    """The name of the line item, typically a product or SKU name."""
+
+    quantity: int
+    """The number of units of a product or service that this line item is for.
+
+    Must be a whole number. Defaults to 1 if not provided.
+    """
+
+    unit_amount: int
+    """
+    The cost per unit of the product or service that this line item is for,
+    specified in the invoice currency's smallest unit.
+    """

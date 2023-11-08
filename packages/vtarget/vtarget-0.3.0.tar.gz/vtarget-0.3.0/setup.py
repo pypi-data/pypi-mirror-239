1efd5c628cd0fbbdedf5f6e73ee7dbe8ba54a906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 06:04:46 +0000

    Add package vtarget/0.3.0/vtarget-0.3.0.tar.gz

---
+++ packages/vtarget/vtarget-0.3.0.tar.gz/vtarget-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import __version__, setup
+
+with open("./README.md", "r") as f:
+    readme = f.read()
+
+with open("./requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="vtarget",
+    packages=[
+        "vtarget",
+        "vtarget.dataprep",
+        "vtarget.dataprep.nodes",
+        "vtarget.handlers",
+        "vtarget.utils",
+    ],
+    version="0.3.0",
+    description="vtarget lib",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="vTarget Team",
+    author_email="contact@vtarget.ai",
+    keywords=["vtarget", "dataprep"],
+    classifiers=[],
+    license="BSD",
+    install_requires=requirements,
+    include_package_data=False,
+    python_requires=">=3.9.10,<3.11.0",
+)

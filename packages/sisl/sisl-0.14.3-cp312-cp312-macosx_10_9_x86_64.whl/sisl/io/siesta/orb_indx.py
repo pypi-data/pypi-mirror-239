4efbcc445a28b7c10ffd03cab3d9c124150f734a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:26 +0000

    Add package sisl/0.14.3/sisl-0.14.3-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/sisl/sisl-0.14.3-cp312-cp312-macosx_10_9_x86_64.whl/sisl/io/siesta/orb_indx.py
@@ -0,0 +1,116 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from sisl import Atom, AtomicOrbital, Atoms, Orbital, PeriodicTable
+from sisl._array import arrayi
+from sisl._internal import set_module
+from sisl.unit.siesta import unit_convert
+
+from ..sile import add_sile, sile_fh_open
+from .sile import SileSiesta
+
+__all__ = ["orbindxSileSiesta"]
+
+
+Bohr2Ang = unit_convert("Bohr", "Ang")
+
+
+@set_module("sisl.io.siesta")
+class orbindxSileSiesta(SileSiesta):
+    """Orbital information file"""
+
+    @sile_fh_open()
+    def read_lattice_nsc(self):
+        """Reads the supercell number of supercell information"""
+        # First line contains no no_s
+        line = self.readline().split()
+        no_s = int(line[1])
+        self.readline()
+        self.readline()
+        nsc = [0] * 3
+
+        def int_abs(i):
+            return abs(int(i))
+
+        for _ in range(no_s):
+            line = self.readline().split()
+            isc = list(map(int_abs, line[12:15]))
+            if isc[0] > nsc[0]:
+                nsc[0] = isc[0]
+            if isc[1] > nsc[1]:
+                nsc[1] = isc[1]
+            if isc[2] > nsc[2]:
+                nsc[2] = isc[2]
+
+        return arrayi([n * 2 + 1 for n in nsc])
+
+    @sile_fh_open()
+    def read_basis(self, atoms=None):
+        """Returns a set of atoms corresponding to the basis-sets in the ORB_INDX file
+
+        The specie names have a short field in the ORB_INDX file, hence the name may
+        not necessarily be the same as provided in the species block
+
+        Parameters
+        ----------
+        atoms : Atoms, optional
+           list of atoms used for the species index
+        """
+
+        # First line contains no no_s
+        line = self.readline().split()
+        no = int(line[0])
+        self.readline()
+        self.readline()
+
+        pt = PeriodicTable()
+
+        def crt_atom(i_s, spec, orbs):
+            if atoms is None:
+                # The user has not specified an atomic basis
+                i = pt.Z(spec)
+                if isinstance(i, int):
+                    return Atom(i, orbs)
+                else:
+                    return Atom(-1, orbs, tag=spec)
+            # Get the atom and add the orbitals
+            return atoms[i_s].copy(orbitals=orbs)
+
+        # Now we begin by reading the atoms
+        atom = []
+        orbs = []
+        specs = []
+        ia = 1
+        i_s = 0
+        for _ in range(no):
+            line = self.readline().split()
+
+            i_a = int(line[1])
+            spec = line[3]
+            if i_a != ia:
+                if i_s not in specs:
+                    atom.append(crt_atom(i_s, spec, orbs))
+                specs.append(i_s)
+                ia = i_a
+                orbs = []
+
+            i_s = int(line[2]) - 1
+
+            if i_s in specs:
+                continue
+            nlmz = list(map(int, line[5:9]))
+            P = line[9] == "T"
+            rc = float(line[11]) * Bohr2Ang
+            # Create the orbital
+            o = AtomicOrbital(n=nlmz[0], l=nlmz[1], m=nlmz[2], zeta=nlmz[3], P=P, R=rc)
+            orbs.append(o)
+
+        if i_s not in specs:
+            atom.append(crt_atom(i_s, spec, orbs))
+        specs.append(i_s)
+
+        # Now re-arrange the atoms and create the Atoms object
+        return Atoms([atom[i] for i in specs])
+
+
+add_sile("ORB_INDX", orbindxSileSiesta, gzip=True)
---
+++ packages/sisl/sisl-0.14.3-cp312-cp312-macosx_10_9_x86_64.whl/sisl/mixing/linear.py
@@ -0,0 +1,142 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from __future__ import annotations
+
+from typing import Any, Optional
+
+from sisl._internal import set_module
+from sisl._typing_ext.numpy import NDArray
+
+from .base import BaseHistoryWeightMixer, T
+
+__all__ = ["LinearMixer", "AndersonMixer"]
+
+
+@set_module("sisl.mixing")
+class LinearMixer(BaseHistoryWeightMixer):
+    r"""Linear mixing
+
+    The linear mixing is solely defined using a weight, and the resulting functional
+    may then be calculated via:
+
+    .. math::
+
+        f^{i+1} = f^i + w \delta f^i
+
+    Parameters
+    ----------
+    weight : float, optional
+       mixing weight
+    """
+    __slots__ = ()
+
+    def __call__(self, f: T, df: T, append: bool = True) -> T:
+        r"""Calculate a new variable :math:`f'` using input and output of the functional
+
+        Parameters
+        ----------
+        f : object
+           input variable for the functional
+        df : object
+           derivative of the functional
+        append : bool, optional
+           whether to append to the history
+        """
+        super().__call__(f, df, append=append)
+        return f + self.weight * df
+
+
+class AndersonMixer(BaseHistoryWeightMixer):
+    r""" Anderson mixing
+
+    The Anderson mixing assumes that the mixed input/output are linearly
+    related. Hence 
+
+    .. math::
+
+       |\bar{n}^{m}_{\mathrm{in}/\mathrm{out}\rangle =
+          (1 - \beta)|n^{m}_{\mathrm{in}/\mathrm{out}\rangle
+          + \beta|n^{m-1}_{\mathrm{in}/\mathrm{out}\rangle
+
+    Here the optimal choice :math:`\beta` is calculated as:
+
+    .. math::
+
+       \delta_i &= F_i^{\mathrm{out}} - F_i^{\mathrm{in}}
+       \\
+       \beta &= \frac{\langle \delta_i | \delta_i - \delta_{i-1}\rangle}
+         {\langle \delta_i - \delta_{i-1}| \delta_i - \delta_{i-1} \rangle}
+
+    Finally the resulting output becomes:
+
+    .. math::
+
+          |n^{m+1}\rangle =
+          (1 - \alpha)|\bar n^m_{\mathrm{in}}\rangle
+          + \alpha|\bar n^m_{\mathrm{out}}\rangle
+
+    See :cite:`Johnson1988` for more details.
+    """
+
+    __slots__ = ()
+
+    @staticmethod
+    def _beta(df1: T, df2: T) -> NDArray:
+        # Minimize the average densities for the delta variable
+        def metric(a, b):
+            return a.ravel().conj().dot(b.ravel()).real
+
+        ddf = df2 - df1
+        beta = metric(df2, ddf) / metric(ddf, ddf)
+
+        return beta
+
+    def __call__(
+        self, f: T, df: T, delta: Optional[Any] = None, append: bool = True
+    ) -> T:
+        r"""Calculate a new variable :math:`f'` using input and output of the functional
+
+        Parameters
+        ----------
+        f : object
+           input variable for the functional
+        df : object
+           derivative of the functional
+        """
+        if delta is None:
+            # not a copy, simply the same reference
+            delta = df
+
+        # Get last elements
+        if len(self.history) > 0:
+            last = self.history[-1]
+            f1 = last[0]
+            fdf1 = last[1]
+            d1 = last[-1]
+        else:
+            f1 = None
+
+        # the current iterations input + output variables
+        f2 = f
+        fdf2 = f + df
+        d2 = delta
+
+        # store new last variables
+        #   delta is used for calculating beta, nothing more
+        # here n refers to the variable (density) we are mixing
+        #  and the integer corresponds to the iteration count
+        super().__call__(f2, fdf2, d2, append=append)
+
+        if f1 is None:
+            # this is linear mixing for the first step
+            return f + self.weight * df
+
+        # calculate next position
+        beta = self._beta(d1, d2)
+
+        # Now calculate the new averages
+        nin = (1 - beta) * f2 + beta * f1
+        nout = (1 - beta) * fdf2 + beta * fdf1
+
+        return (1 - self.weight) * nin + self.weight * nout

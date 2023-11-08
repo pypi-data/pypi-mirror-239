7104e8c7bd14999a36c461e772b6d5f913a8c7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:07 +0000

    Add package azccriminal/1.0.0/azccriminal-1.0.0.tar.gz

---
+++ packages/azccriminal/azccriminal-1.0.0.tar.gz/azccriminal-1.0.0/Azccriminal.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='azccriminal',
+    version='1.0.0',
+    description='azccriminal web and database network traffic monitoring tool ',
+    author='Goychay_23',
+    author_email='goychay_23@gmail.com',
+    packages=['azccriminal'],
+)
---
+++ packages/azccriminal/azccriminal-1.0.0.tar.gz/azccriminal-1.0.0/azccriminal/azccriminal.py
@@ -0,0 +1,10 @@
+import azccriminal from criminal
+#azccriminal web scripting criminal
+azccriminal=All-urls
+criminalshot=huntershooting,webshooting,criminalshooting,sslshooting,bruteforceshooting,web applicationshooting
+shootingmode= Capturing all database and site communication tools and general setup information, network traffic speed measurement
+azccriminalshooting=All infiltration integrated
+shootcriminal=azccriminal
+criminalaz=language integration
+azccriminaldatabase=criminalshoot and criminal
+

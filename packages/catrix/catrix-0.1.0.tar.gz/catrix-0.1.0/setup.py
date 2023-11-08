770e39b822510be6b6ecf87d2e390928456c63a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 03:10:31 +0000

    Add package catrix/0.1.0/catrix-0.1.0.tar.gz

---
+++ packages/catrix/catrix-0.1.0.tar.gz/catrix-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='catrix',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/ishanoshada/catrix',  
+    license='MIT',
+    author='Ishan Oshada',
+    author_email='ic31908@gamil.com',
+    description='soon',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)

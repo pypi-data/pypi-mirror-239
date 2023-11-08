add7d6e806889d4f60af31572205f97df6e2ee50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 04:04:22 +0000

    Add package NexProbe/1.0.3/NexProbe-1.0.3.tar.gz

---
+++ packages/NexProbe/NexProbe-1.0.3.tar.gz/NexProbe-1.0.3/reconnaissance/subdomainsource/crtsh_source.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def query_crtsh(domain):
+    try:
+        url = f"https://crt.sh/?q=%.{domain}&output=json"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            subdomains = set()
+            for entry in data:
+                subdomains.add(entry['name_value'].strip())
+            return list(subdomains)
+        else:
+            return []
+    except Exception as e:
+        return []
---
+++ packages/NexProbe/NexProbe-1.0.3.tar.gz/NexProbe-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.3'
+DESCRIPTION = 'NexProbe is a Python script designed for conducting comprehensive cyber security reconnaissance activities.'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="NexProbe",
+    version="1.0.3",
+    author="Pritam Dash",
+    author_email="pritamdash1997@gmail.com",
+    description="NexProbe is a Python script designed for conducting comprehensive cyber security reconnaissance activities.",
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'NexProbe'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)

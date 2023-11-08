8f22f601014bc790492fbec988e3a95803eb1b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:30 +0000

    Add package go-subtitle/20231106/go_subtitle-20231106.tar.gz

---
+++ packages/go-subtitle/go_subtitle-20231106.tar.gz/go_subtitle-20231106/setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import platform
+import pkg_resources
+from setuptools import setup, find_namespace_packages
+
+
+def read_version(fname="subtitler/version.py"):
+    exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
+    return locals()["__version__"]
+
+
+requirements = []
+if sys.platform.startswith("linux") and platform.machine() == "x86_64":
+    requirements.append("triton==2.0.0")
+
+
+setup(
+    name="go_subtitle",
+    py_modules=["subtitler"],
+    version=read_version(),
+    license="MIT",
+    python_requires=">=3.8",
+    packages=find_namespace_packages(),
+    install_requires=requirements
+    + [
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    entry_points={
+        "console_scripts": ["go_subtitle=subtitler.__main__:main"],
+    },
+    include_package_data=True,
+    author="HaoDaXia",
+    author_email="wh18307094479@gmail.com",
+    description="Generate subtitle from audio or video file",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    url="https://github.com/RedHeartSecretMan/go_subtitle",
+)

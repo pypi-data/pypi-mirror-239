9e4134c3299cba43fbc9d403ee6b94444c2895e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 05:05:46 +0000

    Add package baby-cry-explorer/0.0.0/baby-cry-explorer-0.0.0.tar.gz

---
+++ packages/baby-cry-explorer/baby-cry-explorer-0.0.0.tar.gz/baby-cry-explorer-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="baby-cry-explorer",
+    version="0.0.0",
+    description="BabyCryExplorer",
+    author="Bamdad Sabbagh",
+    author_email="hi@bamdad.fr",
+    url="",
+    packages=["src"],
+)

66ea0dd00ee8b24e6c293e8d8c66de4ee768d3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 06:04:51 +0000

    Add package bananalyzer/0.1.1/bananalyzer-0.1.1-py3-none-any.whl

---
+++ packages/bananalyzer/bananalyzer-0.1.1-py3-none-any.whl/bananalyzer/runner/null_agent_wrapper.py
@@ -0,0 +1,19 @@
+from playwright.async_api import BrowserContext
+
+from bananalyzer.data.schemas import Example
+from bananalyzer.runner.agent_runner import AgentResult, AgentRunner
+
+
+class NullAgentRunner(AgentRunner):
+    """
+    A test agent class that just returns an empty string
+    """
+
+    async def run(
+        self,
+        context: BrowserContext,
+        example: Example,
+    ) -> AgentResult:
+        page = await context.new_page()
+        await page.goto(example.url)
+        return example.evals[0].expected

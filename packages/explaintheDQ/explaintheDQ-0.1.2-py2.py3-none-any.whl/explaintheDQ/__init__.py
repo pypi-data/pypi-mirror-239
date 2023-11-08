3de64b06d1269ca7760652b6fddbd8f2ca29fb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 14:05:08 +0000

    Add package explaintheDQ/0.1.2/explaintheDQ-0.1.2-py2.py3-none-any.whl

---
+++ packages/explaintheDQ/explaintheDQ-0.1.2-py2.py3-none-any.whl/explaintheDQ/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Explain the DQ."""
+
+__author__ = """Everett Schlawin"""
+__email__ = 'granfalloontoyballoon@hotmail.com'
+__version__ = '0.1.2'
+
+from explaintheDQ.explaintheDQ import *
+

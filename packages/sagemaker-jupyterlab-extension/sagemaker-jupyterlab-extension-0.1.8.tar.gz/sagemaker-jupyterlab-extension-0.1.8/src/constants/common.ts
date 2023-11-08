e5b233970788a3ef2ac78ae5426adaa4a37031ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-08 16:06:52 +0000

    Add package sagemaker-jupyterlab-extension/0.1.8/sagemaker-jupyterlab-extension-0.1.8.tar.gz

---
+++ packages/sagemaker-jupyterlab-extension/sagemaker-jupyterlab-extension-0.1.8.tar.gz/sagemaker-jupyterlab-extension-0.1.8/src/constants/common.ts
@@ -0,0 +1,26 @@
+const pluginIds = {
+  ExamplePlugin: '@amzn/sagemaker-jupyterlab-extensions:example',
+  HideShutDownPlugin: '@amzn/sagemaker-jupyterlab-extensions:hideshutdown',
+  SessionManagementPlugin: '@amzn/sagemaker-jupyterlab-extensions:sessionmanagement',
+  ResourceUsagePlugin: '@amzn/sagemaker-jupyterlab-extensions:resourceusage',
+  GitClonePlugin: '@amzn/sagemaker-jupyterlab-extensions:gitclone',
+  PerformanceMeteringPlugin: '@amzn/sagemaker-jupyterlab-extensions:performance-metering',
+};
+
+const JUPYTER_COMMAND_IDS = {
+  mainMenu: {
+    fileMenu: {
+      shutdown: 'filemenu:shutdown',
+    },
+  },
+  gitClone: 'git:clone',
+  createTerminal: 'terminal:create-new',
+  openDocManager: 'docmanager:open',
+  goToPath: 'filebrowser:go-to-path',
+};
+
+const RESOURCE_PLUGIN_ID = '@amzn/sagemaker-jupyterlab-extensions:resourceusage:resource-usage-widget';
+
+const i18nStrings = {};
+
+export { pluginIds, i18nStrings, JUPYTER_COMMAND_IDS, RESOURCE_PLUGIN_ID };

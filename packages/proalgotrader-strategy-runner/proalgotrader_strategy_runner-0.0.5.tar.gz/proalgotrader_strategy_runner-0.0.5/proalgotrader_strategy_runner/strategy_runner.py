89f3c5f83bc064269db66306267f6e88ebe23635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:10:02 +0000

    Add package proalgotrader-strategy-runner/0.0.5/proalgotrader_strategy_runner-0.0.5.tar.gz

---
+++ packages/proalgotrader-strategy-runner/proalgotrader_strategy_runner-0.0.5.tar.gz/proalgotrader_strategy_runner-0.0.5/proalgotrader_strategy_runner/strategy_runner.py
@@ -0,0 +1,39 @@
+import sys
+import subprocess
+
+from multiprocessing import Pool
+
+
+def run_core():
+    args = " ".join(sys.argv[1:])
+
+    subprocess.run(
+        f"trader_core {args}",
+        shell=True,
+        check=True,
+    )
+
+
+def run_api():
+    subprocess.run(
+        "uvicorn proalgotrader_strategy_runner.main:app --host 127.0.0.1 --port 5555",
+        shell=True,
+        check=True,
+    )
+
+
+def run_processes(process):
+    process()
+
+
+def run_strategy():
+    try:
+        processes = (run_api, run_core)
+        pool = Pool(processes=len(processes))
+        pool.map(run_processes, processes)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    run_strategy()

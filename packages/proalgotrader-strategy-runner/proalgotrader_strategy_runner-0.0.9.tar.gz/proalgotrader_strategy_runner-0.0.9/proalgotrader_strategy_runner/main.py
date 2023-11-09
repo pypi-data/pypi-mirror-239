2a9eec4f91e89417e156c9f9f92a4e93ed5a37b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:09:15 +0000

    Add package proalgotrader-strategy-runner/0.0.9/proalgotrader_strategy_runner-0.0.9.tar.gz

---
+++ packages/proalgotrader-strategy-runner/proalgotrader_strategy_runner-0.0.9.tar.gz/proalgotrader_strategy_runner-0.0.9/proalgotrader_strategy_runner/main.py
@@ -0,0 +1,56 @@
+import subprocess
+
+import sys
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from apscheduler.triggers.cron import CronTrigger
+
+from fastapi import FastAPI, Request
+
+from fastapi.responses import JSONResponse
+
+
+def run_core():
+    args = " ".join(sys.argv[1:])
+
+    subprocess.run(
+        f"trader_core {args}",
+        shell=True,
+        check=True,
+    )
+
+
+async def on_startup():
+    sys.argv.extend(["--host", app.state["host"], "--port", app.state["port"]])
+
+    trigger = CronTrigger(
+        year="*", month="*", day="*", hour="17", minute="58", second="1"
+    )
+
+    scheduler = BackgroundScheduler()
+
+    scheduler.add_job(
+        run_core,
+        trigger=trigger,
+        name="scheduled",
+    )
+
+    scheduler.add_job(
+        run_core,
+        name="instant",
+    )
+
+    scheduler.start()
+
+
+app = FastAPI(on_startup=[on_startup])
+
+
+@app.get("/")
+async def status(request: Request):
+    return JSONResponse(
+        {
+            "status": "okay",
+        }
+    )

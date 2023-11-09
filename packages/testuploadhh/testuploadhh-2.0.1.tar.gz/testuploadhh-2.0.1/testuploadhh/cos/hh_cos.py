af860cd001723aed98097537d9b50cb60737f436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:07:24 +0000

    Add package testuploadhh/2.0.1/testuploadhh-2.0.1.tar.gz

---
+++ packages/testuploadhh/testuploadhh-2.0.1.tar.gz/testuploadhh-2.0.1/testuploadhh/cos/hh_cos.py
@@ -0,0 +1,13 @@
+'''
+Author: HuangHao ironlionwg@outlook.com
+Date: 2023-11-08 16:50:14
+LastEditors: HuangHao ironlionwg@outlook.com
+LastEditTime: 2023-11-08 16:51:13
+FilePath: \hhc:\Users\ironlionwg\Desktop\test_for_pypi\test_for_pypi\cos\hh_cos.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import numpy as np
+
+def hh_cos(x: float) -> float:
+     return np.cos(x)
+ 

c5c5132ea9734ecf7f42e8a2209dbcfceccfd039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:10:34 +0000

    Add package mcspy/1.3.1/mcspy-1.3.1.tar.gz

---
+++ packages/mcspy/mcspy-1.3.1.tar.gz/mcspy-1.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcspy',
+    packages=find_packages(),
+    version='1.3.1',
+    description='A Python Library for Mobile Crowdsensing Problems',
+    long_description=open('README.rst').read(),
+    author='Han Yaohui',
+    author_email='han0129@csu.edc.cn',
+    license='MIT',
+)
+

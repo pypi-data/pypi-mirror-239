24dce4dd3f9ca668ef804c109ddf624cfa67bfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:07:04 +0000

    Add package dynamic-competence-map/0.0.2/dynamic_competence_map-0.0.2.tar.gz

---
+++ packages/dynamic-competence-map/dynamic_competence_map-0.0.2.tar.gz/dynamic_competence_map-0.0.2/dcm/dcm_cmd.py
@@ -0,0 +1,45 @@
+"""
+Created on 2023-11-06
+
+@author: wf
+"""
+from ngwidgets.cmd import WebserverCmd
+from dcm.dcm_webserver import DynamiceCompentenceMapWebServer
+import sys
+from dcm.dcm_core import DynamicCompetenceMap
+from argparse import ArgumentParser
+
+class CompetenceCmd(WebserverCmd):
+    """
+    Command line for diagrams server
+    """
+    def __init__(self):
+        """
+        constructor
+        """
+        config=DynamiceCompentenceMapWebServer.get_config()
+        WebserverCmd.__init__(self, config, DynamiceCompentenceMapWebServer, DEBUG)
+        pass
+    
+    def getArgParser(self,description:str,version_msg)->ArgumentParser:
+        """
+        override the default argparser call
+        """        
+        parser=super().getArgParser(description, version_msg)
+        parser.add_argument("-v", "--verbose", action="store_true", help="show verbose output [default: %(default)s]")
+        parser.add_argument("-rp", "--root_path",default=DynamicCompetenceMap.examples_path(),help="path to example json files [default: %(default)s]")
+        return parser
+    
+def main(argv:list=None):
+    """
+    main call
+    """
+    cmd=CompetenceCmd()
+    exit_code=cmd.cmd_main(argv)
+    return exit_code
+        
+DEBUG = 0
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-d")
+    sys.exit(main())

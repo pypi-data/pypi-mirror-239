8dc6f18969eced9e352345b7f4aa05936928a364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:09:14 +0000

    Add package SOMcreator/1.3.4.3/SOMcreator-1.3.4.3-py3-none-any.whl

---
+++ packages/SOMcreator/SOMcreator-1.3.4.3-py3-none-any.whl/SOMcreator/Template/js_templates/end_ungetestet.js
@@ -0,0 +1,26 @@
+﻿id = desiteThis.ID();
+pset = "Allgemeine_Eigenschaften:";
+ident_attrib = "bauteilKlassifikation";
+
+stat = desiteAPI.getPropertyValue(id, "Check_State", "xs:string");
+objekttyp = desiteAPI.getPropertyValue(id, pset + ident_attrib, "xs:string");
+
+if (stat == "Ungeprüft") {
+    desiteResult.setCheckState('failed');
+
+    check_status = "Failed"
+    desiteAPI.setPropertyValue(id, "Check_State", "xs:string", check_status);
+
+}
+
+if (objekttyp == undefined) {
+    desiteResult.addMessage(pset + "Objekttyp existiert nicht -> Prüfung kann nicht durchgeführt Werden [Fehler 8]");
+    desiteAPI.setPropertyValue(id, "Check_State", "xs:string", "Failed");
+    desiteResult.setCheckState("Failed")
+
+} else {
+    desiteResult.addMessage(pset + ident_attrib + " (" + objekttyp + ") konnte nicht in SOM gefunden werden [Fehler 7]")
+}
+
+desiteAPI.setPropertyValue(id, "zu_pruefende_eigenschaften", "xs:int", 1);
+desiteAPI.setPropertyValue(id, "fehlerhafte_eigenschaften", "xs:int", 1);

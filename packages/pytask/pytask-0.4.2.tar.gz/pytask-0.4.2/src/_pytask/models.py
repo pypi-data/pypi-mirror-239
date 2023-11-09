00f4ea80dde0e1ecc236511c8e2790a5c6cad7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:08:36 +0000

    Add package pytask/0.4.2/pytask-0.4.2.tar.gz

---
+++ packages/pytask/pytask-0.4.2.tar.gz/pytask-0.4.2/src/_pytask/models.py
@@ -0,0 +1,38 @@
+"""Contains code on models, containers and there like."""
+from __future__ import annotations
+
+from typing import Any
+from typing import NamedTuple
+from typing import TYPE_CHECKING
+
+from attrs import define
+from attrs import field
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from _pytask.tree_util import PyTree
+    from _pytask.mark import Mark
+
+
+@define
+class CollectionMetadata:
+    """A class for carrying metadata from functions to tasks."""
+
+    id_: str | None = None
+    """The id for a single parametrization."""
+    kwargs: dict[str, Any] = field(factory=dict)
+    """Contains kwargs which are necessary for the task function on execution."""
+    markers: list[Mark] = field(factory=list)
+    """Contains the markers of the function."""
+    name: str | None = None
+    """The name of the task function."""
+    produces: PyTree[Any] | None = None
+    """Definition of products to handle returns."""
+
+
+class NodeInfo(NamedTuple):
+    arg_name: str
+    path: tuple[str | int, ...]
+    value: Any
+    task_path: Path | None
+    task_name: str
---
+++ packages/pytask/pytask-0.4.2.tar.gz/pytask-0.4.2/src/_pytask/node_protocols.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import Any
+from typing import Callable
+from typing import Protocol
+from typing import runtime_checkable
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from _pytask.tree_util import PyTree
+    from pathlib import Path
+    from _pytask.mark import Mark
+
+
+__all__ = ["MetaNode", "PNode", "PPathNode", "PTask", "PTaskWithPath"]
+
+
+@runtime_checkable
+class MetaNode(Protocol):
+    """Protocol for an intersection between nodes and tasks."""
+
+    name: str
+
+    @property
+    def signature(self) -> str:
+        """Return the signature of the node."""
+        ...
+
+    @abstractmethod
+    def state(self) -> str | None:
+        """Return the state of the node.
+
+        The state can be something like a hash or a last modified timestamp. If the node
+        does not exist, you can also return ``None``.
+
+        """
+        ...
+
+
+@runtime_checkable
+class PNode(MetaNode, Protocol):
+    """Protocol for nodes."""
+
+    def load(self, is_product: bool) -> Any:
+        """Return the value of the node that will be injected into the task.
+
+        Parameters
+        ----------
+        is_product
+            Indicates whether the node is loaded as a dependency or as a product. It can
+            be used to return a different value when the node is loaded with a product
+            annotation. Then, we usually want to insert the node itself to allow the
+            user calling :meth:`PNode.load`.
+
+        """
+        ...
+
+    def save(self, value: Any) -> Any:
+        """Save the value that was returned from a task."""
+        ...
+
+
+@runtime_checkable
+class PPathNode(PNode, Protocol):
+    """Nodes with paths.
+
+    Nodes with paths receive special handling when it comes to printing their names.
+
+    """
+
+    path: Path
+
+
+@runtime_checkable
+class PTask(MetaNode, Protocol):
+    """Protocol for nodes."""
+
+    depends_on: dict[str, PyTree[PNode]]
+    produces: dict[str, PyTree[PNode]]
+    markers: list[Mark]
+    report_sections: list[tuple[str, str, str]]
+    attributes: dict[Any, Any]
+    function: Callable[..., Any]
+
+    def execute(self, **kwargs: Any) -> Any:
+        """Return the value of the node that will be injected into the task."""
+        ...
+
+
+@runtime_checkable
+class PTaskWithPath(PTask, Protocol):
+    """Tasks with paths.
+
+    Tasks with paths receive special handling when it comes to printing their names.
+
+    """
+
+    path: Path

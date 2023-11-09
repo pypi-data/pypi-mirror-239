93786e62c6232bae88dac6ba0a0defbdd8bca01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:09:35 +0000

    Add package openkts/1.0.5/openkts-1.0.5.tar.gz

---
+++ packages/openkts/openkts-1.0.5.tar.gz/openkts-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='openkts',
+  version='1.0.5',
+  description='Open Keyshare Threshold Scheme',
+  long_description='This is an m of n key sharing solution with split/join functionality and offers password protection on individual shares with fernet encryption.',
+  url='',
+  author='Anthony Kruger',
+  author_email='devadmin@impression.cloud',
+  license='MIT',
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Information Technology',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'],
+  keywords='keyshare',
+  packages=find_packages(),
+  install_requires=['']
+)

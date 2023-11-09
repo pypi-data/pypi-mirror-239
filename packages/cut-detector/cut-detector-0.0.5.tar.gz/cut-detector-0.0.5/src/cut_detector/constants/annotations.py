c0e5ea9c7a9b1d158ca4f3c8927a9da9ecfd3f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:08:57 +0000

    Add package cut-detector/0.0.5/cut-detector-0.0.5.tar.gz

---
+++ packages/cut-detector/cut-detector-0.0.5.tar.gz/cut-detector-0.0.5/src/cut_detector/constants/annotations.py
@@ -0,0 +1,12 @@
+NAMES_DICTIONARY = {
+    "No membrane cut, double MT": 0,
+    "No membrane cut, simple MT": 1,
+    "No membrane cut, no MT": 2,
+    "Membrane cut, simple MT": 3,
+    "Membrane cut, no MT": 4,
+    "No membrane cut, double MT ?": 5,
+    "No membrane cut, simple MT ?": 6,
+    "No membrane cut, no MT ?": 7,
+    "Membrane cut, simple MT ?": 8,
+    "Membrane cut, no MT ?": 9,
+}

e40c4676536d6d743c469c8b947ead43438c4785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:12:04 +0000

    Add package dcentralab-qa-infra-automation/2.2.2/dcentralab_qa_infra_automation-2.2.2.tar.gz

---
+++ packages/dcentralab-qa-infra-automation/dcentralab_qa_infra_automation-2.2.2.tar.gz/dcentralab_qa_infra_automation-2.2.2/src/dcentralab_qa_infra_automation/pages/namiPages/NamiBasePage.py
@@ -0,0 +1,45 @@
+import time
+
+from dcentralab_qa_infra_automation.pages.BasePage import BasePage
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+"""
+Nami base page
+
+@Author: Efrat Cohen
+@Date: 06.2023
+"""
+
+"""page locators"""
+MENU_BUTTON = (By.ID, "menu-button-8")
+SETTINGS_OPTION = (By.ID, "menu-list-8-menuitem-12")
+
+
+
+class NamiBasePage(BasePage):
+
+    def __init__(self, driver):
+        """ ctor - call to BasePage ctor for initialize """
+        super().__init__(driver)
+
+    def is_page_loaded(self):
+        """
+        check if on current page
+        :return: true if on page, otherwise return false
+        """
+        return self.is_element_exist("MENU_BUTTON", MENU_BUTTON)
+
+    def click_on_menu(self):
+        """
+        click on menu button
+        """
+        self.click("MENU_BUTTON", MENU_BUTTON)
+
+    def click_on_setting(self):
+        """
+        click on settings
+        """
+        self.click("SETTINGS_OPTION", SETTINGS_OPTION)
+
+

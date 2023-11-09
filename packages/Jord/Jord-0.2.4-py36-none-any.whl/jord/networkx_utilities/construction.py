fb7012403d397eceab824edec89a28ee3fac86a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:13:46 +0000

    Add package Jord/0.2.4/Jord-0.2.4-py36-none-any.whl

---
+++ packages/Jord/Jord-0.2.4-py36-none-any.whl/jord/networkx_utilities/construction.py
@@ -0,0 +1,43 @@
+import shapely
+from networkx import MultiDiGraph
+
+__all__ = ["assertive_add_edge", "add_shapely_node"]
+
+
+def assertive_add_edge(
+    graph: MultiDiGraph,
+    u: int,
+    v: int,
+    uniqueid: int,
+    allow_loops: bool = True,
+    allow_duplicates: bool = False,
+    **kwargs,
+) -> None:
+    if not allow_loops:
+        assert u != v, f"{u} == {v}"
+
+    assert isinstance(u, int)
+    assert isinstance(v, int)
+
+    assert graph.has_node(u)
+    assert graph.has_node(v)
+
+    if not allow_duplicates and graph.has_edge(u, v, uniqueid):
+        assert not graph.has_edge(u, v, uniqueid)
+
+    graph.add_edge(u, v, key=uniqueid, uniqueid=uniqueid, **kwargs)
+
+
+def add_shapely_node(
+    graph: MultiDiGraph, u: int, point: shapely.Point, **kwargs
+) -> None:
+    assert isinstance(u, int)
+
+    graph.add_node(
+        u,
+        lon=point.x,
+        lat=point.y,
+        x=point.x,
+        y=point.y,
+        **kwargs,
+    )

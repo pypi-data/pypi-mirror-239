f2228fa1092f379053b758920ac1f4080aaef46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:13:33 +0000

    Add package firedom/0.1.0/firedom-0.1.0.tar.gz

---
+++ packages/firedom/firedom-0.1.0.tar.gz/firedom-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='firedom',
+    version='0.1.0',
+    description='Simple Firestore ORM',
+    url='https://github.com/afuenzalida/firedom',
+    author='Andrés Fuenzalida',
+    author_email='a.fuenzalida1494@gmail.com',
+    license='MIT',
+    packages=['firedom'],
+    install_requires=[
+        'google-cloud-firestore>=2.13.1',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)

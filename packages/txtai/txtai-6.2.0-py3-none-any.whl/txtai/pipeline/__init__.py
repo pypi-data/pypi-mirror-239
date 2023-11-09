f4fa5e3ec1babf8a219d0399a3cf8156321666c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 04:08:25 +0000

    Add package txtai/6.2.0/txtai-6.2.0-py3-none-any.whl

---
+++ packages/txtai/txtai-6.2.0-py3-none-any.whl/txtai/pipeline/__init__.py
@@ -0,0 +1,15 @@
+"""
+Pipeline imports
+"""
+
+from .audio import *
+from .base import Pipeline
+from .data import *
+from .factory import PipelineFactory
+from .hfmodel import HFModel
+from .hfpipeline import HFPipeline
+from .image import *
+from .nop import Nop
+from .text import *
+from .tensors import Tensors
+from .train import *

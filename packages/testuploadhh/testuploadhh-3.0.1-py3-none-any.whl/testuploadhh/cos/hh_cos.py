fa46e9722947c049cf73ae343072892500102f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-09 03:07:53 +0000

    Add package testuploadhh/3.0.1/testuploadhh-3.0.1-py3-none-any.whl

---
+++ packages/testuploadhh/testuploadhh-3.0.1-py3-none-any.whl/testuploadhh/cos/hh_cos.py
@@ -0,0 +1,14 @@
+'''
+Author: HuangHao ironlionwg@outlook.com
+Date: 2023-11-08 16:50:14
+LastEditors: HuangHao ironlionwg@outlook.com
+LastEditTime: 2023-11-08 20:13:06
+Description: 
+
+Copyright (c) 2023 by HuangHao ironlionwg@outlook.com, All Rights Reserved. 
+'''
+import numpy as np
+
+def hh_cos(x: float) -> float:
+     return np.cos(x)
+ 
---
+++ packages/testuploadhh/testuploadhh-3.0.1-py3-none-any.whl/testuploadhh/sin/hh_sin.py
@@ -0,0 +1,14 @@
+'''
+Author: HuangHao ironlionwg@outlook.com
+Date: 2023-11-08 16:51:40
+LastEditors: HuangHao ironlionwg@outlook.com
+LastEditTime: 2023-11-08 20:12:49
+Description: 
+
+Copyright (c) 2023 by HuangHao ironlionwg@outlook.com, All Rights Reserved. 
+'''
+
+import numpy as np
+
+def hh_sin(x: float) -> float:
+     return np.sin(x)
